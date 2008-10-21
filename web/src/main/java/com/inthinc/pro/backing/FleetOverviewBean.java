68251018e8bad602a4bac626e47f1cf16254e927
kselman <kselman@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-10-21 22:58:15 +0000

    ks - added dao test functions

--- web/src/main/java/com/inthinc/pro/backing/FleetOverviewBean.java
+++ web/src/main/java/com/inthinc/pro/backing/FleetOverviewBean.java
@@ -1,18 +1,26 @@
 package com.inthinc.pro.backing;
 
 import org.apache.log4j.Logger;
+import org.springframework.security.context.SecurityContextHolder;
+import org.springframework.security.userdetails.User;
 import com.inthinc.pro.backing.ui.*;
 import java.util.Random;
 import com.inthinc.pro.model.Duration;
+import com.inthinc.pro.dao.*;
 
 public class FleetOverviewBean extends BaseBean
 {
 	private double overallScore;
 	private String overallScoreStyle;
 	private Duration duration = Duration.DAYS;
+	private static final Logger logger = Logger.getLogger(TrendBean.class);
+	private UserDAO userDao;
 	
 	public FleetOverviewBean()
 	{
+		User u = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		logger.debug("getting overall score, user is: " + u.getUsername());
+		
 		setOverallScore(3.1); //Get score from DAO here.
 		ScoreBox sb = new ScoreBox(getOverallScore(), ScoreBoxSizes.LARGE);
 		setOverallScoreStyle(sb.getScoreStyle());
@@ -41,6 +49,11 @@ public class FleetOverviewBean extends BaseBean
 	public void setDuration(Duration duration) {
 		this.duration = duration;
 		
+		com.inthinc.pro.model.User newUser = userDao.findByEmail("rabyma@gmail.com");
+		
+		logger.debug("DAO Test " + newUser.getUsername());
+		
+		
 		// TODO recalculate score. user changed time range
 		Random r = new Random();
 		Double d = r.nextDouble();
@@ -57,4 +70,12 @@ public class FleetOverviewBean extends BaseBean
 	        return Math.round(d * Math.pow(10, (double) places)) / Math.pow(10,
 	            (double) places);
 	    }
+
+	public UserDAO getUserDao() {
+		return userDao;
+	}
+
+	public void setUserDao(UserDAO userDao) {
+		this.userDao = userDao;
+	}
 }
