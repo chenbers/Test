ce4f942722a2265300284bbbc3f873016e66fdd5
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-10-22 22:20:30 +0000

    Baseline.

---
+++ web/src/main/java/com/inthinc/pro/wrapper/ScoreableEntityPkg.java
@@ -0,0 +1,49 @@
+package com.inthinc.pro.wrapper;
+
+import com.inthinc.pro.model.ScoreableEntity;
+
+public class ScoreableEntityPkg {
+	private ScoreableEntity se;
+	
+	private String style;
+	private String colorKey;
+	private String goTo;
+	
+	public ScoreableEntityPkg() {
+		this.se = new ScoreableEntity();
+		this.style = "";
+		this.colorKey = "";
+		this.goTo = "";
+	}
+	
+	
+	public ScoreableEntity getSe() {
+		return se;
+	}
+	public void setSe(ScoreableEntity se) {
+		this.se = se;
+	}
+	
+	public String getStyle() {
+		return style;
+	}
+	public void setStyle(String style) {
+		this.style = style;
+	}
+	public String getColorKey() {
+		return colorKey;
+	}
+	public void setColorKey(String colorKey) {
+		this.colorKey = colorKey;
+	}
+	public String getGoTo() {
+		return goTo;
+	}
+	public void setGoTo(String goTo) {
+		this.goTo = goTo;
+	}
+	public String goTo() {
+		return this.goTo;
+	}
+
+}
