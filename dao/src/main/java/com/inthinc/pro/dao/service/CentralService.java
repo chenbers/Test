39040e5998c2cad9cd8abe3d13b0e4d45f900694
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-10-23 23:40:13 +0000

    Add getscores as list.

--- dao/src/main/java/com/inthinc/pro/dao/service/CentralService.java
+++ dao/src/main/java/com/inthinc/pro/dao/service/CentralService.java
@@ -1,12 +1,14 @@
 package com.inthinc.pro.dao.service;
 
+import java.util.List;
 import java.util.Map;
 
 import com.inthinc.pro.ProDAOException;
+import com.inthinc.pro.model.ScoreableEntity;
 
 public interface CentralService extends DAOService
 {
-
+	
   Map<String, Object> getUserByAccountID(Integer accountID) throws ProDAOException;
 
   Map<String, Object> getUserIDByName(String username) throws ProDAOException;
@@ -14,5 +16,7 @@ public interface CentralService extends DAOService
   Map<String, Object> getUserIDByEmail(String email) throws ProDAOException;
   
   Map<String, Object> getOverallScore(Integer userID, Integer levelID, Integer startDate, Integer endDate ) throws ProDAOException;
+  
+  List<ScoreableEntity> getScores(Integer userID, Integer levelID, Integer startDate, Integer endDate ) throws ProDAOException;
 
 }
