0a03e871b59c2c64c3634c247b2d75542157b578
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-10-24 14:26:30 +0000

    Add score stuff.

--- dao/src/main/java/com/inthinc/pro/dao/mock/service/impl/CentralServiceMockImpl.java
+++ dao/src/main/java/com/inthinc/pro/dao/mock/service/impl/CentralServiceMockImpl.java
@@ -1,19 +1,26 @@
 package com.inthinc.pro.dao.mock.service.impl;
 
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.inthinc.pro.ProDAOException;
+import com.inthinc.pro.dao.hessian.GraphicHessianDAO;
 import com.inthinc.pro.dao.hessian.exceptions.EmptyResultSetException;
 import com.inthinc.pro.dao.mock.data.MockData;
 import com.inthinc.pro.dao.mock.data.MockDataContainer;
 import com.inthinc.pro.dao.mock.data.SearchCriteria;
 import com.inthinc.pro.dao.service.CentralService;
 import com.inthinc.pro.model.OverallScore;
+import com.inthinc.pro.model.ScoreableEntity;
 import com.inthinc.pro.model.User;
 
 public class CentralServiceMockImpl extends MockImpl implements CentralService
 {
+	private static final Logger logger = Logger.getLogger(CentralServiceMockImpl.class);
 
     @Override
     public Map<String, Object> getUserByAccountID(Integer accountID) throws ProDAOException
@@ -63,4 +70,51 @@ public class CentralServiceMockImpl extends MockImpl implements CentralService
         return returnMap;
     }
 
+    @Override
+    public List<ScoreableEntity> getScores(Integer userID, Integer levelID, Integer startDate, Integer endDate)
+            throws ProDAOException
+    {
+    	logger.debug("getting trend table");
+       
+        ArrayList <ScoreableEntity> scoreableEntities = new ArrayList<ScoreableEntity>();
+        
+        ScoreableEntity se = new ScoreableEntity();
+		se.setEntityID(0);
+		se.setIdentifier("New England");
+		se.setScore("0.9");
+		scoreableEntities.add(se);
+		
+		se = new ScoreableEntity();
+		se.setEntityID(1);
+		se.setIdentifier("South");
+		se.setScore("1.8");	
+		scoreableEntities.add(se);
+        
+		se = new ScoreableEntity();	
+		se.setEntityID(2);
+		se.setIdentifier("Lakes");
+		se.setScore("2.9");
+		scoreableEntities.add(se);
+		
+		se = new ScoreableEntity();	
+		se.setEntityID(3);
+		se.setIdentifier("Midwest");
+		se.setScore("3.9");
+		scoreableEntities.add(se);
+		
+		se = new ScoreableEntity();	
+		se.setEntityID(4);
+		se.setIdentifier("West Coast");
+		se.setScore("4.6");
+		scoreableEntities.add(se);		
+		
+		logger.debug("size of trendtable: " + scoreableEntities.size());		
+
+        
+        if (scoreableEntities == null)
+        {
+            throw new EmptyResultSetException("No table data for: " + userID, "getScores", 0);
+        }
+        return scoreableEntities;
+    }
 }
