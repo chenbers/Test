21d05b5f71ffa0bcbe5222181bf3ee230a9a2949
kselman <kselman@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-10-28 14:58:01 +0000

    ks - create distance enum for use with chart options. 500mi - 10,000mi

---
+++ dao/src/main/java/com/inthinc/pro/model/Distance.java
@@ -0,0 +1,42 @@
+package com.inthinc.pro.model;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Distance
+{
+    FIVEHUNDRED("500mi", 500), ONETHOUSAND("1000mi", 1000), TENTHOUSAND("10,000mi", 10000);
+
+    private final int numberOfMiles;
+    private String distanceValue;
+    private static final Map<Integer, Distance> lookup = new HashMap<Integer, Distance>();
+    
+    static
+    {
+        for(Distance d : EnumSet.allOf(Distance.class))
+            lookup.put(d.getNumberOfMiles(), d);
+    }
+
+    Distance(String distanceValue, int numberOfMiles)
+    {
+        this.distanceValue = distanceValue;
+        this.numberOfMiles = numberOfMiles;
+    }
+
+    public int getNumberOfMiles()
+    {
+        return this.numberOfMiles;
+    }
+    
+    public static Distance getDistanceByDays(int numberOfMiles)
+    {
+        return lookup.get(numberOfMiles);
+    }
+    
+    @Override
+    public String toString()
+    {
+        return this.distanceValue;
+    }
+}
