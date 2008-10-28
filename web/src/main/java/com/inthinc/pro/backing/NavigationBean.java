8756b997a5469b9d9508fbd801878fc180286887
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-10-28 23:36:19 +0000

    Add listener for selected table element.

--- web/src/main/java/com/inthinc/pro/backing/NavigationBean.java
+++ web/src/main/java/com/inthinc/pro/backing/NavigationBean.java
@@ -1,7 +1,16 @@
 package com.inthinc.pro.backing;
 
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import org.apache.log4j.Logger;
+
+import com.inthinc.pro.wrapper.ScoreableEntityPkg;
+
 public class NavigationBean extends BaseBean {
+    private static final Logger logger = Logger.getLogger(NavigationBean.class);    
+    
 	private String location = "home";
+	private Integer groupID = -1;
 
 	public String getLocation() {
 		return location;
@@ -11,4 +20,22 @@ public class NavigationBean extends BaseBean {
 		this.location = location;
 	}
 
+    public Integer getGroupID()
+    {
+        return groupID;
+    }
+
+    public void setGroupID(Integer groupID)
+    {
+        this.groupID = groupID;
+    }
+
+    public void listener(ActionEvent evt) {
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        String groupID = 
+            (String) ctx.getExternalContext().getRequestParameterMap().get("selectedGroup");
+        
+        logger.debug("selected groupID is: " + groupID);
+        this.groupID = new Integer(groupID);
+    }
 }
