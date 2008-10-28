049df16318a5703e06dc4423ee43b4c393e08989
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-10-28 16:12:23 +0000

    Baseline.

---
+++ web/src/main/java/com/inthinc/pro/backing/NavigationBean.java
@@ -0,0 +1,14 @@
+package com.inthinc.pro.backing;
+
+public class NavigationBean extends BaseBean {
+	private String location = "home";
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
