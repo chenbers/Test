b634d4ee4df50ddd870baa0cc1b95b16f147689d
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-10-29 22:13:58 +0000

    Add data access methods to mock.

--- web/src/main/java/com/inthinc/pro/backing/BreakdownBean.java
+++ web/src/main/java/com/inthinc/pro/backing/BreakdownBean.java
@@ -1,13 +1,31 @@
 package com.inthinc.pro.backing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 
+import com.inthinc.pro.dao.GraphicDAO;
+import com.inthinc.pro.dao.util.DateUtil;
 import com.inthinc.pro.model.Duration;
+import com.inthinc.pro.model.ScoreType;
+import com.inthinc.pro.model.ScoreableEntity;
 import com.inthinc.pro.util.GraphicUtil;
 
 public class BreakdownBean extends BaseBean {
 
 	private static final Logger logger = Logger.getLogger(BreakdownBean.class);
+	
+	private static List <String> entityColorKey = new ArrayList<String>(){
+	        {
+	            add(new String("FF0101"));add(new String("6B9D1B"));
+	            add(new String("1E88C8"));add(new String("F6B305"));
+	            add(new String("FF6601"));
+	        }
+	    };
+    
+    private GraphicDAO graphicDAO;
+    private NavigationBean navigation;
 	
 	private String pieDef;	
 	
@@ -33,10 +51,46 @@ public class BreakdownBean extends BaseBean {
 		
 		//Control parameters
 		sb.append(GraphicUtil.getPieControlParameters());
-			
-		//Pie parameters
-		sb.append(GraphicUtil.createFakePieData());		
-		
+        
+        //Is the group id initialized?
+        if ( this.navigation.getGroupID() == null ) {
+            this.navigation.setGroupID(getUser().getGroupID());
+        }       
+
+        //Date range qualifiers
+        Integer endDate = DateUtil.getTodaysDate();
+        Integer startDate = DateUtil.getDaysBackDate(endDate, duration.getNumberOfDays());
+        
+        //Fetch, qualifier is groupId (parent), date from, date to
+        List<ScoreableEntity> s = null;
+        try {          
+            // TODO: This is not correct.  getUser().getGroupID() needs to be changed to the current group in the navigation
+            logger.debug("getting scores for groupID: " + this.navigation.getGroupID());            
+            s = graphicDAO.getScores(this.navigation.getGroupID(),
+                    startDate, endDate, ScoreType.SCORE_OVERALL_PERCENTAGES);
+        } catch (Exception e) {
+            logger.debug("graphicDao error: " + e.getMessage());
+        }    
+        logger.debug("found: " + s.size());
+        
+        //Calculate total observations and set the pie data
+        // this may change to be actual percentages
+        ScoreableEntity se = null;
+        int total = 0;
+        for ( int i = 0; i < s.size(); i++ ) {            
+            se = (ScoreableEntity)s.get(i);
+            logger.debug("score is: " + se.getScore());
+            total += se.getScore();            
+        }
+        logger.debug("total is: " + total);
+        Integer percent = 0;
+        for ( int i = 0; i < s.size(); i++ ) {
+            se = (ScoreableEntity)s.get(i);
+            percent = (se.getScore()*100)/total;
+            sb.append("<set value=\'" + percent.toString() + "\' " + "label=\'\' color=\'" +                
+                    (BreakdownBean.entityColorKey.get(i)) + "\'/>");
+        }
+
 		sb.append("</chart>");
 		
 		return sb.toString();
@@ -109,6 +163,26 @@ public class BreakdownBean extends BaseBean {
 		this.styleClass6Months = "";
 		this.styleClass12Months = "";
 	}
+
+    public GraphicDAO getGraphicDAO()
+    {
+        return graphicDAO;
+    }
+
+    public void setGraphicDAO(GraphicDAO graphicDAO)
+    {
+        this.graphicDAO = graphicDAO;
+    }
+
+    public NavigationBean getNavigation()
+    {
+        return navigation;
+    }
+
+    public void setNavigation(NavigationBean navigation)
+    {
+        this.navigation = navigation;
+    }
 
 
 }
