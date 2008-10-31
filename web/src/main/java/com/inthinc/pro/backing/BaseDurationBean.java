cdcdd4afbc0f22345e4784c692acf3c347861709
cjennings <cjennings@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-10-31 19:59:37 +0000

    cj - refactor

--- web/src/main/java/com/inthinc/pro/backing/BaseDurationBean.java
+++ web/src/main/java/com/inthinc/pro/backing/BaseDurationBean.java
@@ -4,14 +4,14 @@ import com.inthinc.pro.model.Duration;
 
 public class BaseDurationBean extends BaseBean
 {
-    private Duration duration           = Duration.DAYS;
-    private String   styleClass30Days   = "on";
-    private String   styleClass3Months  = "";
-    private String   styleClass6Months  = "";
-    private String   styleClass12Months = "";
-    private final static String   ON = "on";
+    private Duration            duration           = Duration.DAYS;
+    private String              styleClass30Days;
+    private String              styleClass3Months;
+    private String              styleClass6Months;
+    private String              styleClass12Months;
+    private final static String ON                 = "on";
+    private final static String OFF                 = "";
 
-    
     public BaseDurationBean()
     {
 
@@ -27,81 +27,24 @@ public class BaseDurationBean extends BaseBean
         this.duration = duration;
     }
 
-    public String getStyleClass30Days() {
-//        reset();
-        return ( duration.equals(Duration.DAYS)) ? "on" : ""; 
-    
-//        return styleClass30Days;
+    public String getStyleClass30Days()
+    {
+        return (duration.equals(Duration.DAYS)) ? ON : OFF;
     }
-
-//    public void setStyleClass30Days(String styleClass30Days)
-//    {
-//        this.styleClass30Days = styleClass30Days;
-//    }
 
     public String getStyleClass3Months()
     {
-/*        
-        reset();
-        if (this.duration.toString().equalsIgnoreCase(Duration.THREE.toString()))
-        {
-            styleClass3Months = "on";
-        }
-
-        return styleClass3Months;
-*/        
-        return ( duration.equals(Duration.THREE)) ? "on" : ""; 
-    }
-
-    public void setStyleClass3Months(String styleClass3Months)
-    {
-        this.styleClass3Months = styleClass3Months;
+        return (duration.equals(Duration.THREE)) ? ON : OFF;
     }
 
     public String getStyleClass6Months()
     {
-/*        
-        reset();
-        if (this.duration.toString().equalsIgnoreCase(Duration.SIX.toString()))
-        {
-            styleClass6Months = "on";
-        }
-
-        return styleClass6Months;
-*/        
-        return ( duration.equals(Duration.SIX)) ? "on" : ""; 
-    }
-
-    public void setStyleClass6Months(String styleClass6Months)
-    {
-        this.styleClass6Months = styleClass6Months;
+        return (duration.equals(Duration.SIX)) ? ON : OFF;
     }
 
     public String getStyleClass12Months()
     {
-/*        
-        reset();
-        if (this.duration.toString().equalsIgnoreCase(Duration.TWELVE.toString()))
-        {
-            styleClass12Months = "on";
-        }
-
-        return styleClass12Months;
-*/        
-        return ( duration.equals(Duration.TWELVE)) ? "on" : ""; 
-    }
-
-    public void setStyleClass12Months(String styleClass12Months)
-    {
-        this.styleClass12Months = styleClass12Months;
-    }
-
-    private void reset()
-    {
-        this.styleClass30Days = "";
-        this.styleClass3Months = "";
-        this.styleClass6Months = "";
-        this.styleClass12Months = "";
+        return (duration.equals(Duration.TWELVE)) ? ON : OFF;
     }
 
 }
