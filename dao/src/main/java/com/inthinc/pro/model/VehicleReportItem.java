4de33b7634eb1e7a4dd3c9ee44fbfc613212ba23
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-11-07 18:58:55 +0000

    Baseline.

---
+++ dao/src/main/java/com/inthinc/pro/model/VehicleReportItem.java
@@ -0,0 +1,106 @@
+package com.inthinc.pro.model;
+
+public class VehicleReportItem
+{
+    private String group;
+    private Integer vehicleID;
+    private String makeModelYear;
+    private String driver;
+    private Integer milesDriven;
+    private Integer overallScore;
+    private Integer speedScore;
+    private Integer styleScore;
+    private String styleOverall;
+    private String styleSpeed;
+    private String styleStyle;
+    
+    public String getGroup()
+    {
+        return group;
+    }
+    public void setGroup(String group)
+    {
+        this.group = group;
+    }
+    public Integer getVehicleID()
+    {
+        return vehicleID;
+    }
+    public void setVehicleID(Integer vehicleID)
+    {
+        this.vehicleID = vehicleID;
+    }
+    public String getMakeModelYear()
+    {
+        return makeModelYear;
+    }
+    public void setMakeModelYear(String makeModelYear)
+    {
+        this.makeModelYear = makeModelYear;
+    }
+    public String getDriver()
+    {
+        return driver;
+    }
+    public void setDriver(String driver)
+    {
+        this.driver = driver;
+    }
+    public Integer getMilesDriven()
+    {
+        return milesDriven;
+    }
+    public void setMilesDriven(Integer milesDriven)
+    {
+        this.milesDriven = milesDriven;
+    }
+    public Integer getOverallScore()
+    {
+        return overallScore;
+    }
+    public void setOverallScore(Integer overallScore)
+    {
+        this.overallScore = overallScore;
+    }
+    public Integer getSpeedScore()
+    {
+        return speedScore;
+    }
+    public void setSpeedScore(Integer speedScore)
+    {
+        this.speedScore = speedScore;
+    }
+    public Integer getStyleScore()
+    {
+        return styleScore;
+    }
+    public void setStyleScore(Integer styleScore)
+    {
+        this.styleScore = styleScore;
+    }
+    public String getStyleOverall()
+    {
+        return styleOverall;
+    }
+    public void setStyleOverall(String styleOverall)
+    {
+        this.styleOverall = styleOverall;
+    }
+    public String getStyleSpeed()
+    {
+        return styleSpeed;
+    }
+    public void setStyleSpeed(String styleSpeed)
+    {
+        this.styleSpeed = styleSpeed;
+    }
+    public String getStyleStyle()
+    {
+        return styleStyle;
+    }
+    public void setStyleStyle(String styleStyle)
+    {
+        this.styleStyle = styleStyle;
+    }
+
+}
