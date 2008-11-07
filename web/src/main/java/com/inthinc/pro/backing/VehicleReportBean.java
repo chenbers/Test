de9deafb2c64e3770b28b41397d0500140594002
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-11-07 20:37:17 +0000

    Baseline.

---
+++ web/src/main/java/com/inthinc/pro/backing/VehicleReportBean.java
@@ -0,0 +1,236 @@
+package com.inthinc.pro.backing;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.richfaces.event.DataScrollerEvent;
+
+import com.inthinc.pro.backing.ui.ScoreBox;
+import com.inthinc.pro.backing.ui.ScoreBoxSizes;
+import com.inthinc.pro.model.VehicleReportItem;
+
+public class VehicleReportBean extends BaseBean
+{
+    private static final Logger logger = Logger.getLogger(VehicleReportBean.class);
+    
+    private List <VehicleReportItem> vehicleData = new ArrayList<VehicleReportItem>();
+    private static final List<String> AVAILABLE_COLUMNS;
+    private Map<String, Boolean> vehicleColumns = new HashMap<String, Boolean>();
+    
+    private VehicleReportItem vrt = null;
+    
+    private Integer numRowsPerPg = 2;
+    
+    private Integer maxCount = null;
+    private Integer start = 1;
+    private Integer end = numRowsPerPg;
+    
+    private String searchFor = null;
+
+    
+    static
+    {
+        // available columns
+        AVAILABLE_COLUMNS = new ArrayList<String>();
+        AVAILABLE_COLUMNS.add("group");
+        AVAILABLE_COLUMNS.add("vehicleID");
+        AVAILABLE_COLUMNS.add("makeModelYear");
+        AVAILABLE_COLUMNS.add("driver");
+        AVAILABLE_COLUMNS.add("milesDriven");
+        AVAILABLE_COLUMNS.add("overall");
+        AVAILABLE_COLUMNS.add("speed");
+        AVAILABLE_COLUMNS.add("style");
+    }
+    
+    public void init() {               
+
+//--->Replace this with DAO for a "search" for all drivers for a given logged-in user 
+        initData();
+//--->Replace this with DAO for a "load" of the preferences for this particular table        
+        for ( int i = 0; i < VehicleReportBean.AVAILABLE_COLUMNS.size(); i++ ) {
+            this.vehicleColumns.put(VehicleReportBean.AVAILABLE_COLUMNS.get(i),true);
+        }
+    }
+    
+    private void initData() {
+        vrt = new VehicleReportItem();
+        vrt.setGroup("North");
+        vrt.setVehicleID(123456);
+        vrt.setMakeModelYear("Porsche/911/2008");
+        vrt.setDriver("John Doe");
+        vrt.setMilesDriven(202114);
+        vrt.setOverallScore(43);
+        vrt.setSpeedScore(12);
+        vrt.setStyleScore(34);
+        setStyles();
+        vehicleData.add(vrt);
+        
+        maxCount = vehicleData.size();
+        resetCounts();
+    }
+        
+
+    public List<VehicleReportItem> getVehicleData()
+    {
+        logger.debug("getting");   
+        if ( vehicleData.size() > 0 ) {
+            return vehicleData;
+        } else {
+            return new ArrayList<VehicleReportItem>();
+        }
+    }
+
+    public void setDriverData(List<VehicleReportItem> vehicleData)
+    {
+        this.vehicleData = vehicleData;
+    }
+    
+    public void search() {     
+        logger.debug("searching");
+        
+        if ( this.vehicleData.size() > 0 ) {
+            this.vehicleData.clear();
+        }
+//--->Replace this with DAO for searching
+        //Test search reset                
+        if ( this.searchFor.trim().length() != 0 ) {
+            vrt = new VehicleReportItem();
+            vrt.setGroup("Hidden");
+            vrt.setVehicleID(123456);
+            vrt.setMakeModelYear("Studebaker/S20/1908");
+            vrt.setDriver("Old Guy");
+            vrt.setMilesDriven(190833);
+            vrt.setOverallScore(34);
+            vrt.setSpeedScore(21);
+            vrt.setStyleScore(45);
+            setStyles();                
+            vehicleData.add(vrt);
+            
+            this.maxCount = vehicleData.size();
+        } else {
+            initData();
+        }
+        
+        resetCounts();       
+    }
+    
+    private void resetCounts() {
+        this.start = 1;
+        this.end = this.numRowsPerPg;
+        if ( this.vehicleData.size() <= this.end ) {
+            this.end = this.vehicleData.size();
+        } 
+    }
+    
+    public void saveColumns() {  
+        //In here for saving the column preferences 
+        logger.debug("save columns");
+    }
+    
+    public Map<String, Boolean> getVehicleColumns()    
+    {
+        //Need to do the check to prevent odd access behavior
+        if ( vehicleColumns.size() > 0 ) {     
+            return vehicleColumns;
+        } else {
+            return new HashMap<String, Boolean>();
+        }
+    }
+
+    public void setVehicleColumns(Map<String, Boolean> vehicleColumns)
+    {
+        this.vehicleColumns = vehicleColumns;
+    }
+    
+    private void setStyles() {
+        ScoreBox sb = new ScoreBox(0,ScoreBoxSizes.SMALL);  
+        
+        sb.setScore(vrt.getOverallScore());
+        vrt.setStyleOverall(sb.getScoreStyle());
+       
+        sb.setScore(vrt.getSpeedScore());
+        vrt.setStyleSpeed(sb.getScoreStyle());
+        
+        sb.setScore(vrt.getStyleScore());
+        vrt.setStyleStyle(sb.getScoreStyle());
+        
+    }
+    
+    public void scrollerListener(DataScrollerEvent se)     
+    {        
+        logger.debug("scoll event page: " + se.getPage() + 
+                " old " + se.getOldScrolVal() + " new " + se.getNewScrolVal() +
+                " total " + this.vehicleData.size());
+        
+        this.start = (se.getPage()-1)*this.numRowsPerPg + 1;
+        this.end = (se.getPage())*this.numRowsPerPg;
+        //Partial page
+        if ( this.end > this.vehicleData.size() ) {
+            this.end = this.start + ( this.end - this.vehicleData.size() ) - 1;
+        }
+    }
+    
+
+    public Integer getMaxCount()
+    {
+        return maxCount;
+    }
+
+
+    public void setMaxCount(Integer maxCount)
+    {        
+        this.maxCount = maxCount;
+    }
+
+
+    public Integer getStart()
+    {
+        return start;
+    }
+
+
+    public void setStart(Integer start)
+    {
+        this.start = start;
+    }
+
+
+    public Integer getEnd()
+    {
+        return end;
+    }
+
+
+    public void setEnd(Integer end)
+    {
+        this.end = end;
+    }
+
+
+    public Integer getNumRowsPerPg()
+    {
+        return numRowsPerPg;
+    }
+
+
+    public void setNumRowsPerPg(Integer numRowsPerPg)
+    {
+        this.numRowsPerPg = numRowsPerPg;
+    }
+
+
+    public String getSearchFor()
+    {
+        return searchFor;
+    }
+
+
+    public void setSearchFor(String searchFor)
+    {
+        this.searchFor = searchFor;
+    }
+
+}
