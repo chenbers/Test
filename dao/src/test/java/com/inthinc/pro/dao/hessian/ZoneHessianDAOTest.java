bf1d67796c4c6cf08142d20f74b39028891c9dab
dgileadi <dgileadi@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-11-18 17:11:55 +0000

    Zone DAO test.

---
+++ dao/src/test/java/com/inthinc/pro/dao/hessian/ZoneHessianDAOTest.java
@@ -0,0 +1,56 @@
+package com.inthinc.pro.dao.hessian;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.inthinc.pro.dao.mock.proserver.SiloServiceCreator;
+import com.inthinc.pro.model.Zone;
+
+public class ZoneHessianDAOTest
+{
+    ZoneHessianDAO zoneHessianDAO;
+
+    @BeforeClass
+    public static void runOnceBeforeAllTests() throws Exception
+    {
+
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception
+    {
+    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+        zoneHessianDAO = new ZoneHessianDAO();
+        zoneHessianDAO.setSiloServiceCreator(new SiloServiceCreator());
+    }
+
+    @Test
+    public void findInGroupHierarchy()
+    {
+        try
+        {
+            List<Zone> zones = zoneHessianDAO.getZonesInGroupHierarchy(-1);
+
+            assertTrue("expected no zones to be returned", zones.size() == 0);
+
+            zones = zoneHessianDAO.getZonesInGroupHierarchy(101);
+
+            assertTrue("expected to retrieve zone records", zones.size() > 0);
+        }
+        catch (RuntimeException t)
+        {
+            t.printStackTrace();
+            throw t;
+        }
+    }
+}
