f0ad199c3ee4af8ce7027179fe100cd8377de6c2
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-11-19 22:50:25 +0000

    Baseline.

---
+++ dao/src/main/java/com/inthinc/pro/model/IdlingReportItem.java
@@ -0,0 +1,134 @@
+package com.inthinc.pro.model;
+
+public class IdlingReportItem extends BaseEntity
+{
+    private String group;
+    private String driver;
+    private String driveTime;
+    private Integer miles;
+    private String lowHrs;
+    private Integer lowPercent;
+    private String highHrs;
+    private Integer highPercent;
+    private String totalHrs;
+    private Integer totalPercent;
+    
+    private Integer lowPerSort;
+    private Integer highPerSort;
+    private Integer totalPerSort;
+    private Integer driveTimeSort;
+    
+    public String getGroup()
+    {
+        return group;
+    }
+    public void setGroup(String group)
+    {
+        this.group = group;
+    }
+    public String getDriver()
+    {
+        return driver;
+    }
+    public void setDriver(String driver)
+    {
+        this.driver = driver;
+    }
+    public String getDriveTime()
+    {
+        return driveTime;
+    }
+    public void setDriveTime(String driveTime)
+    {
+        this.driveTime = driveTime;
+    }
+    public Integer getMiles()
+    {
+        return miles;
+    }
+    public void setMiles(Integer miles)
+    {
+        this.miles = miles;
+    }
+    public String getLowHrs()
+    {
+        return lowHrs;
+    }
+    public void setLowHrs(String lowHrs)
+    {
+        this.lowHrs = lowHrs;
+    }
+    public Integer getLowPercent()
+    {
+        return lowPercent;
+    }
+    public void setLowPercent(Integer lowPercent)
+    {
+        this.lowPercent = lowPercent;
+    }
+    public String getHighHrs()
+    {
+        return highHrs;
+    }
+    public void setHighHrs(String highHrs)
+    {
+        this.highHrs = highHrs;
+    }
+    public Integer getHighPercent()
+    {
+        return highPercent;
+    }
+    public void setHighPercent(Integer highPercent)
+    {
+        this.highPercent = highPercent;
+    }
+    public String getTotalHrs()
+    {
+        return totalHrs;
+    }
+    public void setTotalHrs(String totalHrs)
+    {
+        this.totalHrs = totalHrs;
+    }
+    public Integer getTotalPercent()
+    {
+        return totalPercent;
+    }
+    public void setTotalPercent(Integer totalPercent)
+    {
+        this.totalPercent = totalPercent;
+    }
+    public Integer getLowPerSort()
+    {
+        return lowPerSort;
+    }
+    public void setLowPerSort(Integer lowPerSort)
+    {
+        this.lowPerSort = lowPerSort;
+    }
+    public Integer getHighPerSort()
+    {
+        return highPerSort;
+    }
+    public void setHighPerSort(Integer highPerSort)
+    {
+        this.highPerSort = highPerSort;
+    }
+    public Integer getTotalPerSort()
+    {
+        return totalPerSort;
+    }
+    public void setTotalPerSort(Integer totalPerSort)
+    {
+        this.totalPerSort = totalPerSort;
+    }
+    public Integer getDriveTimeSort()
+    {
+        return driveTimeSort;
+    }
+    public void setDriveTimeSort(Integer driveTimeSort)
+    {
+        this.driveTimeSort = driveTimeSort;
+    }
+    
+}
