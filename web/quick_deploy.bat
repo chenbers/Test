1978bc50074199976775a00a7bb4425c1060ffe4
sbrady <sbrady@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-11-21 19:37:09 +0000

    Added batch file for copying modified files to tomcat

---
+++ web/quick_deploy.bat
@@ -0,0 +1,5 @@
+xcopy /Y /D /S src\main\webapp\*.xhtml %CATALINA_HOME%\webapps\tiwipro
+xcopy /Y /D /S src\main\webapp\*.xhtml %CATALINA_HOME%\webapps\tiwipro\WEB-INF\classes
+xcopy /Y /D /S target\classes\*.properties %CATALINA_HOME%\webapps\tiwipro\WEB-INF\classes\
+xcopy /Y /D /S target\classes\*.class %CATALINA_HOME%\webapps\tiwipro\WEB-INF\classes\
+
