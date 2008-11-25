7401c62e8c254ef8eb4b4d098605ca3b5be27b27
kselman <kselman@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-11-25 02:50:16 +0000

    ks - more work on flyout and return from flyout.  added facelets context param to strip html comments from build.

---
+++ web/src/main/java/com/inthinc/pro/backing/FlyOutBean.java
@@ -0,0 +1,58 @@
+package com.inthinc.pro.backing;
+
+import org.apache.log4j.Logger;
+
+public class FlyOutBean 
+{
+    private String navigationRule;
+    private String returnNavRule;
+    private String selectedTab;
+    
+    private static final Logger logger = Logger.getLogger(DriverSeatBeltBean.class);
+    
+    //FLY OUT-IN  ACTION
+    public String flyNavRuleAction()
+    {
+        logger.debug("ACTION: " + returnNavRule);
+        return this.navigationRule;
+    }
+    
+    public String returnNavRuleAction()
+    {
+        logger.debug("ACTION: " + returnNavRule);
+        return returnNavRule;
+        
+    }
+    
+    public String getNavigationRule()
+    {
+        return navigationRule;
+    }
+    public void setNavigationRule(String navigationRule)
+    {
+        logger.debug("SETTING: " + navigationRule);
+        this.navigationRule = navigationRule;
+    }
+    
+    //SELECTED TAB PROPERTIES
+    public String getSelectedTab()
+    {
+        return selectedTab;
+    }
+    public void setSelectedTab(String selectedTab)
+    {
+        this.selectedTab = selectedTab;
+    }
+
+    //RETURN NAV RULE PROPERTIES
+    public String getReturnNavRule()
+    {
+        return returnNavRule;
+    }
+    public void setReturnNavRule(String returnNavRule)
+    {
+        logger.debug("SETTING: " + returnNavRule);
+        this.returnNavRule = returnNavRule;
+    }
+        
+}
