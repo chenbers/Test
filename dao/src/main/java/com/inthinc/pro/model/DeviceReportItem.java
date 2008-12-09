0c48871f9b554a921761826f27efd0a08a4c5af7
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-12-09 23:14:50 +0000

    Baseline.

---
+++ dao/src/main/java/com/inthinc/pro/model/DeviceReportItem.java
@@ -0,0 +1,16 @@
+package com.inthinc.pro.model;
+
+public class DeviceReportItem extends BaseEntity
+{
+    private Device device;
+
+    public Device getDevice()
+    {
+        return device;
+    }
+
+    public void setDevice(Device device)
+    {
+        this.device = device;
+    }
+}
