8acf18fee9ddd88464560b0c4ff0ce5bf3826829
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-12-10 22:43:31 +0000

    Baseline.

---
+++ dao/src/main/java/com/inthinc/pro/model/DVQMap.java
@@ -0,0 +1,33 @@
+package com.inthinc.pro.model;
+
+public class DVQMap extends BaseEntity
+{
+    private Driver driver;
+    private Vehicle vehicle;
+    private DriveQMap driveQ;
+    
+    public Driver getDriver()
+    {
+        return driver;
+    }
+    public void setDriver(Driver driver)
+    {
+        this.driver = driver;
+    }
+    public Vehicle getVehicle()
+    {
+        return vehicle;
+    }
+    public void setVehicle(Vehicle vehicle)
+    {
+        this.vehicle = vehicle;
+    }
+    public DriveQMap getDriveQ()
+    {
+        return driveQ;
+    }
+    public void setDriveQ(DriveQMap driveQ)
+    {
+        this.driveQ = driveQ;
+    }
+}
