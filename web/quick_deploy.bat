aef2c0b22373953275cb2236fc57dd0884d54581
dgileadi <dgileadi@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-12-15 17:20:48 +0000

    Updated to work with new target/tiwipro deploy location.

--- web/quick_deploy.bat
+++ web/quick_deploy.bat
@@ -1,7 +1,5 @@
-xcopy /Y /D /S src\main\webapp\*.xhtml %CATALINA_HOME%\webapps\tiwipro
-xcopy /Y /D /S src\main\webapp\images\* %CATALINA_HOME%\webapps\tiwipro\images
-xcopy /Y /D /S src\main\webapp\css\*.css %CATALINA_HOME%\webapps\tiwipro\css
-xcopy /Y /D /S src\main\webapp\*.js %CATALINA_HOME%\webapps\tiwipro
-xcopy /Y /D /S target\classes\*.properties %CATALINA_HOME%\webapps\tiwipro\WEB-INF\classes\
-xcopy /Y /D /S target\classes\*.class %CATALINA_HOME%\webapps\tiwipro\WEB-INF\classes\
-xcopy /Y /D /S ..\dao\target\classes\*.class %CATALINA_HOME%\webapps\tiwipro\WEB-INF\classes\
+xcopy /Y /D /S src\main\webapp\*.xhtml target\tiwipro
+xcopy /Y /D /S src\main\webapp\images\* target\tiwipro\images
+xcopy /Y /D /S src\main\webapp\css\*.css target\tiwipro\css
+xcopy /Y /D /S src\main\webapp\*.js target\tiwipro
+xcopy /Y /D /S ..\dao\target\classes\*.class target\tiwipro\WEB-INF\classes\
