86881fabc1c3ac61c2cfc2f755fe4e892b58fbf4
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-12-16 21:07:32 +0000

    Baseline.

---
+++ dao/src/main/java/com/inthinc/pro/dao/GQVMapDAO.java
@@ -0,0 +1,11 @@
+package com.inthinc.pro.dao;
+
+import java.util.List;
+
+import com.inthinc.pro.model.GQVMap;
+
+public interface GQVMapDAO extends GenericDAO<GQVMap, Integer>
+{
+    List<GQVMap> getSDTrendsByGTC(Integer groupID, Integer duration, Integer metric);
+
+}
