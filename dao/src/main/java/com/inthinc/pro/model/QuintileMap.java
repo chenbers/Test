bb8fc895448af5f76b5c4a1d6003036dabae9e86
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2008-12-16 00:19:14 +0000

    Baseline.

---
+++ dao/src/main/java/com/inthinc/pro/model/QuintileMap.java
@@ -0,0 +1,52 @@
+package com.inthinc.pro.model;
+
+public class QuintileMap extends BaseEntity
+{
+    private Integer percent1;
+    private Integer percent2;
+    private Integer percent3;
+    private Integer percent4;
+    private Integer percent5;
+    
+    public Integer getPercent1()
+    {
+        return percent1;
+    }
+    public void setPercent1(Integer percent1)
+    {
+        this.percent1 = percent1;
+    }
+    public Integer getPercent2()
+    {
+        return percent2;
+    }
+    public void setPercent2(Integer percent2)
+    {
+        this.percent2 = percent2;
+    }
+    public Integer getPercent3()
+    {
+        return percent3;
+    }
+    public void setPercent3(Integer percent3)
+    {
+        this.percent3 = percent3;
+    }
+    public Integer getPercent4()
+    {
+        return percent4;
+    }
+    public void setPercent4(Integer percent4)
+    {
+        this.percent4 = percent4;
+    }
+    public Integer getPercent5()
+    {
+        return percent5;
+    }
+    public void setPercent5(Integer percent5)
+    {
+        this.percent5 = percent5;
+    }
+
+}
