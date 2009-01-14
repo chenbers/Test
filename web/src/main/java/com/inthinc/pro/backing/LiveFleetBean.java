5bbe30502a6d10d8db50e98d64ef33e2985e04ca
kselman <kselman@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-01-14 05:49:42 +0000

    ks - more work liveFleet page.

--- web/src/main/java/com/inthinc/pro/backing/LiveFleetBean.java
+++ web/src/main/java/com/inthinc/pro/backing/LiveFleetBean.java
@@ -21,34 +21,71 @@ import com.inthinc.pro.util.WebUtil;
 
 public class LiveFleetBean extends BaseBean
 {
+    private static final Logger  logger        = Logger.getLogger(LiveFleetBean.class);
+    
     private NavigationBean       navigation;
     private GroupDAO             groupDAO;
     private DriverDAO            driverDAO;
-    private static final Logger  logger        = Logger.getLogger(LiveFleetBean.class);
-
-    private LatLng               addressLatLng = null;
+    private LatLng               addressLatLng;
     private Integer              addressZoom;
-    private Integer              maxCount      = 10;
-    private Integer              driverPager   = 0;
-
-    private List<DriverLocation> drivers       = new ArrayList<DriverLocation>();
-
+    private Integer              maxCount;
+    private Integer              numRecords;
+    private List<DriverLocation> drivers = new ArrayList<DriverLocation>();   
     private IconMap              mapIconMap;
- //   private IconMap              legendIconMap;
-
-
+    private IconMap              legendIconMap;
     private GroupHierarchy       organizationHierarchy;
 
     public void initBean()
     {
-        logger.debug("initBean.");
+        logger.debug("*** init LiveFleetBean.");
         organizationHierarchy = new GroupHierarchy(groupDAO.getGroupsByAcctID(getAccountID()));
         addressLatLng = organizationHierarchy.getTopGroup().getMapCenter();
         addressZoom = organizationHierarchy.getTopGroup().getMapZoom();
-
-
+    }
+    
+    public void FormAction()
+    {
+        logger.debug("ACTION drivers count: " + maxCount.toString());
+        logger.debug("ACTION driver size " + drivers.size());
+    }
+    
+    // DRIVER LAST LOCATION PROPERTIES
+    public List<DriverLocation> getDrivers()
+    {
+        logger.debug("getDriver hit.");
         
-       
+        if(maxCount == null) setMaxCount(10);
+   
+        
+        logger.debug("getDrivers trying to get: " + getMaxCount().toString());
+        
+        //Get drivers
+        Group fleetGroup = organizationHierarchy.getTopGroup();
+        setDrivers(driverDAO.getDriversNearLoc(fleetGroup.getGroupID(), getMaxCount(), addressLatLng.getLat(), addressLatLng.getLng()));
+        setNumRecords(drivers.size());
+        
+        logger.debug("getDrivers retieved: " + drivers.size());
+        
+        //Get colored map icons.
+        MapIconFactory mif = new MapIconFactory();
+        List<MapIcon> mapIcons = mif.getMapIcons(MapIconFactory.IconType.MARKER, 24);
+        Iterator<MapIcon> mapIconIt = mapIcons.iterator();
+        mapIconMap = new IconMap();
+
+        for (DriverLocation driver : drivers)
+        {
+            if (!mapIconMap.icons.containsKey(driver.getGroupID()))
+            {
+                mapIconMap.addIcon(driver.getGroupID(), mapIconIt.next().getUrl());
+            }
+        }
+    
+        return drivers;
+    }
+   
+    public void setDrivers(List<DriverLocation> drivers)
+    {
+        this.drivers = drivers;
     }
 
     // DAO PROPERTIES
@@ -70,17 +107,6 @@ public class LiveFleetBean extends BaseBean
     public void setDriverDAO(DriverDAO driverDAO)
     {
         this.driverDAO = driverDAO;
-    }
-
-    // DRIVER PAGER PROPERTIES
-    public Integer getDriverPager()
-    {
-        return driverPager;
-    }
-
-    public void setDriverPager(Integer driverPager)
-    {
-        this.driverPager = driverPager;
     }
 
     // ADDRESS LATLNG PROPERTIES
@@ -128,46 +154,6 @@ public class LiveFleetBean extends BaseBean
         this.maxCount = maxCount;
     }
 
-    // DRIVER LAST LOCATION PROPERTIES
-    public List<DriverLocation> getDrivers()
-    {
-        if(drivers.size() < 1)
-        {
-            Group fleetGroup = organizationHierarchy.getTopGroup();
-            drivers = driverDAO.getDriversNearLoc(fleetGroup.getGroupID(), maxCount, addressLatLng.getLat(), addressLatLng.getLng());
-            logger.debug("drivers retieved: " + drivers.size());
-            
-            MapIconFactory mif = new MapIconFactory();
-
-            List<MapIcon> mapIcons = mif.getMapIcons(MapIconFactory.IconType.MARKER, 24);
-//            List<MapIcon> legendIcons = mif.getMapIcons(MapIconFactory.IconType.LEGEND, 24);
-      
-            Iterator<MapIcon> mapIconIt = mapIcons.iterator();
-//            Iterator<MapIcon> legendIconIt = legendIcons.iterator();
-
-            mapIconMap = new IconMap();
-
-            for (DriverLocation driver : drivers)
-            {
-                if (!mapIconMap.icons.containsKey(driver.getGroupID()))
-                {
-                    mapIconMap.addIcon(driver.getGroupID(), mapIconIt.next().getUrl());
-                }
-                
-//                if(!legendIconMap.icons.containsKey(driver.getGroupID()))
-//                {
-//                    legendIconMap.addIcon(driver.getGroupID(), legendIconIt.next().getUrl());
-//                }
-            }
-        }
-        return drivers;
-    }
-
-    public void setDrivers(List<DriverLocation> drivers)
-    {
-        this.drivers = drivers;
-    }
-
     public IconMap getMapIconMap()
     {
         return mapIconMap;
@@ -178,6 +164,16 @@ public class LiveFleetBean extends BaseBean
         this.mapIconMap = mapIconMap;
     }
     
+    public Integer getNumRecords()
+    {
+        return numRecords;
+    }
+
+    public void setNumRecords(Integer numRecords)
+    {
+        this.numRecords = numRecords;
+    }
+
     public String driverAction() {
         
         Map<String,String> requestMap = new WebUtil().getRequestParameterMap();
