8b9f7c2937a4de6d7d735f3e7ebe3eec424413e1
dgileadi <dgileadi@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-01-15 17:52:43 +0000

    Added toString for help with admin searching (bug 3308).

--- dao/src/main/java/com/inthinc/pro/model/State.java
+++ dao/src/main/java/com/inthinc/pro/model/State.java
@@ -1,61 +1,76 @@
 package com.inthinc.pro.model;
 
-
+import com.inthinc.pro.dao.annotations.Column;
 import com.inthinc.pro.dao.annotations.ID;
 import com.inthinc.pro.model.app.States;
 
 public class State extends BaseEntity implements ReferenceEntity
 {
+    @Column(updateable = false)
+    private static final long serialVersionUID = -5519902086559670626L;
+
     @ID
-    private Integer stateID;
-    
-    private String name;
-    private String abbrev;
-    
+    private Integer           stateID;
+
+    private String            name;
+    private String            abbrev;
+
     public State()
     {
         super();
     }
+
     public State(Integer id, String name, String abbrev)
     {
         this.stateID = id;
         this.name = name;
         this.abbrev = abbrev;
     }
+
     public String getAbbrev()
     {
         return abbrev;
     }
+
     public void setAbbrev(String abbrev)
     {
         this.abbrev = abbrev;
     }
+
     public String getName()
     {
         return name;
     }
+
     public void setName(String name)
     {
         this.name = name;
     }
+
     public Integer getStateID()
     {
         return stateID;
     }
+
     public void setStateID(Integer stateID)
     {
         this.stateID = stateID;
     }
+
     @Override
     public Integer retrieveID()
     {
         return stateID;
     }
-        
+
     public static State valueOf(Integer ID)
     {
         return States.getStateById(ID);
     }
 
+    @Override
+    public String toString()
+    {
+        return getName();
+    }
 }
-
