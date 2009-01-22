6d12231f0dd4136be11bd3f91c1e69fe5738895d
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-01-22 18:37:19 +0000

    Baseline.

---
+++ web/src/main/java/com/inthinc/pro/convert/FloatConverter.java
@@ -0,0 +1,33 @@
+package com.inthinc.pro.convert;
+
+import java.text.NumberFormat;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.ConverterException;
+
+import org.apache.log4j.Logger;
+
+public class FloatConverter extends BaseConverter
+{
+    private static final Logger logger = Logger.getLogger(FloatConverter.class);
+    
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) throws ConverterException
+    {
+        return new Float(value);
+    }
+    
+    public String getAsString(FacesContext context, UIComponent component, Object value) throws ConverterException
+    {
+        Float idleTime = (Float)value;
+        NumberFormat format = NumberFormat.getInstance();
+        format.setMaximumFractionDigits(2);
+        format.setMinimumFractionDigits(2);
+
+        return format.format(idleTime);
+
+    }
+
+}
+
