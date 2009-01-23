736b9d9cc0b21ef32110d163501cee7c75e79aba
cjennings <cjennings@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-01-23 16:04:16 +0000

    cj - fix for 3861

--- web/src/main/java/com/inthinc/pro/backing/ui/DriverScoreItem.java
+++ web/src/main/java/com/inthinc/pro/backing/ui/DriverScoreItem.java
@@ -1,13 +1,11 @@
 package com.inthinc.pro.backing.ui;
 
 import com.inthinc.pro.model.DriverScore;
-import com.inthinc.pro.util.MessageUtil;
 
 public class DriverScoreItem
 {
     private DriverScore driverScore;
     private String driverName;
-    private String vehicleID;
     private String style;
     private Integer position;
     
@@ -19,15 +17,6 @@ public class DriverScoreItem
             if (driverScore.getDriver() != null && driverScore.getDriver().getPerson() != null)
                 driverName = driverScore.getDriver().getPerson().getFullName();
             else driverName = "";
-            
-            if (driverScore.getVehicle() != null)
-                vehicleID = driverScore.getVehicle().getName(); 
-            else vehicleID = MessageUtil.getMessageString("teamTop_noVehicle");
-        }
-        else
-        {
-            driverName = "";
-            vehicleID = MessageUtil.getMessageString("teamTop_noVehicle");
         }
     }
     public DriverScore getDriverScore()
@@ -45,14 +34,6 @@ public class DriverScoreItem
     public void setDriverName(String driverName)
     {
         this.driverName = driverName;
-    }
-    public String getVehicleID()
-    {
-        return vehicleID;
-    }
-    public void setVehicleID(String vehicleID)
-    {
-        this.vehicleID = vehicleID;
     }
     public String getStyle()
     {
