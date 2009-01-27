f331f222f1c1129d544c81bdfcc1dacf2ad71a3b
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-01-27 21:29:07 +0000

    Capture idling info across date picker date range.

--- web/src/main/java/com/inthinc/pro/backing/VehicleTripsBean.java
+++ web/src/main/java/com/inthinc/pro/backing/VehicleTripsBean.java
@@ -146,8 +146,15 @@ public class VehicleTripsBean extends BaseBean
             Long tempLong = (trip.getDurationMiliSeconds() / 1000L);
             totalDriveSeconds += tempLong.intValue();
         }
+        
+        // Grabbing events over date picker date range
+        List<Integer> idleTypes = new ArrayList<Integer>();
+        idleTypes.add(EventMapper.TIWIPRO_EVENT_IDLE);
+        List<Event> tmpIdleEvents = new ArrayList<Event>();
+        tmpIdleEvents = eventDAO.getEventsForVehicle(
+                navigation.getVehicle().getVehicleID(), startDate, endDate, idleTypes);        
 
-        for (Event event : idleEvents)
+        for (Event event : tmpIdleEvents)
         {
             idleSeconds += ((IdleEvent) event).getHighIdle();
             idleSeconds += ((IdleEvent) event).getLowIdle();
