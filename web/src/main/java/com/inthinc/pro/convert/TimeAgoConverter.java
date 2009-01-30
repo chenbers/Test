369c508c17f608dd9c2b439984c2618d2b868c92
kselman <kselman@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-01-30 23:55:56 +0000

    ks - added time ago text to dispatch column in LiveFleet.  Added Converter.

---
+++ web/src/main/java/com/inthinc/pro/convert/TimeAgoConverter.java
@@ -0,0 +1,64 @@
+package com.inthinc.pro.convert;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.ConverterException;
+import com.inthinc.pro.dao.util.DateUtil;
+
+/**
+ * TimeAgoConverter
+ * converts a time to "XX hrs XX min ago" or "XX minutes ago" or "XX seconds ago"
+ */
+public class TimeAgoConverter extends BaseConverter
+{
+    public Object getAsObject(FacesContext context, UIComponent component, String value) throws ConverterException
+    {
+        return new Integer(value);
+    }
+    
+    public String getAsString(FacesContext context, UIComponent component, Object value) throws ConverterException
+    {
+        Integer nowSeconds = (int)DateUtil.convertDateToSeconds(new Date());
+        Integer seconds = 0;
+        
+        //Get loggedin user's time zone.
+        TimeZone tz = super.getUser().getUser().getPerson().getTimeZone();
+
+        if( value instanceof Date)
+        {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeZone(tz);
+            calendar.setTime((Date)value);
+            seconds = (int)DateUtil.convertDateToSeconds(calendar.getTime());
+        }
+        
+        if(seconds != 0)        
+            return getDiffString(seconds, nowSeconds); // + " " + DateUtil.getDisplayDate(seconds);
+        else
+            return "Unknown Format";
+    }
+    
+    private String getDiffString(Integer thenSecs, Integer nowSecs)
+    {
+        Integer diff = nowSecs - thenSecs;
+        
+        int hours = diff / 3600;
+        int minutes = (diff % 3600) / 60;
+        int seconds = (diff % 3600) % 60;
+        
+        if(hours > 1)
+            return hours + " hrs " + minutes + " min ago";
+        else if (hours == 1)
+            return hours + " hr " + minutes + " min ago";
+        else if (minutes > 0)
+            return minutes + " minutes ago";
+        else if (seconds > 0)
+            return seconds + " seconds ago";
+        else
+            return "";
+    }
+}
