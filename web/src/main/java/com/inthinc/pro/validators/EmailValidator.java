06a280e129a68c5e3ad972ac8f886d4c4f29fa20
dgileadi <dgileadi@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-02-04 19:26:32 +0000

    Changed to not accept "deleted" e-mail addresses (in the form of del<ID>@tiwi.com).

--- web/src/main/java/com/inthinc/pro/validators/EmailValidator.java
+++ web/src/main/java/com/inthinc/pro/validators/EmailValidator.java
@@ -9,11 +9,20 @@ import com.inthinc.pro.util.MessageUtil;
 public class EmailValidator extends RegexValidator
 {
     public static final Pattern EMAIL_REGEX = Pattern.compile("([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$");
+    public static final Pattern DELETED_REGEX = Pattern.compile("del[0-9]+@tiwi.com");
 
     @Override
     protected Pattern getRegex(UIComponent component)
     {
         return EMAIL_REGEX;
+    }
+
+    @Override
+    public boolean isValid(String value, UIComponent component)
+    {
+        if (super.isValid(value, component))
+            return !DELETED_REGEX.matcher(value).matches();
+        return false;
     }
 
     @Override
