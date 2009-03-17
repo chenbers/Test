71977e2171d7cdac5b8dbcbeb3888cf3ab7b4383
mraby <mraby@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-03-17 16:54:50 +0000

    the previous commit, i stated i changed the toString() method, I meant getDetails()

--- dao/src/main/java/com/inthinc/pro/model/SpeedingEvent.java
+++ dao/src/main/java/com/inthinc/pro/model/SpeedingEvent.java
@@ -5,81 +5,95 @@ import java.util.Date;
 
 public class SpeedingEvent extends Event
 {
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
-	private Integer topSpeed;
-	private Integer avgSpeed;
-	private Integer speedLimit;
-	private Integer distance;
+    private static final long serialVersionUID = 1L;
+    private Integer topSpeed;
+    private Integer avgSpeed;
+    private Integer speedLimit;
+    private Integer distance;
     private Integer avgRPM;
-	
-	public SpeedingEvent()
-	{
-		super();
-	}
-	public SpeedingEvent(Long noteID, Integer vehicleID, Integer type, Date time, Integer speed, Integer odometer, Double latitude, Double longitude,
-						Integer topSpeed, Integer avgSpeed, Integer speedLimit, Integer distance, Integer avgRPM)
-	{
-		super(noteID, vehicleID, type, time, speed, odometer, latitude, longitude);
-		this.topSpeed = topSpeed;
-		this.avgSpeed = avgSpeed;
-		this.speedLimit = speedLimit;
-		this.distance = distance;
+
+    public SpeedingEvent()
+    {
+        super();
+    }
+
+    public SpeedingEvent(Long noteID, Integer vehicleID, Integer type, Date time, Integer speed, Integer odometer, Double latitude, Double longitude, Integer topSpeed,
+            Integer avgSpeed, Integer speedLimit, Integer distance, Integer avgRPM)
+    {
+        super(noteID, vehicleID, type, time, speed, odometer, latitude, longitude);
+        this.topSpeed = topSpeed;
+        this.avgSpeed = avgSpeed;
+        this.speedLimit = speedLimit;
+        this.distance = distance;
         this.avgRPM = avgRPM;
-	}
-	public Integer getAvgSpeed()
-	{
-		return avgSpeed;
-	}
-	public void setAvgSpeed(Integer avgSpeed)
-	{
-		this.avgSpeed = avgSpeed;
-	}
-	public Integer getDistance()
-	{
-		return distance;
-	}
-	public void setDistance(Integer distance)
-	{
-		this.distance = distance;
-	}
-	public Integer getSpeedLimit()
-	{
-		return speedLimit;
-	}
-	public void setSpeedLimit(Integer speedLimit)
-	{
-		this.speedLimit = speedLimit;
-	}
-	public Integer getTopSpeed()
-	{
-		return topSpeed;
-	}
-	public void setTopSpeed(Integer topSpeed)
-	{
-		this.topSpeed = topSpeed;
-	}
-	public EventType getEventType()
-	{
-		return EventType.SPEEDING;
-	}
+    }
+
+    public Integer getAvgSpeed()
+    {
+        return avgSpeed;
+    }
+
+    public void setAvgSpeed(Integer avgSpeed)
+    {
+        this.avgSpeed = avgSpeed;
+    }
+
+    public Integer getDistance()
+    {
+        return distance;
+    }
+
+    public void setDistance(Integer distance)
+    {
+        this.distance = distance;
+    }
+
+    public Integer getSpeedLimit()
+    {
+        return speedLimit;
+    }
+
+    public void setSpeedLimit(Integer speedLimit)
+    {
+        this.speedLimit = speedLimit;
+    }
+
+    public Integer getTopSpeed()
+    {
+        return topSpeed;
+    }
+
+    public void setTopSpeed(Integer topSpeed)
+    {
+        this.topSpeed = topSpeed;
+    }
+
+    public EventType getEventType()
+    {
+        return EventType.SPEEDING;
+    }
+
     public Integer getAvgRPM()
     {
         return avgRPM;
     }
+
     public void setAvgRPM(Integer avgRPM)
     {
         this.avgRPM = avgRPM;
     }
+
     public EventCategory getEventCategory()
     {
         return EventCategory.VIOLATION;
     }
+
     public String getDetails(String formatStr)
     {
-        return MessageFormat.format(formatStr, new Object[] {topSpeed, speedLimit});
+        return MessageFormat.format(formatStr, new Object[] { topSpeed, speedLimit });
     }
 
 }
