23684c2ccf3b5101c55bf55d25d0e5e112ce45cc
mstrong <mstrong@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-06-16 17:01:06 +0000

    Minor changes to the measurement converter

--- dao/src/main/java/com/inthinc/pro/dao/util/MeasurementConversionUtil.java
+++ dao/src/main/java/com/inthinc/pro/dao/util/MeasurementConversionUtil.java
@@ -2,9 +2,11 @@ package com.inthinc.pro.dao.util;
 
 import java.math.BigDecimal;
 
+import com.inthinc.pro.model.MeasurementType;
+
 /**
- * Utility for converty units of measurment from metric to english and eglish to metric MPH = Miles Per Hour KPH = Kilometers Per Hour MPS = Meters Per Second
- * MPG = Miles Per Gallon KPL = Kilometers Per Liter
+ * Utility for converty units of measurment from metric to english and eglish to metric MPH = Miles Per Hour KPH = Kilometers Per Hour MPS = Meters Per Second MPG = Miles Per
+ * Gallon KPL = Kilometers Per Liter
  * 
  * 
  * @author mstrong
@@ -13,28 +15,41 @@ import java.math.BigDecimal;
 
 public class MeasurementConversionUtil
 {
+    /**
+     * 
+     * @param miles
+     * @return kilometers rounded to the nearest tenth
+     */
     public static Float fromMilesToKilometers(Long miles)
     {
         Double kilometers = miles * 1.609344;
-        BigDecimal bigDecimal =  new BigDecimal(Math.round(kilometers * 100));
-        return bigDecimal.movePointLeft(2).floatValue();
+        BigDecimal bigDecimal = new BigDecimal(Math.round(kilometers * 10));
+        return bigDecimal.movePointLeft(1).floatValue();
+
     }
-    
+
+    /**
+     * 
+     * @param kilometers
+     * @return miles rounded to the nearest tenth
+     */
     public static Float fromKilometersToMiles(Long kilometers)
     {
         Double miles = kilometers * 0.62137;
-        BigDecimal bigDecimal =  new BigDecimal(Math.round(miles * 100));
-        return bigDecimal.movePointLeft(2).floatValue();
+        BigDecimal bigDecimal = new BigDecimal(Math.round(miles * 10));
+        return bigDecimal.movePointLeft(1).floatValue();
     }
-    
-    public static Float convertDistance(Long distance,boolean convertToMetric)
+
+    public static Float convertDistance(Integer distance, MeasurementType convertToMeasurementType)
     {
-        if(convertToMetric){
-            return fromMilesToKilometers(distance);
-        }else
-            return fromKilometersToMiles(distance);
+        if (convertToMeasurementType == MeasurementType.METRIC)
+        {
+            return fromMilesToKilometers(distance.longValue());
+        }
+        else
+            return fromKilometersToMiles(distance.longValue());
     }
-    
+
     public static Long fromMPHtoKPH(Long milesPerHour)
     {
         Double kilometersPerHour = milesPerHour * 1.609344;
@@ -52,12 +67,21 @@ public class MeasurementConversionUtil
         Double milesPerLiter = milesPerGallon * 0.42514;
         return Math.round(milesPerLiter);
     }
-    
-    public static Long convertSpeed(Long speed,boolean convertToMetric){
-        if(convertToMetric)
-            return fromMPHtoKPH(speed);
+
+    public static Long convertMileage(Integer mileage, MeasurementType convertToMeasurmentType)
+    {
+        if (convertToMeasurmentType == MeasurementType.METRIC)
+            return fromKPHtoMPH(mileage.longValue());
         else
-            return fromKPHtoMPH(speed);
+            return fromKPHtoMPH(mileage.longValue());
+    }
+
+    public static Long convertSpeed(Integer speed,  MeasurementType convertToMeasurmentType)
+    {
+        if (convertToMeasurmentType == MeasurementType.METRIC)
+            return fromMPHtoKPH(speed.longValue());
+        else
+            return fromKPHtoMPH(speed.longValue());
     }
 
 }
