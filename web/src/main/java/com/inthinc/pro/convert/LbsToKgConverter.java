befe0e49fa70d55ba325ce3755bcfbc752d394b5
mstrong <mstrong@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-06-19 18:07:58 +0000

    bug 5417 - Added metric conversion to weight field in vehicle page. Added new translation Files

---
+++ web/src/main/java/com/inthinc/pro/convert/LbsToKgConverter.java
@@ -0,0 +1,41 @@
+package com.inthinc.pro.convert;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
+import com.inthinc.pro.dao.util.MeasurementConversionUtil;
+import com.inthinc.pro.model.MeasurementType;
+
+public class LbsToKgConverter extends BaseConverter
+{
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent compoent, String value)
+    {
+        if(value != null){
+            if(getUser().getUser().getPerson().getMeasurementType().equals(MeasurementType.METRIC))
+            {
+                Long weight = Long.valueOf(value);
+                return MeasurementConversionUtil.fromKgToPounds(weight).intValue();
+            }
+            else
+            {
+                return Integer.valueOf(value);
+            }
+        }
+        return null;
+    }
+    
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value)
+    {
+        if (Integer.class.isInstance(value))
+        {
+            if(getUser().getUser().getPerson().getMeasurementType().equals(MeasurementType.METRIC))
+                return MeasurementConversionUtil.fromPoundsToKg(((Integer)value).longValue()).toString();
+            else
+                return value.toString();
+        }
+        return null;
+    }
+    
+}
