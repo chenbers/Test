6ed1ae604c174dfc6f9fffceb85dbade57bb6944
mstrong <mstrong@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-07-14 21:17:17 +0000

    Locale was not used when retrieving values from the scheduler resrouce bundle. Added code to use the users locale

--- scheduler/src/main/java/com/inthinc/pro/scheduler/i18n/LocalizedMessage.java
+++ scheduler/src/main/java/com/inthinc/pro/scheduler/i18n/LocalizedMessage.java
@@ -8,14 +8,20 @@ public class LocalizedMessage
 {
     private static final String BUNDLE_NAME = "messages";
     
-    public static String getString(String key)
+    public static String getString(String key,Locale locale)
     {   
-        return getStringWithValues(key);
+        return getStringWithValues(key,locale);
     }
     
-    public static String getStringWithValues(String key,String... parameters)
+    public static String getStringWithValues(String key,Locale locale,String... parameters)
     {
-        ResourceBundle myResources = ResourceBundle.getBundle(BUNDLE_NAME, Locale.getDefault());
+        Locale currentLocale = null;
+        if(locale != null)
+            currentLocale = locale;
+        else
+            currentLocale = Locale.getDefault();
+            
+        ResourceBundle myResources = ResourceBundle.getBundle(BUNDLE_NAME, locale);
         
         String message = null;
         if (myResources.containsKey(key))
