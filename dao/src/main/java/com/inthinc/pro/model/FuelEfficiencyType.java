63fd2aabf91911af049e7a87daa752618557a31f
mraby <mraby@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-07-30 17:29:35 +0000

    adding FuelEfficiencyType enum

---
+++ dao/src/main/java/com/inthinc/pro/model/FuelEfficiencyType.java
@@ -0,0 +1,38 @@
+package com.inthinc.pro.model;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+public enum FuelEfficiencyType implements BaseEnum {
+	MPG_US(1), MPG_UK(2), KMPL(3), LP100KM(4);
+	private int code;
+	
+	private FuelEfficiencyType(int code) {
+	    this.code = code;
+	}
+	
+	private static final Map<Integer, FuelEfficiencyType> lookup = new HashMap<Integer, FuelEfficiencyType>();
+	static {
+	    for(FuelEfficiencyType type : EnumSet.allOf(FuelEfficiencyType.class)) {
+	        lookup.put(type.code, type);       
+	    }
+	}
+	
+	private static FuelEfficiencyType valueOf(Integer code) {
+	    return lookup.get(code);
+	}
+	
+    @Override
+    public Integer getCode() {
+        return this.code;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(this.getClass().getSimpleName());
+        sb.append(".");
+        sb.append(this.name());
+        return sb.toString();
+    }	
+}
--- dao/src/main/java/com/inthinc/pro/model/MeasurementType.java
+++ dao/src/main/java/com/inthinc/pro/model/MeasurementType.java
@@ -3,42 +3,32 @@ package com.inthinc.pro.model;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
-
-
-public enum MeasurementType implements BaseEnum
-{
+public enum MeasurementType implements BaseEnum {
     ENGLISH(1), METRIC(2);
-    
     private int code;
-    
-    private MeasurementType(int code)
-    {
+
+    private MeasurementType(int code) {
         this.code = code;
     }
 
     private static final Map<Integer, MeasurementType> lookup = new HashMap<Integer, MeasurementType>();
-    static
-    {
-        for (MeasurementType type : EnumSet.allOf(MeasurementType.class))
-        {
+    static {
+        for (MeasurementType type : EnumSet.allOf(MeasurementType.class)) {
             lookup.put(type.code, type);
         }
     }
-    
-    public static MeasurementType valueOf(Integer code)
-    {
+
+    public static MeasurementType valueOf(Integer code) {
         return lookup.get(code);
     }
 
     @Override
-    public Integer getCode()
-    {
+    public Integer getCode() {
         return this.code;
     }
-    
+
     @Override
-    public String toString()
-    {
+    public String toString() {
         StringBuilder sb = new StringBuilder(this.getClass().getSimpleName());
         sb.append(".");
         sb.append(this.name());
