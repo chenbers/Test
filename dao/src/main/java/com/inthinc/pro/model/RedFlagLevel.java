334e261c634a20eeed854cae0a3dd2ddf95fa513
mstrong <mstrong@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-08-03 22:19:15 +0000

    Added Tamper field to redFlag.xhtml

--- dao/src/main/java/com/inthinc/pro/model/RedFlagLevel.java
+++ dao/src/main/java/com/inthinc/pro/model/RedFlagLevel.java
@@ -43,7 +43,10 @@ public enum RedFlagLevel implements BaseEnum
     @Override
     public String toString()
     {
-        return this.description;
+        StringBuilder sb = new StringBuilder(this.getClass().getSimpleName());
+        sb.append(".");
+        sb.append(this.name());
+        return sb.toString();
     }
 
     public String getDescription()
