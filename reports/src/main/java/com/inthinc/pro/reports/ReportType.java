481703700af44125468002199442ab479e9f0166
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-08-14 23:25:46 +0000

    Add new Crash History reports.

--- reports/src/main/java/com/inthinc/pro/reports/ReportType.java
+++ reports/src/main/java/com/inthinc/pro/reports/ReportType.java
@@ -28,7 +28,7 @@ public enum ReportType
     EMERGENCY_REPORT("Emergency Report", "EventReport.jrxml","EventReportRaw.jrxml"),
     ZONE_ALERT_REPORT("Zone Alert Report", "EventReport.jrxml","EventReportRaw.jrxml"),
     RED_FLAG_REPORT("Red Flag Report", "RedFlagReport.jrxml","RedFlagReportRaw.jrxml"),
-    CRASH_HISTORY_REPORT("Crash History Report", "CrashHistory.jrxml", "CrashHistoryReportRaw.jrxml");
+    CRASH_HISTORY_REPORT("Crash History Report", "CrashHistoryReport.jrxml", "CrashHistoryReportRaw.jrxml");
   
     private String prettyTemplate;
     private String rawTemplate;
