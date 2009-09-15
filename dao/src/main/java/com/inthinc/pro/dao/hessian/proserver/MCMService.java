80b560410d5c8635edbddcc65dd849c1017dd98e
mstrong <mstrong@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-09-15 03:19:24 +0000

    Matt - Committing Mike's Code for creating crashes.

--- dao/src/main/java/com/inthinc/pro/dao/hessian/proserver/MCMService.java
+++ dao/src/main/java/com/inthinc/pro/dao/hessian/proserver/MCMService.java
@@ -3,9 +3,12 @@ package com.inthinc.pro.dao.hessian.proserver;
 import java.util.List;
 import java.util.Map;
 
+import com.inthinc.pro.ProDAOException;
+
 
 
 public interface MCMService extends HessianService
 {
 	List<Map> note(String mcmID, List<byte[]> noteList);
+	List<Map> crash(String mcmID, List<byte[]> noteList) throws ProDAOException;
 }
