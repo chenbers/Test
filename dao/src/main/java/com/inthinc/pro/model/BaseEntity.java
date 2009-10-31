2704bd1b40ebcdac612918c67f9d65547a14d02f
mraby <mraby@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-10-31 05:49:57 +0000

    Added @XmlTransient to Locale field on BaseEntity.java (not sure it should exist here, going to ask Jacquie if it is needed).

--- dao/src/main/java/com/inthinc/pro/model/BaseEntity.java
+++ dao/src/main/java/com/inthinc/pro/model/BaseEntity.java
@@ -5,41 +5,46 @@ import java.util.Date;
 import java.util.Locale;
 
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 import com.inthinc.pro.dao.annotations.Column;
 
 @XmlRootElement
 public abstract class BaseEntity implements Serializable {
 
-  @Column(updateable=false)
-  private Date modified;
-  @Column(updateable=false)
-  private Date created;
-  
-  private Locale locale;
+    @Column(updateable = false)
+    private Date modified;
+    @Column(updateable = false)
+    private Date created;
 
-  public Locale getLocale() {
-	return locale;
-}
+    private Locale locale;
 
-public void setLocale(Locale locale) {
-	this.locale = locale;
-}
+    @XmlTransient
+    public Locale getLocale() {
+        return locale;
+    }
 
-public Date getModified() {
-    return modified;
-  }
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+    }
 
-  public void setModified(Date modified) {
-    this.modified = modified;
-  }
+    public Date getModified() {
+        return modified;
+    }
 
-  public Date getCreated() {
-    return created;
-  }
+    public void setModified(Date modified) {
+        this.modified = modified;
+    }
 
-  public void setCreated(Date created) {
-    this.created = created;
-  }
-  public void prepareForDisplay(){}
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    //TODO: is this necessary in the BaseEntity? Display what? The DAO shouldn't know about the view.
+    public void prepareForDisplay() {
+    }
 }
