1a2848fdec15383a7b836c9c32b6a03ca02effcd
cjennings <cjennings@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-11-14 00:18:10 +0000

    added helps for new crash report pages
    Changed to show english help always (for now).

--- web/src/main/java/com/inthinc/pro/backing/HelpBean.java
+++ web/src/main/java/com/inthinc/pro/backing/HelpBean.java
@@ -1,5 +1,7 @@
 package com.inthinc.pro.backing;
 
+import java.util.Locale;
+
 import javax.faces.context.FacesContext;
 
 public class HelpBean extends BaseBean {
@@ -10,6 +12,9 @@ public class HelpBean extends BaseBean {
 		if (path == null)
 		{
 			String lang = this.getLocale().getLanguage();
+			// TODO: remove this when romanian help is available
+			lang = Locale.ENGLISH.getLanguage();
+			
 			path = FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath() + "/secured/lochelp/" + lang + "/"  + lang + "/";  
 
 		}
