678e885453a589c2756da83b4a8e5cdf92a95efb
dhock <dhock@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-11-21 06:53:04 +0000

    added create driver and vehicle

--- webfulfillment/src/main/java/com/inthinc/pro/fulfillment/backing/DAOUtilBean.java
+++ webfulfillment/src/main/java/com/inthinc/pro/fulfillment/backing/DAOUtilBean.java
@@ -10,16 +10,21 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.TimeZone;
 
+import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
 import javax.faces.model.SelectItem;
+import javax.servlet.http.HttpSession;
 
-import org.springframework.security.AuthenticationException;
 import org.springframework.security.context.SecurityContextHolder;
+
 
 import com.caucho.hessian.client.HessianRuntimeException;
 import com.inthinc.pro.dao.AccountDAO;
+import com.inthinc.pro.dao.AddressDAO;
 import com.inthinc.pro.dao.DeviceDAO;
 import com.inthinc.pro.dao.DriverDAO;
 import com.inthinc.pro.dao.EventDAO;
@@ -29,11 +34,14 @@ import com.inthinc.pro.dao.RoleDAO;
 import com.inthinc.pro.dao.UserDAO;
 import com.inthinc.pro.dao.VehicleDAO;
 import com.inthinc.pro.model.Account;
+import com.inthinc.pro.model.Address;
 import com.inthinc.pro.model.Device;
 import com.inthinc.pro.model.Driver;
 import com.inthinc.pro.model.Event;
 import com.inthinc.pro.model.EventMapper;
 import com.inthinc.pro.model.Group;
+import com.inthinc.pro.model.GroupStatus;
+import com.inthinc.pro.model.GroupType;
 import com.inthinc.pro.model.LastLocation;
 import com.inthinc.pro.model.Person;
 import com.inthinc.pro.model.Status;
@@ -43,7 +51,7 @@ import com.inthinc.pro.model.app.Roles;
 import com.inthinc.pro.security.userdetails.ProUser;
 import com.inthinc.pro.util.RFIDBean;
 
-public class DAOUtilBean {
+public class DAOUtilBean implements PhaseListener {
 
 	private AccountDAO accountDAO;
 	private GroupDAO groupDAO;
@@ -53,6 +61,7 @@ public class DAOUtilBean {
 	private UserDAO userDAO;
 	private PersonDAO personDAO;
 	private DriverDAO driverDAO;
+	private AddressDAO addressDAO;
 
 	private RoleDAO roleDAO;
 
@@ -61,24 +70,37 @@ public class DAOUtilBean {
 	private Device device;
 
 	private Map<Integer, String> accountMap;
-	List<SelectItem> groupSelectList;
-	private String imei;
-	private String serialNum;
-	private String vehicleName;
-	private String VIN;
+	private Map<Integer, String> groupMap;
 	private Integer selectedAccountID;
 	private Integer selectedGroupID;
 
+		
+	private String imei;
+	private String serialNum;
+	
+	private String vehicleName;
+	private String VIN;
+	
+	private String make;
+	private String model;
+	private String year;
+	private String license;
+
+	private String lastName;
+	private String firstName;
+	private String empID;
+	private String driverID;
+	private String RFID;
+	private boolean useFOB;	
+	
 	private String errorMsg;
 	private String successMsg;
 	private List<String> messageList;
 	private Account rmaAccount;
 	private Account shipAccount;
 
-	private String driverID;
-	private String RFID;
-	private boolean useFOB;
 	private boolean assignedVehiclesOnly;
+	private String vehicleGrid;
 	private RFIDBean rfidBean;
 
 	private static final String rmausername = "RMA";
@@ -324,17 +346,25 @@ public class DAOUtilBean {
 		setSerialNum(null);
 	}
 
-	public String getVehicleGrid() {
-		if (selectedGroupID == null || selectedGroupID < 0) {
-			return "";
+	public String getVehicleGrid()
+	{
+		return vehicleGrid;
+	}
+	public void getVehiclesAction() {
+		vehicleGrid = "";	
+		if (selectedAccountID == null || selectedAccountID < 0) {
+			return;
 		}
-		String vehicleGrid = "";	
+		Integer groupID=selectedGroupID;
+		if (groupID == null || groupID < 0) {
+			groupID=findTopGroup(selectedAccountID).getGroupID();
+		}
 		
 		List<Vehicle> vehicles;
 		if (assignedVehiclesOnly)		
-			vehicles = getAssignedVehicles(selectedGroupID);
+			vehicles = getAssignedVehicles(groupID);
 		else
-			vehicles = vehicleDAO.getVehiclesInGroupHierarchy(selectedGroupID);
+			vehicles = vehicleDAO.getVehiclesInGroupHierarchy(groupID);
 			
 
 		vehicleGrid = "<table class=\"itable\" width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"3\">";
@@ -343,34 +373,34 @@ public class DAOUtilBean {
 		int row = 0;
 		for (Iterator<Vehicle> viter = vehicles.iterator(); viter.hasNext();) {
 			Vehicle vehicle = viter.next();
-			String rowStyle = "\"reg\"";
-			String serialnum = "---";
-			if (vehicle.getDeviceID() != null) {
-				Device device = deviceDAO.findByID(vehicle.getDeviceID());
-				serialnum = device.getSerialNum();
-			}
+			if (vehicle.getStatus().equals(Status.ACTIVE))
+			{
+				String rowStyle = "\"reg\"";
+				String serialnum = "---";
+				if (vehicle.getDeviceID() != null) {
+					Device device = deviceDAO.findByID(vehicle.getDeviceID());
+					serialnum = device.getSerialNum();
+				}
 
-			String barcode = "---";
-			if (vehicle.getDriverID() != null) {
-				Driver driver = driverDAO.findByID(vehicle.getDeviceID());
-				Long rfid = rfidBean.findBarcode(driver.getRFID());
-				if (rfid != null)
-					barcode = rfid.toString();
-			}
+				String barcode = "---";
+				if (vehicle.getDriverID() != null) {
+					Driver driver = driverDAO.findByID(vehicle.getDeviceID());
+					Long rfid = rfidBean.findBarcode(driver.getRFID());
+					if (rfid != null)
+						barcode = rfid.toString();
+				}
 
-			if (row % 2 == 1)
-				rowStyle = "\"alt\"";
-			vehicleGrid += "<tr class=" + rowStyle + ">";
-			vehicleGrid += "<td class=\"first\">" + vehicle.getName() + "</td>";
-			vehicleGrid += "<td>" + serialnum + "</td>";
-			vehicleGrid += "<td class=\"last\">" + barcode + "</td>";
-			vehicleGrid += "</tr>";
+				if (row % 2 == 1)
+					rowStyle = "\"alt\"";
+				vehicleGrid += "<tr class=" + rowStyle + ">";
+				vehicleGrid += "<td class=\"first\">" + vehicle.getName() + "</td>";
+				vehicleGrid += "<td>" + serialnum + "</td>";
+				vehicleGrid += "<td class=\"last\">" + barcode + "</td>";
+				vehicleGrid += "</tr>";
+			}
 		}
 
 		vehicleGrid += "</table>";
-
-		return vehicleGrid;
-
 	}
 
 	public void editVINAction() {
@@ -413,15 +443,14 @@ public class DAOUtilBean {
 
 	public void assignRFIDAction() {
 		reInitAction();
-		Integer did = 0;
-		Long barcode = 0L;
-
-		try {
-			did = Integer.parseInt(driverID);
-		} catch (NumberFormatException e) {
-			setErrorMsg("Error: Invalid Driver ID Number" + driverID);
+		
+		if (selectedAccountID == null || selectedAccountID < 0) {
+			setErrorMsg("Error: Please select a customer account");
 			return;
 		}
+		
+		Long barcode = 0L;
+
 		try {
 			barcode = Long.parseLong(RFID);
 		} catch (NumberFormatException e) {
@@ -431,7 +460,7 @@ public class DAOUtilBean {
 
 		Long rfid = rfidBean.findRFID(barcode, useFOB);
 
-		Driver driver = driverDAO.findByID(did);
+		Driver driver = findDriver(selectedAccountID, driverID);
 
 		if (driver == null) {
 			setErrorMsg("Error: Driver not found " + driverID);
@@ -520,70 +549,140 @@ public class DAOUtilBean {
 		setVehicleName(null);
 	}
 
-	public List<SelectItem> getAccountSelectList() {
-		List<SelectItem> accountList = new ArrayList<SelectItem>();
-		accountList.add(new SelectItem(-1, "--Select a Account--"));
-
-		// Put keys and values in to an arraylist using entryset
-		ArrayList sortedArrayList = new ArrayList(getAccountMap().entrySet());
-
-		// Sort the values based on values first and then keys.
-		Collections.sort(sortedArrayList, new AlphaComparator());
-
-		// Show sorted results
-		Iterator itr = sortedArrayList.iterator();
-		Integer id = -1;
-		String name = "";
-
-		while (itr.hasNext()) {
-			Map.Entry e = (Map.Entry) itr.next();
-			id = (Integer) e.getKey();
-			name = ((String) e.getValue());
-			if (!id.equals(this.shipAccountID) && !id.equals(this.rmaAccountID))
-				accountList.add(new SelectItem(id, name));
+	public void createDriverAction() {
+		reInitAction();
+		if (selectedAccountID == null || selectedAccountID < 0) {
+			setErrorMsg("Error: Please select a customer account");
+			return;
+		} else if (selectedGroupID == null || selectedGroupID < 0) {
+			setErrorMsg("Error: Please select a Team");
+			return;
+		} else if (lastName == null || lastName.trim().length() == 0) {
+			setErrorMsg("Error: Please enter a valid lastName");
+			return;
 		}
-		return accountList;
-	}
+		
+		Address address = new Address();
+		Integer addressID = addressDAO.create(selectedAccountID, address);
+		if (addressID==null || addressID<1)
+		{
+			setErrorMsg("Error creating address");
+			return;
+		}
+		
+		
+		Person person = new Person();
+		person.setLast(lastName.trim());
+		person.setFirst(firstName.trim());
+		person.setAddressID(addressID);
+		person.setTimeZone(TimeZone.getDefault());
+		person.setEmpid(empID.trim());
 
-	public List<SelectItem> getGroupSelectList() {
+		Integer personID = personDAO.create(selectedAccountID, person);
+		if (personID==null || personID<1)
+		{
+			setErrorMsg("Error creating person");
+			return;
+		}
+					
+		Driver driver = new Driver();
+		driver.setGroupID(selectedGroupID);
+		driver.setPersonID(personID);
+		driver.setStatus(Status.ACTIVE);
 
-		if (groupSelectList == null) {
-			groupSelectList = new ArrayList<SelectItem>();
-			if (selectedAccountID != null && selectedAccountID >= 0) {
-
-				groupSelectList = new ArrayList<SelectItem>();
-
-				groupSelectList.add(new SelectItem(-1, "--Select a Group--"));
-
-				// Put keys and values in to an arraylist using entryset
-
-				Hashtable<Integer, String> groupMap = new Hashtable<Integer, String>();
-				List<Group> groups = groupDAO
-						.getGroupsByAcctID(selectedAccountID);
-				for (Iterator<Group> giter = groups.iterator(); giter.hasNext();) {
-					Group group = giter.next();
-					groupMap.put(group.getGroupID(), getGroupPath(group));
+		if (RFID!=null && RFID.trim().length()>0)
+		{
+			Long barcode = 0L;
+			try {
+				barcode = Long.parseLong(RFID);
+				if (barcode!=null)
+				{
+					Long rfid = rfidBean.findRFID(barcode, useFOB);
+					if (rfid==null)
+					{
+						setErrorMsg("Error: RFID not found for barcode: " + RFID);
+						return;
+					}
+					driver.setRFID(rfid);
 				}
-
-				ArrayList sortedArrayList = new ArrayList(groupMap.entrySet());
-				// Sort the values based on values first and then keys.
-				Collections.sort(sortedArrayList, new AlphaComparator());
-
-				// Show sorted results
-				Iterator itr = sortedArrayList.iterator();
-				Integer id = -1;
-				String name = "";
-
-				while (itr.hasNext()) {
-					Map.Entry e = (Map.Entry) itr.next();
-					id = (Integer) e.getKey();
-					name = ((String) e.getValue());
-					groupSelectList.add(new SelectItem(id, name));
-				}
+			} catch (NumberFormatException e) {
 			}
 		}
+		
+		Integer driverID = driverDAO.create(personID, driver);
+		if (driverID==null || driverID<1)
+		{
+			setErrorMsg("Error creating driver");
+			return;
+		}
 
-		return groupSelectList;
+		setSuccessMsg("driverID " + driverID + "created successfully");
+		
+		lastName=null;
+		firstName=null;
+		RFID=null;
+		empID=null;
+
+	}
+
+	public void createVehicleAction() {
+		reInitAction();
+		
+		Integer yearint=null;
+		
+		if (selectedAccountID == null || selectedAccountID < 0) {
+			setErrorMsg("Error: Please select a customer account");
+			return;
+		} else if (selectedGroupID == null || selectedGroupID < 0) {
+			setErrorMsg("Error: Please select a Team");
+			return;
+		} else if (VIN == null || VIN.trim().length() == 0) {
+			setErrorMsg("Error: Please enter a valid VIN");
+			return;
+		} else if (year != null && year.trim().length() > 0) {
+			try {
+				yearint=Integer.parseInt(year);
+			} catch (NumberFormatException e) {
+			}
+			if (yearint==null || yearint<1900 || yearint>3000)
+			{
+				setErrorMsg("Error: Please enter a valid Year");
+				return;
+			}
+		} else if (serialNum!=null && serialNum.trim().length()>0) {
+			loadDevice();
+			if (device==null)
+				return;
+		}
+	
+		
+		Vehicle vehicle = new Vehicle();
+		vehicle.setName(vehicleName.trim());
+		vehicle.setLicense(license.trim());
+		vehicle.setVIN(VIN.trim());
+		vehicle.setMake(make.trim());
+		vehicle.setModel(model.trim());
+		vehicle.setYear(yearint);
+		vehicle.setGroupID(selectedGroupID);
+		vehicle.setStatus(Status.ACTIVE);
+		Integer vehicleID = vehicleDAO.create(selectedGroupID, vehicle);
+		if (vehicleID==null || vehicleID<1)
+		{
+			setErrorMsg("Error creating vehicleID " + vehicleID);
+			return;
+		}
+		if (device!=null)
+			vehicleDAO.setVehicleDevice(vehicleID, device.getDeviceID());
+
+		setSuccessMsg("VehicleID " + vehicleID + " successfully created");
+		setVIN(null);
+		setVehicleName(null);
+		serialNum=null;
+		make=null;
+		model=null;
+		year=null;
+		license=null;
+
 	}
 
 	private String getGroupPath(Group group) {
@@ -620,6 +719,40 @@ public class DAOUtilBean {
 		return topGroup;
 	}
 
+	public Driver findDriver(Integer accountID, String name)
+	{
+		Driver driver = null;
+
+		Group topGroup = findTopGroup(accountID);
+		if (topGroup == null)
+			return null;
+
+		List<Person> persons = personDAO.getPeopleInGroupHierarchy(topGroup.getGroupID());
+		Person person = null;
+		for (Iterator<Person> piter = persons.iterator(); piter.hasNext();) {
+			person = piter.next();
+			if (person.getEmpid()!=null)
+			{
+				if (name.toUpperCase().trim().equals(person.getEmpid().trim().toUpperCase()))
+				{
+					driver = driverDAO.findByPersonID(person.getPersonID());
+					if (driver!=null)
+						return driver;
+				}
+			}
+		}
+		Integer did=0;
+		try {
+			did = Integer.parseInt(name);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+
+		driver = driverDAO.findByID(did);
+		
+		return driver;
+	}
+	
 	public Vehicle findVehicle(Integer accountID, String name) {
 		Vehicle vehicle = null;
 
@@ -651,7 +784,7 @@ public class DAOUtilBean {
 		if (accountMap == null || accountMap.isEmpty()) {
 			accountMap = new Hashtable<Integer, String>();
 			int limit = 700; // TODO Lose this!!
- //limit=10;
+limit=10;
 			List<Account> accounts=null;
 try {			
 			accounts = accountDAO.getAllAcctIDs();
@@ -676,6 +809,76 @@ try {
 
 	}
 
+	public List<SelectItem> getAccountSelectList() {
+		List<SelectItem> accountList = new ArrayList<SelectItem>();
+
+		// Put keys and values in to an arraylist using entryset
+		ArrayList sortedArrayList = new ArrayList(getAccountMap().entrySet());
+
+		// Sort the values based on values first and then keys.
+		Collections.sort(sortedArrayList, new AlphaComparator());
+
+		// Show sorted results
+		Iterator itr = sortedArrayList.iterator();
+		Integer id = -1;
+		String name = "";
+
+		while (itr.hasNext()) {
+			Map.Entry e = (Map.Entry) itr.next();
+			id = (Integer) e.getKey();
+			name = ((String) e.getValue());
+			if (!id.equals(this.shipAccountID) && !id.equals(this.rmaAccountID))
+				accountList.add(new SelectItem(id, name));
+		}
+		return accountList;
+	}
+
+	public Map<Integer, String> getGroupMap() {
+		if (groupMap==null || groupMap.isEmpty())
+		{
+			groupMap = new Hashtable<Integer, String>();
+			List<Group> groups = groupDAO
+					.getGroupsByAcctID(selectedAccountID);
+			for (Iterator<Group> giter = groups.iterator(); giter.hasNext();) {
+				Group group = giter.next();
+				if (group.getStatus().equals(GroupStatus.GROUP_ACTIVE)
+					&& group.getType().equals(GroupType.TEAM))
+					groupMap.put(group.getGroupID(), getGroupPath(group));
+			}			
+		}
+		return groupMap;
+	}
+
+	public List<SelectItem> getGroupSelectList() {
+
+		List<SelectItem> groupSelectList = new ArrayList<SelectItem>();
+		if (selectedAccountID != null && selectedAccountID >= 0) {
+
+			// Put keys and values in to an arraylist using entryset
+
+
+			ArrayList sortedArrayList = new ArrayList(getGroupMap().entrySet());
+			// Sort the values based on values first and then keys.
+			Collections.sort(sortedArrayList, new AlphaComparator());
+
+			// Show sorted results
+			Iterator itr = sortedArrayList.iterator();
+			Integer id = -1;
+			String name = "";
+
+			while (itr.hasNext()) {
+				Map.Entry e = (Map.Entry) itr.next();
+				id = (Integer) e.getKey();
+				name = ((String) e.getValue());
+				groupSelectList.add(new SelectItem(id, name));
+			}
+		}
+
+		return groupSelectList;
+	}
+
+	
+	
 	public void clearErrorAction() {
 		reInitAction();
 		setSerialNum(null);
@@ -685,6 +888,7 @@ try {
 	public void reInitAction() {
 		setErrorMsg(null);
 		setSuccessMsg(null);
+		vehicleGrid="";
 		if (messageList != null)
 			messageList.clear();
 	}
@@ -752,15 +956,24 @@ try {
 	public void setSelectedAccountID(Integer selectedAccountID) {
 		if (!selectedAccountID.equals(this.selectedAccountID)) {
 			selectedGroupID = null;
-			groupSelectList = null;
+			groupMap = null;
 		}
 		this.selectedAccountID = selectedAccountID;
 	}
 
 	public String getSelectedAccountName() {
 		if (selectedAccountID == null || selectedAccountID < 0)
-			return "--Select a Account--";
+			return "--Select an Customer--";
 		return getAccountMap().get(this.selectedAccountID);
+	}
+
+	public String getSelectedGroupName() {
+		if (selectedAccountID == null || selectedAccountID < 0)
+			return "--Select an Customer--";
+		if (selectedGroupID == null || selectedGroupID < 0)
+			return "--Select a Team--";
+		
+		return getGroupMap().get(this.selectedGroupID);
 	}
 
 	public List<String> getMessageList() {
@@ -905,7 +1118,7 @@ try {
 	}
 
 	public void setSelectedGroupID(Integer selectedGroupID) {
-		if (groupSelectList == null || groupSelectList.isEmpty())
+		if (groupMap == null || groupMap.isEmpty())
 			selectedGroupID = null;
 		this.selectedGroupID = selectedGroupID;
 	}
@@ -916,5 +1129,92 @@ try {
 
 	public void setAssignedVehiclesOnly(boolean assignedVehiclesOnly) {
 		this.assignedVehiclesOnly = assignedVehiclesOnly;
+	}
+
+	@Override
+	public void afterPhase(PhaseEvent event) {
+		// TODO Auto-generated method stub
+		if (event.getPhaseId().equals(PhaseId.RENDER_RESPONSE))
+		{
+	    	FacesContext context = event.getFacesContext();  
+	    	HttpSession session = (HttpSession) context.getExternalContext().getSession(true);  
+	    	DAOUtilBean bean = (DAOUtilBean) session.getAttribute("daoUtilBean");
+	    	if (bean!=null)
+	    		bean.reInitAction();
+		}
+	}
+
+	@Override
+	public void beforePhase(PhaseEvent event) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public PhaseId getPhaseId() {
+		return PhaseId.RENDER_RESPONSE;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getLicense() {
+		return license;
+	}
+
+	public void setLicense(String license) {
+		this.license = license;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getEmpID() {
+		return empID;
+	}
+
+	public void setEmpID(String empID) {
+		this.empID = empID;
+	}
+
+	public AddressDAO getAddressDAO() {
+		return addressDAO;
+	}
+
+	public void setAddressDAO(AddressDAO addressDAO) {
+		this.addressDAO = addressDAO;
 	}
 }
