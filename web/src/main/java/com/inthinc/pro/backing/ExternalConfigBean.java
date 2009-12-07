bf3c428da206dba62edaa2f25fa9a84411ddaac9
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2009-12-07 21:05:36 +0000

    Rally defect DE3716.

---
+++ web/src/main/java/com/inthinc/pro/backing/ExternalConfigBean.java
@@ -0,0 +1,21 @@
+package com.inthinc.pro.backing;
+
+public class ExternalConfigBean extends BaseBean {
+    
+    private String phoneNumber01;
+    private String phoneNumber02;
+    
+    public String getPhoneNumber01() {
+        return phoneNumber01;
+    }
+    public void setPhoneNumber01(String phoneNumber01) {
+        this.phoneNumber01 = phoneNumber01;
+    }
+    public String getPhoneNumber02() {
+        return phoneNumber02;
+    }
+    public void setPhoneNumber02(String phoneNumber02) {
+        this.phoneNumber02 = phoneNumber02;
+    }
+    
+}
