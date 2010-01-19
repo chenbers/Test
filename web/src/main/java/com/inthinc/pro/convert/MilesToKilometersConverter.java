1acd29c823d2a86d5dcb7997b79ebfab24452cfd
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-01-19 16:45:39 +0000

    Add conversion code.

--- web/src/main/java/com/inthinc/pro/convert/MilesToKilometersConverter.java
+++ web/src/main/java/com/inthinc/pro/convert/MilesToKilometersConverter.java
@@ -3,7 +3,9 @@ package com.inthinc.pro.convert;
 import java.text.NumberFormat;
 import java.util.Formatter.BigDecimalLayoutForm;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.ConverterException;
 
@@ -11,11 +13,37 @@ import org.apache.commons.lang.NotImplementedException;
 
 import com.inthinc.pro.dao.util.MeasurementConversionUtil;
 import com.inthinc.pro.model.MeasurementType;
+import com.inthinc.pro.util.MessageUtil;
 
 public class MilesToKilometersConverter extends BaseConverter {
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) throws ConverterException {
-        throw new NotImplementedException();
+        try
+        {
+            if (value != null && !value.equals(""))
+            {
+                if (getMeasurementType().equals(MeasurementType.METRIC))
+                {
+                    Long dist = Long.valueOf(value);
+                    return MeasurementConversionUtil.fromKilometersToMiles(dist).intValue();                    
+                }
+                else
+                {
+                    return Integer.valueOf(value);
+                }
+            }
+        }
+        catch (NumberFormatException e)
+        {
+            final String summary = MessageUtil.getMessageString("error_number_format");
+            final FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, summary, null);
+            context.addMessage(component.getClientId(context), message);
+            if(component instanceof UIInput){
+                ((UIInput)component).setValid(false);
+            }
+        }
+
+        return null;
     }
 
     @Override
