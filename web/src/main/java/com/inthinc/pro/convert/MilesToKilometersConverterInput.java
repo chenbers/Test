aae29413563507f8087983173643f173902cc959
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-01-22 00:02:10 +0000

    New class and changes to get the comma correct on the admin pages.

---
+++ web/src/main/java/com/inthinc/pro/convert/MilesToKilometersConverterInput.java
@@ -0,0 +1,59 @@
+package com.inthinc.pro.convert;
+
+import java.text.NumberFormat;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.ConverterException;
+
+import com.inthinc.pro.dao.util.MeasurementConversionUtil;
+import com.inthinc.pro.model.MeasurementType;
+import com.inthinc.pro.util.MessageUtil;
+
+public class MilesToKilometersConverterInput extends BaseConverter  {
+    
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) throws ConverterException {
+        try
+        {
+            if (value != null && !value.equals(""))
+            {
+                if (getMeasurementType().equals(MeasurementType.METRIC))
+                {
+                    Long dist = Long.valueOf(value);
+                    return MeasurementConversionUtil.fromKilometersToMiles(dist).intValue();                    
+                }
+                else
+                {
+                    return Integer.valueOf(value);
+                }
+            }
+        }
+        catch (NumberFormatException e)
+        {
+            final String summary = MessageUtil.getMessageString("error_number_format");
+            final FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, summary, null);
+            context.addMessage(component.getClientId(context), message);
+            if(component instanceof UIInput){
+                ((UIInput)component).setValid(false);
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) throws ConverterException {
+
+        if (Integer.class.isInstance(value))
+        {
+            if (getMeasurementType().equals(MeasurementType.METRIC))
+                return MeasurementConversionUtil.fromKilometersToMiles(((Integer) value).longValue()).toString();
+            else
+                return value.toString();
+        }
+        return null;
+    }
+}
