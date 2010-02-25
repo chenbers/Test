f1385dd394c02e4b92cfaf1d07f3677f13bcda15
jhoward <jhoward@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-02-25 18:58:21 +0000

    new team page with first attempt at displaying trips

--- web/src/main/java/com/inthinc/pro/backing/TeamTripsBean.java
+++ web/src/main/java/com/inthinc/pro/backing/TeamTripsBean.java
@@ -1,14 +1,20 @@
 package com.inthinc.pro.backing;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
+import com.inthinc.pro.backing.ui.TripDisplay;
 import com.inthinc.pro.dao.DriverDAO;
 import com.inthinc.pro.map.MapIcon;
 import com.inthinc.pro.map.MapIconFactory;
 import com.inthinc.pro.model.Driver;
-import com.inthinc.pro.model.Group;
+import com.inthinc.pro.model.LatLng;
 import com.inthinc.pro.model.Trip;
+import com.inthinc.pro.util.MiscUtil;
 
 public class TeamTripsBean extends BaseBean {
 
@@ -18,55 +24,59 @@ public class TeamTripsBean extends BaseBean {
 	private static final long serialVersionUID = 1L;
 	
 	private List<Driver> drivers;
-	private List<Trip> selectedDriverTrips;
+	private List<TripDisplay> selectedDriverTrips;
 	private Driver selectedDriver;
-    private Integer groupID;
-    private Group group;
     private DriverDAO driverDAO;
     private List<MapIcon> icons;
+    private TeamCommonBean teamCommonBean;
 	
+    public void init(){
+    	
+		icons = MapIconFactory.IconType.TEAM_LEGEND.getIconList(15);
+		drivers = driverDAO.getDrivers(teamCommonBean.getGroupID());
+    }
 	public List<Driver> getDrivers() {
 		
-		if (drivers == null){
-			
-			drivers = driverDAO.getDrivers(getGroupID());
-			icons = MapIconFactory.IconType.TEAM_LEGEND.getIconList(15);
-		}
 		return drivers;
 	}
 	public void setDrivers(List<Driver> drivers) {
 		this.drivers = drivers;
 	}
-	public List<Trip> getSelectedDriverTrips() {
-		selectedDriverTrips = new ArrayList<Trip>();
+	public void loadTrips(){
+		
+		Date endDate = new Date();
+		Date startDate = new Date();
+        Calendar gc = new GregorianCalendar();
+        gc.setTime(startDate);
+        gc.add(Calendar.HOUR, -(24*30));
+        startDate = gc.getTime();
+
+		List<Trip> trips = driverDAO.getTrips(selectedDriver.getDriverID(), startDate, endDate);
+		selectedDriverTrips = new ArrayList<TripDisplay>();
+        for (Trip trip : trips) {
+            TripDisplay td = new TripDisplay(trip, selectedDriver.getPerson().getTimeZone(), addressLookup);
+            // If starting or ending address is null, try to set a zone name
+            if ( td.getStartAddress() == null ) {
+                LatLng latLng = new LatLng(td.getRoute().get(0).getLat(),td.getRoute().get(0).getLng());
+                td.setStartAddress(MiscUtil.findZoneName(this.getProUser().getZones(), latLng));
+            }                
+            if ( td.getEndAddress() == null ) {
+                LatLng latLng = new LatLng(td.getEndPointLat(),td.getEndPointLng());
+                td.setEndAddress(MiscUtil.findZoneName(this.getProUser().getZones(), latLng));
+            }
+            selectedDriverTrips.add(td);
+            
+
+        }
+        Collections.sort(selectedDriverTrips);
+        Collections.reverse(selectedDriverTrips);
+	}
+	public List<TripDisplay> getSelectedDriverTrips() {
+
 		return selectedDriverTrips;
 	}
-	public void setSelectedDriverTrips(List<Trip> selectedDriverTrips) {
+	public void setSelectedDriverTrips(List<TripDisplay> selectedDriverTrips) {
 		this.selectedDriverTrips = selectedDriverTrips;
-	}
-	public Driver getSelectedDriver() {
-		return selectedDriver;
-	}
-	public void setSelectedDriver(Driver selectedDriver) {
-		this.selectedDriver = selectedDriver;
-	}
-	public Integer getGroupID() {
-		return groupID;
-	}
-	public void setGroupID(Integer groupID) {
-
-		if(groupID!= null){
-			
-		    group = getGroupHierarchy().getGroup(groupID);
-		}
-		this.groupID = groupID;
-		    
-	}
-	public Group getGroup() {
-		return group;
-	}
-	public void setGroup(Group group) {
-		this.group = group;
 	}
 	public DriverDAO getDriverDAO() {
 		return driverDAO;
@@ -80,5 +90,19 @@ public class TeamTripsBean extends BaseBean {
 	public void setIcons(List<MapIcon> icons) {
 		this.icons = icons;
 	}
-
+	public Integer getGroupID() {
+		return teamCommonBean.getGroupID();
+	}
+	public TeamCommonBean getTeamCommonBean() {
+		return teamCommonBean;
+	}
+	public void setTeamCommonBean(TeamCommonBean teamCommonBean) {
+		this.teamCommonBean = teamCommonBean;
+	}
+	public Driver getSelectedDriver() {
+		return selectedDriver;
+	}
+	public void setSelectedDriver(Driver selectedDriver) {
+		this.selectedDriver = selectedDriver;
+	}
 }
