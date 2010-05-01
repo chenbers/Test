8f5ae1031e5811e6a9166dee87733fdd44da45ca
mstrong <mstrong@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-05-01 20:46:13 +0000

    Updating changes made to new scorebox

--- web/src/main/webapp/js/scorebox.js
+++ web/src/main/webapp/js/scorebox.js
@@ -1,3 +1,73 @@
-window.ScoreBox = {};
 
-ScoreBox.
+
+(function($){ 
+
+	$.fn.scoreBox = function(options){
+		var settings = $.extend({
+			size:"small",
+			notApplicableText:"N/A"
+		},options||{});
+		this.each(function(){
+			var wrappedSet = $(this);
+			while(wrappedSet.html().indexOf("<") >= 0){
+				wrappedSet = $.getChildWrappedSet(wrappedSet);
+			}
+			
+			var score = wrappedSet.html();
+			wrappedSet.html(score != parseFloat(score)?settings.notApplicableText:score);
+			
+			var scoreCssClass = ScoreBox.getScoreCssClass(score);
+			var sizeCssClass = ScoreBox.getSizeCssClass(settings.size);
+			$(this).attr("class",$(this).attr("class") + " score " + scoreCssClass + " " + sizeCssClass);
+			var innerHtml = $(this).html();
+			$(this).html("");
+			$("<div>" + innerHtml + "</div>").appendTo($(this));
+		});
+	};	
+	
+	$.getChildWrappedSet = function(parent){
+		return parent.children();
+	};
+	
+	
+	
+})(jQuery);
+
+
+var ScoreBox = {};
+
+ScoreBox.getScoreCssClass = function(score){
+	
+	if(score != parseFloat(score)){
+		return "score_na";
+	}
+	
+	score = new Number(score);
+	
+	if(score >= 0 && score < 1)
+		return "score_1";
+	else if(score >= 1 && score < 2)
+		return "score_2";
+	else if(score >= 2 && score < 3)
+		return "score_3";
+	else if(score >= 3 && score < 4)
+		return "score_4";
+	else if(score >= 4)
+		return "score_5";
+		
+	return "score_na";
+};
+
+ScoreBox.getSizeCssClass = function(size){
+	if(size == "xx-small")
+		return "score_xx-small";
+	else if(size == "small")
+		return "score_small";
+	else if(size == "medium")
+		return "score_medium";
+	else if(size == "large")
+		return "score_large";
+	else if(size == "xx-large")
+		return "score_xx-large";
+};
+
