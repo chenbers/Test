11e3f69e056eac62b9f774e26c0cc966893b7ecb
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-05-06 16:32:37 +0000

    US1287, set default for phone number on error pages.

--- web/src/main/java/com/inthinc/pro/backing/PhoneBean.java
+++ web/src/main/java/com/inthinc/pro/backing/PhoneBean.java
@@ -8,12 +8,30 @@ public class PhoneBean extends BaseBean {
 
     public String getPhoneNumber01() {
         Account acct = this.getAccountDAO().findByID(this.getProUser().getUser().getPerson().getAcctID());
-        return acct.getProps().getSupportPhone1();
+        
+        // this is required, so load something
+        phoneNumber01 = "1888888TIWI";
+        
+        if ( acct.getProps() != null &&
+             acct.getProps().getSupportPhone1() != null &&
+             acct.getProps().getSupportPhone1().trim().length() != 0 ) {
+            phoneNumber01 = acct.getProps().getSupportPhone1();
+        }
+        return phoneNumber01;
     }
 
     public String getPhoneNumber02() {
-        Account acct = this.getAccountDAO().findByID(this.getProUser().getUser().getPerson().getAcctID());        
-        return acct.getProps().getSupportPhone2();
+        Account acct = this.getAccountDAO().findByID(this.getProUser().getUser().getPerson().getAcctID());
+        
+        // this is not required, load blank then see if another number is supplied
+        phoneNumber02 = "";
+        
+        if ( acct.getProps() != null &&
+             acct.getProps().getSupportPhone2() != null &&
+             acct.getProps().getSupportPhone2().trim().length() != 0 ) {
+               phoneNumber02 = acct.getProps().getSupportPhone2();
+           }        
+        return phoneNumber02;
     }
     
 }
