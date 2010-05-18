c25c926155d4b2112f08066418887567c264232b
cjennings <cjennings@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-05-18 18:14:46 +0000

    US1384 - temporary fix for 307 error being thrown for existing zone/red flag alerts -- will need back end fix for this

--- dao/src/main/java/com/inthinc/pro/dao/hessian/RedFlagAlertHessianDAO.java
+++ dao/src/main/java/com/inthinc/pro/dao/hessian/RedFlagAlertHessianDAO.java
@@ -1,15 +1,14 @@
 package com.inthinc.pro.dao.hessian;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import com.inthinc.pro.dao.RedFlagAlertDAO;
 import com.inthinc.pro.dao.hessian.exceptions.EmptyResultSetException;
-import com.inthinc.pro.dao.hessian.exceptions.ProxyException;
+import com.inthinc.pro.dao.hessian.exceptions.RemoteServerException;
 import com.inthinc.pro.dao.hessian.mapper.RedFlagsAlertMapper;
 import com.inthinc.pro.model.RedFlagAlert;
-import com.inthinc.pro.model.ZoneAlert;
+import com.inthinc.pro.model.User;
 
 public class RedFlagAlertHessianDAO extends GenericHessianDAO<RedFlagAlert, Integer> implements RedFlagAlertDAO
 {
@@ -32,29 +31,28 @@ public class RedFlagAlertHessianDAO extends GenericHessianDAO<RedFlagAlert, Inte
     }
 
     @Override
-    public List<RedFlagAlert> getRedFlagAlertsByUserID(Integer userID)
-    {
-        try
-        {
+    public List<RedFlagAlert> getRedFlagAlertsByUserID(Integer userID) {
+        try {
             return getMapper().convertToModelObject(getSiloService().getRedFlagAlertsByUserID(userID), RedFlagAlert.class);
-        }
-        catch (EmptyResultSetException e)
-        {
-            return Collections.emptyList();
-        }
-        
-    }
-    @Override
-    public List<RedFlagAlert> getRedFlagAlertsByUserIDDeep(Integer userID)
-    {
-        try
-        {
-            return getMapper().convertToModelObject(getSiloService().getRedFlagAlertsByUserIDDeep(userID), RedFlagAlert.class);
-        }
-        catch (EmptyResultSetException e)
-        {
+        } catch (EmptyResultSetException e) {
             return Collections.emptyList();
         }
     }
 
+    @Override
+    public List<RedFlagAlert> getRedFlagAlertsByUserIDDeep(Integer userID) {
+        try {
+            return getMapper().convertToModelObject(getSiloService().getRedFlagAlertsByUserIDDeep(userID), RedFlagAlert.class);
+        } catch (EmptyResultSetException e) {
+            return Collections.emptyList();
+        }
+        // TODO: THIS IS A KLUDGE SO THIS WILL WORK UNTIL DS GETS BACK AND CAN FIX IT!!!!!!
+        catch (RemoteServerException ex) {
+            if (ex.getErrorCode() == 307) {
+                User user = getMapper().convertToModelObject(getSiloService().getUser(userID), User.class);
+                return getRedFlagAlerts(user.getPerson().getAcctID());
+            }
+            return Collections.emptyList();
+        }
+    }
 }
--- dao/src/main/java/com/inthinc/pro/dao/hessian/ZoneAlertHessianDAO.java
+++ dao/src/main/java/com/inthinc/pro/dao/hessian/ZoneAlertHessianDAO.java
@@ -1,13 +1,12 @@
 package com.inthinc.pro.dao.hessian;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import com.inthinc.pro.dao.ZoneAlertDAO;
 import com.inthinc.pro.dao.hessian.exceptions.EmptyResultSetException;
-import com.inthinc.pro.dao.hessian.exceptions.ProxyException;
-import com.inthinc.pro.model.RedFlagAlert;
+import com.inthinc.pro.dao.hessian.exceptions.RemoteServerException;
+import com.inthinc.pro.model.User;
 import com.inthinc.pro.model.ZoneAlert;
 
 public class ZoneAlertHessianDAO extends GenericHessianDAO<ZoneAlert, Integer> implements ZoneAlertDAO
@@ -25,30 +24,31 @@ public class ZoneAlertHessianDAO extends GenericHessianDAO<ZoneAlert, Integer> i
         }
         
     }
+
     @Override
-    public List<ZoneAlert> getZoneAlertsByUserID(Integer userID)
-    {
-        try
-        {
+    public List<ZoneAlert> getZoneAlertsByUserID(Integer userID) {
+        try {
             return getMapper().convertToModelObject(getSiloService().getZoneAlertsByUserID(userID), ZoneAlert.class);
-        }
-        catch (EmptyResultSetException e)
-        {
+        } catch (EmptyResultSetException e) {
             return Collections.emptyList();
         }
     }
+
     @Override
-    public List<ZoneAlert> getZoneAlertsByUserIDDeep(Integer userID)
-    {
-        try
-        {
+    public List<ZoneAlert> getZoneAlertsByUserIDDeep(Integer userID) {
+        try {
             return getMapper().convertToModelObject(getSiloService().getZoneAlertsByUserIDDeep(userID), ZoneAlert.class);
-        }
-        catch (EmptyResultSetException e)
-        {
+        } catch (EmptyResultSetException e) {
             return Collections.emptyList();
         }
-        
+        // TODO: THIS IS A KLUDGE SO THIS WILL WORK UNTIL DS GETS BACK AND CAN FIX IT!!!!!!
+        catch (RemoteServerException ex) {
+            if (ex.getErrorCode() == 307) {
+                User user = getMapper().convertToModelObject(getSiloService().getUser(userID), User.class);
+                return getZoneAlerts(user.getPerson().getAcctID());
+            }
+            return Collections.emptyList();
+        }
     }
 
     @Override
