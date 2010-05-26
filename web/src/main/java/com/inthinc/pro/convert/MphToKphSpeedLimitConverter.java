c036255ea44cb410045d70526c94898f37daf887
jhoward <jhoward@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-05-26 16:34:43 +0000

    new converter MphToKphSpeedLimitConverter converts mph to nearest 5kph, used for speed limits on driver, vehicle speed pages and in the event bubble.

---
+++ web/src/main/java/com/inthinc/pro/convert/MphToKphSpeedLimitConverter.java
@@ -0,0 +1,49 @@
+package com.inthinc.pro.convert;
+
+import java.text.NumberFormat;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.ConverterException;
+
+import com.inthinc.pro.dao.util.MeasurementConversionUtil;
+import com.inthinc.pro.model.MeasurementType;
+
+public class MphToKphSpeedLimitConverter extends BaseConverter {
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) throws ConverterException
+    {
+        if(value != null){
+            if(getMeasurementType().equals(MeasurementType.METRIC))
+            {
+                Integer speed = Integer.valueOf(value);
+                return MeasurementConversionUtil.fromKPHtoMPH(speed).intValue(); 
+            }
+            else
+            {
+                return Integer.valueOf(value);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) throws ConverterException
+    {
+        if (Number.class.isInstance(value))
+        {
+            if (getMeasurementType().equals(MeasurementType.METRIC)){
+                Double addSome= new Double(MeasurementConversionUtil.fromMPHtoKPH(Number.class.cast(value)).doubleValue())+2.5d;
+                   int nearestFive = addSome.intValue()/5 * 5;
+                return NumberFormat.getInstance(getLocale()).format(nearestFive);
+            }
+        }
+      
+        if(value != null)
+            return NumberFormat.getInstance(getLocale()).format(value);
+        else
+            return null;
+    }    
+
+}
