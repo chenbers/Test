771b09d721acd3c6a3cc7f2a156da4e04a399d43
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-08-02 20:45:49 +0000

    Change from drive_time to duration.

--- web/src/main/java/com/inthinc/pro/table/columns/IdlingReportTableColumns.java
+++ web/src/main/java/com/inthinc/pro/table/columns/IdlingReportTableColumns.java
@@ -14,7 +14,7 @@ public class IdlingReportTableColumns extends BaseTableColumns {
         AVAILABLE_COLUMNS = new ArrayList<String>();
         AVAILABLE_COLUMNS.add("group");
         AVAILABLE_COLUMNS.add("driver_person_fullName");
-        AVAILABLE_COLUMNS.add("driveTime");
+        AVAILABLE_COLUMNS.add("duration");
         AVAILABLE_COLUMNS.add("hasRPM");
         AVAILABLE_COLUMNS.add("lowHrs");
         AVAILABLE_COLUMNS.add("lowPercent");
