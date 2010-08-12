13dd875ca093aa82473e8b8d606b530c4fc6657e
jhoward <jhoward@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-08-12 16:02:15 +0000

    TimeFrameTest for testing fixes to wrong interval bug

---
+++ dao/src/test/java/com/inthinc/pro/model/TimeFrameTest.java
@@ -0,0 +1,39 @@
+package com.inthinc.pro.model;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.Interval;
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore
+public class TimeFrameTest {
+
+	   @Test
+	    public void timeIntervalTest(){
+		   
+		   //South Africa GMT+2.00
+	    	DateTimeZone southAfricaTimeZone = DateTimeZone.forOffsetHours(2);
+	    	Interval sainterval = TimeFrame.ONE_DAY_AGO.getInterval(southAfricaTimeZone);
+	        //Original calculation
+	    	DateTime saStart = sainterval.getStart().toDateTime(DateTimeZone.UTC);
+	        DateTime saintervalToUse = sainterval.getStart().toDateTime(DateTimeZone.UTC).toDateMidnight().toDateTime();
+	        //Revised calculation
+	        DateTime saOffsetStart = sainterval.getStart().plusMillis(southAfricaTimeZone.getOffset(sainterval.getStart()));
+	        DateTime sautcOffsetStart = sainterval.getStart().plusMillis(southAfricaTimeZone.getOffset(sainterval.getStart())).toDateTime(DateTimeZone.UTC);
+	        DateTime sautcOffsetInterval = sainterval.getStart().plusMillis(southAfricaTimeZone.getOffset(sainterval.getStart())).toDateTime(DateTimeZone.UTC).toDateMidnight().toDateTime();
+
+	        //Utah GMT-7
+	    	DateTimeZone utahTimeZone = DateTimeZone.forOffsetHours(-7);
+	    	Interval uinterval = TimeFrame.ONE_DAY_AGO.getInterval(utahTimeZone);
+	        //Original calculation
+	    	DateTime utStart = uinterval.getStart().toDateTime(DateTimeZone.UTC);
+	        DateTime utintervalToUse = uinterval.getStart().toDateTime(DateTimeZone.UTC).toDateMidnight().toDateTime();
+	        //Revised calculation
+	        DateTime utOffsetStart = uinterval.getStart().plusMillis(utahTimeZone.getOffset(uinterval.getStart()));
+	        DateTime ututcOffsetStart = uinterval.getStart().plusMillis(utahTimeZone.getOffset(uinterval.getStart())).toDateTime(DateTimeZone.UTC);
+	        DateTime ututcOffsetInterval = uinterval.getStart().plusMillis(utahTimeZone.getOffset(uinterval.getStart())).toDateTime(DateTimeZone.UTC).toDateMidnight().toDateTime();
+	        int j=1;
+	   }
+
+}
