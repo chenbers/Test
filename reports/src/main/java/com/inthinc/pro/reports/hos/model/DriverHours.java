205d40df8e606fbd12e8c885a4324a00a971a6aa
icazacu <icazacu@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-10-05 20:27:39 +0000

    US1958: Driver Hours Report merged in tiwiPro

---
+++ reports/src/main/java/com/inthinc/pro/reports/hos/model/DriverHours.java
@@ -0,0 +1,53 @@
+package com.inthinc.pro.reports.hos.model;
+
+public class DriverHours {
+    private String groupName;
+    private String date;
+    private String driverName;
+    private Double hours;
+
+    public DriverHours(){
+        
+    }
+    
+    public DriverHours(String groupName, String date, String driverName, Double hours) {
+        this.groupName = groupName;
+        this.date = date;
+        this.driverName = driverName;
+        this.hours = hours;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public Double getHours() {
+        return hours;
+    }
+
+    public void setHours(Double hoursThisDay) {
+        this.hours = hoursThisDay;
+    }
+
+
+}
