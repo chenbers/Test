bf207d394b61a7d468f88471f6e0a5d355bb9e71
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-10-13 21:53:06 +0000

    TA2947: added getTextMsgPage() and getTextMsgCount()

--- dao/src/main/java/com/inthinc/pro/dao/TextMsgAlertDAO.java
+++ dao/src/main/java/com/inthinc/pro/dao/TextMsgAlertDAO.java
@@ -1,12 +1,31 @@
 package com.inthinc.pro.dao;
 
+import java.util.Date;
 import java.util.List;
 
-import com.inthinc.pro.model.TextMsgAlert;
-import com.inthinc.pro.model.ZoneAlert;
+import com.inthinc.pro.model.MessageItem;
+import com.inthinc.pro.model.pagination.PageParams;
+import com.inthinc.pro.model.pagination.TableFilterField;
 
-public interface TextMsgAlertDAO extends GenericDAO<TextMsgAlert, Integer>{
+public interface TextMsgAlertDAO extends GenericDAO<MessageItem, Integer>{
     
-    List<TextMsgAlert> getTextMsgAlertsByAcctID(Integer acctID);
+    List<MessageItem> getTextMsgAlertsByAcctID(Integer acctID);
+    
+    /**
+     * Retrieve the total count of filtered list of text messages for all of the drivers under the specified group and time frame.
+     * 
+     * @param groupID
+     * @return
+     */
+    Integer getTextMsgCount(Integer groupID, Date startDate, Date endDate,  List<TableFilterField> filterList);
+
+
+    /**
+     * Retrieve the sorted/filtered sublist of text messages for all of the drivers under the specified group and time frame.
+     * 
+     * @param groupID
+     * @return
+     */
+    List<MessageItem> getTextMsgPage(Integer groupID, Date startDate, Date endDate, List<TableFilterField> filterList, PageParams pageParams);
 
 }
--- dao/src/main/java/com/inthinc/pro/dao/hessian/TextMsgAlertHessianDAO.java
+++ dao/src/main/java/com/inthinc/pro/dao/hessian/TextMsgAlertHessianDAO.java
@@ -1,22 +1,50 @@
 package com.inthinc.pro.dao.hessian;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 import com.inthinc.pro.dao.TextMsgAlertDAO;
 import com.inthinc.pro.dao.hessian.exceptions.EmptyResultSetException;
-import com.inthinc.pro.model.TextMsgAlert;
-import com.inthinc.pro.model.ZoneAlert;
+import com.inthinc.pro.dao.util.DateUtil;
+import com.inthinc.pro.model.MessageItem;
+import com.inthinc.pro.model.pagination.PageParams;
+import com.inthinc.pro.model.pagination.TableFilterField;
 
 
-public class TextMsgAlertHessianDAO extends GenericHessianDAO<TextMsgAlert, Integer> implements TextMsgAlertDAO{
+public class TextMsgAlertHessianDAO extends GenericHessianDAO<MessageItem, Integer> implements TextMsgAlertDAO{
 
     @Override
-    public List<TextMsgAlert> getTextMsgAlertsByAcctID(Integer acctID) {
+    public List<MessageItem> getTextMsgAlertsByAcctID(Integer acctID) {
         try {
-            return getMapper().convertToModelObject(getSiloService().getTextMsgAlertsByAcctID(acctID), TextMsgAlert.class);
+            return getMapper().convertToModelObject(getSiloService().getTextMsgAlertsByAcctID(acctID), MessageItem.class);
         } catch (EmptyResultSetException e) {
             return Collections.emptyList();
+        }
+    }
+    
+    @Override
+    public List<MessageItem> getTextMsgPage(Integer groupID, Date startDate, Date endDate, List<TableFilterField> filterList, PageParams pageParams) {
+        try {
+            List<MessageItem> messageItemList = getMapper().convertToModelObject(
+                    getSiloService().getTextMsgPage(groupID, DateUtil.convertDateToSeconds(startDate), DateUtil.convertDateToSeconds(endDate), getMapper().convertList(filterList), getMapper().convertToMap(pageParams)),
+                    MessageItem.class);
+            return messageItemList;
+        } catch (EmptyResultSetException e) {
+            return Collections.emptyList();
+        }
+    }
+    
+    @Override
+    public Integer getTextMsgCount(Integer groupID, Date startDate, Date endDate, List<TableFilterField> filterList) {
+        try {
+            if (filterList == null)
+                filterList = new ArrayList<TableFilterField>();
+            return getChangedCount(getSiloService().getTextMsgCount(groupID, DateUtil.convertDateToSeconds(startDate), DateUtil.convertDateToSeconds(endDate), getMapper().convertList(filterList)));
+
+        } catch (EmptyResultSetException e) {
+            return 0;
         }
     }
 
