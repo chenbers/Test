cf1fde6693a620a5dcec5974e527636080f23c3b
icazacu <icazacu@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-10-18 16:31:30 +0000

    US2147: DOT: State Mileage by Vehicle report merge

---
+++ reports/src/main/java/com/inthinc/pro/reports/ifta/DOTReportCriteria.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ */
+package com.inthinc.pro.reports.ifta;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.joda.time.Interval;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import com.inthinc.pro.dao.GroupDAO;
+import com.inthinc.pro.dao.StateMileageDAO;
+import com.inthinc.pro.model.MeasurementType;
+import com.inthinc.pro.reports.ReportCriteria;
+import com.inthinc.pro.reports.ReportType;
+
+/**
+ * ReportCriteria parent class for DOT/IFTA reports.
+ */
+public class DOTReportCriteria extends ReportCriteria {
+    protected DateTimeFormatter dateTimeFormatter; 
+    protected String units;
+    protected GroupDAO groupDAO;
+    protected StateMileageDAO stateMileageDAO;
+
+    /**
+     * Default constructor.
+     * @param locale
+     */
+    public DOTReportCriteria(ReportType reportType, Locale locale) {
+        super(reportType, "", locale);
+        dateTimeFormatter = DateTimeFormat.forPattern("MM/dd/yyyy").withLocale(locale);
+        setMeasurementType(MeasurementType.ENGLISH);
+    }
+
+    /**
+     * @{inherit-doc}
+     * @see com.inthinc.pro.reports.ReportCriteria#setMeasurementType(com.inthinc.pro.model.MeasurementType)
+     */
+    @Override
+    public void setMeasurementType(MeasurementType measurementType) {
+        super.setMeasurementType(measurementType);
+        if (measurementType != null)
+            units = measurementType.toString();
+    }
+
+    /**
+     * Setter for Group DAO. 
+     * @param groupDAO
+     */
+    public void setGroupDAO(GroupDAO groupDAO) {
+        this.groupDAO = groupDAO;
+    }
+
+    /**
+     * Initiate the data set and the parameters for the report.
+     * @param groupId the groupId chosen by the user
+     * @param interval the date period
+     * @param iftaOnly the flag to consider only IFTA 
+     */
+    public void init(List<Integer> groupIDList, Interval interval, boolean dotOnly) {
+        addParameter(ReportCriteria.REPORT_START_DATE, dateTimeFormatter.print(interval.getStart()));
+        addParameter(ReportCriteria.REPORT_END_DATE, dateTimeFormatter.print(interval.getEnd()));
+        addParameter("units", units);
+    }
+
+    /**
+     * The StateMileageDAO setter.
+     * @param stateMileageDAO the DAO to set
+     */
+    public void setStateMileageDAO(StateMileageDAO stateMileageDAO) {
+        this.stateMileageDAO = stateMileageDAO;
+    }   
+}
---
+++ reports/src/main/java/com/inthinc/pro/reports/ifta/StateMileageByVehicleReportCriteria.java
@@ -0,0 +1,81 @@
+package com.inthinc.pro.reports.ifta;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+
+import org.joda.time.Interval;
+
+import com.inthinc.pro.dao.util.MeasurementConversionUtil;
+import com.inthinc.pro.model.StateMileage;
+import com.inthinc.pro.reports.ReportType;
+import com.inthinc.pro.reports.ifta.model.MileageByVehicle;
+
+/**
+ * Report Criteria for StateMileageByVehicle report.
+ */
+public class StateMileageByVehicleReportCriteria extends DOTReportCriteria {
+
+    /**
+     * Default constructor.
+     * @param locale
+     */
+    public StateMileageByVehicleReportCriteria(Locale locale) {
+        super(ReportType.STATE_MILEAGE_BY_VEHICLE, locale);
+    }
+
+    /**
+     * Initiate the DataSet and the parameters for the report.
+     * @param groupId the groupId chosen by the user
+     * @param interval the date period
+     * @param iftaOnly the flag to consider only DOT/IFTA 
+     */
+    @Override
+    public void init(List<Integer> groupIDList, Interval interval, boolean dotOnly) {
+        super.init(groupIDList, interval, dotOnly);
+
+        List<StateMileage> dataList = new ArrayList<StateMileage>();
+        for (Integer groupID : groupIDList) {
+            List<StateMileage> list = stateMileageDAO.getStateMileageByVehicle(groupID, interval, dotOnly);
+            if (list != null) {
+                dataList.addAll(list);
+            }
+        }      
+        initDataSet(interval, dataList);
+    }
+
+    /*
+     * Populate the data set with data.
+     */
+    void initDataSet(Interval interval, List<StateMileage> records)
+    {   
+        List<MileageByVehicle> dataList = new ArrayList<MileageByVehicle>();
+        for (StateMileage item : records) {
+            MileageByVehicle rec = new MileageByVehicle();
+            rec.setVehicle(item.getVehicleName());
+            rec.setState(item.getStateName());
+            rec.setGroupName(item.getGroupName());
+            rec.setTotal(MeasurementConversionUtil.convertMilesToKilometers(
+                        item.getMiles(), getMeasurementType()).doubleValue());
+            dataList.add(rec);
+        }
+        Collections.sort(dataList, new StateMileageByVehicleComparator());        
+        setMainDataset(dataList);
+    }
+
+    /* Comparator for StateMileageByVehicle report */
+    class StateMileageByVehicleComparator implements Comparator<MileageByVehicle> {
+
+        @Override
+        public int compare(MileageByVehicle o1, MileageByVehicle o2) {
+            int equal = o1.getGroupName().compareTo(o2.getGroupName());
+            if (equal == 0) {
+                return o1.getVehicle().compareTo(o2.getVehicle());                
+            } else {
+                return equal;
+            }
+        }
+    }
+}
