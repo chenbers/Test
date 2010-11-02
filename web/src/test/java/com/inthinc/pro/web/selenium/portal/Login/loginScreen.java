ab57f660ca3dff8564a528d1c0ca5e56e5eeb4a8
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-11-02 21:32:03 +0000

    massive changes

--- web/src/test/java/com/inthinc/pro/web/selenium/portal/Login/loginScreen.java
+++ web/src/test/java/com/inthinc/pro/web/selenium/portal/Login/loginScreen.java
@@ -2,96 +2,303 @@ package com.inthinc.pro.web.selenium.portal.Login;
 
 import static org.junit.Assert.*;
 
-import org.openqa.selenium.server.SeleniumServer;
-import org.junit.Test;
+import java.util.Calendar;
 
+import com.inthinc.pro.web.selenium.Debug.Error_Catcher;
 import com.inthinc.pro.web.selenium.portal.Singleton;
 import com.thoughtworks.selenium.*;
 
 
-@SuppressWarnings("unused")
 public class loginScreen {
+	
+	private final String forgot_text = "Forgot your user name or password?";
+	private final String forgot_link = "//a[@title='" + forgot_text + "']";
+	
+	private final String copyright_loc = "//li[@class='first']";
+	private final String version_loc = "//li[@class='last']";
+	
+	
+	private final Calendar cal = Calendar.getInstance();
+	private final String copyright = "\u00a9" + String.valueOf(cal.get(Calendar.YEAR)) + " inthinc";
+	
+	private Error_Catcher errors;
+	public static Selenium selenium;
 
-	//define local vars
-	Singleton tvar = Singleton.getSingleton() ; 
-	Selenium selenium = tvar.getSelenium();
 	
-	
-	//Maintain the keyword Count
-	private int KeywordCount;
-	
-	
-	public int getKeywordCount(){
-		return KeywordCount;
+	public loginScreen(){
+		this(Singleton.getSingleton());
 	}
 	
+	public loginScreen( Singleton tvar ){
+		
+		selenium = tvar.getSelenium();
+		errors = new Error_Catcher();
+	}
+	
+	public loginScreen( Selenium sel ){
+		selenium = sel;
+		errors = new Error_Catcher();
+	}
+	
+	public loginScreen(Singleton tvar, Error_Catcher errors ){
+		
+		selenium = tvar.getSelenium();
+		this.errors = errors;
+	}
+	
+	public loginScreen( Error_Catcher errors, Selenium sel){
+		this( sel, errors );
+	}
+	
+	public loginScreen( Selenium sel, Error_Catcher errors){
+		selenium = sel;
+		this.errors = errors;
+	}
+	
+
 	public void login_screen(){
 		
 		//go to login screen
 		selenium.open("login");  
-		selenium.waitForPageToLoad("10000");	
+		//Verify Home Screen is displayed
+		ck_login_page();
 	}
 	
-	@Test
+
 	public void ForgotPassword(){
+		ForgotPassword( false, "" ); //Set up a default value, so we don't always have to specify
+	}
+	
+	public void ForgotPassword( String email ){
+		ForgotPassword( false, email );
+	}
+	
+	public void ForgotPassword( Boolean cancel ){
+		ForgotPassword( cancel, "" );
+	}
+	
+	public void ForgotPassword( String email, Boolean cancel){
+		ForgotPassword( cancel, email );
+	}
+	
+	
+	public void ForgotPassword(Boolean cancel, String email){
 		
 		//verify all screen elements and text
 		selenium.open("/tiwipro/login");
-		selenium.click("j_id28:loginForgotPassword");
-		assertTrue(selenium.isTextPresent("exact:Forgot User Name or Password?"));
-		assertTrue(selenium.isTextPresent("E-mail Address"));
-		assertTrue(selenium.isElementPresent("changePasswordForm:email"));
-		assertTrue(selenium.isTextPresent("Send"));
-		assertTrue(selenium.isTextPresent("Cancel"));
-		assertTrue(selenium.isTextPresent("exact:Forgot User Name or Password?"));
+		
+		try{
+			selenium.click("j_id28:loginForgotPassword");
+		}catch( SeleniumException e){ errors.Error("Forgot Password Link", e);
+		}
+		
+		try{
+			assertTrue(selenium.isTextPresent("Forgot User Name or Password?"));
+		}catch(AssertionError e){
+			errors.Error("Forgot User Name or Password Text", e);
+			}
+		
+		try{
+			assertTrue(selenium.isTextPresent("E-mail Address"));
+		}catch(AssertionError e){
+			errors.Error("E-mail Address", e);
+		}
+		
+		try{
+			assertTrue(selenium.isElementPresent("changePasswordForm:email"));
+		}catch(AssertionError e){
+			errors.Error("email element", e);
+		}
+		
+		try{
+			assertTrue(selenium.isTextPresent("Send"));
+		}catch(AssertionError e){ 
+			errors.Error("Send", e);
+		}
+		
+		try{
+			assertTrue(selenium.isTextPresent("Cancel"));
+		}catch(AssertionError e){ 
+			errors.Error("Cancel", e);
+		}
+		
+		try{
+			assertTrue(selenium.isTextPresent("Forgot User Name or Password?"));
+		}catch(AssertionError e){ 
+			errors.Error("Forgot User Name Password", e);
+		}
 		
 		
 		//select Cancel Button
-		selenium.click("changePasswordForm:PasswordCancelButton");
-		//Verify Home Screen is displayed again
-		ck_Homepage();
+		if (cancel){
+			try{
+				selenium.click("changePasswordForm:PasswordCancelButton");
+			}catch(SeleniumException	e){
+				errors.Error("Change Password Cancel Button",e);
+			}
+		}
 		
 		//select close window
-		selenium.click("//img[@onclick=\"Richfaces.hideModalPanel('forgotPasswordPanel')\"]");	
+		try{
+			selenium.click("//img[@onclick=\"Richfaces.hideModalPanel('forgotPasswordPanel')\"]");
+		}catch(SeleniumException e){
+			errors.Error("Click X", e);
+		}
+		
 		//Verify Home Screen is displayed again
-		ck_Homepage();
+		ck_login_page();
 		
 		//request password email
-		selenium.click("j_id28:loginForgotPassword");
+		try{
+			selenium.click("j_id28:loginForgotPassword");
+		}catch(SeleniumException e){
+			errors.Error("",e);
+		}
+		
 		// email needs to be verbalized
-		selenium.type("changePasswordForm:email", "larringt@teksystems.com");
-		selenium.click("changePasswordForm:PasswordSubmitButton");
+		try{
+			selenium.type("changePasswordForm:email", email);
+		}catch(SeleniumException e){
+			errors.Error("email field", e);
+				}
 		
-		//attach to new tab
-		String feedWinId = selenium.getEval("{var windowId; for(var x in selenium.browserbot.openedWindows ) {windowId=x;} }");
-		selenium.selectWindow(feedWinId);
-		selenium.windowFocus();
-		//verify message
-		selenium.open("/tiwipro/sent");
-		assertTrue(selenium.isTextPresent("Message Sent Successfully"));
-		assertEquals(selenium.getText("//ul[@id='grid_nav']/li[2]"), "Message Sent Successfully");
+		try{
+			selenium.click("changePasswordForm:PasswordSubmitButton");
+		}catch(SeleniumException e){
+			errors.Error("Click Submit",e);
+		}
+		
+		if ( email=="" ){
+			try{
+				assertTrue(selenium.isElementPresent("changePasswordForm:j_id37"));
+			}catch(AssertionError e){
+				errors.Error("Required message", e);
+			}
+		}
+		
+		
+		
+//		What does this section do???		
+//		//attach to new tab
+//		String feedWinId = selenium.getEval("{var windowId; for(var x in selenium.browserbot.openedWindows ) {windowId=x;} }");
+//		selenium.selectWindow(feedWinId);
+//		selenium.windowFocus();
+//		//verify message
+//		selenium.open("/tiwipro/sent");
+//
+//		assertTrue(selenium.isTextPresent("Message Sent Successfully"));
+//		assertEquals(selenium.getText("//ul[@id='grid_nav']/li[2]"), "Message Sent Successfully");
 		
 	}
-	
-		
-	
-	public void ck_Homepage(){
-		selenium.open("/tiwipro/login");
-		assertTrue(selenium.isTextPresent("exact:Forgot your user name or password?"));
-		assertEquals(selenium.getTitle(), "tiwiPRO");
-		assertTrue(selenium.isTextPresent("Log In"));
-		assertTrue(selenium.isTextPresent("User Name:"));
-		assertTrue(selenium.isTextPresent("exact:Password:"));
-		assertTrue(selenium.isElementPresent("j_username"));
-		assertTrue(selenium.isElementPresent("j_password"));
-		assertTrue(selenium.isTextPresent("2009 inthinc"));
-	}
-	
-	
 	
 
 	
+		
+	
+	public void ck_login_page(){
+		
+		try{
+			assertTrue(selenium.getText(this.forgot_link)==this.forgot_text);
+		}catch(AssertionError e){
+			errors.Error(forgot_text + " text",selenium.getText(this.forgot_link));
+		}
+		
+		try{
+			assertTrue(selenium.isElementPresent(forgot_link));
+		}
+		catch(AssertionError e){
+			errors.Error( forgot_text + " element", e);
+		}
+		
+		
+		try{
+			assertEquals(selenium.getTitle(), "tiwiPRO");
+		}catch(AssertionError e){
+			errors.Error("Title",e);
+		}
+		
+		try{
+			assertTrue(selenium.isTextPresent("Log In"));
+		}catch(AssertionError e){
+			errors.Error("Log In text",e);
+		}
+		
+		try{
+			assertTrue(selenium.isTextPresent("User Name:"));
+		}catch(AssertionError e){
+			errors.Error("User Name: text",e);
+		}
+		
+		try{
+			assertTrue(selenium.isTextPresent("Password:"));
+		}catch(AssertionError e){
+			errors.Error("Password: text",e);
+		}
+		
+		try{
+			assertTrue(selenium.isElementPresent("j_username"));
+		}catch(AssertionError e){
+			errors.Error("User Name element",e);
+		}
+		
+		try{
+			assertTrue(selenium.isElementPresent("j_password"));
+		}catch(AssertionError e){
+			errors.Error("Password element",e);
+		}
+		
+		try{
+			
+			assertTrue(selenium.getText(this.copyright_loc)==this.copyright);
+		}catch(AssertionError e){
+			errors.Error("Copyright text", selenium.getText(this.copyright_loc));
+			errors.Expected("Copyright text", this.copyright);
+		}catch(SeleniumException e){
+			errors.Error("Copyright element", e);
+		}
+	}
 	
 	
+	public void ck_sent_page(){
+		try{
+			assertTrue(selenium.isTextPresent("Message Sent Successfully"));
+		}catch(AssertionError e){
+			errors.Error("Message Sent Successfully text", e);
+		}
+		
+		try{
+			assertEquals(selenium.getText("//ul[@id='grid_nav']/li[2]"), "Message Sent Successfully");
+		}catch(AssertionError e){
+			errors.Error("Message Sent Successfully text", e);
+		}
+
+	}
+	
+	public Error_Catcher get_errors(){
+		return this.errors;
+	}
+	
+	
+	
+	public static void main( String[] args){
+//		NAVIGATE nav = new NAVIGATE();
+//		try {
+//			nav.setUp();
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		try{
+			loginScreen login = new loginScreen();
+//			nav.set_selenium(loginScreen.selenium);
+			login.ForgotPassword("failure@fail.com");
+			loginScreen.selenium.close();
+			loginScreen.selenium.stop();
+			System.out.print(login.get_errors().get_errors().toString());
+//			nav.tearDown();
+//		}catch(Exception e){
+//			nav.tearDown();
+//		}
+	}
 }
 
