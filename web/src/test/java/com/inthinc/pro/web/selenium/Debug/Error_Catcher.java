b4b15b7c35f3eaac02f146db1f084109e93ecd69
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-11-02 20:29:41 +0000

    error keeper for selenium

---
+++ web/src/test/java/com/inthinc/pro/web/selenium/Debug/Error_Catcher.java
@@ -0,0 +1,71 @@
+package com.inthinc.pro.web.selenium.Debug;
+
+import java.util.HashMap;
+
+import com.thoughtworks.selenium.SeleniumException;
+
+
+
+public class Error_Catcher {
+	
+	
+	private static HashMap<String, HashMap<String, Object>> errors = new HashMap<String, HashMap<String, Object>>();
+	private HashMap<String, Object> errorList = new HashMap<String, Object>();
+	
+	public void Error(String name, AssertionError error){
+		
+		if (!errors.containsKey(name)){
+			add_error(name);
+		}
+		
+		errors.get(name).put("AssertionError", error);
+	}
+	
+	public void Error(String name, SeleniumException error){
+		
+		if (!errors.containsKey(name)){
+			add_error(name);
+		}
+		
+		errors.get(name).put("SeleniumException", error);
+	}
+	
+	
+	public void Error(String name, Exception error){
+		
+		if (!errors.containsKey(name)){
+			add_error(name);
+		}
+		
+		errors.get(name).put("Exception", error);
+	}
+	
+	
+	public void Error(String name, String error){
+		
+		if (!errors.containsKey(name)){
+			add_error(name);
+		}
+		
+		errors.get(name).put("Actual", error);
+	}
+	
+	public void Expected( String name, String expected){
+
+		if (!errors.containsKey(name)){
+			add_error(name);
+		}
+		
+		errors.get(name).put("Expected", expected);
+	}
+	
+	public void add_error(String name){
+		errors.put(name, errorList);
+		
+	}
+	
+	public HashMap<String, HashMap<String, Object>> get_errors(){
+		
+		return Error_Catcher.errors;
+	}
+}
