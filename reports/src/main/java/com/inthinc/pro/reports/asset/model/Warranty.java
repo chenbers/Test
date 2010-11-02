079a568d2cef549241edd7e292983d8e9621c511
dfreitas <dfreitas@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-11-02 14:24:45 +0000

    US2151 - Created Jasper report and Warranty bean.

---
+++ reports/src/main/java/com/inthinc/pro/reports/asset/model/Warranty.java
@@ -0,0 +1,143 @@
+package com.inthinc.pro.reports.asset.model;
+
+import java.util.Date;
+
+/**
+ * Bean for warranty report.
+ */
+public class Warranty {
+
+    private String vehicleName;
+    private String imei;
+    private Date warrantyStartDate;
+    private Date warrantyEndDate;
+    private Boolean expired;
+    private String groupName;
+    private String customerName;
+    private String customerId;
+
+    /**
+     * Default constructor.
+     */
+    public Warranty() {}
+
+    /**
+     * @return the vehicleName
+     */
+    public String getVehicleName() {
+        return vehicleName;
+    }
+
+    /**
+     * @param vehicleName
+     *            the vehicleName to set
+     */
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
+
+    /**
+     * @return the imei
+     */
+    public String getImei() {
+        return imei;
+    }
+
+    /**
+     * @param imei
+     *            the imei to set
+     */
+    public void setImei(String imei) {
+        this.imei = imei;
+    }
+
+    /**
+     * @return the warrantyStartDate
+     */
+    public Date getWarrantyStartDate() {
+        return warrantyStartDate;
+    }
+
+    /**
+     * @param warrantyStartDate
+     *            the warrantyStartDate to set
+     */
+    public void setWarrantyStartDate(Date warrantyStartDate) {
+        this.warrantyStartDate = warrantyStartDate;
+    }
+
+    /**
+     * @return the warrantyEndDate
+     */
+    public Date getWarrantyEndDate() {
+        return warrantyEndDate;
+    }
+
+    /**
+     * @param warrantyEndDate
+     *            the warrantyEndDate to set
+     */
+    public void setWarrantyEndDate(Date warrantyEndDate) {
+        this.warrantyEndDate = warrantyEndDate;
+    }
+
+    /**
+     * @return the expired
+     */
+    public Boolean getExpired() {
+        return expired;
+    }
+
+    /**
+     * @param expired
+     *            the expired to set
+     */
+    public void setExpired(Boolean expired) {
+        this.expired = expired;
+    }
+
+    /**
+     * @return the groupName
+     */
+    public String getGroupName() {
+        return groupName;
+    }
+
+    /**
+     * @param groupName
+     *            the groupName to set
+     */
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    /**
+     * @return the customerName
+     */
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    /**
+     * @param customerName
+     *            the customerName to set
+     */
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    /**
+     * @return the customerId
+     */
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    /**
+     * @param customerId
+     *            the customerId to set
+     */
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+}
