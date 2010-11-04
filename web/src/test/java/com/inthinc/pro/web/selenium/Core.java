844d0063aa9403d6658fcb703c74a5f188d2b510
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-11-04 21:01:55 +0000

    Merged changes, and updated a couple of things.

--- web/src/test/java/com/inthinc/pro/web/selenium/Core.java
+++ web/src/test/java/com/inthinc/pro/web/selenium/Core.java
@@ -1,42 +1,152 @@
 package com.inthinc.pro.web.selenium;
 
-import com.thoughtworks.selenium.DefaultSelenium;
-import static org.testng.AssertJUnit.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-public class Core extends DefaultSelenium {
+import com.inthinc.pro.web.selenium.Debug.Error_Catcher;
+import com.thoughtworks.selenium.CommandProcessor;
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.SeleniumException;
+
+public class Core extends DefaultSelenium{
+
+	private Error_Catcher errors = new Error_Catcher();
+	
+	public Core(CommandProcessor processor) {
+		super(processor);
+	}
 
 	public Core(String serverHost, int serverPort, String browserStartCommand,
 			String browserURL) {
-		  super(serverHost, serverPort, browserStartCommand, browserURL);
-	//  // TODO Auto-generated constructor stub
+		super(serverHost, serverPort, browserStartCommand, browserURL);
 	}
 	
-	public void TextPresent( String Text){
+	
+	public void click(String locator, String error_name) {
+		try{
+			click(locator);
+		}catch(SeleniumException e){
+			errors.Error(error_name, e);
+		}catch(Exception e){
+			errors.Error(error_name, e);
+		}
+	}
+	
+	public void isElementPresent(String element, String error_name){
+		try{
+			assertTrue( isElementPresent(element));
+		}catch(AssertionError e){
+			errors.Error(error_name, e);
+		}catch(SeleniumException e){
+			errors.Error(error_name, e);
+		}catch(Exception e){
+			errors.Error(error_name, e);
+		}
+	}
+	
+	public void isElementNotPresent(String element, String error_name){
+		try{
+			assertFalse( isElementPresent(element));
+		}catch(AssertionError e){
+			errors.Error(error_name, e);
+		}catch(SeleniumException e){
+			errors.Error(error_name, e);
+		}catch(Exception e){
+			errors.Error(error_name, e);
+		}
+	}
+	
+	public void getText(String locator, String expected, String error_name){
+		try{
+			assertFalse( getText(locator)==expected);
+		}catch(AssertionError e){
+			errors.Error(error_name, getText(locator));
+			errors.Expected(error_name, expected);
+		}catch(SeleniumException e){
+			errors.Error(error_name, e);
+		}catch(Exception e){
+			errors.Error(error_name, e);
+		}
+	}
+	
+	public void getTable(String locator, String expected, String error_name){
+		try{
+			assertFalse( getTable(locator)==expected);
+		}catch(AssertionError e){
+			errors.Error(error_name, getText(locator));
+			errors.Expected(error_name, expected);
+		}catch(SeleniumException e){
+			errors.Error(error_name, e);
+		}catch(Exception e){
+			errors.Error(error_name, e);
+		}
+	}
+	
+	public void type(String locator, String text, String error_name){
+		try{
+			type(locator, text);
+		}catch(SeleniumException e){
+			errors.Error(error_name, e);
+		}catch(Exception e){
+			errors.Error(error_name, e);
+		}
+	}
+	
+	
+	public void isTextPresent(String text, String error_name){
+		try{
+			assertTrue( isTextPresent(text));
+		}catch(AssertionError e){
+			errors.Error(error_name, e);
+		}catch(SeleniumException e){
+			errors.Error(error_name, e);
+		}catch(Exception e){
+			errors.Error(error_name, e);
+		}
+	}
+	
+	public void isTextNotPresent(String text, String error_name){
+		try{
+			assertFalse( isTextPresent(text));
+		}catch(AssertionError e){
+			errors.Error(error_name, e);
+		}catch(SeleniumException e){
+			errors.Error(error_name, e);
+		}catch(Exception e){
+			errors.Error(error_name, e);
+		}
+	}
+	
+	public Error_Catcher getErrors(){
+		return errors;
+	}
+
+	public void TextPresent( String text){
 		try {
-			assertTrue(isTextPresent(Text));
+			assertTrue(isTextPresent(text));
 			}catch (AssertionError e){
 				
 			}
 	}
 	
-	public void ElementPresent( String Text){
+	public void ElementPresent( String element){
 		try {
-			assertTrue(isElementPresent(Text));
+			assertTrue(isElementPresent(element));
 			}catch (AssertionError e){
 				
 			}
 	}
 	
-	public void GetText( String Text){
+	public void GetText( String location, String text){
 	try {
-		assertEquals(getText("//b"), Text);
+		AssertEquals(getText(location), text);
 		}catch (AssertionError e){
 		}
 	}
 	
-	public void AssertEquals( String local, String Text){
+	public void AssertEquals( String local, String text){
 		try {
-			assertEquals(local, Text);
+			assertTrue(local==Text);
 			}catch (AssertionError e){
 			}
 		}
