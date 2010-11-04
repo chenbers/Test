f91138308bc128f69018557d0b30ab3d83b303c2
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-11-04 22:36:09 +0000

    US2311: TA3998: added method to get SENT text messages;

--- dao/src/main/java/com/inthinc/pro/dao/mock/proserver/SiloServiceMockImpl.java
+++ dao/src/main/java/com/inthinc/pro/dao/mock/proserver/SiloServiceMockImpl.java
@@ -1425,4 +1425,28 @@ public class SiloServiceMockImpl extends AbstractServiceMockImpl implements Silo
         return null;
     }
 
+//    @Override
+//    public List<Map<String, Object>> getCrashTraces(String eventID) throws ProDAOException {
+//        // TODO Auto-generated method stub
+//        return null;
+//    }
+//
+//    @Override
+//    public Map<String, Object> getCrashTraceCount(String eventID, Long startDate, Long stopDate, List<Map<String, Object>> filterList) {
+//        // TODO Auto-generated method stub
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Map<String, Object>> getCrashTracePage(String eventID, Long startDate, Long stopDate, List<Map<String, Object>> filterList, Map<String, Object> pageParams) {
+//        // TODO Auto-generated method stub
+//        return null;
+//    }
+
+    @Override
+    public List<Map<String, Object>> getSentTextMsgsByGroupID(Integer groupID, Long startDate, Long endDate) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
