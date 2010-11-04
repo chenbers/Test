164a840db222b657d5b4fd5230b86f807ad22409
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-11-04 22:37:46 +0000

    US2311: TA3998: added fields to facilitate sending person/driver/vehicle ID's with forward commands

--- dao/src/main/java/com/inthinc/pro/model/ForwardCommand.java
+++ dao/src/main/java/com/inthinc/pro/model/ForwardCommand.java
@@ -12,19 +12,31 @@ public class ForwardCommand extends BaseEntity implements Comparable<ForwardComm
     private Integer cmd;
     private Object data;        // can be integer or string
     private ForwardCommandStatus status; 
-
+    private Integer personID;
+    private Integer driverID;
+    private Integer vehicleID;
     
     public ForwardCommand()
     {
         super();
     }
-    public ForwardCommand(Integer fwdID, Integer cmd, Object data, ForwardCommandStatus status)
+    public ForwardCommand(Integer fwdId, Integer cmd, Object data, ForwardCommandStatus status)
     {
         super();
-        this.fwdID = fwdID;
+        this.fwdID = fwdId;
         this.cmd = cmd;
         this.data = data;
         this.status = status;
+    }
+    public ForwardCommand(Integer fwdId, Integer cmd, Object data, ForwardCommandStatus status, Integer sendingPersonId, Integer receivingDriverId, Integer receivingVehicleId){
+        super();
+        this.fwdID = fwdId;
+        this.cmd = cmd;
+        this.data = data;
+        this.status = status;
+        this.personID = sendingPersonId;
+        this.driverID = receivingDriverId;
+        this.vehicleID = receivingVehicleId;
     }
     public Integer getCmd()
     {
@@ -61,6 +73,24 @@ public class ForwardCommand extends BaseEntity implements Comparable<ForwardComm
     public void setFwdID(Integer fwdID)
     {
         this.fwdID = fwdID;
+    }
+    public Integer getPersonID() {
+        return personID;
+    }
+    public void setPersonID(Integer personID) {
+        this.personID = personID;
+    }
+    public Integer getDriverID() {
+        return driverID;
+    }
+    public void setDriverID(Integer driverID) {
+        this.driverID = driverID;
+    }
+    public Integer getVehicleID() {
+        return vehicleID;
+    }
+    public void setVehicleID(Integer vehicleID) {
+        this.vehicleID = vehicleID;
     }
 
 }
