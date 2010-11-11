08b7fa3e357f0181103ab7ddaff26a16606b665a
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-11-11 01:32:45 +0000

    Commenting Code

--- web/src/test/java/com/inthinc/pro/web/selenium/Data_Reader.java
+++ web/src/test/java/com/inthinc/pro/web/selenium/Data_Reader.java
@@ -17,10 +17,8 @@ public class Data_Reader{
 	private HashMap<String, HashMap<String, String>> ID;
 	private HashMap<String, String> keys;
 	
-	private final String testcase = "TestCase";
-
 	private void setInputFile(String inputFile) {
-		this.inputFile = inputFile;
+		this.inputFile = inputFile; //Set the Input file, makes for easy reuse
 	}
 
 	private void read() throws IOException  {
@@ -28,79 +26,81 @@ public class Data_Reader{
 		Workbook w;
 		try {
 			w = Workbook.getWorkbook(inputWorkbook);
-			Sheet[] sheet = w.getSheets();
-			// Loop over first 10 column and lines
-			
-			for (int s = 0; s < sheet.length; s++){
-				if (sheet[s].getRows() != 0){
-					Cell master = sheet[s].getCell(0, 0);
+			Sheet[] sheet = w.getSheets(); // Get the sheets as an array
+			Cell master = null; // Establish the first master
+			for (int s = 0; s < sheet.length; s++){ // Loop through the sheets
+				if (sheet[s].getRows() != 0){ // If there are no rows, it is an empty sheet
+					master = sheet[s].getCell(0, 0); // Return the heading in the first column as our master key to that sheet
 				}
-				for (int r=1; r < sheet[s].getRows(); r++){
-					Cell master = sheet[s].getCell(0, 0);
-					for (int c=0; c < sheet[s].getColumns(); c++){
-						add_data(sheet[s], master, sheet[s].getCell(0,r),
-								sheet[s].getCell(c,0), sheet[s].getCell(c,r));
+				for (int r=1; r < sheet[s].getRows(); r++){// Loop through each row
+					for (int c=0; c < sheet[s].getColumns(); c++){ // Now loop through every column for this row
+						add_data(sheet[s], master, sheet[s].getCell(0,r),sheet[s].getCell(c,0), sheet[s].getCell(c,r));
+						// Send the data for what Sheet, master, first column, and then the first row for that column
+						// as the header, and finally the cell its self as the value
 					}
 				}
 			}
 	
 		}catch (BiffException e){
-			e.printStackTrace();
+			e.printStackTrace(); // If we crash, print a stack trace
 		}
 	}
 	
-	private void add_sheet(String sheet){
-		testCase = new HashMap<String,HashMap<String, HashMap<String, String>>>();
-		data_file.put(sheet, testCase);
+	private void add_sheet(String sheet){ // Add the Sheet to our HashMap
+		testCase = new HashMap<String,HashMap<String, HashMap<String, String>>>();// necessary to instantiate a value for the key
+		data_file.put(sheet, testCase); // add the sheet and new value to our HashMap
 	}
 		
 	private void add_master(String sheet, String master){
-		ID = new HashMap<String, HashMap<String, String>>();
-		data_file.get(sheet).put(master, ID);
+		ID = new HashMap<String, HashMap<String, String>>(); // necessary to instantiate a value for the key
+		data_file.get(sheet).put(master, ID); // add the master label and then the new ID value to our HashMap
 	}
 	
 	private void add_ID(String sheet, String master, String ID){
-		keys = new HashMap<String, String>();
-		data_file.get(sheet).get(master).put(ID, keys);
+		keys = new HashMap<String, String>(); // necessary to instantiate a value for the key
+		data_file.get(sheet).get(master).put(ID, keys); // add the ID from the first column x row and new value to our HashMap
 	}
 	
 	
-	private void add_data( Sheet sheetSheet, Cell masterCell, Cell IDCell, Cell keyCell, Cell valueCell){
+	private void add_data( Sheet sheetSheet, Cell masterCell, Cell IDCell, Cell keyCell, Cell valueCell){ // Add values to the HashMap
+		//Get the name for ease of refence for each entry
 		String sheet = sheetSheet.getName();
 		String master = masterCell.getContents();
 		String ID = IDCell.getContents();
 		String key = keyCell.getContents();
 		String value = valueCell.getContents();
 		
-
+		//Make sure our sheet is added first
 		if (!data_file.containsKey(sheet)){
 			add_sheet(sheet);
 		}
+		//Then make sure our Master is a value for the sheet
 		if (!data_file.get(sheet).containsKey(master)){
 			add_master(sheet, master);
 		}
+		//Finally add our individual ID to the master key
 		if (!data_file.get(sheet).get(master).containsKey(ID)){
 			add_ID(sheet, master, ID);
 		}
-		data_file.get(sheet).get(master).get(ID).put(key, value);
+		data_file.get(sheet).get(master).get(ID).put(key, value); // Add the row data as values, with the column headers as keys
 	}
 	
 	public HashMap<String, HashMap<String,HashMap<String, HashMap<String, String>>>> get_data(String input_file){
-		setInputFile(input_file);
-		data_file = new HashMap<String, HashMap<String,HashMap<String, HashMap<String, String>>>>();
+		setInputFile(input_file); //set the input file
+		data_file = new HashMap<String, HashMap<String,HashMap<String, HashMap<String, String>>>>(); //Instantiate our overall Map
 		try{
-			read();
+			read();//Try to read the data and create the HashMaps
 		}catch( IOException e){
-			e.printStackTrace();
+			e.printStackTrace(); //Oops we broke something
 		}
-		return data_file;		
+		return data_file; // Return the newly created HashMap		
 	}
 	
 
 	public static void main(String[] args) throws IOException {
-		Data_Reader test = new Data_Reader();
-		HashMap<String, HashMap<String,HashMap<String, HashMap<String, String>>>> data = test.get_data("c:/zohoData.xls");
-		System.out.println(data.get("login").get("Username").get("larringt").get("Password"));
+		Data_Reader test = new Data_Reader(); // Instantiate our class
+		HashMap<String, HashMap<String,HashMap<String, HashMap<String, String>>>> data = test.get_data("c:/zohoData.xls"); //Get the data
+		System.out.println(data.get("login").get("Username").get("larringt").get("Password")); // Test that it works
 	}
 
 }
