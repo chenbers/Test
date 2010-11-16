eaf35bc3042fbf6563d10a6356c40c7e36ba24ca
bmiller <bmiller@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-11-16 05:23:49 +0000

    IFTA JDBC class implemented

--- dao/src/main/java/com/inthinc/pro/dao/jdbc/StateMileageJDBCDAO.java
+++ dao/src/main/java/com/inthinc/pro/dao/jdbc/StateMileageJDBCDAO.java
@@ -11,9 +11,11 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.joda.time.Interval;
 
+import com.inthinc.pro.ProDAOException;
 import com.inthinc.pro.dao.StateMileageDAO;
 
 import com.inthinc.pro.model.StateMileage;
+import com.inthinc.pro.model.hos.HOSGroupMileage;
 
 public class StateMileageJDBCDAO  extends GenericJDBCDAO  implements StateMileageDAO{
 
@@ -22,32 +24,284 @@ public class StateMileageJDBCDAO  extends GenericJDBCDAO  implements StateMileag
 
     public List<StateMileage> getStateMileageByGroupAndMonth(Integer groupID, Interval interval, Boolean dotOnly)
     {
-        return null;
+        Connection conn = null;
+        CallableStatement statement = null;
+        ResultSet resultSet = null;
+
+        ArrayList<StateMileage> recordList = new ArrayList<StateMileage>();
+        
+        try
+        {
+            conn = getConnection();
+            statement = conn.prepareCall("{call ifta_getStateMileageByGroupAndMonth(?, ?, ?, ?)}");
+            statement.setInt(1, groupID);
+            statement.setLong(2, interval.getStartMillis());
+            statement.setLong(3, interval.getEndMillis());
+            statement.setBoolean(4, dotOnly);
+            
+            resultSet = statement.executeQuery();
+
+            StateMileage record = null;
+            while (resultSet.next())
+            {
+                record = new StateMileage();
+
+                record.setGroupID(resultSet.getInt(1));
+                record.setStateName(resultSet.getString(2));
+                record.setMonth(resultSet.getString(3));
+                record.setMiles(resultSet.getLong(4));
+                record.setTruckGallons(resultSet.getFloat(5));
+                record.setTrailerGallons(resultSet.getFloat(6));
+                
+                recordList.add(record);
+            }
+        }   // end try
+        catch (SQLException e)
+        { // handle database hosLogs in the usual manner
+            throw new ProDAOException(statement.toString(), e);
+        }   // end catch
+        finally
+        { // clean up and release the connection
+            close(resultSet);
+            close(statement);
+            close(conn);
+        } // end finally
+
+        return recordList;
     }
     
     public List<StateMileage> getStateMileageByGroup(Integer groupID, Interval interval, Boolean dotOnly)
     {
-        return null;
+        Connection conn = null;
+        CallableStatement statement = null;
+        ResultSet resultSet = null;
+
+        ArrayList<StateMileage> recordList = new ArrayList<StateMileage>();
+        
+        try
+        {
+            conn = getConnection();
+            statement = conn.prepareCall("{call ifta_getStateMileageByGroup(?, ?, ?, ?)}");
+            statement.setInt(1, groupID);
+            statement.setLong(2, interval.getStartMillis());
+            statement.setLong(3, interval.getEndMillis());
+            statement.setBoolean(4, dotOnly);
+            
+            resultSet = statement.executeQuery();
+
+            StateMileage record = null;
+            while (resultSet.next())
+            {
+                record = new StateMileage();
+
+                record.setGroupID(resultSet.getInt(1));
+                record.setStateName(resultSet.getString(2));
+                record.setMiles(resultSet.getLong(3));
+                record.setTruckGallons(resultSet.getFloat(4));
+                record.setTrailerGallons(resultSet.getFloat(5));
+                recordList.add(record);
+            }
+        }   // end try
+        catch (SQLException e)
+        { // handle database hosLogs in the usual manner
+            throw new ProDAOException(statement.toString(), e);
+        }   // end catch
+        finally
+        { // clean up and release the connection
+            close(resultSet);
+            close(statement);
+            close(conn);
+        } // end finally
+
+        return recordList;
     }
     
     public List<StateMileage> getStateMileageByVehicle(Integer groupID, Interval interval, Boolean dotOnly)
     {
-        return null;
+        Connection conn = null;
+        CallableStatement statement = null;
+        ResultSet resultSet = null;
+
+        ArrayList<StateMileage> recordList = new ArrayList<StateMileage>();
+        
+        try
+        {
+            conn = getConnection();
+            statement = conn.prepareCall("{call ifta_getStateMileageByVehicle(?, ?, ?, ?)}");
+            statement.setInt(1, groupID);
+            statement.setLong(2, interval.getStartMillis());
+            statement.setLong(3, interval.getEndMillis());
+            statement.setBoolean(4, dotOnly);
+            
+            resultSet = statement.executeQuery();
+
+            StateMileage record = null;
+            while (resultSet.next())
+            {
+                record = new StateMileage();
+
+                record.setGroupID(resultSet.getInt(1));
+                record.setVehicleName(resultSet.getString(2));
+                record.setStateName(resultSet.getString(3));
+                record.setMiles(resultSet.getLong(4));
+                record.setTruckGallons(resultSet.getFloat(5));
+                record.setTrailerGallons(resultSet.getFloat(6));
+                recordList.add(record);
+            }
+        }   // end try
+        catch (SQLException e)
+        { // handle database hosLogs in the usual manner
+            throw new ProDAOException(statement.toString(), e);
+        }   // end catch
+        finally
+        { // clean up and release the connection
+            close(resultSet);
+            close(statement);
+            close(conn);
+        } // end finally
+
+        return recordList;
     }
     
     public List<StateMileage> getStateMileageByVehicleRoad(Integer groupID, Interval interval, Boolean dotOnly)
     {
-        return null;
+        Connection conn = null;
+        CallableStatement statement = null;
+        ResultSet resultSet = null;
+
+        ArrayList<StateMileage> recordList = new ArrayList<StateMileage>();
+        
+        try
+        {
+            conn = getConnection();
+            statement = conn.prepareCall("{call ifta_getStateMileageByVehicleRoad(?, ?, ?, ?)}");
+            statement.setInt(1, groupID);
+            statement.setLong(2, interval.getStartMillis());
+            statement.setLong(3, interval.getEndMillis());
+            statement.setBoolean(4, dotOnly);
+            
+            resultSet = statement.executeQuery();
+
+            StateMileage record = null;
+            while (resultSet.next())
+            {
+                record = new StateMileage();
+
+                record.setGroupID(resultSet.getInt(1));
+                record.setVehicleName(resultSet.getString(2));
+                record.setStateName(resultSet.getString(3));
+                record.setOnRoadFlag(resultSet.getBoolean(4));
+                record.setMiles(resultSet.getLong(5));
+                record.setTruckGallons(resultSet.getFloat(6));
+                record.setTrailerGallons(resultSet.getFloat(7));
+                recordList.add(record);
+            }
+        }   // end try
+        catch (SQLException e)
+        { // handle database hosLogs in the usual manner
+            throw new ProDAOException(statement.toString(), e);
+        }   // end catch
+        finally
+        { // clean up and release the connection
+            close(resultSet);
+            close(statement);
+            close(conn);
+        } // end finally
+
+        return recordList;
     }
     
     public List<StateMileage> getFuelStateMileageByVehicle(Integer groupID, Interval interval, Boolean dotOnly)
     {
-        return null;
+        Connection conn = null;
+        CallableStatement statement = null;
+        ResultSet resultSet = null;
+
+        ArrayList<StateMileage> recordList = new ArrayList<StateMileage>();
+        
+        try
+        {
+            conn = getConnection();
+            statement = conn.prepareCall("{call ifta_getFuelStateMileageByVehicle(?, ?, ?, ?)}");
+            statement.setInt(1, groupID);
+            statement.setLong(2, interval.getStartMillis());
+            statement.setLong(3, interval.getEndMillis());
+            statement.setBoolean(4, dotOnly);
+            
+            resultSet = statement.executeQuery();
+
+            StateMileage record = null;
+            while (resultSet.next())
+            {
+                record = new StateMileage();
+
+                record.setGroupID(resultSet.getInt(1));
+                record.setVehicleName(resultSet.getString(2));
+                record.setStateName(resultSet.getString(3));
+                record.setMiles(resultSet.getLong(4));
+                record.setTruckGallons(resultSet.getFloat(5));
+                record.setTrailerGallons(resultSet.getFloat(6));
+                recordList.add(record);
+            }
+        }   // end try
+        catch (SQLException e)
+        { // handle database hosLogs in the usual manner
+            throw new ProDAOException(statement.toString(), e);
+        }   // end catch
+        finally
+        { // clean up and release the connection
+            close(resultSet);
+            close(statement);
+            close(conn);
+        } // end finally
+
+        return recordList;
     }
 
     public List<StateMileage> getMileageByVehicle(Integer groupID, Interval interval, Boolean dotOnly)
     {
-        return null;
+        Connection conn = null;
+        CallableStatement statement = null;
+        ResultSet resultSet = null;
+
+        ArrayList<StateMileage> recordList = new ArrayList<StateMileage>();
+        
+        try
+        {
+            conn = getConnection();
+            statement = conn.prepareCall("{call ifta_getMileageByVehicle(?, ?, ?, ?)}");
+            statement.setInt(1, groupID);
+            statement.setLong(2, interval.getStartMillis());
+            statement.setLong(3, interval.getEndMillis());
+            statement.setBoolean(4, dotOnly);
+            
+            resultSet = statement.executeQuery();
+
+            StateMileage record = null;
+            while (resultSet.next())
+            {
+                record = new StateMileage();
+
+                record.setGroupID(resultSet.getInt(1));
+                record.setVehicleName(resultSet.getString(2));
+                record.setMiles(resultSet.getLong(3));
+                record.setTruckGallons(resultSet.getFloat(4));
+                record.setTrailerGallons(resultSet.getFloat(5));
+                recordList.add(record);
+            }
+        }   // end try
+        catch (SQLException e)
+        { // handle database hosLogs in the usual manner
+            throw new ProDAOException(statement.toString(), e);
+        }   // end catch
+        finally
+        { // clean up and release the connection
+            close(resultSet);
+            close(statement);
+            close(conn);
+        } // end finally
+
+        return recordList;
     }
 
 }
