db1f1627127851828a0bd248d01acd44449d22c9
larrington <larrington@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-11-19 15:47:10 +0000

    Template Test Case

---
+++ web/src/test/java/com/inthinc/pro/web/selenium/Test_Cases/TemplateTest.java
@@ -0,0 +1,86 @@
+package com.inthinc.pro.web.selenium.Test_Cases;
+
+import org.junit.Test;
+import com.inthinc.pro.web.selenium.portal.NAVIGATE;
+import com.inthinc.pro.web.selenium.portal.Login.Login;
+import com.inthinc.pro.web.selenium.Data_Reader;
+import com.inthinc.pro.web.selenium.portal.Masthead.*;
+
+public class TemplateTest extends NAVIGATE {
+	//instantiate var for data reader
+	Data_Reader testdata = new Data_Reader();
+	
+		
+
+	@Test	//Each Test Case shall have a separate function
+	public void TestCaseName1() {
+		//create instance of library objects
+		//Login and Master Heading Screens are done as examples
+		Login l = new Login();
+		Masthead mh = new Masthead();
+		
+		//Set up test data
+		set_test_case("ExcelFileName", "TestCaseName");
+	
+		//login to portal
+		l.login_to_portal(get_data("WorksheetName","ColumnHeader"), get_data("WorksheetName","ColumnHeader"));
+			
+				
+		/*Call Test Methods using the syntax below
+		//ScreenClass.FunctionName
+		//See Examples Below
+		*
+		*
+		*
+		*
+		*
+		*
+		*/
+		mh.ck_header();		//Check Header on Master Heading Screen
+		mh.ck_footer();		//Check Footer on Master Heading Screen
+		
+		
+		//exit Portal
+		mh.click_logout();
+		
+	}
+	
+	@Test	//Each Test Case shall have a separate function
+	public void TestCaseName2() {
+		//create instance of library objects
+		//Login and Master Heading Screens are done as examples
+		Login l = new Login();
+		Masthead mh = new Masthead();
+		
+		//Set up test data
+		set_test_case("ExcelFileName", "TestCaseName");
+	
+		//login to portal
+		l.login_to_portal(get_data("WorksheetName","ColumnHeader"), get_data("WorksheetName","ColumnHeader"));
+			
+				
+		/*Call Test Methods using the syntax below
+		//ScreenClass.FunctionName
+		//See Examples Below
+		*
+		*
+		*
+		*
+		*
+		*
+		*/
+		mh.ck_header();		//Check Header on Master Heading Screen
+		mh.ck_footer();		//Check Footer on Master Heading Screen
+		
+		
+		//exit Portal
+		mh.click_logout();
+		
+	}
+	
+	
+}
+
+
+
+
