d59ad911cf2aaced64afd7733f131c82db69fb3a
dfreitas <dfreitas@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-11-26 16:10:46 +0000

    US2429 - Updated javadoc.

--- service/src/main/java/com/inthinc/pro/service/phonecontrol/Clock.java
+++ service/src/main/java/com/inthinc/pro/service/phonecontrol/Clock.java
@@ -4,6 +4,9 @@ import java.util.Date;
 
 /**
  * Date and time abstraction utility interface.
+ * <p/>
+ * Querying time through this interface allow for better testable code and avoid the use of calling <tt>new Date()</tt>, practically allowing the code to have time objects to be
+ * injected instead of creating new objects when the current time is required.
  */
 public interface Clock {
 
