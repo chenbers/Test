916374f9e5c555fd2f5e386a8f473e6f97b77975
icazacu <icazacu@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-12-02 22:20:19 +0000

    US2656: TA5034 - Added ReportsFacade for reports services

---
+++ service/src/test/java/com/inthinc/pro/service/reports/facade/ReportsFacade.java
@@ -0,0 +1,44 @@
+package com.inthinc.pro.service.reports.facade;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.joda.time.Interval;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.inthinc.pro.model.GroupHierarchy;
+import com.inthinc.pro.reports.ReportCriteria;
+import com.inthinc.pro.reports.performance.model.TenHoursViolation;
+import com.inthinc.pro.reports.service.ReportCriteriaService;
+
+/**
+ * Facade to ReportCriteriaService.
+ */
+public class ReportsFacade {
+
+    @Autowired
+    private ReportCriteriaService reportService;
+    
+    @SuppressWarnings("unchecked")
+    public List<TenHoursViolation> getTenHourViolations(Integer groupID, Interval interval) {
+        ReportCriteria criteria = reportService.getTenHoursDayViolationsCriteria(getGroupHierarchy(), groupID, interval, getLocale());
+        return criteria.getMainDataset();
+    }
+
+    /**
+     * Returns the user group hierarchy.
+     * @return
+     */
+    private GroupHierarchy getGroupHierarchy() {
+        // TODO Add method body
+        return null;
+    }
+
+    /**
+     * Returns the user Locale.
+     * @return Locale
+     */
+    private Locale getLocale() {
+        return Locale.US;
+    }
+}
