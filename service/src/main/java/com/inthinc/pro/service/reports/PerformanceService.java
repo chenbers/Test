03f0487d2a7d7fa4b75fae5c4eb3b20008041d7b
icazacu <icazacu@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-12-02 14:54:29 +0000

    US2665: TA4917 - Changed HOSService to PerformanceService

---
+++ service/src/main/java/com/inthinc/pro/service/reports/PerformanceService.java
@@ -0,0 +1,47 @@
+package com.inthinc.pro.service.reports;
+
+import java.util.Date;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import com.inthinc.pro.service.annotations.DateFormat;
+
+/**
+ * Interface for HOS Reports Services.
+ */
+@Path("/group/{groupID}/report/performance")
+public interface PerformanceService {
+    String DATE_FORMAT = "yyyyMMdd";
+
+    /**
+     * Service for 10 Hours Violations Report with default Interval.
+     * @param groupID the Group ID
+     * @returnWrapped List<TenHoursViolation> the list of violations
+     * @HTTP HTTP 200 - OK if any violations found
+     * @HTTP HTTP 404 - NOT FOUND if no violations found 
+     */
+    @GET
+    @Path("/TenHourViolations")
+    @Produces("{application/xml, application/json}")
+    Response getTenHourViolations(@PathParam("groupID") Integer groupID); 
+
+    /**
+     * Service for 10 Hours Violations Report with an explicite Interval.
+     * @param groupID the Group ID
+     * @param startDate the start date 
+     * @param endDate the end date
+     * @returnWrapped List<TenHoursViolation> the list of violations
+     * @HTTP HTTP 200 - OK if any violations found
+     * @HTTP HTTP 404 - NOT FOUND if no violations found 
+     */
+    @GET
+    @Path("/TenHourViolations/{startDate}/{endDate}")
+    @Produces("{application/xml, application/json}")
+    Response getTenHourViolations(@PathParam("groupID") Integer groupID, 
+            @PathParam("startDate") @DateFormat(DATE_FORMAT) Date startDate, 
+            @PathParam("endDate") @DateFormat(DATE_FORMAT) Date endDate);
+}
