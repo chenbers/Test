f1f312e04139c9cef5e8374362b0474771e60c7d
frushishikata <frushishikata@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-12-03 18:59:38 +0000

    US2663 - Implement REST Service method - unit testing - part 2

--- service/src/test/java/com/inthinc/pro/service/reports/impl/IFTAServiceImplTest.java
+++ service/src/test/java/com/inthinc/pro/service/reports/impl/IFTAServiceImplTest.java
@@ -7,6 +7,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -24,169 +25,197 @@ import com.inthinc.pro.service.impl.BaseUnitTest;
 import com.inthinc.pro.service.reports.facade.ReportsFacade;
 
 public class IFTAServiceImplTest extends BaseUnitTest {
-   
+
     @Autowired 
     @Mocked 
     private ReportsFacade reportsFacadeMock;
-    
+
     IFTAServiceImpl serviceSUT = new IFTAServiceImpl(reportsFacadeMock);
 
     private Integer expectedGroupID = 1504;
 
     @Test
     public void getStateMileageByVehicleRoadStatusTestWihInvalidInput1(){
-        
+
         Response response = serviceSUT.getStateMileageByVehicleRoadStatus(expectedGroupID, 
-                                                      "20110101",
-                                                      "20100202", 
-                                                      true);
-        
+                "20110101",
+                "20100202", 
+                true);
+
         assertNotNull(response);
         assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),response.getStatus() );
     }
-    
+
     @Test
     public void getStateMileageByVehicleRoadStatusTestWihInvalidInput2(){
-        
+
         Response response = serviceSUT.getStateMileageByVehicleRoadStatus(null, 
-                                                      "20100101",
-                                                      "20100202", 
-                                                      true);
-        
+                "20100101",
+                "20100202", 
+                true);
+
         assertNotNull(response);
         assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),response.getStatus() );
     }
-    
+
     @Test
     public void getStateMileageByVehicleRoadStatusTestWihInvalidInput3(){
-        
+
         Response response = serviceSUT.getStateMileageByVehicleRoadStatus(expectedGroupID, 
-                                                      null,
-                                                      "20100202", 
-                                                      true);
-        
+                null,
+                "20100202", 
+                true);
+
         assertNotNull(response);
         assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),response.getStatus() );
     }
-    
+
     @Test
     public void getStateMileageByVehicleRoadStatusTestWihInvalidInput4(){
-        
+
         Response response = serviceSUT.getStateMileageByVehicleRoadStatus(expectedGroupID, 
-                                                      "20100202",
-                                                      null, 
-                                                      true);
-        
+                "20100202",
+                null, 
+                true);
+
         assertNotNull(response);
         assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),response.getStatus() );
     }
-    
+
     @Test
     public void getStateMileageByVehicleRoadStatusTestWihInvalidInput5(){
-        
+
         Response response = serviceSUT.getStateMileageByVehicleRoadStatus(expectedGroupID, 
-                                                      "20aaa0202",
-                                                      "20100202", 
-                                                      true);
-        
+                "20aaa0202",
+                "20100202", 
+                true);
+
         assertNotNull(response);
         assertEquals(Response.Status.BAD_REQUEST.getStatusCode(),response.getStatus() );
     }
-    
+
     @Test
     public void getStateMileageByVehicleRoadStatusTest(@Mocked final ReportsFacade reportsFacadeMock){
-        
+
         final boolean expectedDot = true;
         final String expectedStrStartDate = "20100101";
         final String expectedStrEndDate = "20100202";
-        
+
         Date startDate = buildDateFromString(expectedStrStartDate);
         Date endDate = buildDateFromString(expectedStrEndDate);
-        
+
         final Interval interval = new Interval(startDate.getTime(), endDate.getTime());
         final List<StateMileageByVehicleRoadStatus> list = new ArrayList<StateMileageByVehicleRoadStatus>();
         list.add(new StateMileageByVehicleRoadStatus());
-        
+
         new Expectations() {
             {
                 reportsFacadeMock.getStateMileageByVehicleRoadStatus(expectedGroupID, interval, expectedDot);
                 result = list;
             }
         };
-        
+
         serviceSUT.setFacade(reportsFacadeMock);
-        
+
         Response response = serviceSUT.getStateMileageByVehicleRoadStatus(expectedGroupID, 
-                                                      expectedStrStartDate,
-                                                      expectedStrEndDate, 
-                                                      expectedDot);
-        
+                expectedStrStartDate,
+                expectedStrEndDate, 
+                expectedDot);
+
         assertNotNull(response);
         assertEquals(Response.Status.OK.getStatusCode(),response.getStatus() );
     }
-    
+
     @Test
     public void getStateMileageByVehicleRoadStatusWithEmptyResultTest1(@Mocked final ReportsFacade reportsFacadeMock){
-        
+
         final boolean expectedDot = true;
         final String expectedStrStartDate = "20100101";
         final String expectedStrEndDate = "20100202";
-        
+
         Date startDate = buildDateFromString(expectedStrStartDate);
         Date endDate = buildDateFromString(expectedStrEndDate);
-        
+
         final Interval interval = new Interval(startDate.getTime(), endDate.getTime());
         final List<StateMileageByVehicleRoadStatus> list = new ArrayList<StateMileageByVehicleRoadStatus>();
-         
+
         new Expectations() {
             {
                 reportsFacadeMock.getStateMileageByVehicleRoadStatus(expectedGroupID, interval, expectedDot);
                 result = list;
             }
         };
-        
+
         serviceSUT.setFacade(reportsFacadeMock);
-        
+
         Response response = serviceSUT.getStateMileageByVehicleRoadStatus(expectedGroupID, 
-                                                      expectedStrStartDate,
-                                                      expectedStrEndDate, 
-                                                      expectedDot);
-        
+                expectedStrStartDate,
+                expectedStrEndDate, 
+                expectedDot);
+
         assertNotNull(response);
         assertEquals(Response.Status.NOT_FOUND.getStatusCode(),response.getStatus() );
     }
-    
+
     @Test
     public void getStateMileageByVehicleRoadStatusWithEmptyResultTest2(@Mocked final ReportsFacade reportsFacadeMock){
-        
+
         final boolean expectedDot = true;
         final String expectedStrStartDate = "20100101";
         final String expectedStrEndDate = "20100202";
-        
+
         Date startDate = buildDateFromString(expectedStrStartDate);
         Date endDate = buildDateFromString(expectedStrEndDate);
-        
+
         final Interval interval = new Interval(startDate.getTime(), endDate.getTime());
         final List<StateMileageByVehicleRoadStatus> list = new ArrayList<StateMileageByVehicleRoadStatus>();
-         
+
         new Expectations() {
             {
                 reportsFacadeMock.getStateMileageByVehicleRoadStatus(expectedGroupID, interval, expectedDot);
                 result = null;
             }
         };
-        
+
         serviceSUT.setFacade(reportsFacadeMock);
-        
+
         Response response = serviceSUT.getStateMileageByVehicleRoadStatus(expectedGroupID, 
-                                                      expectedStrStartDate,
-                                                      expectedStrEndDate, 
-                                                      expectedDot);
-        
+                expectedStrStartDate,
+                expectedStrEndDate, 
+                expectedDot);
+
         assertNotNull(response);
         assertEquals(Response.Status.NOT_FOUND.getStatusCode(),response.getStatus() );
     }
-    
+
+    @Test
+    public void getStateMileageByVehicleRoadStatusDefaultRangeTest(@Mocked final ReportsFacade reportsFacadeMock){
+
+        final boolean expectedDot = true;
+
+        Date startDate = buildDateFromString(daysAgoAsString(7));
+        Date endDate = buildDateFromString(todayAsString());
+
+        final Interval interval = new Interval(startDate.getTime(), endDate.getTime());
+        final List<StateMileageByVehicleRoadStatus> list = new ArrayList<StateMileageByVehicleRoadStatus>();
+        list.add(new StateMileageByVehicleRoadStatus());
+
+        new Expectations() {
+            {
+                  reportsFacadeMock.getStateMileageByVehicleRoadStatus(expectedGroupID, interval, expectedDot);
+                  result = list;
+            }
+        };
+
+        serviceSUT.setFacade(reportsFacadeMock);
+
+        Response response = serviceSUT.getStateMileageByVehicleRoadStatusDefaultRange(expectedGroupID, 
+                                                                                      expectedDot);
+
+        assertNotNull(response);
+        assertEquals(Response.Status.OK.getStatusCode(),response.getStatus() );
+    }
+
     private Date buildDateFromString(String strDate) {
         DateFormat df = new SimpleDateFormat(IFTAServiceImpl.getSimpleDateFormat()); 
         try
@@ -199,5 +228,18 @@ public class IFTAServiceImplTest extends BaseUnitTest {
             return null;
         }
     }
-    
+
+    private String todayAsString() {
+        DateFormat df = new SimpleDateFormat(IFTAServiceImpl.getSimpleDateFormat()); 
+        Calendar c = Calendar.getInstance();     
+        return df.format(c.getTime());
+    }
+
+    private String daysAgoAsString(Integer daysBack) {
+        DateFormat df = new SimpleDateFormat(IFTAServiceImpl.getSimpleDateFormat()); 
+        Calendar c = Calendar.getInstance(); 
+        c.add(Calendar.DATE, -daysBack);      
+        return df.format(c.getTime());
+    }
+
 }
