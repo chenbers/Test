c6f9c281800055750e88332f38b204ed421ec32e
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-12-06 21:58:26 +0000

    changes from Lee

--- web/src/test/java/com/inthinc/pro/web/selenium/Singleton.java
+++ web/src/test/java/com/inthinc/pro/web/selenium/Singleton.java
@@ -1,17 +1,14 @@
 package com.inthinc.pro.web.selenium;
+import com.inthinc.pro.web.selenium.InthincTest;
 
-
-
-
-
-public class Singleton {
+public class Singleton extends InthincTest {
 	 
     // volatile is needed so that multiple thread can reconcile the instance
     // semantics for volatile changed in Java 5.
     private volatile static Singleton singleton;
 
     private Core selenium;
-    
+  
     public Core getSelenium(){
     	
     	return selenium;
@@ -19,7 +16,28 @@ public class Singleton {
     private Singleton() {
     	
     }
-    // synchronized keyword has been removed from here
+    
+  //overloaded singleton used by the start_selenium method to pass data file information
+    public static Singleton getSingleton(String file_name, String test_case) {
+    	if(singleton==null) {
+            synchronized(Singleton.class){
+            // this is needed if two threads are waiting at the monitor at the
+            // time when singleton was getting instantiated
+              if(singleton == null) {
+                    try {
+                    	singleton = new Singleton();
+                    	set_test_case(file_name, test_case);
+                    	singleton.selenium = new Core((get_data("Settings","Host")), 4444, (get_data("Settings","Browser")), (get_data("Settings","URL")));
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					}
+            	} 
+    		}
+    return singleton;
+}
+    
     public static Singleton getSingleton() {
         // needed because once there is singleton available no need to acquire
         // monitor again & again as it is costly
@@ -30,8 +48,7 @@ public class Singleton {
                 if(singleton == null) {
                     try {
                     	singleton = new Singleton();
-						//singleton.selenium = (Selenium) new SeleniumServer();
-						singleton.selenium = new Core("localhost", 4444, "*firefox", "https://qa.tiwipro.com:8423/tiwipro/");
+                    	singleton.selenium = new Core(System.getenv("Selenium_host"), 4444, System.getenv("Selenium_Browser"), System.getenv("Selenium_Url"));
 					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -42,4 +59,5 @@ public class Singleton {
             }
         return singleton;
     }
-}
+    
+    }
