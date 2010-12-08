55baf102677c4676fc67d79f669fc5bf0c45fc6b
dfreitas <dfreitas@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-12-08 20:36:37 +0000

    NA - Added example code for exception mapping for future implementations.

---
+++ service/src/main/java/com/inthinc/pro/service/providers/InvalidDateException.java
@@ -0,0 +1,25 @@
+package com.inthinc.pro.service.providers;
+
+/**
+ * TODO This is a sample exception for exception mapping proof of context. Remove when exception mapping framework is in place.
+ */
+public class InvalidDateException extends RuntimeException {
+
+    public InvalidDateException() {
+        super();
+    }
+
+    public InvalidDateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidDateException(String message) {
+        super(message);
+    }
+
+    public InvalidDateException(Throwable cause) {
+        super(cause);
+    }
+
+    private static final long serialVersionUID = -4575786200821688363L;
+}
---
+++ service/src/main/java/com/inthinc/pro/service/providers/InvalidDateExceptionMapper.java
@@ -0,0 +1,22 @@
+package com.inthinc.pro.service.providers;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * TODO Example provider to map exception {@link InvalidDateException} to HTTP error codes.
+ */
+@Provider
+public class InvalidDateExceptionMapper implements ExceptionMapper<InvalidDateException> {
+
+    /**
+     * @see javax.ws.rs.ext.ExceptionMapper#toResponse(java.lang.Throwable)
+     */
+    @Override
+    public Response toResponse(InvalidDateException exception) {
+        return Response.status(Status.BAD_REQUEST.getStatusCode()).entity(exception.getMessage()).build();
+    }
+
+}
