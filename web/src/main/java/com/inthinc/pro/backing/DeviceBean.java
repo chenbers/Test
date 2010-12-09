f80c6c8747be6006a467debeef977baf12f4846d
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-12-09 06:48:25 +0000

    US2518: TA5018: pulled into it's own bean for easier inclusion in other pages as per pWehan

---
+++ web/src/main/java/com/inthinc/pro/backing/DeviceBean.java
@@ -0,0 +1,158 @@
+package com.inthinc.pro.backing;
+
+import org.apache.log4j.Logger;
+
+import com.inthinc.pro.dao.DeviceDAO;
+import com.inthinc.pro.model.Device;
+import com.inthinc.pro.model.EntityType;
+import com.inthinc.pro.model.ForwardCommand;
+import com.inthinc.pro.model.ForwardCommandID;
+import com.inthinc.pro.model.ForwardCommandStatus;
+import com.inthinc.pro.model.Vehicle;
+
+public class DeviceBean extends BaseBean implements IdentifiableEntityBean {
+
+    private static final long serialVersionUID = 1L;
+    private static final Logger logger = Logger.getLogger(DeviceBean.class);
+    
+    private Integer deviceID;
+    private Device device;
+    private DeviceDAO deviceDAO;
+
+    public DeviceBean() {
+        super();
+    }
+    public DeviceBean(Device device) {
+        this.deviceID = device.getDeviceID();
+        this.device = device;
+    }
+    public DeviceBean(Integer deviceID){
+        if(null != deviceID)
+            this.device = deviceDAO.findByID(deviceID);
+    }
+    public DeviceBean(Vehicle vehicle){
+        logger.debug("DeviceBean(Vehicle "+vehicle+")");
+        logger.debug("35: deviceDAO should NOT be NULL? deviceDAO: "+deviceDAO);
+        if(null != vehicle){
+            if(null != vehicle.getDeviceID()) {
+                this.deviceID = vehicle.getDeviceID();
+                logger.debug("39: deviceDAO: "+deviceDAO);
+                this.device = deviceDAO.findByID(vehicle.getDeviceID());
+            }
+        }
+    }
+    public void loadDeviceBean(){
+        if(null != this.deviceID){
+            this.device = deviceDAO.findByID(this.deviceID);
+        }
+    }
+    public void loadDeviceBean(Vehicle vehicle){
+        if(null != vehicle){
+            if(null != vehicle.getDeviceID()) {
+                this.deviceID = vehicle.getDeviceID();
+                this.device = deviceDAO.findByID(vehicle.getDeviceID());
+            }
+        }
+    }
+
+    public void requestGPSLocation() {
+        Integer deviceID = getDeviceID();
+        Device device;
+        if (null != deviceID) {
+            
+            device = deviceDAO.findByID(deviceID);
+            if (null != device && device.isWaySmart()) {
+                ForwardCommand fwdCmd = new ForwardCommand(0, ForwardCommandID.GET_GPS_GET_LOCATION, 0, ForwardCommandStatus.STATUS_QUEUED);
+                Integer cmdCount = deviceDAO.queueForwardCommand(deviceID, fwdCmd);
+                System.out.println("cmdCount: "+cmdCount);
+            }
+        } else {
+            // TODO: alert page that there was a problem
+        }
+    }
+    
+    public boolean isWaysmart() {
+        logger.debug("public boolean isWaysmart()");
+        logger.debug("79: deviceDAO: "+deviceDAO);
+        boolean result = false;
+        Integer deviceID = getDeviceID();
+        Device device;
+        if(null != deviceID) {
+            device = deviceDAO.findByID(deviceID);
+            result = (null != device && device.isWaySmart());
+            logger.debug("productVersion: "+device.getProductVersion());
+        }
+        logger.debug("isWaysmart() returning: "+result);
+        return result;
+    }
+
+
+    public Integer getDeviceID() {
+        return deviceID;
+    }
+
+    public void setDeviceID(Integer deviceID) {
+        logger.debug("98: deviceDAO: "+deviceDAO);
+        this.device = deviceDAO.findByID(deviceID);
+        this.deviceID = deviceID;
+    }
+
+    public Device getDevice() {
+        return device;
+    }
+
+    public void setDevice(Device device) {
+        this.device = device;
+    }
+
+    public DeviceDAO getDeviceDAO() {
+        return deviceDAO;
+    }
+
+    public void setDeviceDAO(DeviceDAO deviceDAO) {
+        this.deviceDAO = deviceDAO;
+    }
+
+    @Override
+    public EntityType getEntityType() {
+        return EntityType.ENTITY_DEVICE;
+    }
+
+    @Override
+    public Integer getId() {
+        return getDeviceID();
+    }
+
+    @Override
+    public String getName() {
+        if (device != null) {
+            return device.getName();
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void setId(Integer id) {
+        setDeviceID(id);
+    }
+
+    @Override
+    public Object getEntity() {
+        return device;
+    }
+
+    @Override
+    public String getLongName() {
+        if (device != null) {
+            return device.getName() + " - " + device.getFirmwareVersion(); 
+            // TODO: jwimmer: not sure what intention of getLongName is, or IF/WHEN/WHERE it would get used ?
+        }
+        return null;
+    }
+
+    @Override
+    public int compareTo(IdentifiableEntityBean o) {
+        return o != null ? this.getLongName().compareTo(o.getLongName()) : 0;
+    }
+}
