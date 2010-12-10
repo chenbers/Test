2c538f0f72058d0cf94485b73a5a903eb957fff0
cjennings <cjennings@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-12-10 03:52:39 +0000

    fixed test

--- dao/src/test/java/com/inthinc/pro/model/AlertMessageTypeTest.java
+++ dao/src/test/java/com/inthinc/pro/model/AlertMessageTypeTest.java
@@ -3,6 +3,7 @@ package com.inthinc.pro.model;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Collections;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -21,12 +22,16 @@ public class AlertMessageTypeTest {
     public void testAlertMessageTypeMapping(){
         
         RedFlagAlert rfa = new RedFlagAlert();
-        Long value = 2l;
+        Long value = 1l;
         rfa.setAlertTypeMask(value);
         assertEquals(rfa.getTypes().get(0),AlertMessageType.ALERT_TYPE_SPEEDING); 
-        value = 8l;
+        value = 4l;
         rfa.setAlertTypeMask(value);
         assertEquals(rfa.getTypes().get(0),AlertMessageType.ALERT_TYPE_SEATBELT); 
+        rfa.setAlertTypeMask(2l);
+        assertEquals(rfa.getTypes().get(0),AlertMessageType.ALERT_TYPE_AGGRESSIVE_DRIVING); 
+
+        
         value = AlertMessageType.ALERT_TYPE_WITNESS_UPDATED.getBitMask() +
         AlertMessageType.ALERT_TYPE_ZONES_CURRENT.getBitMask() + 
         AlertMessageType.ALERT_TYPE_WITNESS_HEARTBEAT_VIOLATION.getBitMask();
@@ -44,4 +49,26 @@ public class AlertMessageTypeTest {
         rfa.setAlertTypeMask(0L);
         assertEquals(rfa.getTypes(),Collections.emptyList()); 
      }
+
+    @Test
+    public void testAlertMessageTypeMapping2() {
+        Long mask = 0l;
+        
+        
+        for (AlertMessageType type : AlertMessageType.values()) {
+            mask |= type.getBitMask();
+            System.out.println("mask " + Long.toHexString(mask) +" bitmask " + Long.toHexString(type.getBitMask()));
+        }
+        
+        assertEquals("3ffdfffff", Long.toHexString(mask));
+        
+        Set<AlertMessageType> allTypes = AlertMessageType.getAlertMessageTypes(mask);
+        for (AlertMessageType type : allTypes)
+            System.out.println(type.toString());
+
+        assertEquals(allTypes.size(), AlertMessageType.values().length);
+        
+    }
+    
+
 }
