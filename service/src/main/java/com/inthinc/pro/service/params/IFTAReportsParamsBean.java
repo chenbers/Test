0400695413e3f5584155110ac9ed067e3e14244e
dcueva <dcueva@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-12-14 19:10:04 +0000

    US2703 Bean for web services parameters

---
+++ service/src/main/java/com/inthinc/pro/service/params/IFTAReportsParamsBean.java
@@ -0,0 +1,74 @@
+package com.inthinc.pro.service.params;
+
+import java.util.Date;
+import java.util.Locale;
+
+import com.inthinc.pro.model.MeasurementType;
+import com.inthinc.pro.service.security.TiwiproPrincipal;
+
+/**
+ * Simple JavaBean to encapsulate the parameters received by a Web service
+ * This bean will be used for validation of incoming parameters by applying the JSR-303
+ * It will also be used for retrieving default values for null parameters  
+ * 
+ * @author dcueva
+ *
+ */
+public class IFTAReportsParamsBean {
+
+	// Common parameters for all IFTA web services
+	Integer groupID;
+	Date startDate;
+	Date endDate;
+	Boolean iftaOnly;
+	Locale locale;
+	MeasurementType measurementType;
+
+	// Helper to obtain default values
+	TiwiproPrincipal principal;
+	
+	
+	/**
+	 * @return the groupID
+	 */
+	public Integer getGroupID() {
+		return groupID;
+	}
+
+	/**
+	 * @return the endDate
+	 */
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	/**
+	 * @return the iftaOnly
+	 */
+	public Boolean getIftaOnly() {
+		return iftaOnly;
+	}
+
+	/**
+	 * @return the locale
+	 */
+	public Locale getLocale() {
+		return locale;
+	}
+
+	/**
+	 * @return the measurementType
+	 */
+	public MeasurementType getMeasurementType() {
+		return measurementType;
+	}
+
+	/**
+	 * @param principal the principal to set
+	 */
+	public void setPrincipal(TiwiproPrincipal principal) {
+		this.principal = principal;
+	}
+
+	
+}
