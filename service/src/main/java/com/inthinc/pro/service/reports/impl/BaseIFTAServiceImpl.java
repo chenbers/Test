0a75ef1b5ada7e6293a981aecb4031f450f8b374
icazacu <icazacu@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-12-16 20:11:12 +0000

    NA: Added the getInterval() to super iftaService

--- service/src/main/java/com/inthinc/pro/service/reports/impl/BaseIFTAServiceImpl.java
+++ service/src/main/java/com/inthinc/pro/service/reports/impl/BaseIFTAServiceImpl.java
@@ -1,5 +1,10 @@
 package com.inthinc.pro.service.reports.impl;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import org.joda.time.Interval;
+
 import com.inthinc.pro.service.reports.facade.ReportsFacade;
 import com.inthinc.pro.util.ReportsUtil;
 
@@ -9,7 +14,7 @@ import com.inthinc.pro.util.ReportsUtil;
 public class BaseIFTAServiceImpl {
 
     public static String DATE_FORMAT = "yyyyMMdd";
-    protected static final Integer DAYS_BACK = -6;
+    static final Integer DAYS_BACK = -6;
 
     protected ReportsFacade reportsFacade;
     protected ReportsUtil reportsUtil;
@@ -26,4 +31,30 @@ public class BaseIFTAServiceImpl {
     public ReportsFacade getReportsFacade() {
         return reportsFacade;
     }
+    
+    /**
+     * Create an Interval from the Date range.
+     * If the Dates are null the default one is created.
+     * @return the Date set to default days back
+     */
+    public Interval getInterval(Date startDate, Date endDate) {
+        if (startDate == null || endDate == null) {
+            Calendar start = getMidnight();
+            start.add(Calendar.DAY_OF_YEAR, DAYS_BACK);
+            startDate = start.getTime();
+            endDate = getMidnight().getTime();
+        }        
+        return new Interval(startDate.getTime(), endDate.getTime());
+    }
+    
+    private Calendar getMidnight() {
+        Calendar date = Calendar.getInstance();
+        // set time to midnight
+       date.set(Calendar.HOUR_OF_DAY, 0);
+       date.set(Calendar.MINUTE, 0);
+       date.set(Calendar.SECOND, 0);
+       date.set(Calendar.MILLISECOND, 0);
+       return date;
+    }
+
 }
