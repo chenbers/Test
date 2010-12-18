6329a22b4e1ff61cb8dfc871d7baeca499d733ae
cjennings <cjennings@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-12-18 18:48:32 +0000

    DE5695 - score boxes on driver/vehicle reports - switched from jquery to using a component

---
+++ web/src/main/java/com/inthinc/pro/backing/ui/component/ScoreBoxComponent.java
@@ -0,0 +1,126 @@
+package com.inthinc.pro.backing.ui.component;
+
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import javax.faces.component.html.HtmlOutputText;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+
+import org.springframework.security.context.SecurityContextHolder;
+
+import com.inthinc.pro.security.userdetails.ProUser;
+
+/**
+ * Control for displaying score with the appropriate size and color.
+ */
+
+public class ScoreBoxComponent extends HtmlOutputText
+{
+    public static final String COMPONENT_TYPE = "ScoreBoxDisplay";
+    public static final String OUTER_DIV = "<div style='display: table; #position: relative; overflow: hidden; width:100%;height:100%;'>";
+    public static final String MIDDLE_DIV = "<div style='#position: relative;  #top: 50%; display: table-cell; vertical-align: middle;width:100%;'>";
+    public static final String INNER_DIV = "<div style='#position: relative;  #top: -50%; width:100%;text-align:center'>";
+    public static final String DEFAULT_SIZE = "x_sm";
+    public static final String sizes[] = {
+        DEFAULT_SIZE,   //0
+        "x_sm",   //1
+        "sm",   //2
+        "med",   //3
+        "lg",   //4
+        "x_lg"  //5
+    };
+
+    public ScoreBoxComponent()
+    {
+        super();
+    }
+
+    @Override
+    public void encodeEnd(FacesContext context) throws IOException
+    {
+        ResponseWriter out = context.getResponseWriter();
+
+        StringBuffer buffer = new StringBuffer();
+        String scoreCssClass = "score_" + getSuffix(getValue());
+        String sizeCssClass = "score_" + getSize(); 
+        
+        buffer.append("<span><div class='score " + scoreCssClass + " " + sizeCssClass + "'");
+        buffer.append(OUTER_DIV);
+        buffer.append(MIDDLE_DIV);
+        buffer.append(INNER_DIV);
+        buffer.append(getDisplayValue(getValue()));
+        buffer.append("</div></div></div></div></span>");
+        out.write(buffer.toString());
+    }
+
+    private String getSize() {
+        Object sizeAttr = getAttributes().get("size");
+        
+        if (sizeAttr == null)
+            return DEFAULT_SIZE;
+        
+        int sizeInt = 0;
+        try {
+            sizeInt = Integer.valueOf(sizeAttr.toString());
+        }
+        catch (NumberFormatException ex) {
+            
+        }
+        if (sizeInt < 0 || sizeInt > 5)
+            return DEFAULT_SIZE;
+        return sizes[sizeInt];
+    }
+
+    private String getDisplayValue(Object value) {
+        if (value == null)
+            return "N/A";
+        
+        NumberFormat format = NumberFormat.getNumberInstance(getLocale());
+        format.setMaximumFractionDigits(1);
+        format.setMinimumFractionDigits(1);
+    
+        return format.format(Integer.class.cast(value).doubleValue()/10.0d);
+    }
+    
+    private ProUser getUser() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof ProUser)
+            return (ProUser) principal;
+        return null;
+    }
+    private Locale getLocale() {
+        if (getUser() != null)
+            return getUser().getUser().getPerson().getLocale();
+         return Locale.getDefault();
+    }
+
+
+    private String getSuffix(Object value) {
+        if (value == null)
+            return "na";
+        int score = 0;
+        try {
+            score = Integer.valueOf(value.toString());
+        }
+        catch (NumberFormatException ex){
+            return "na";
+        }
+        
+        if(score < 11)
+            return "1";
+        if(score < 21)
+            return "2";
+        if(score < 31)
+            return "3";
+        if(score < 41)
+            return "4";
+        return "5";
+
+        
+    }
+
+
+
+}
--- web/src/test/java/com/inthinc/pro/backing/model/HelpConfigPropertiesTest.java
+++ web/src/test/java/com/inthinc/pro/backing/model/HelpConfigPropertiesTest.java
@@ -12,7 +12,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
@@ -31,12 +30,14 @@ public class HelpConfigPropertiesTest {
         if (webInfPath == null) {
             webInfPath = "src/main/webapp/WEB-INF/"; 
         }
-System.out.println("webInfPath: " + webInfPath);        
+//System.out.println("webInfPath: " + webInfPath);        
     }
     
     @Test
     public void testPrettyIDsExist()
     {
+        // make sure all of the page definitions in the pretty config have a matching help link associated
+        
         HelpConfigProperties helpConfigProperties = new HelpConfigProperties();
         helpConfigProperties.setLocation("helpConfig.properties");
         helpConfigProperties.init();
@@ -50,24 +51,17 @@ System.out.println("webInfPath: " + webInfPath);
         }
     }
 
-    // TODO: need to figure out how to get this xml file when running test from Hudson 
     private List<String> getPrettyIDList() {
         List<String> prettyIDList = new ArrayList<String>();
         try
         {
             File file = new File(webInfPath + "pretty-config.xml");
-            System.out.println("prettyconfig path is " + file.getAbsoluteFile() + " exists: " + file.exists());
-            
-//            InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream("src/main/webapp/WEB-INF/pretty-config.xml");
-//            if (stream == null) {
-//                throw new Exception("cannot open pretty-config.xml");
-//            }
+//            System.out.println("prettyconfig path is " + file.getAbsoluteFile() + " exists: " + file.exists());
             Document doc = parse(file);
             
             NodeList nodeList = doc.getChildNodes().item(0).getChildNodes();
             for (int i = 0; i < nodeList.getLength(); i++) {
                 Node node = nodeList.item(i);
-//                System.out.println("node " + node.getNodeName());
                 if (node.getNodeName().equalsIgnoreCase("url-mapping")) {
                     NamedNodeMap attribs = node.getAttributes();
                 
@@ -87,12 +81,6 @@ System.out.println("webInfPath: " + webInfPath);
         return prettyIDList;
     }
 
-    
-//    public static Document parse(InputStream is) throws Exception
-//    {
-//        Document doc = createDocumentBuilder().parse(is);
-//        return doc;
-//    }
     
     public static Document parse(File file) throws Exception
     {
