b4c7083077210f33f75bcb560944ec42bd56bb14
dfreitas <dfreitas@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-12-20 19:36:43 +0000

    US2658 - Added an integration test for parameter validation.

--- service/src/test/java/com/inthinc/pro/service/reports/impl/it/MileageByVehicleIFTAServiceTest.java
+++ service/src/test/java/com/inthinc/pro/service/reports/impl/it/MileageByVehicleIFTAServiceTest.java
@@ -1,6 +1,10 @@
 package com.inthinc.pro.service.reports.impl.it;
 
+import static junit.framework.Assert.assertEquals;
+
 import java.util.List;
+
+import javax.ws.rs.core.Response.Status;
 
 import org.jboss.resteasy.client.ClientResponse;
 import org.junit.Test;
@@ -19,81 +23,92 @@ public class MileageByVehicleIFTAServiceTest extends BaseEmbeddedServerITCase {
     private static final Integer GROUP_ID_NOT_IN_HIERARCHY = 8;
     private static final Integer GROUP_ID_BAD = 99999;
     private static final Integer GROUP_ID_NEGATIVE = -5;
-    
+
     @Test
     public void testDummy() {}
 
     /**
      * Integration test for getMileageByVehicleWithDates().
      */
-//    @Test
+    // @Test
     public void testGetMileageByVehicleWithDates() {
         // test case when Group has data
-        ClientResponse<List<MileageByVehicle>> response = 
-            client.getMileageByVehicleWithDates(GROUP_ID_WITH_DATA, TEST_START_DATE, TEST_END_DATE);
-//        assertEquals(Status.OK.getStatusCode(), response.getStatus());      
-//        List<MileageByVehicle> list = response.getEntity();
-//        assertTrue(list.size() > 0);
-//
-//        // test case when wrong date format
-//        response = client.getMileageByVehicleWithIftaAndDates(GROUP_ID_WITH_DATA, "2010-10-01", "2010-10-30");
-//        assertEquals(Status.BAD_REQUEST.getStatusCode(), response.getStatus());        
-//
-//        // test case when wrong date range
-//        response = client.getMileageByVehicleWithIftaAndDates(GROUP_ID_WITH_DATA, TEST_END_DATE, TEST_START_DATE);
-//        assertEquals(Status.BAD_REQUEST.getStatusCode(), response.getStatus());        
+        ClientResponse<List<MileageByVehicle>> response = client.getMileageByVehicleWithDates(GROUP_ID_WITH_DATA, TEST_START_DATE, TEST_END_DATE);
+        // assertEquals(Status.OK.getStatusCode(), response.getStatus());
+        // List<MileageByVehicle> list = response.getEntity();
+        // assertTrue(list.size() > 0);
+        //
+        // // test case when wrong date format
+        // response = client.getMileageByVehicleWithIftaAndDates(GROUP_ID_WITH_DATA, "2010-10-01", "2010-10-30");
+        // assertEquals(Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+        //
+        // // test case when wrong date range
+        // response = client.getMileageByVehicleWithIftaAndDates(GROUP_ID_WITH_DATA, TEST_END_DATE, TEST_START_DATE);
+        // assertEquals(Status.BAD_REQUEST.getStatusCode(), response.getStatus());
     }
 
     /**
      * Integration test for getMileageByVehicleWithIftaAndDates().
      */
-//    @Test
+    // @Test
     public void testGetMileageByVehicleWithIftaAndDates() {
-        
+
         // test case when Group has data
-        ClientResponse<List<MileageByVehicle>> response = 
-            client.getMileageByVehicleWithIftaAndDates(GROUP_ID_WITH_DATA, TEST_START_DATE, TEST_END_DATE);
-//        assertEquals(Status.OK.getStatusCode(), response.getStatus());        
-//        List<MileageByVehicle> list = response.getEntity();
-//        assertTrue(list.size() > 0);
+        ClientResponse<List<MileageByVehicle>> response = client.getMileageByVehicleWithIftaAndDates(GROUP_ID_WITH_DATA, TEST_START_DATE, TEST_END_DATE);
+        // assertEquals(Status.OK.getStatusCode(), response.getStatus());
+        // List<MileageByVehicle> list = response.getEntity();
+        // assertTrue(list.size() > 0);
     }
 
     /**
      * Integration test for getMileageByVehicleWithIfta().
      */
-//    @Test
+    // @Test
     public void testGetMileageByVehicleWithIfta() {
-        
+
         // test case when Group has data
         ClientResponse<List<MileageByVehicle>> response = client.getMileageByVehicleWithIfta(GROUP_ID_WITH_DATA);
-//        assertEquals(Status.OK.getStatusCode(), response.getStatus());        
-//        List<MileageByVehicle> list = response.getEntity();
-//        assertTrue(list.size() > 0);
+        // assertEquals(Status.OK.getStatusCode(), response.getStatus());
+        // List<MileageByVehicle> list = response.getEntity();
+        // assertTrue(list.size() > 0);
     }
 
     /**
      * Integration test for getMileageByVehicleDefaults().
      */
-//    @Test
+    // @Test
     public void testGetMileageByVehicleDefaults() {
         // test case when Group not in Hierarchy
-        ClientResponse<List<MileageByVehicle>> response = 
-            client.getMileageByVehicleDefaults(GROUP_ID_NOT_IN_HIERARCHY);
-        // TODO fix it 
-//         assertEquals(Status.FORBIDDEN.getStatusCode(), response.getStatus());
-//        
-//        // test case when Group doesn't exist
-//        response = client.getMileageByVehicleDefaults(GROUP_ID_BAD);
-//        assertEquals(Status.NOT_FOUND.getStatusCode(), response.getStatus());
-//
-//        // test case when Group is negative
-//        response = client.getMileageByVehicleDefaults(GROUP_ID_NEGATIVE);
-//        assertEquals(Status.BAD_REQUEST.getStatusCode(), response.getStatus());
-//        
-//        // test case when Group has data
-//        response = client.getMileageByVehicleDefaults(GROUP_ID_WITH_DATA);
-//        assertEquals(Status.OK.getStatusCode(), response.getStatus());
-//        List<MileageByVehicle> list = response.getEntity();
-//        assertTrue(list.size() > 0);
+        ClientResponse<List<MileageByVehicle>> response = client.getMileageByVehicleDefaults(GROUP_ID_NOT_IN_HIERARCHY);
+        // TODO fix it
+        // assertEquals(Status.FORBIDDEN.getStatusCode(), response.getStatus());
+        //        
+        // // test case when Group doesn't exist
+        // response = client.getMileageByVehicleDefaults(GROUP_ID_BAD);
+        // assertEquals(Status.NOT_FOUND.getStatusCode(), response.getStatus());
+        //
+        // // test case when Group is negative
+        // response = client.getMileageByVehicleDefaults(GROUP_ID_NEGATIVE);
+        // assertEquals(Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+        //        
+        // // test case when Group has data
+        // response = client.getMileageByVehicleDefaults(GROUP_ID_WITH_DATA);
+        // assertEquals(Status.OK.getStatusCode(), response.getStatus());
+        // List<MileageByVehicle> list = response.getEntity();
+        // assertTrue(list.size() > 0);
+    }
+
+//    @Test
+    public void testValidationOfParameters() {
+        ClientResponse<List<MileageByVehicle>> response = client.getMileageByVehicleWithDates(-1, "19790303", "20100303");
+        // Verifies the body can be unmarshalled as string for stack trace.
+        response.getEntity(String.class);
+        assertEquals(Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+
+        client.getMileageByVehicleWithDates(-1, "20100303", "19790303");
+        // Verifies the body can be unmarshalled as string for stack trace.
+        response.getEntity(String.class);
+        assertEquals(Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+
     }
 }
