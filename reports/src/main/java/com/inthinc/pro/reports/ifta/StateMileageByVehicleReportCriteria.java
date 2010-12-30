98d9bd7ae46dd77bd16d7ad5c1c8c3df2e18bea8
dfreitas <dfreitas@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2010-12-30 16:45:51 +0000

    US2852 - Added null checks on comparator.        - Created additional unit tests.

--- reports/src/main/java/com/inthinc/pro/reports/ifta/StateMileageByVehicleReportCriteria.java
+++ reports/src/main/java/com/inthinc/pro/reports/ifta/StateMileageByVehicleReportCriteria.java
@@ -20,6 +20,7 @@ public class StateMileageByVehicleReportCriteria extends DOTReportCriteria {
 
     /**
      * Default constructor.
+     * 
      * @param locale
      */
     public StateMileageByVehicleReportCriteria(Locale locale) {
@@ -28,6 +29,7 @@ public class StateMileageByVehicleReportCriteria extends DOTReportCriteria {
 
     /**
      * {@inheritDoc}
+     * 
      * @see com.inthinc.pro.reports.ifta.DOTReportCriteria#getDataByGroup(java.lang.Integer, org.joda.time.Interval, boolean)
      */
     @Override
@@ -36,38 +38,70 @@ public class StateMileageByVehicleReportCriteria extends DOTReportCriteria {
     }
 
     /**
-     * Populate the data set with data.
-     * Copy the fields from the record returned from the Back End to the fields in the beans to be used by Jasper.
+     * Populate the data set with data. Copy the fields from the record returned from the Back End to the fields in the beans to be used by Jasper.
      * 
-     * @param records The records retrieved from the Back End
+     * @param records
+     *            The records retrieved from the Back End
      */
-    void initDataSet(List<StateMileage> records)
-    {   
+    void initDataSet(List<StateMileage> records) {
         List<MileageByVehicle> dataList = new ArrayList<MileageByVehicle>();
         for (StateMileage item : records) {
             MileageByVehicle rec = new MileageByVehicle();
             rec.setVehicleName(item.getVehicleName());
             rec.setState(item.getStateName());
             rec.setGroupName(getShortGroupName(item.getGroupID()));
-            rec.setTotal(MeasurementConversionUtil.convertMilesToKilometers(
-                        item.getMiles(), getMeasurementType()).doubleValue());
+            rec.setTotal(MeasurementConversionUtil.convertMilesToKilometers(item.getMiles(), getMeasurementType()).doubleValue());
             dataList.add(rec);
         }
-        Collections.sort(dataList, new StateMileageByVehicleComparator());        
+        Collections.sort(dataList, new StateMileageByVehicleComparator());
         setMainDataset(dataList);
     }
 
     /* Comparator for StateMileageByVehicle report */
     class StateMileageByVehicleComparator implements Comparator<MileageByVehicle> {
 
+        private static final int COMPARISON_SAME = 0;
+        private static final int COMPARISON_BEFORE = -1;
+        private static final int COMPARISON_AFTER = 1;
+
         @Override
         public int compare(MileageByVehicle o1, MileageByVehicle o2) {
-            int equal = o1.getGroupName().compareTo(o2.getGroupName());
-            if (equal == 0) {
-                return o1.getVehicleName().compareTo(o2.getVehicleName());                
+
+            // Checking for nulls on properties. Null values always goes at the end.
+            int comparison = compareValues(o1.getGroupName(), o2.getGroupName());
+
+            if (comparison == 0) {
+                comparison = compareValues(o1.getVehicleName(), o2.getVehicleName());
+            }
+
+            return comparison;
+        }
+
+        @SuppressWarnings("unchecked")
+        private int compareValues(Comparable o1, Object o2) {
+            if (o1 == null) {
+                if (o2 != null) {
+                    return COMPARISON_AFTER;
+                } else {
+                    return COMPARISON_SAME;
+                }
             } else {
-                return equal;
+                if (o2 == null) {
+                    return COMPARISON_BEFORE;
+                } else {
+                    return o1.compareTo(o2);
+                }
             }
         }
+
+        // @Override
+        // public int compare(MileageByVehicle o1, MileageByVehicle o2) {
+        // int equal = o1.getGroupName().compareTo(o2.getGroupName());
+        // if (equal == 0) {
+        // return o1.getVehicleName().compareTo(o2.getVehicleName());
+        // } else {
+        // return equal;
+        // }
+        // }
     }
 }
