e370ece4bec46bb01ddc4388ed7008c5dd182ad1
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-01-26 21:05:07 +0000

    changed from byte array insertions to ByteArrayOutputStream

--- web/src/test/java/com/inthinc/pro/device/tiwiPro/Package_tiwiPro_Note.java
+++ web/src/test/java/com/inthinc/pro/device/tiwiPro/Package_tiwiPro_Note.java
@@ -1,5 +1,7 @@
 package com.inthinc.pro.device.tiwiPro;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -136,40 +138,40 @@ public class Package_tiwiPro_Note {
     }
     
     public byte[] Package(){
+    	
+    	ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    	
         
         Iterator<Integer> keys = attrs.keySet().iterator();
         
         if (lng < 0.0 ){
             lng += 360.0;
         }
-       
-        byte[] eventBytes = new byte[200];
         
         double latitude = ( 90.0 - lat ) / 180.0;
         double longitude = ( lng / 360.0 );
         
-        int idx   = 0;
         int val1  = (int)( latitude  * 0x00FFFFFF );
         int val2  = (int)( longitude * 0x00FFFFFF );
         int flags = (int)(  heading << 4)  & 0xF0 | sats & 0x0F;
         
         //Headers  Convert the value to an integer, then pack it as a byte in the array
-        eventBytes[idx++] = (byte)(  nType          & 0xFF );
-        eventBytes[idx++] = (byte)(( nTime >> 24 )  & 0xFF );
-        eventBytes[idx++] = (byte)(( nTime >> 16 )  & 0xFF );
-        eventBytes[idx++] = (byte)(( nTime >>  8 )  & 0xFF );
-        eventBytes[idx++] = (byte)(  nTime          & 0xFF );
-        eventBytes[idx++] = (byte) flags;
-        eventBytes[idx++] = (byte)(  maprev         & 0xFF );
-        eventBytes[idx++] = (byte)(( val1 >> 16 )   & 0xFF );
-        eventBytes[idx++] = (byte)(( val1 >>  8 )   & 0xFF );
-        eventBytes[idx++] = (byte)(  val1           & 0xFF );
-        eventBytes[idx++] = (byte)(( val2 >> 16 )   & 0xFF );
-        eventBytes[idx++] = (byte)(( val2 >>  8 )   & 0xFF );
-        eventBytes[idx++] = (byte)(  val2           & 0xFF );
-        eventBytes[idx++] = (byte)(  Speed          & 0xFF );
-        eventBytes[idx++] = (byte)(( odometer >> 8) & 0xFF );
-        eventBytes[idx++] = (byte)(  odometer       & 0xFF );
+        bos.write(  nType          & 0xFF );
+        bos.write(( nTime >> 24 )  & 0xFF );
+        bos.write(( nTime >> 16 )  & 0xFF );
+        bos.write(( nTime >>  8 )  & 0xFF );
+        bos.write(  nTime          & 0xFF );
+        bos.write( flags);
+        bos.write(  maprev         & 0xFF );
+        bos.write(( val1 >> 16 )   & 0xFF );
+        bos.write(( val1 >>  8 )   & 0xFF );
+        bos.write(  val1           & 0xFF );
+        bos.write(( val2 >> 16 )   & 0xFF );
+        bos.write(( val2 >>  8 )   & 0xFF );
+        bos.write(  val2           & 0xFF );
+        bos.write(  Speed          & 0xFF );
+        bos.write(( odometer >> 8) & 0xFF );
+        bos.write(  odometer       & 0xFF );
         
         
         while( keys.hasNext()){
@@ -178,27 +180,31 @@ public class Package_tiwiPro_Note {
             int value = attrs.get(key);
             
             if ( key < 128 ){
-                eventBytes[idx++] = (byte)(  key   & 0xFF );
-                eventBytes[idx++] = (byte)(  value & 0xFF );                
+                bos.write( key   & 0xFF );
+                bos.write( value & 0xFF );                
             }
             
             else if ( key < 192 && key >= 128 ){
-                eventBytes[idx++] = (byte)(   key           & 0xFF );
-                eventBytes[idx++] = (byte)( ( value >>  8 ) & 0xFF );
-                eventBytes[idx++] = (byte)(   value         & 0xFF );                
+                bos.write(  key           & 0xFF );
+                bos.write(( value >>  8 ) & 0xFF );
+                bos.write(  value         & 0xFF );                
             }
             
             else if ( key < 255 && key >= 192 ){
-                eventBytes[idx++] = (byte)(   key           & 0xFF );
-                eventBytes[idx++] = (byte)( ( value >> 24 ) & 0xFF );
-                eventBytes[idx++] = (byte)( ( value >> 16 ) & 0xFF );
-                eventBytes[idx++] = (byte)( ( value >>  8 ) & 0xFF );
-                eventBytes[idx++] = (byte)(   value         & 0xFF );
+                bos.write(  key           & 0xFF );
+                bos.write(( value >> 24 ) & 0xFF );
+                bos.write(( value >> 16 ) & 0xFF );
+                bos.write(( value >>  8 ) & 0xFF );
+                bos.write(  value         & 0xFF );
             }
-            
         }
-        
+        byte[] eventBytes = bos.toByteArray();
+        try {
+			bos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
         return eventBytes;   
-    }   
+    }
 }
     
