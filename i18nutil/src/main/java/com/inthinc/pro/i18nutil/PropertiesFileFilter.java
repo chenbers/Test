1e93fee3fbab7c64218984623d69a4a3de233862
cjennings <cjennings@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-02-18 23:13:48 +0000

    Added some utilities/process for handling translations.

---
+++ i18nutil/src/main/java/com/inthinc/pro/i18nutil/PropertiesFileFilter.java
@@ -0,0 +1,30 @@
+package com.inthinc.pro.i18nutil;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class PropertiesFileFilter  implements FilenameFilter {
+
+    String lang;
+    public PropertiesFileFilter(String lang) {
+        this.lang = lang;
+    }
+    @Override
+    public boolean accept(File dir, String name) {
+        if (dir.getAbsolutePath().contains("target"))
+            return false;
+        if (lang == null) {
+            if (name.toLowerCase().endsWith(".properties") && !name.contains("_"))
+                return true;
+            return false;
+        }
+            
+        if (name.toLowerCase().endsWith("_"+lang.toLowerCase()+".properties"))
+            return true;
+        
+        return false;
+    }
+    
+}
+
+
