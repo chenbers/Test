3a6d82e565f3782aeb4c64481004b6b2c2aa4baf
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-03-04 16:19:31 +0000

    Baseline.

---
+++ webutil/src/main/java/com/inthinc/pro/tripviewer/model/EventAndAttr.java
@@ -0,0 +1,47 @@
+package com.inthinc.pro.tripviewer.model;
+
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.inthinc.pro.model.event.Event;
+
+public class EventAndAttr extends Event {
+    private Map<Object,Object> decodedAttrMap;
+    
+    public EventAndAttr() {
+        super();
+    }
+    
+    public EventAndAttr(int sats,int speed,int speedLimit, double latitude, double longitude, Date time, Map<Object,Object> attrMap) {
+        setSats(sats);
+        setSpeed(speed);
+        setSpeedLimit(speedLimit);
+        setLongitude(longitude);
+        setLatitude(latitude);
+        setTime(time);
+        this.setAttrMap(attrMap);
+    }
+
+    public String getDecodedAttrMap() {
+        if ( decodedAttrMap == null ) {
+            return "";
+        }
+        
+        StringBuffer sb = new StringBuffer();
+        sb.append("");
+        
+        Iterator it = decodedAttrMap.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pairs = (Map.Entry)it.next();
+            sb.append(pairs.getKey() + " = " + pairs.getValue() + " , ");
+        }        
+        
+        return sb.toString();
+    }
+
+    public void setDecodedAttrMap(Map<Object,Object> decodedAttrMap) {
+        this.decodedAttrMap = decodedAttrMap;
+    }
+
+}
