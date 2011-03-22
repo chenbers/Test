f839868a3ed30a4b1d15adf8a7a61c7f6a81ca30
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-03-22 07:22:28 +0000

    US3053: TA6076: fixing build errors

---
+++ automation/src/test/java/com/inthinc/pro/automation/SeleniumEnum.java
@@ -0,0 +1,16 @@
+package com.inthinc.pro.automation;
+
+public interface SeleniumEnum { 
+	
+	public String getText();
+	
+	public void setText(String text);
+
+	public String getID();
+
+	public String getXpath();
+
+	public String getXpath_alt();
+	
+	public String getError();
+}
