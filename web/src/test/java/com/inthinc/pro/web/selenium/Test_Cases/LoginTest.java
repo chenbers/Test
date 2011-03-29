1a00da929dc41793d7368517cd86e6f98757e21c
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-03-29 19:05:27 +0000

    Ignoring automation tests

--- web/src/test/java/com/inthinc/pro/web/selenium/Test_Cases/LoginTest.java
+++ web/src/test/java/com/inthinc/pro/web/selenium/Test_Cases/LoginTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import com.inthinc.pro.web.selenium.InthincTest;
 import com.inthinc.pro.web.selenium.portal.Login.Login;
 import com.inthinc.pro.web.selenium.portal.Masthead.Masthead;
-
+@Ignore
 public class LoginTest extends InthincTest {
     Login l;
     //TODO: jwimmer: question for DTanner: I can see a benefit from having SOME of these types of things defined in a non-page-specific enum?  email is a good example (actually not sure there are (m)any others???_) since there are lots of pages where an email address is expected, and it could be nice to have these three quickly available for tests on ANY page
