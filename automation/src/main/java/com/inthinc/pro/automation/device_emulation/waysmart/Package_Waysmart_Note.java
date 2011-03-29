8b903c6b2b0be9f3b73fa0ecd91b99cd362ceeb3
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-03-29 21:38:52 +0000

    beginnings of Waysmart emulator

---
+++ automation/src/main/java/com/inthinc/pro/automation/device_emulation/waysmart/Package_Waysmart_Note.java
@@ -0,0 +1,206 @@
+//package com.inthinc.QA.waysmart;
+//
+//import java.text.ParseException;
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//import java.util.HashMap;
+//import java.util.Iterator;
+//import java.util.Set;
+//
+//import com.inthinc.QA.enums.Waysmart;
+//import com.inthinc.QA.tiwiPro.enums.TiwiPro;
+//
+//public class Package_Waysmart_Note {
+//    
+//    private Integer nType, sats, heading, maprev, Speed, odometer, nTime;
+//    private Double lat, lng;
+//    private HashMap<Integer, Integer> attrs;
+//    
+//    
+//    public Package_Waysmart_Note(Waysmart type, String time, int Sats, int Heading, int Maprev, Double Lat, Double Lng, int speed, int Odometer, HashMap<Waysmart, Integer> Attrs){
+//        nType = type.getCode();
+//        nTime = (int)DateToLong(time);
+//        sats = Sats;
+//        heading = Heading;
+//        maprev = Maprev;
+//        lat = Lat;
+//        lng = Lng;
+//        Speed = speed;
+//        odometer = Odometer;
+//        processAttrs(Attrs);
+//    }
+//    
+//    
+//    public Package_Waysmart_Note(Waysmart type, long time, int Sats, int Heading, int Maprev, Double Lat, Double Lng, int speed, int Odometer, HashMap<Waysmart, Integer> Attrs){
+//        nType = type.getCode();
+//        if ( time > (System.currentTimeMillis()/100 )) nTime = (int)( time/1000 );
+//        else{ nTime = (int)time; }
+//        sats = Sats;
+//        heading = Heading;
+//        maprev = Maprev;
+//        lat = Lat;
+//        lng = Lng;
+//        Speed = speed;
+//        odometer = Odometer;
+//        processAttrs(Attrs);
+//    }
+//    
+//    public Package_Waysmart_Note( Waysmart type, HashMap<Waysmart, Integer> Attrs ){
+//        nType = type.getCode();
+//        nTime = (int)(System.currentTimeMillis()/1000);
+//        sats = 0;
+//        heading = 0;
+//        maprev = 0;
+//        lat = 0.0;
+//        lng = 0.0;
+//        Speed = 0;
+//        odometer = 0;
+//        processAttrs(Attrs);
+//    }
+//    
+//    public Package_Waysmart_Note( Waysmart type ){
+//        nType = type.getCode();
+//        nTime = (int)(System.currentTimeMillis()/1000);
+//        sats = 0;
+//        heading = 0;
+//        maprev = 0;
+//        lat = 0.0;
+//        lng = 0.0;
+//        Speed = 0;
+//        odometer = 0;
+//        attrs = new HashMap<Integer, Integer>();
+//    }
+//    
+//    public Package_Waysmart_Note(){
+//        nType = TiwiPro.NOTE_TYPE_LOCATION.getCode();
+//        nTime = (int)(System.currentTimeMillis()/1000);
+//        sats = 0;
+//        heading = 0;
+//        maprev = 0;
+//        lat = 0.0;
+//        lng = 0.0;
+//        Speed = 0;
+//        odometer = 0;
+//        attrs = new HashMap<Integer, Integer>();
+//    }
+//    
+//    
+//
+//	public void AddAttrs(HashMap<Waysmart, Integer> Attrs){
+//    	processAttrs(Attrs);
+//    }
+//    public void AddAttrs(Waysmart cmd, Integer reply){
+//    	HashMap<Waysmart, Integer> Attrs = new HashMap<Waysmart, Integer>();
+//    	Attrs.put(cmd, reply);
+//    	processAttrs(Attrs);
+//    }
+//    public void AddAttrs(Waysmart cmd, TiwiPro reply){
+//    	HashMap<Waysmart, Integer> Attrs = new HashMap<Waysmart, Integer>();
+//    	Attrs.put(cmd, reply.getCode());
+//    	processAttrs(Attrs);
+//    }
+//        
+//        
+//    public long DateToLong( String datetime ){
+//        long epoch_time = (int)(System.currentTimeMillis()/1000);
+//        
+//        try {
+//            Date date ; 
+//            date = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").parse(datetime); 
+//            epoch_time = date.getTime();
+//            
+//          }
+//          catch (ParseException e){
+//            System.out.println("Exception :"+e);   
+//          }
+//        
+//        return epoch_time;
+//    }
+//    
+//    public String noteToString(){
+//    	String note = "";
+//    	try{
+//    		note = String.format("DeviceNote(nType=%d, nTime=\"%s\", sats=%d, heading=%d, maprev=%d, lat=%.5f, lng=%.5f, speed=%d, odometer=%d)", nType, nTime, sats, heading, maprev, lat, lng, Speed, odometer);
+//    	}catch(Exception e){
+//    		e.printStackTrace();
+//    	}
+//    	return note;
+//    }
+//    
+//    public void processAttrs(HashMap<Waysmart, Integer> Attrs){
+//    	attrs = new HashMap<Integer, Integer>();
+//    	Waysmart attrID;
+//    	Set<Waysmart> keys = Attrs.keySet();
+//    	Iterator<Waysmart> itr = keys.iterator();
+//    	while (itr.hasNext()){
+//    		attrID = itr.next();
+//    		attrs.put(attrID.getCode(), Attrs.get(attrID));
+//    	}
+//    }
+//    
+//    public byte[] Package(){
+//        
+//        Iterator<Integer> keys = attrs.keySet().iterator();
+//        
+//        if (lng < 0.0 ){
+//            lng += 360.0;
+//        }
+//       
+//        byte[] eventBytes = new byte[200];
+//        
+//        double latitude = ( 90.0 - lat ) / 180.0;
+//        double longitude = (int)( lng / 360.0 );
+//        
+//        int idx   = 0;
+//        int val1  = (int)( latitude  * 0x00FFFFFF );
+//        int val2  = (int)( longitude * 0x00FFFFFF );
+//        int flags = (int)(  heading << 4)  & 0xF0 | sats & 0x0F;
+//        
+//        //Headers  Convert the value to an integer, then pack it as a byte in the array
+//        eventBytes[idx++] = (byte)(  nType          & 0xFF );
+//        eventBytes[idx++] = (byte)(( nTime >> 24 )  & 0xFF );
+//        eventBytes[idx++] = (byte)(( nTime >> 16 )  & 0xFF );
+//        eventBytes[idx++] = (byte)(( nTime >>  8 )  & 0xFF );
+//        eventBytes[idx++] = (byte)(  nTime          & 0xFF );
+//        eventBytes[idx++] = (byte) flags;
+//        eventBytes[idx++] = (byte)(  maprev         & 0xFF );
+//        eventBytes[idx++] = (byte)(( val1 >> 16 )   & 0xFF );
+//        eventBytes[idx++] = (byte)(( val1 >>  8 )   & 0xFF );
+//        eventBytes[idx++] = (byte)(  val1           & 0xFF );
+//        eventBytes[idx++] = (byte)(( val2 >> 16 )   & 0xFF );
+//        eventBytes[idx++] = (byte)(( val2 >>  8 )   & 0xFF );
+//        eventBytes[idx++] = (byte)(  val2           & 0xFF );
+//        eventBytes[idx++] = (byte)(  Speed          & 0xFF );
+//        eventBytes[idx++] = (byte)(( odometer >> 8) & 0xFF );
+//        eventBytes[idx++] = (byte)(  odometer       & 0xFF );
+//        
+//        
+//        while( keys.hasNext()){
+//    
+//            int key = keys.next();
+//            int value = attrs.get(key);
+//            
+//            if ( key < 128 ){
+//                eventBytes[idx++] = (byte)(  key   & 0xFF );
+//                eventBytes[idx++] = (byte)(  value & 0xFF );                
+//            }
+//            
+//            else if ( key < 192 && key >= 128 ){
+//                eventBytes[idx++] = (byte)(   key           & 0xFF );
+//                eventBytes[idx++] = (byte)( ( value >>  8 ) & 0xFF );
+//                eventBytes[idx++] = (byte)(   value         & 0xFF );                
+//            }
+//            
+//            else if ( key < 255 && key >= 192 ){
+//                eventBytes[idx++] = (byte)(   key           & 0xFF );
+//                eventBytes[idx++] = (byte)( ( value >> 24 ) & 0xFF );
+//                eventBytes[idx++] = (byte)( ( value >> 16 ) & 0xFF );
+//                eventBytes[idx++] = (byte)( ( value >>  8 ) & 0xFF );
+//                eventBytes[idx++] = (byte)(   value         & 0xFF );
+//            }
+//            
+//        }
+//        
+//        return eventBytes;   
+//    }   
+//}
---
+++ automation/src/main/java/com/inthinc/pro/automation/device_emulation/waysmart/WaysmartDevice.java
@@ -0,0 +1,131 @@
+package com.inthinc.QA.waysmart;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import com.inthinc.QA.deviceBase.Base;
+import com.inthinc.QA.hessian.CreateHessian;
+import com.inthinc.QA.util.QALogger;
+import com.inthinc.QA.waysmart.enums.Ways_SETTINGS;
+
+public class WaysmartDevice extends Base {
+	
+	private final static Logger logger = Logger.getLogger(QALogger.class);
+	
+	protected final static Integer productVersion = 2;
+	private CreateHessian hessian;
+
+	public WaysmartDevice(String IMEI, String server, HashMap<Integer, String> settings) {
+		super(IMEI, server, settings, productVersion);
+	}
+	
+	public WaysmartDevice(String IMEI, String server){
+		this(IMEI, server, Waysmart_Defaults.get_defaults());
+	}
+	
+	public WaysmartDevice(String IMEI){
+		this(IMEI, "QA");
+	}
+
+	@Override
+	public void add_location() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add_note() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void construct_note() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void createAckNote(Map<String, Object> reply) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+	@Override
+	protected Integer get_note_count() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String get_setting() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String get_setting(Ways_SETTINGS setting) {
+		return Settings.get(setting.getCode());
+	}
+
+	@Override
+	public Integer get_setting_int() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private Integer get_setting_int(Ways_SETTINGS setting) {
+		return setting.getCode();
+	}
+
+	@Override
+	public Integer processCommand(Map<String, Object> reply) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void set_ignition(Integer timeDelta) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	protected void set_IMEI( String imei, String server, HashMap<Integer, String> settings ){
+		logger.debug("IMEI: "+imei+", Server: " + server);
+		hessian = new CreateHessian();
+        super.set_IMEI(imei, server, settings, productVersion);
+        Settings.put(Ways_SETTINGS.MCM_ID.getCode(), imei);
+        imei = imei.replaceAll("MCM", "WW");
+        Settings.put(Ways_SETTINGS.WITNESS_ID.getCode(), imei);
+    }
+
+	@Override
+	protected void set_power() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void set_server(String server) {
+		mcmProxy = hessian.getMcmProxy(server);
+		String url, port;
+		url = hessian.getUrl(false);
+		port = hessian.getPort(false).toString();
+		Settings.put(get_setting_int(Ways_SETTINGS.SERVER_IP), url+":"+port);
+	}
+
+	@Override
+	public void set_speed_limit(Integer limit) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	protected void was_speeding() {
+		// TODO Auto-generated method stub
+		
+	}
+}
