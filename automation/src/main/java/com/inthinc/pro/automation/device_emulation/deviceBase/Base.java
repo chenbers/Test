d0910709b8676071d0fdc7cc06f080426e5db3d2
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-03-30 01:03:03 +0000

    changes to get build to complete

--- automation/src/main/java/com/inthinc/pro/automation/device_emulation/deviceBase/Base.java
+++ automation/src/main/java/com/inthinc/pro/automation/device_emulation/deviceBase/Base.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
-import org.junit.runner.notification.StoppedByUserException;
+//import org.junit.runner.notification.StoppedByUserException;
 
 //import com.inthinc.QA.hessian.MCMProxy;
 //import com.inthinc.QA.util.QALogger;
@@ -329,7 +329,7 @@ public abstract class Base {
 			}
 			if (reply instanceof Integer){
 				logger.info(reply.toString() + " We failed to send a note");
-				throw new StoppedByUserException();
+				//throw new StoppedByUserException();//TODO: jwimmer: dtanner: commented to get this to compile
 			}
 			else if (reply instanceof ArrayList<?>){
 				ackFwdCmds((List<HashMap<String, Object>>) reply);
