cc90c88b8b5fce765f435b3e77540b712963d4ff
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-04-05 22:36:52 +0000

    commenting out template

--- web/src/test/java/com/inthinc/pro/selenium/testSuites/TemplateTest.java
+++ web/src/test/java/com/inthinc/pro/selenium/testSuites/TemplateTest.java
@@ -1,90 +1,90 @@
-package com.inthinc.pro.selenium.testSuites;
-
-import org.junit.Ignore;
-import org.junit.Test;
-
-import com.inthinc.pro.automation.selenium.DataReaderLib;
-import com.inthinc.pro.selenium.pageObjects.Login;
-import com.inthinc.pro.selenium.pageObjects.Masthead;
-import com.inthinc.pro.selenium.testSuites.WebTest;
-import com.inthinc.pro.web.selenium.portal.Masthead.*;
-
-@Ignore
-public class TemplateTest extends WebTest {
-	//instantiate VAR for data reader
-	DataReaderLib testdata = new DataReaderLib();
-	
-		
-
-	@Test	//Each Test Case shall have a separate function
-	public void TestCaseName1() {
-		//create instance of library objects
-		//Login and Master Heading Screens are done as examples
-		Login l = new Login();
-		Masthead mh = new Masthead();
-		
-		//Set up test data
-		set_test_case("TestCaseName");
-	
-		//login to portal
-//		l.portal_log_in_process(get_data("WorksheetName","ColumnHeader"), get_data("WorksheetName","ColumnHeader"));
-			
-				
-		/*Call Test Methods using the syntax below
-		//ScreenClass.FunctionName
-		//See Examples Below
-		*
-		*
-		*
-		*
-		*
-		*
-		*/
-		mh.section_header_validate();		//Check Header on Master Heading Screen
-		mh.section_footer_validate();		//Check Footer on Master Heading Screen
-		
-		
-		//exit Portal
-		mh.link_logout_click();
-		
-	}
-	
-	@Test	//Each Test Case shall have a separate function
-	public void TestCaseName2() {
-		//create instance of library objects
-		//Login and Master Heading Screens are done as examples
-		Login l = new Login();
-		Masthead mh = new Masthead();
-		
-		//Set up test data
-//		set_test_case("ExcelFileName", "TestCaseName");
-	
-		//login to portal
-//		l.portal_log_in_process(get_data("WorksheetName","ColumnHeader"), get_data("WorksheetName","ColumnHeader"));
-			
-				
-		/*Call Test Methods using the syntax below
-		//ScreenClass.FunctionName
-		//See Examples Below
-		*
-		*
-		*
-		*
-		*
-		*
-		*/
-		mh.section_header_validate();		//Check Header on Master Heading Screen
-		mh.section_footer_validate();		//Check Footer on Master Heading Screen
-		
-		
-		//exit Portal
-		mh.link_logout_click();
-		
-	}
-	
-	
-}
-
-
-
+//package com.inthinc.pro.selenium.testSuites;
+//
+//import org.junit.Ignore;
+//import org.junit.Test;
+//
+//import com.inthinc.pro.automation.selenium.DataReaderLib;
+//import com.inthinc.pro.selenium.pageObjects.Login;
+//import com.inthinc.pro.selenium.pageObjects.Masthead;
+//import com.inthinc.pro.selenium.testSuites.WebTest;
+//import com.inthinc.pro.web.selenium.portal.Masthead.*;
+//
+//@Ignore
+//public class TemplateTest extends WebTest {
+//	//instantiate VAR for data reader
+//	DataReaderLib testdata = new DataReaderLib();
+//	
+//		
+//
+//	@Test	//Each Test Case shall have a separate function
+//	public void TestCaseName1() {
+//		//create instance of library objects
+//		//Login and Master Heading Screens are done as examples
+//		Login l = new Login();
+//		Masthead mh = new Masthead();
+//		
+//		//Set up test data
+//		set_test_case("TestCaseName");
+//	
+//		//login to portal
+////		l.portal_log_in_process(get_data("WorksheetName","ColumnHeader"), get_data("WorksheetName","ColumnHeader"));
+//			
+//				
+//		/*Call Test Methods using the syntax below
+//		//ScreenClass.FunctionName
+//		//See Examples Below
+//		*
+//		*
+//		*
+//		*
+//		*
+//		*
+//		*/
+//		mh.section_header_validate();		//Check Header on Master Heading Screen
+//		mh.section_footer_validate();		//Check Footer on Master Heading Screen
+//		
+//		
+//		//exit Portal
+//		mh.link_logout_click();
+//		
+//	}
+//	
+//	@Test	//Each Test Case shall have a separate function
+//	public void TestCaseName2() {
+//		//create instance of library objects
+//		//Login and Master Heading Screens are done as examples
+//		Login l = new Login();
+//		Masthead mh = new Masthead();
+//		
+//		//Set up test data
+////		set_test_case("ExcelFileName", "TestCaseName");
+//	
+//		//login to portal
+////		l.portal_log_in_process(get_data("WorksheetName","ColumnHeader"), get_data("WorksheetName","ColumnHeader"));
+//			
+//				
+//		/*Call Test Methods using the syntax below
+//		//ScreenClass.FunctionName
+//		//See Examples Below
+//		*
+//		*
+//		*
+//		*
+//		*
+//		*
+//		*/
+//		mh.section_header_validate();		//Check Header on Master Heading Screen
+//		mh.section_footer_validate();		//Check Footer on Master Heading Screen
+//		
+//		
+//		//exit Portal
+//		mh.link_logout_click();
+//		
+//	}
+//	
+//	
+//}
+//
+//
+//
 
