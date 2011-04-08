7731e181093bfe53e0f9afac7e83dfb3d24b09ca
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-04-08 22:09:33 +0000

    refactored RallyTest from InthincTest

--- web/src/test/java/com/inthinc/pro/selenium/pageObjects/UserAddEdit.java
+++ web/src/test/java/com/inthinc/pro/selenium/pageObjects/UserAddEdit.java
@@ -3,14 +3,14 @@ package com.inthinc.pro.selenium.pageObjects;
 import com.inthinc.pro.automation.selenium.CoreMethodLib;
 import com.inthinc.pro.automation.selenium.ErrorCatcher;
 import com.inthinc.pro.automation.selenium.GlobalSelenium;
-import com.inthinc.pro.automation.selenium.InthincTest;
+import com.inthinc.pro.automation.selenium.RallyTest;
 /****************************************************************************************
  * @author : Lee Arrington
  * Purpose: Define methods and objects to enter and manipulate information on Admin - User Screen
  * Last Update:  11/24/10
  ****************************************************************************************/
 
-public class UserAddEdit extends InthincTest {
+public class UserAddEdit extends RallyTest {
 	
 	//Define Class Objects
 	private final String add_user_page = "/tiwipro/app/admin/editPerson";
