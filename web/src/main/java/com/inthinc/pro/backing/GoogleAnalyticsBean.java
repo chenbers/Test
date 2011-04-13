6ec4747a4ecdc087d23a754399344f6fcae39057
mraby <mraby@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-04-13 21:30:49 +0000

    US3176 - added the google analytics javascript to layout.xhtml and properties to tiwipro.properties to configure and disable the script

---
+++ web/src/main/java/com/inthinc/pro/backing/GoogleAnalyticsBean.java
@@ -0,0 +1,22 @@
+package com.inthinc.pro.backing;
+
+public class GoogleAnalyticsBean {
+    private Boolean track;
+    private String account;
+
+    public Boolean getTrack() {
+        return track;
+    }
+
+    public void setTrack(Boolean track) {
+        this.track = track;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+}
