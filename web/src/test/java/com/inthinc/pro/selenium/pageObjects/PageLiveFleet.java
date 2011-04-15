ce1b8a07199efc903d4616227e35c8f2500fb513
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-04-15 16:47:08 +0000

    fix build error

--- web/src/test/java/com/inthinc/pro/selenium/pageObjects/PageLiveFleet.java
+++ web/src/test/java/com/inthinc/pro/selenium/pageObjects/PageLiveFleet.java
@@ -1,9 +1,6 @@
 package com.inthinc.pro.selenium.pageObjects;
 
-import com.inthinc.pro.automation.SeleniumEnum;
 import com.inthinc.pro.automation.selenium.AbstractPage;
-import com.inthinc.pro.automation.selenium.Page;
-import com.inthinc.pro.selenium.pageEnums.LiveFleetEnum;
 
 public class PageLiveFleet extends AbstractPage {
 
