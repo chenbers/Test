c5f5255d98e071af4ca7af33f237070244758949
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-04-18 20:31:10 +0000

    minor fix

--- web/src/test/java/com/inthinc/pro/selenium/pageObjects/MyAccount.java
+++ web/src/test/java/com/inthinc/pro/selenium/pageObjects/MyAccount.java
@@ -2,7 +2,7 @@ package com.inthinc.pro.selenium.pageObjects;
 
 import com.inthinc.pro.selenium.pageEnums.MyAccountEnum;
 
-public class MyAccount extends Masthead {
+public class MyAccount extends NavigationBar {
 
     public static enum RedFlagPrefs {
         EMAIL1("1", MyAccountEnum.EMAIL1_TEXTFIELD, MyAccountEnum.EMAIL1_TITLE),
@@ -25,21 +25,21 @@ public class MyAccount extends Masthead {
             return ID;
         }
 
+        public MyAccountEnum getPrefix() {
+            return prefix;
+        }
+
         public String getValue() {
             return value;
         }
-        
-        public MyAccountEnum getPrefix(){
-            return prefix;
-        }
     };
 
-    public MyAccount button_cancel_click(){
+    public MyAccount button_cancel_click() {
         selenium.click(MyAccountEnum.CANCEL_BUTTON);
         return this;
     }
 
-    public MyAccount button_change_click(){
+    public MyAccount button_change_click() {
         selenium.click(MyAccountEnum.CANCEL_BUTTON);
         return this;
     }
@@ -49,7 +49,7 @@ public class MyAccount extends Masthead {
         return this;
     }
 
-    public MyAccount button_changePasswordX_click(){
+    public MyAccount button_changePasswordX_click() {
         selenium.click(MyAccountEnum.CHANGE_PASSWORD_X);
         return this;
     }
@@ -59,7 +59,7 @@ public class MyAccount extends Masthead {
         return this;
     }
 
-    public MyAccount button_save_click(){
+    public MyAccount button_save_click() {
         selenium.click(MyAccountEnum.SAVE_BUTTON);
         return this;
     }
@@ -72,13 +72,13 @@ public class MyAccount extends Masthead {
     }
 
     public MyAccount dropDown_critical_selectValue(RedFlagPrefs selection) {
-        selenium.select(MyAccountEnum.CRITICAL_SELECT, "index="+selection.getValue());
+        selenium.select(MyAccountEnum.CRITICAL_SELECT, "index=" + selection.getValue());
         String selected = selenium.getSelectedLabel(MyAccountEnum.CRITICAL_SELECT);
         assertEquals(getTextValue(selection), selected);
         return this;
     }
 
-    public MyAccount dropDown_fuelEfficiency_selectText(String selection){
+    public MyAccount dropDown_fuelEfficiency_selectText(String selection) {
         selenium.select(MyAccountEnum.FUEL_EFFICIENCY_SELECT, selection);
         String selected = selenium.getSelectedLabel(MyAccountEnum.FUEL_EFFICIENCY_SELECT);
         assertEquals(selection, selected);
@@ -93,20 +93,20 @@ public class MyAccount extends Masthead {
     }
 
     public MyAccount dropDown_information_selectValue(RedFlagPrefs selection) {
-        selenium.select(MyAccountEnum.INFORMATION_SELECT, "index="+selection.getValue());
+        selenium.select(MyAccountEnum.INFORMATION_SELECT, "index=" + selection.getValue());
         String selected = selenium.getSelectedLabel(MyAccountEnum.INFORMATION_SELECT);
         assertEquals(getTextValue(selection), selected);
         return this;
     }
 
-    public MyAccount dropDown_locale_selectText(String selection){
+    public MyAccount dropDown_locale_selectText(String selection) {
         selenium.select(MyAccountEnum.LOCALE_SELECT, selection);
         String selected = selenium.getSelectedLabel(MyAccountEnum.LOCALE_SELECT);
         assertEquals(selection, selected);
         return this;
     }
 
-    public MyAccount dropDown_measurement_selectText(String selection){
+    public MyAccount dropDown_measurement_selectText(String selection) {
         selenium.select(MyAccountEnum.MEASUREMENT_SELECT, selection);
         String selected = selenium.getSelectedLabel(MyAccountEnum.MEASUREMENT_SELECT);
         assertEquals(selection, selected);
@@ -121,7 +121,7 @@ public class MyAccount extends Masthead {
     }
 
     public MyAccount dropDown_warning_selectValue(RedFlagPrefs selection) {
-        selenium.select(MyAccountEnum.WARNING_SELECT, "index="+selection.getValue());
+        selenium.select(MyAccountEnum.WARNING_SELECT, "index=" + selection.getValue());
         String selected = selenium.getSelectedLabel(MyAccountEnum.WARNING_SELECT);
         assertEquals(getTextValue(selection), selected);
         return this;
@@ -132,10 +132,10 @@ public class MyAccount extends Masthead {
     }
 
     private String getTextValue(RedFlagPrefs selection) {
-    	return selection.getPrefix() + selenium.getText(selection.getID());
+        return selection.getPrefix().getText() + selenium.getText(selection.getID());
     }
 
-    public String label_confirmPassword_getText(){
+    public String label_confirmPassword_getText() {
         return selenium.getText(MyAccountEnum.CHANGE_PASSWORD_CONFIRM_TITLE);
     }
 
@@ -143,7 +143,7 @@ public class MyAccount extends Masthead {
         return selenium.getText(MyAccountEnum.CRITICAL_TITLE);
     }
 
-    public String label_currentPassword_getText(){
+    public String label_currentPassword_getText() {
         return selenium.getText(MyAccountEnum.CHANGE_PASSWORD_CURRENT_TITLE);
     }
 
@@ -179,7 +179,7 @@ public class MyAccount extends Masthead {
         return selenium.getText(MyAccountEnum.NAME_TITLE);
     }
 
-    public String label_newPassword_getText(){
+    public String label_newPassword_getText() {
         return selenium.getText(MyAccountEnum.CHANGE_PASSWORD_NEW_TITLE);
     }
 
@@ -190,7 +190,7 @@ public class MyAccount extends Masthead {
     public String label_phoneNumber2_getText() {
         return selenium.getText(MyAccountEnum.PHONE2_TITLE);
     }
-    
+
     public String label_team_getText() {
         return selenium.getText(MyAccountEnum.TEAM_TITLE);
     }
@@ -211,53 +211,56 @@ public class MyAccount extends Masthead {
         return selenium.getText(MyAccountEnum.WARNING_TITLE);
     }
 
-
-    public MyAccount page_titlesAndLabels_validate(){
+    public MyAccount page_titlesAndLabels_validate() {
         /* Buttons on the main page */
         assertEquals(selenium.getText(MyAccountEnum.CHANGE_PASSWORD_BUTTON), MyAccountEnum.CHANGE_PASSWORD_BUTTON);
         assertEquals(selenium.getText(MyAccountEnum.EDIT_BUTTON), MyAccountEnum.EDIT_BUTTON);
-        
+
         /* Titles for the separate sections */
         assertEquals(title_myAccountMain_getText(), MyAccountEnum.MAIN_TITLE);
         assertEquals(title_accountInformation_getText(), MyAccountEnum.ACCOUNT_TITLE);
         assertEquals(title_loginInformation_getText(), MyAccountEnum.LOGIN_TITLE);
         assertEquals(title_redFlagPreferences_getText(), MyAccountEnum.RED_FLAGS_TITLE);
         assertEquals(title_contactInformation_getText(), MyAccountEnum.CONTACT_TITLE);
-        
+
         /* Labels for the seperate rows */
         /* Account Information */
         assertEquals(label_name_getText(), MyAccountEnum.NAME_TITLE);
         assertEquals(label_group_getText(), MyAccountEnum.GROUP_TITLE);
         assertEquals(label_team_getText(), MyAccountEnum.TEAM_TITLE);
-        
+
         /* Login Information */
         assertEquals(label_userName_getText(), MyAccountEnum.USER_NAME_TITLE);
         assertEquals(label_locale_getText(), MyAccountEnum.LOCALE_TITLE);
         assertEquals(label_measurement_getText(), MyAccountEnum.MEASUREMENT_TITLE);
         assertEquals(label_fuelEfficiency_getText(), MyAccountEnum.FUEL_EFFICIENCY_RATIO_TITLE);
-        
+
         /* Red Flag Preferences */
         assertEquals(label_informationRedFlag_getText(), MyAccountEnum.INFORMATION_TITLE);
         assertEquals(label_warningRedFlag_getText(), MyAccountEnum.WARNING_TITLE);
         assertEquals(label_criticalRedFlag_getText(), MyAccountEnum.CRITICAL_TITLE);
-        
+
         /* Contact Information */
         assertEquals(title_emailAddresses_getText(), MyAccountEnum.EMAIL_TITLE);
         assertEquals(label_emailAddress1_getText(), MyAccountEnum.EMAIL1_TITLE);
         assertEquals(label_emailAddress2_getText(), MyAccountEnum.EMAIL2_TITLE);
-        
+
         assertEquals(title_phoneNumbers_getText(), MyAccountEnum.EMAIL_TITLE);
         assertEquals(label_phoneNumber1_getText(), MyAccountEnum.PHONE1_TITLE);
         assertEquals(label_phoneNumber2_getText(), MyAccountEnum.PHONE2_TITLE);
-        
+
         assertEquals(title_textMessages_getText(), MyAccountEnum.EMAIL_TITLE);
         assertEquals(label_textMessage1_getText(), MyAccountEnum.TEXT_MESSAGES1_TITLE);
         assertEquals(label_textMessage2_getText(), MyAccountEnum.TEXT_MESSAGES2_TITLE);
-        
+
         return this;
     }
 
-    public MyAccount textField_confirmPassword_type(String textToSend){
+    public Boolean popup_changePassword_isVisible() {
+        return selenium.isVisible(MyAccountEnum.CHANGE_PASSWORD_CHANGE);
+    }
+
+    public MyAccount textField_confirmPassword_type(String textToSend) {
         selenium.type(MyAccountEnum.CHANGE_PASSWORD_CONFIRM_TEXTFIELD, textToSend);
         return this;
     }
@@ -266,7 +269,7 @@ public class MyAccount extends Masthead {
         return selenium.getText(MyAccountEnum.CRITICAL_TEXT);
     }
 
-    public MyAccount textField_currentPassword_click(String textToSend){
+    public MyAccount textField_currentPassword_click(String textToSend) {
         selenium.type(MyAccountEnum.CHANGE_PASSWORD_TITLE, textToSend);
         return this;
     }
@@ -313,12 +316,12 @@ public class MyAccount extends Masthead {
         return selenium.getText(MyAccountEnum.NAME_TEXT);
     }
 
-    public MyAccount textField_newPassword_click(String textToSend){
+    public MyAccount textField_newPassword_click(String textToSend) {
         selenium.type(MyAccountEnum.CHANGE_PASSWORD_NEW_TEXTFIELD, textToSend);
         return this;
     }
 
-    public String textField_passwordStrength_getText(){
+    public String textField_passwordStrength_getText() {
         return selenium.getText(MyAccountEnum.CHANGE_PASSWORD_STRENGTH_MSG);
     }
 
@@ -347,69 +350,64 @@ public class MyAccount extends Masthead {
     public String textField_textMessage1_getText() {
         return selenium.getText(MyAccountEnum.TEXT_MESSAGES1_TEXT);
     }
-    
+
     public MyAccount textField_textMessage1_type(String textToType) {
         selenium.type(MyAccountEnum.TEXT_MESSAGES1_TEXTFIELD, textToType);
         return this;
     }
-    
+
     public String textField_textMessage2_getText() {
         return selenium.getText(MyAccountEnum.TEXT_MESSAGES2_TEXT);
     }
-    
+
     public MyAccount textField_textMessage2_type(String textToType) {
         selenium.type(MyAccountEnum.TEXT_MESSAGES2_TEXTFIELD, textToType);
         return this;
     }
-    
+
     public String textField_userName_getText() {
         return selenium.getText(MyAccountEnum.USER_NAME_TEXT);
     }
-    
+
     public String textField_warningRedFlag_getText() {
         return selenium.getText(MyAccountEnum.WARNING_TEXT);
     }
-    
+
     public String title_accountInformation_getText() {
         return selenium.getText(MyAccountEnum.ACCOUNT_TITLE);
     }
-    
+
     public String title_contactInformation_getText() {
         return selenium.getText(MyAccountEnum.CONTACT_TITLE);
     }
-    
+
     public String title_emailAddresses_getText() {
         return selenium.getText(MyAccountEnum.EMAIL_TITLE);
     }
-    
+
     public String title_loginInformation_getText() {
         return selenium.getText(MyAccountEnum.LOGIN_TITLE);
     }
-    
+
     public String title_myAccountMain_getText() {
         return selenium.getText(MyAccountEnum.MAIN_TITLE);
     }
-    
+
     public String title_phoneNumbers_getText() {
         return selenium.getText(MyAccountEnum.PHONE_TITLE);
     }
-    
+
     public String title_redFlagPreferences_getText() {
         return selenium.getText(MyAccountEnum.RED_FLAGS_TITLE);
     }
-    
-    
+
     public String title_textMessages_getText() {
         return selenium.getText(MyAccountEnum.TEXT_TITLE);
     }
-    
+
     public MyAccount validate() {
         page_titlesAndLabels_validate();
         return this;
     }
-    
-    public Boolean popup_changePassword_isVisible(){
-        return selenium.isVisible(MyAccountEnum.CHANGE_PASSWORD_CHANGE);
-    }
-    
+
 }
