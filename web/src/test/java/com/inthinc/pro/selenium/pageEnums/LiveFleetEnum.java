9bff1c729e46462292b918a42c3dac40868f357b
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-04-25 21:55:14 +0000

    US3056: attempt at a more segmented way of getting from pageObject, ... , to action

--- web/src/test/java/com/inthinc/pro/selenium/pageEnums/LiveFleetEnum.java
+++ web/src/test/java/com/inthinc/pro/selenium/pageEnums/LiveFleetEnum.java
@@ -4,16 +4,49 @@ import com.inthinc.pro.automation.enums.SeleniumEnums;
 
 // Enums have format NAME( Text, ID, X-Path, X-Path-Alternate )
 
-public enum LiveFleetEnum implements SeleniumEnums {
+public enum LiveFleetEnum  implements SeleniumEnums {
 	
-    DEFAULT_URL("liveFleet"),
+    DEFAULT_URL("app/liveFleet"),
     
 	/* Main LiveFleet Page Elements*/
-	TABLE_DRIVER_DATA(null, "driversDataTablePanel", null, null),
-	LABEL_DISPATCH("Dispatch",null,"//div[@id=wrapper]/table/tbody/tr[1]/td[1]/div/div/span",null ),
-	USERNAME_LABEL("User Name:", null, "//form[@id='loginForm']/table/tbody/tr[1]/td[1]", null),
+	HEADER_BOX_DISPATCH("Dispatch",null,"//div[@id=wrapper]/table/tbody/tr[1]/td[1]/div/div/span",null ),
+	LINK_SORT_DISPATCH_BY_NUMBER("#", null, "//div[@id='dispatchForm:driversDataTable:positionheader:sortDiv']/span", null),
+	LINK_SORT_DISPATCH_BY_DRIVER("Driver", null, "//div[@id='dispatchForm:driversDataTable:driverheader:sortDiv']/span",  null),
+	LINK_SORT_DISPATCH_BY_VEHICLE("Vehicle", null, "//div[@id='dispatchForm:driversDataTable:vehicleheader:sortDiv']/span", null),
+	LINK_SORT_DISPATCH_BY_GROUP(null, null, "//div[@id='dispatchForm:driversDataTable:groupheader:sortDiv']/span/img", null),
+	LINK_DISPATCH_DRIVER_NEED_INDEX("_VAR_DRIVER_NAME_", null, "dispatchForm:driversDataTable:_INDEX_:liveFleet-driverPerformance", null),
+	LINK_DISPATCH_VEHICLE_NEED_INDEX("_VAR_VEHICLE_NAME_", null, "dispatchForm:driversDataTable:_INDEX_:liveFleetsVehiclePerformance", null),
+	LINK_DISPATCH_ICON_NEED_INDEX("_VAR_GROUP_ICON_", null, "//tr[_INDEX_]/td[4]/a/img", null), //TODO: jwimmer: find the best way to pull/insert indexes/variables into xpaths
 	
-	LINK_CHANGE_DEFAULT_VIEW("change", "liveFleetMapForm:liveFleetGo", null, null)
+	
+	HEADER_BOX_LIVE_FLEET("Live Fleet", null, "//div[@id='defaultMessage']/h2", null),
+	LINK_LIVE_FLEET_REFRESH(null, null, "//img[@alt='Refresh']", null),
+	TEXTFIELD_LIVE_FLEET_FIND_ADDRESS(null, "liveFleetAddressTextBox", null, null),
+	DROPDOWN_LIVE_FLEET_NUM_NEAREST_VEHICLES(null, "countComboBox", null, null),
+	BUTTON_LIVE_FLEET_LOCATE("Locate", "liveFleetSearch", null, null),
+	LINK_MAP_BUBBLE_DEFAULT_CHANGE_VIEW("change", "liveFleetMapForm:liveFleetGo", null, null),
+	TEXT_MAP_BUBBLE_DEFAULT_MESSAGE("This is the default view for your division.", "defaultMessage", null, null ),
+	TEXT_MAP_BUBBLE_HEADER("Live Fleet", null, "//div[@id='defaultMessage']/h2", null),
+	MAP_BUBBLE_VEHICLE_HEADER(null, null, null, null),
+	LABEL_MAP_BUBBLE_VEHICLE_PHONE1("Phone 1:", null, "//div[10]/div/div[1]/div/div/table/tbody/tr[1]/td[1]", null),
+	LABEL_MAP_BUBBLE_VEHICLE_PHONE2("Phone 2:", null, "//div[10]/div/div[1]/div/div/table/tbody/tr[2]/td[1]", null),
+	LABEL_MAP_BUBBLE_VEHICLE_DRIVER("Driver:", null, "//div[10]/div/div[1]/div/div/table/tbody/tr[3]/td[1]", null),
+	LABEL_MAP_BUBBLE_VEHICLE_DEVICE("Device:", null, "//div[10]/div/div[1]/div/div/table/tbody/tr[4]/td[1]", null),
+	LABEL_MAP_BUBBLE_VEHICLE_UPDATED("Updated:", null, "//div[10]/div/div[1]/div/div/table/tbody/tr[5]/td[1]", null),
+	LABEL_MAP_BUBBLE_VEHICLE_LOCATION("Locateion", null, "//div[10]/div/div[1]/div/div/table/tbody/tr[6]/td[1]", null),
+	LABEL_MAP_BUBBLE_VEHICLE_DISTANCE_TO_ADDRESS("Distance to address:", null, "//div[10]/div/div[1]/div/div/table/tbody/tr[7]/td[1]", null),
+    VALUE_MAP_BUBBLE_VEHICLE_PHONE1("Phone 1:", null, "//div[10]/div/div[1]/div/div/table/tbody/tr[1]/td[2]", null),
+    VALUE_MAP_BUBBLE_VEHICLE_PHONE2("Phone 2:", null, "//div[10]/div/div[1]/div/div/table/tbody/tr[2]/td[2]", null),
+    VALUE_MAP_BUBBLE_VEHICLE_DRIVER("Driver:", null, "//div[10]/div/div[1]/div/div/table/tbody/tr[3]/td[2]", null),
+    VALUE_MAP_BUBBLE_VEHICLE_DEVICE("Device:", null, "//div[10]/div/div[1]/div/div/table/tbody/tr[4]/td[2]", null),
+    VALUE_MAP_BUBBLE_VEHICLE_UPDATED("Updated:", null, "//div[10]/div/div[1]/div/div/table/tbody/tr[5]/td[2]", null),
+    VALUE_MAP_BUBBLE_VEHICLE_LOCATION("Location", null, "//div[10]/div/div[1]/div/div/table/tbody/tr[6]/td[2]", null),
+    VALUE_MAP_BUBBLE_VEHICLE_DISTANCE_TO_ADDRESS("Distance to address:", null, "//div[10]/div/div[1]/div/div/table/tbody/tr[7]/td[2]", null),
+	IMG_LINK_MAP_ICONS_NEED_INDEX(null, null, "mtgt_unnamed__INDEX_", null),
+    
+    HEADER_BOX_FLEET_LEGEND("Fleet Legend", null, "//div[@id='wrapper']/table/tbody/tr/td[3]/div[3]/div[1]/div/span[1]", null),
+    LINK_BOX_FLEET_LEGEND_GROUP_NEED_INDEX(null, "liveFleetLegend:_INDEX_:liveFleetsDashboard2", null, null)
+	
 	;
 	
 	
--- web/src/test/java/com/inthinc/pro/selenium/pageObjects/PageLiveFleet.java
+++ web/src/test/java/com/inthinc/pro/selenium/pageObjects/PageLiveFleet.java
@@ -1,7 +1,61 @@
 package com.inthinc.pro.selenium.pageObjects;
 
+import com.inthinc.pro.automation.elements.SelectBox;
+import com.inthinc.pro.automation.elements.Text;
+import com.inthinc.pro.automation.elements.TextButton;
+import com.inthinc.pro.automation.elements.TextField;
+import com.inthinc.pro.automation.elements.TextLink;
+import com.inthinc.pro.selenium.pageEnums.LiveFleetEnum;
 
 public class PageLiveFleet extends NavigationBar {
+    public LiveFleetTextFields _textField = new LiveFleetTextFields();
+    public LiveFleetTexts _text = new LiveFleetTexts();
+    public LiveFleetLinks _link = new LiveFleetLinks();
+    public LiveFleetButtons _button = new LiveFleetButtons();
+    public LiveFleetSelects _select = new LiveFleetSelects();
+   
+   
+    public class LiveFleetLinks{
+        public TextLink sortDispatchByNumber(){ return new TextLink(selenium, LiveFleetEnum.LINK_SORT_DISPATCH_BY_NUMBER); }
+        public TextLink sortDispatchByDriver(){ return new TextLink(selenium, LiveFleetEnum.LINK_SORT_DISPATCH_BY_DRIVER); }
+        public TextLink sortDispatchByVehicle(){ return new TextLink(selenium, LiveFleetEnum.LINK_SORT_DISPATCH_BY_VEHICLE); }
+        public TextLink sortDispatchByGroup(){ return new TextLink(selenium, LiveFleetEnum.LINK_SORT_DISPATCH_BY_GROUP); }
+      //TODO: jwimmer: discuss with team: additional instances of Link objects on this page would go here
+    }
+    public class LiveFleetTexts{
+        public Text mapBubbleDefaultChangeView(){return new Text(selenium, LiveFleetEnum.TEXTFIELD_LIVE_FLEET_FIND_ADDRESS);}
+        public Text liveFleetHeader(){return new Text(selenium, LiveFleetEnum.HEADER_BOX_DISPATCH);}
+        public Text labelVehiclePhone1(){return new Text(selenium, LiveFleetEnum.LABEL_MAP_BUBBLE_VEHICLE_PHONE1);};
+        public Text labelVehiclePhone2(){return new Text(selenium, LiveFleetEnum.LABEL_MAP_BUBBLE_VEHICLE_PHONE2);};
+        public Text labelVehicleDriver(){return new Text(selenium, LiveFleetEnum.LABEL_MAP_BUBBLE_VEHICLE_DRIVER);};
+        public Text labelVehicleDevice(){return new Text(selenium, LiveFleetEnum.LABEL_MAP_BUBBLE_VEHICLE_DEVICE);};
+        public Text labelVehicleUpdated(){return new Text(selenium, LiveFleetEnum.LABEL_MAP_BUBBLE_VEHICLE_UPDATED);};
+        public Text labelVehicleLocation(){return new Text(selenium, LiveFleetEnum.LABEL_MAP_BUBBLE_VEHICLE_LOCATION);};
+        public Text labelVehicleDistToAddress(){return new Text(selenium, LiveFleetEnum.LABEL_MAP_BUBBLE_VEHICLE_DISTANCE_TO_ADDRESS);};
 
-
+        //TODO: jwimmer: discuss with team: this begs the question, is there a beneficial naming convention for a piece of text that we KNOW is going to be dynamic from the database?
+        public Text valueVehiclePhone1(){return new Text(selenium, LiveFleetEnum.VALUE_MAP_BUBBLE_VEHICLE_PHONE1);};
+        public Text valueVehiclePhone2(){return new Text(selenium, LiveFleetEnum.VALUE_MAP_BUBBLE_VEHICLE_PHONE2);};
+        public Text valueVehicleDriver(){return new Text(selenium, LiveFleetEnum.VALUE_MAP_BUBBLE_VEHICLE_DRIVER);};
+        public Text valueVehicleDevice(){return new Text(selenium, LiveFleetEnum.VALUE_MAP_BUBBLE_VEHICLE_DEVICE);};
+        public Text valueVehicleUpdated(){return new Text(selenium, LiveFleetEnum.VALUE_MAP_BUBBLE_VEHICLE_UPDATED);};
+        public Text valueVehicleLocation(){return new Text(selenium, LiveFleetEnum.VALUE_MAP_BUBBLE_VEHICLE_LOCATION);};
+        public Text valueVehicleDistToAddress(){return new Text(selenium, LiveFleetEnum.VALUE_MAP_BUBBLE_VEHICLE_DISTANCE_TO_ADDRESS);};
+        
+      //TODO: jwimmer: discuss with team: additional instances of Text objects on this page would go here
+    }
+    public class LiveFleetTextFields{
+        public TextField findAddress(){return new TextField(selenium, LiveFleetEnum.TEXTFIELD_LIVE_FLEET_FIND_ADDRESS);};
+        //TODO: jwimmer: discuss with team: additional instances of TextField objects on this page would go here
+    }
+    public class LiveFleetButtons{
+        public TextButton locate(){return new TextButton(selenium, LiveFleetEnum.BUTTON_LIVE_FLEET_LOCATE);};
+    }
+    public class LiveFleetSelects{
+        public SelectBox numNearestVehicles(){return new SelectBox(selenium, LiveFleetEnum.DROPDOWN_LIVE_FLEET_NUM_NEAREST_VEHICLES);};
+    }    
+    
+    public String getExpectedPath() {
+        return LiveFleetEnum.DEFAULT_URL.getURL();
+    }
 }
