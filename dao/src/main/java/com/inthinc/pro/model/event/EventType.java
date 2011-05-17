e64a82329821f9887ff6ce32d9e7998561d2faab
pwehan <pwehan@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-05-17 19:22:47 +0000

    Clean up class.

--- dao/src/main/java/com/inthinc/pro/model/event/EventType.java
+++ dao/src/main/java/com/inthinc/pro/model/event/EventType.java
@@ -51,8 +51,7 @@ public enum EventType implements BaseEnum {
     HOS_NO_HOURS(35, EnumSet.of(EventAccountFilter.HOS)),
     TEXT_MESSAGE(36, EnumSet.of(EventAccountFilter.WAYSMART)),
     OFF_HOURS(37, EnumSet.of(EventAccountFilter.WAYSMART)),
-    IGNITION_OFF(38),
-    RF_SWITCH(39, EnumSet.of(EventAccountFilter.WAYSMART));
+    IGNITION_OFF(38);
     
     private int code;
     private AggressiveDrivingEventType noteSubType;
