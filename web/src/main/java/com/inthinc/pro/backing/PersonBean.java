193b2c86fd3c26cdbd5e5c07b63326089ad3def5
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-05-18 18:52:03 +0000

    de6557: ta6949: updated password reminder to only render once per login

--- web/src/main/java/com/inthinc/pro/backing/PersonBean.java
+++ web/src/main/java/com/inthinc/pro/backing/PersonBean.java
@@ -23,6 +23,7 @@ import java.util.TreeMap;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.faces.model.SelectItem;
 
 import org.apache.commons.lang.StringUtils;
@@ -284,9 +285,15 @@ public class PersonBean extends BaseAdminBean<PersonBean.PersonView> implements 
     public boolean isPasswordUpdated() {
         return passwordUpdated;
     }
-
     public void setPasswordUpdated(boolean passwordUpdated) {
         this.passwordUpdated = passwordUpdated;
+    }
+    public String ignorePasswordWarning() {
+        this.passwordUpdated = true;
+        return "Set password as updated (for this login) to avoid passwordReminder rendering on ";
+    }
+    public boolean isShowPasswordReminder() {
+        return (((getPasswordDaysRemaining() < 16)  || ((isInitialLogin()) && (isPasswordChangeWarn())))  && !isPasswordUpdated());
     }
     public static Integer getLoginDaysRemaining(Account account, User user) {
         Integer loginExpire;
