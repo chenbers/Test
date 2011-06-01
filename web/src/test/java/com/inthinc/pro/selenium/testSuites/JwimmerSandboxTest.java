3208091638c48245f456e3dfb136ce7715686388
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-06-01 06:28:46 +0000

    automation: adding timeouts for tests

--- web/src/test/java/com/inthinc/pro/selenium/testSuites/JwimmerSandboxTest.java
+++ web/src/test/java/com/inthinc/pro/selenium/testSuites/JwimmerSandboxTest.java
@@ -16,7 +16,7 @@ import com.inthinc.pro.selenium.pageObjects.PageLogin;
 public class JwimmerSandboxTest extends WebTest {
     private PageLogin l;
     private PageLiveFleet liveFleet;
-    private String NONADMIN_USERNAME = "jwimmer"; //TODO: jwimmer: dtanner: more candidates for non-page specific enum???
+    private String NONADMIN_USERNAME = "jwimmer"; //TODO: jwimmer: dtanner: users/password The problem is with test cases using the same user they CAN end up stomping on each other and inadvertently fail/pass the test
     private String NONADMIN_PASSWORD = "password";
     private String ADMIN_USERNAME = "mraby";
     private String ADMIN_PASSWORD = "password";	
@@ -43,7 +43,7 @@ public class JwimmerSandboxTest extends WebTest {
     	liveFleet = new PageLiveFleet();
     }
 
-    @Test
+    @Test(timeout=300000)
     public void liveFleet_byNavBar_openPage() {
         l.loginProcess("jwimmer", "password");
         liveFleet._link().liveFleet().click();
@@ -51,19 +51,17 @@ public class JwimmerSandboxTest extends WebTest {
         liveFleet._textField().findAddress().type("Selenium navigated here by clicking on the liveFleet link... page._link.liveFleet.click() ");
     }
     
-    @Test
+    @Test(timeout=300000)
     public void liveFleet_clickHelp_newWindow() {
         l.loginProcess("jwimmer", "password");
         liveFleet._link().liveFleet().click();//TODO: jwimmer: could this be failing on EC2 only, because the pageObject after loginProcess hasn't loaded yet???
         System.out.println("help().getText(): "+liveFleet._link().help().getText());
-        liveFleet._link().help().click();//TODO: jwimmer: question for dTanner: are we sure that the textLinkNewWindow.click ever works?
-        //TODO: jwimmer: validate help page/window opened
         
-        //pause(15);//TODO: jwimmer: prop integer value for watching? and put in teardown
+        liveFleet._link().help().click();
     }
     
     @Ignore
-    @Test
+    @Test(timeout=300000)
     public void liveFleet_sandbox_taeThrownError() {
         int waitTime = 6;
         System.out.println("liveFleet_sandbox_taeThrownError: ");
@@ -123,44 +121,36 @@ public class JwimmerSandboxTest extends WebTest {
         //pause(60);
     }
     
-    @Test
+    @Test(timeout=300000)
     public void liveFleet_changeDefaultViewAsNONAdmin_shouldNotSeeLink() {
-    	//TODO: jwimmer: pwehan: dtanner: discuss if this method shows justification of page_bareMinimum_validate()
-    	//liveFleet.page_loginFirst_open(NONADMIN_USERNAME, NONADMIN_PASSWORD);//TODO: jwimmer: I still think there is benefit to page.loadLoginFirst(username, password) or something similar... consider this for automation backlog post sprint 22
         l.load();
         l.loginProcess(NONADMIN_USERNAME, NONADMIN_PASSWORD);
     	liveFleet.page_bareMinimum_validate();//only continue if the page meets bare minimum this step has more validity in LONGER tests... where we want to fail the test as early as possible
     	//assertTrue(!liveFleet.page_admin_validate()); //NON-ADMIN users should NOT see the change link
     }
     
-    @Test
+    @Test(timeout=300000)
     public void liveFleet_changeDefaultViewAsAdmin_canChange() {
-    	//TODO: jwimmer: pwehan: dtanner: discuss if this method shows justification of page_bareMinimum)_validate
-    	//liveFleet.page_loginFirst_open(ADMIN_USERNAME, ADMIN_PASSWORD);
         l.load();
         l.loginProcess(ADMIN_USERNAME, ADMIN_PASSWORD);
     	liveFleet.page_bareMinimum_validate();//only continue if the page meets bare minimum
     	//assertTrue(liveFleet.page_admin_validate()); //NON-ADMIN users should NOT see the change link
     }
     
-    @Test
+    @Test(timeout=300000)
     public void liveFleet_fromBookmarkNotLoggedIn_mustLoginFirst() {
-    	//TODO: jwimmer: pwehan: dtanner: discuss if this method shows justification of page_directURL_load()
-    	//TODO: jwimmer: pwehan: dtanner: discuss if this method shows justification of page_URL_validate()
-    	//TODO: jwimmer: pwehan: dtanner: discuss if this method shows justification of page_bareMinimum_validate()
+    	//TODO: jwimmer: dtanner: discuss if this method shows justification of page_directURL_load(String password, String usernamer)... NOT needed, we want a bookmark that returns a STRING that is the URL, that can later be used by some other method navigateTOBookmark
+    	//TODO: jwimmer: dtanner: discuss if this method shows justification of page.validate()... it shouldn't be called VALIDATE unless actually validates EVERYTHING expected to be on the page.  a better sanity check is something more like "didPageLoad()" which merely checks the URL for correctness (expected) AND makes sure the appError element in NOT present
         PageLogin login = new PageLogin(); //going to use this later
     	liveFleet.load();
     	//liveFleet.validateURL();  //this WOULD fail the test since the browser should NOT automatically go to the liveFleet page without requiring login.
     	login.validateURL();	//test should fail UNLESS the login page was loaded (since the user has not logged in yet)
-    	
-    	login.loginProcess("jwimmer","password");
     	liveFleet.validateURL();   //test should fail on assertion of any page except liveFleet is loaded
     	liveFleet.validate();      //validate the liveFleet page... just because we can.
     }
     
-    @Test
+    @Test(timeout=300000)
     public void liveFleet_fromBookmarkLoggedIn_goDirectlyToLiveFleet() {
-    	//TODO: jwimmer: current loginProcess(String, String) does NOT function for bookmarked pages...
     	l.load();
     	l.page_bareMinimum_validate();
     	l.loginProcess("jwimmer", "password");
@@ -171,7 +161,7 @@ public class JwimmerSandboxTest extends WebTest {
     }
     
     
-    @Test
+    @Test(timeout=300000)
     public void login_nullUsernamePassword_appError() {
         l.load();
         l.validate();
@@ -179,7 +169,7 @@ public class JwimmerSandboxTest extends WebTest {
         l._popUp().loginError()._text().message().validate("Incorrect user name or password.\n\nPlease try again.");
     }
 
-    @Test
+    @Test(timeout=300000)
     public void login_closeBadCredModal_noModal() {
         l.load();
         l.validate();
@@ -196,7 +186,7 @@ public class JwimmerSandboxTest extends WebTest {
         l._popUp().forgotPassword()._button().send().click();
     }
 
-    @Test
+    @Test(timeout=300000)
     public void forgotPassword_badEmailManual_incorrectFormat() {
         l.load();
         l._link().forgotUsernamePassword().click();
@@ -206,32 +196,31 @@ public class JwimmerSandboxTest extends WebTest {
         l._popUp().forgotPassword()._text().error().validate("Incorrect format (jdoe@tiwipro.com)");
     }
 
-    @Test
+    @Test(timeout=300000)
     public void forgotPassword_badEmail_incorrectFormat() {
         forgotPassword_Scenario_enterEmailClickSend(EMAIL_INVALID);
         l._popUp().forgotPassword()._text().error().validate("Incorrect format (jdoe@tiwipro.com)");
-        //TODO: jwimmer: PopUps do not extend BaseElement so we LOST the ability to validate();
     }
 
-    @Test
+    @Test(timeout=300000)
     public void forgotPassword_noEmail_required() {
         forgotPassword_Scenario_enterEmailClickSend(null);
         l._popUp().forgotPassword()._text().error().validate("Required");
     }
 
-    @Test
+    @Test(timeout=300000)
     public void forgotPassword_unknownEmail_incorrect() {
         forgotPassword_Scenario_enterEmailClickSend(EMAIL_UNKNOWN);
         l._popUp().forgotPassword()._text().error().validate("Incorrect e-mail address"); 
     }
 
-    @Test
+    @Test(timeout=300000)
     public void forgotPassword_usersEmail_success() {
         forgotPassword_Scenario_enterEmailClickSend(EMAIL_KNOWN);
-        //l._popUp().messageSent().validate(); //TODO: jwimmer: follow up with dTanner about PopUps extending BaseElement, circle back and fix test
+        l._popUp().messageSent();
     }
 
-    @Test
+    @Test(timeout=300000)
     public void forgotPassword_cancel_closePopup() {
         l.load();
         l._link().forgotUsernamePassword().click(); System.out.println("1");
@@ -240,6 +229,7 @@ public class JwimmerSandboxTest extends WebTest {
         l._popUp().forgotPassword()._text().title().validate(); System.out.println("3");
         //l._popUp().forgotPassword()._text().label().validate();  //TODO: jwimmer: to dTanner: FYI, it looks like this CHOKES
         
+        //jason, see if this is now checking during the "click()" action
         l._popUp().forgotPassword()._button().cancel().click();//TODO: jwimmer: to dTanner: this one fails gracefully (but probably shouldn't fail?)
         //l.validate();  //TODO: jwimmer: need/want a way to fail the test if the forgotPassword popup is STILL open
         pause(20, "did the last click() action work ?");System.out.println("6");
@@ -247,13 +237,13 @@ public class JwimmerSandboxTest extends WebTest {
         pause(20, "end of test pause");
     }
 
-    @Test
+    @Test(timeout=300000)
     public void login_forgotPassClose_closePopup() {
         l.load();
         l.validate();
         l._link().forgotUsernamePassword().click();
-        //l._popUp().forgotPassword().validate();//TODO: jwimmer: replace after PopUps extend BaseElement?
         pause(10, "waiting for forgotUsernamePassword popup to open");
+        //TODO: jwimmer: double check the next line jason
         l._popUp().forgotPassword()._button().close().click();//TODO: jwimmer: to dTanner: this one fails gracefully (but probably shouldn't fail?)
         l.validate();
     }
