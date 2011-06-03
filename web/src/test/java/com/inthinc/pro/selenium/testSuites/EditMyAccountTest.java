2723ed2636af3f71f3a438f77f46f0e4ed3dada3
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-06-03 21:26:36 +0000

    automation: test fixes, pulled USERNAME and PASSWORD into single variables

--- web/src/test/java/com/inthinc/pro/selenium/testSuites/EditMyAccountTest.java
+++ web/src/test/java/com/inthinc/pro/selenium/testSuites/EditMyAccountTest.java
@@ -14,6 +14,8 @@ public class EditMyAccountTest extends WebRallyTest {
 
 	private PageMyAccount myAccountPage;
 	private RandomValues random;
+	private String USERNAME = "tnilson";
+	private String PASSWORD = "password123";
 
 	@Before
 	public void setupPage() {
@@ -22,111 +24,9 @@ public class EditMyAccountTest extends WebRallyTest {
 	}
 
 	@Test
-	public void CancelButton_Changes() {
-		set_test_case("TC1271");
-		myAccountPage.loginProcess("tnilson", "password123");
-		myAccountPage._link().myAccount().click();
-		/* Get original Values */
-		String originalEmail1 = myAccountPage._text().email1().getText();
-		String originalEmail2 = myAccountPage._text().email2().getText();
-		
-		String originalPhone1 = myAccountPage._text().phone1().getText();
-		String originalPhone2 = myAccountPage._text().phone2().getText();
-		
-		String originalText1 = myAccountPage._text().textMessage1().getText();
-		String originalText2 = myAccountPage._text().textMessage2().getText();
-		
-		
-		String originalCritical = myAccountPage._text().redFlagCritical().getText();
-		String originalWarning = myAccountPage._text().redFlagWarn().getText();
-		String originalInformation = myAccountPage._text().redFlagInfo().getText();
-		
-		String originalLocale = myAccountPage._text().locale().getText();
-		String originalMeasurement = myAccountPage._text().measurement().getText();
-		String originalFuelRatio = myAccountPage._text().fuelEfficiency().getText();
-		
-		
-		/* Edit button */
-		myAccountPage._button().edit().click();
-
-		/* Login Info */
-		myAccountPage._select().locale().select(Locale.ENGLISH);
-		myAccountPage._select().measurement().select(Measurement.METRIC);
-		myAccountPage._select().fuelEfficiency().select(Fuel_Ratio.METRIC_LITER_PER_KILO);
-
-		/* Contact Info */
-		myAccountPage._textField().email1().type(random.getEmail());
-		myAccountPage._textField().email2().type(random.getEmail());
-		myAccountPage._textField().phone1().type(random.getPhoneNumber());
-		myAccountPage._textField().phone2().type(random.getPhoneNumber());
-		myAccountPage._textField().textMessage1().type(random.getTextMessageNumber());
-		myAccountPage._textField().textMessage2().type(random.getTextMessageNumber());
-
-		/* Red Flags */
-		myAccountPage._select().information().select(random.getEnum(RedFlagPrefs.TEXT1, originalInformation));
-		myAccountPage._select().warning().select(random.getEnum(RedFlagPrefs.TEXT1, originalWarning));
-		myAccountPage._select().critical().select(random.getEnum(RedFlagPrefs.TEXT1, originalCritical));
-
-		/* Cancel Changes */
-		myAccountPage._button().cancel().click();
-
-		/* Verify Changes did not take effect */
-		/* Login Info */        
-		myAccountPage._text().userName().validate("tnilson");
-		myAccountPage._text().locale().validate(originalLocale);
-		myAccountPage._text().measurement().validate(originalMeasurement);
-		myAccountPage._text().fuelEfficiency().validate(originalFuelRatio);
-
-		/* Account Info */
-		myAccountPage._text().name().validate("Tina L Nilson");
-		myAccountPage._text().group().validate("Top");
-		myAccountPage._text().team().validate("Skip's Team");
-
-		/* Red Flags */
-		myAccountPage._text().redFlagInfo().validate(originalInformation);
-		myAccountPage._text().redFlagWarn().validate(originalWarning);
-		myAccountPage._text().redFlagCritical().validate(originalCritical);
-
-		/* Contact Info */
-		myAccountPage._text().email1().validate(originalEmail1);
-		myAccountPage._text().email2().validate(originalEmail2);
-		myAccountPage._text().phone1().validate(originalPhone1);
-		myAccountPage._text().phone2().validate(originalPhone2);
-		myAccountPage._text().textMessage1().validate(originalText1);
-		myAccountPage._text().textMessage2().validate(originalText2);
-	}
-
-	
-
-	@Test
-	public void EmailFormatError() {
-		set_test_case("TC1271");//TODO: find the actual testcase for this
-		myAccountPage.loginProcess("tnilson", "password");
-		myAccountPage._link().myAccount().click();
-
-		myAccountPage._button().edit().click();
-		myAccountPage._textField().email1().type("tina1965test.com");
-		myAccountPage._button().save().click();
-
-		// Clear?//
-		myAccountPage._textField().email1().type("tina1965@test.com");
-		myAccountPage._button().save().click();
-		myAccountPage._text().email1().validate("tina1965@test.com");
-
-		myAccountPage._button().edit().click();
-		myAccountPage._textField().email2().type("tlc1965@test");
-		myAccountPage._button().save().click();
-		// Validate Error//
-		// Clear?//
-		myAccountPage._textField().email2().type("tlc1965@test.com");
-		myAccountPage._button().save().click();
-		myAccountPage._text().email2().validate("tlc1965@test.com");
-	}
-
-	@Test
 	public void MeasurementValidation() {
 		set_test_case("TC1275");
-		myAccountPage.loginProcess("tnilson", "password");
+		myAccountPage.loginProcess(USERNAME, PASSWORD);
 		myAccountPage._link().myAccount().click();
 		
 
@@ -149,7 +49,7 @@ public class EditMyAccountTest extends WebRallyTest {
 	@Test
 	public void FuelRatioValidation() {
 		set_test_case("TC1273");
-		myAccountPage.loginProcess("tnilson", "password");
+		myAccountPage.loginProcess(USERNAME, PASSWORD);
 		myAccountPage._link().myAccount().click();
 
 		myAccountPage._button().edit().click();
@@ -169,7 +69,7 @@ public class EditMyAccountTest extends WebRallyTest {
 	@Test
 	public void ClearFieldsValidation() {
 		set_test_case("TC1276");
-		myAccountPage.loginProcess("tnilson", "password");
+		myAccountPage.loginProcess(USERNAME, PASSWORD);
 		myAccountPage._link().myAccount().click();
 
 		myAccountPage._button().edit().click();
@@ -193,7 +93,7 @@ public class EditMyAccountTest extends WebRallyTest {
 	@Test
 	public void PhoneMaxCharError() {
 		set_test_case("TC1277");
-		myAccountPage.loginProcess("tnilson", "password");
+		myAccountPage.loginProcess(USERNAME, PASSWORD);
 		myAccountPage._link().myAccount().click();
 		// my.link_myAccount().click();
 		myAccountPage._button().edit().click();
@@ -218,7 +118,7 @@ public class EditMyAccountTest extends WebRallyTest {
 	@Test
 	public void PhoneMissingCharError() {
 		set_test_case("TC1278");
-		myAccountPage.loginProcess("tnilson", "password");
+		myAccountPage.loginProcess(USERNAME, PASSWORD);
 		myAccountPage._link().myAccount().click();
 		// my.link_myAccount().click();
 
@@ -244,7 +144,7 @@ public class EditMyAccountTest extends WebRallyTest {
 	@Test
 	public void PhoneSpecialCharError() {
 		set_test_case("TC1279");
-		myAccountPage.loginProcess("tnilson", "password");
+		myAccountPage.loginProcess(USERNAME, PASSWORD);
 		myAccountPage._link().myAccount().click();
 		// my.link_myAccount().click();
 		myAccountPage._button().edit().click();
@@ -270,7 +170,7 @@ public class EditMyAccountTest extends WebRallyTest {
 	@Test
 	public void TextMsgFormatError() {
 		set_test_case("TC1282");
-		myAccountPage.loginProcess("tnilson", "password");
+		myAccountPage.loginProcess(USERNAME, PASSWORD);
 		myAccountPage._link().myAccount().click();
 
 		myAccountPage._button().edit().click();
@@ -294,7 +194,7 @@ public class EditMyAccountTest extends WebRallyTest {
 	@Test
     public void SaveButton_Changes() {
         set_test_case("TC1280");
-        myAccountPage.loginProcess("tnilson", "password");
+        myAccountPage.loginProcess(USERNAME, PASSWORD);
         myAccountPage._link().myAccount().click();
 
         /* Edit button */
@@ -324,7 +224,7 @@ public class EditMyAccountTest extends WebRallyTest {
         myAccountPage.getSelenium().pause(10, "wait for page to save");
         /* Verify Changes Display */
         /* Login Info */
-        myAccountPage._text().userName().validate("tnilson");
+        myAccountPage._text().userName().validate(USERNAME);
         myAccountPage._text().locale().validate(Locale.ENGLISH.getText());
         myAccountPage._text().measurement().validate("English");
         myAccountPage._text().fuelEfficiency().validate("Miles Per Gallon (UK)");
@@ -347,4 +247,103 @@ public class EditMyAccountTest extends WebRallyTest {
         myAccountPage._text().textMessage1().validate("8017779999@tmomail.net");
         myAccountPage._text().textMessage2().validate("8019997777@tmomail.net");
     }
+	
+	   @Test
+	    public void EmailFormatError() {
+	        set_test_case("TC1271");//TODO: find the actual testcase for this
+	        myAccountPage.loginProcess(USERNAME, PASSWORD);
+	        myAccountPage._link().myAccount().click();
+
+	        myAccountPage._button().edit().click();
+	        myAccountPage._textField().email1().type("tina1965test.com");
+	        myAccountPage._button().save().click();
+
+	        // Clear?//
+	        myAccountPage._textField().email1().type("tina1965@test.com");
+	        myAccountPage._button().save().click();
+	        myAccountPage._text().email1().validate("tina1965@test.com");
+
+	        myAccountPage._button().edit().click();
+	        myAccountPage._textField().email2().type("tlc1965@test");
+	        myAccountPage._button().save().click();
+	        // Validate Error//
+	        // Clear?//
+	        myAccountPage._textField().email2().type("tlc1965@test.com");
+	        myAccountPage._button().save().click();
+	        myAccountPage._text().email2().validate("tlc1965@test.com");
+	    }
+	   @Test
+	    public void CancelButton_Changes() {
+	        set_test_case("TC1271");
+	        myAccountPage.loginProcess(USERNAME, PASSWORD);
+	        myAccountPage._link().myAccount().click();
+	        /* Get original Values */
+	        String originalEmail1 = myAccountPage._text().email1().getText();
+	        String originalEmail2 = myAccountPage._text().email2().getText();
+	        
+	        String originalPhone1 = myAccountPage._text().phone1().getText();
+	        String originalPhone2 = myAccountPage._text().phone2().getText();
+	        
+	        String originalText1 = myAccountPage._text().textMessage1().getText();
+	        String originalText2 = myAccountPage._text().textMessage2().getText();
+	        
+	        
+	        String originalCritical = myAccountPage._text().redFlagCritical().getText();
+	        String originalWarning = myAccountPage._text().redFlagWarn().getText();
+	        String originalInformation = myAccountPage._text().redFlagInfo().getText();
+	        
+	        String originalLocale = myAccountPage._text().locale().getText();
+	        String originalMeasurement = myAccountPage._text().measurement().getText();
+	        String originalFuelRatio = myAccountPage._text().fuelEfficiency().getText();
+	        
+	        
+	        /* Edit button */
+	        myAccountPage._button().edit().click();
+
+	        /* Login Info */
+	        myAccountPage._select().locale().select(Locale.ENGLISH);
+	        myAccountPage._select().measurement().select(Measurement.METRIC);
+	        myAccountPage._select().fuelEfficiency().select(Fuel_Ratio.METRIC_LITER_PER_KILO);
+
+	        /* Contact Info */
+	        myAccountPage._textField().email1().type(random.getEmail());
+	        myAccountPage._textField().email2().type(random.getEmail());
+	        myAccountPage._textField().phone1().type(random.getPhoneNumber());
+	        myAccountPage._textField().phone2().type(random.getPhoneNumber());
+	        myAccountPage._textField().textMessage1().type(random.getTextMessageNumber());
+	        myAccountPage._textField().textMessage2().type(random.getTextMessageNumber());
+
+	        /* Red Flags */
+	        myAccountPage._select().information().select(random.getEnum(RedFlagPrefs.TEXT1, originalInformation));
+	        myAccountPage._select().warning().select(random.getEnum(RedFlagPrefs.TEXT1, originalWarning));
+	        myAccountPage._select().critical().select(random.getEnum(RedFlagPrefs.TEXT1, originalCritical));
+
+	        /* Cancel Changes */
+	        myAccountPage._button().cancel().click();
+
+	        /* Verify Changes did not take effect */
+	        /* Login Info */        
+	        myAccountPage._text().userName().validate(USERNAME);
+	        myAccountPage._text().locale().validate(originalLocale);
+	        myAccountPage._text().measurement().validate(originalMeasurement);
+	        myAccountPage._text().fuelEfficiency().validate(originalFuelRatio);
+
+	        /* Account Info */
+	        myAccountPage._text().name().validate("Tina L Nilson");
+	        myAccountPage._text().group().validate("Top");
+	        myAccountPage._text().team().validate("Skip's Team");
+
+	        /* Red Flags */
+	        myAccountPage._text().redFlagInfo().validate(originalInformation);
+	        myAccountPage._text().redFlagWarn().validate(originalWarning);
+	        myAccountPage._text().redFlagCritical().validate(originalCritical);
+
+	        /* Contact Info */
+	        myAccountPage._text().email1().validate(originalEmail1);
+	        myAccountPage._text().email2().validate(originalEmail2);
+	        myAccountPage._text().phone1().validate(originalPhone1);
+	        myAccountPage._text().phone2().validate(originalPhone2);
+	        myAccountPage._text().textMessage1().validate(originalText1);
+	        myAccountPage._text().textMessage2().validate(originalText2);
+	    }
 }
