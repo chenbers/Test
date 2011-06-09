d3edbf2ebc09bdf845a70ef803ce67a6a4eb75e1
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-06-09 16:36:20 +0000

    automation: clicking cancel button should close popup

--- web/src/test/java/com/inthinc/pro/selenium/pageObjects/PopUps.java
+++ web/src/test/java/com/inthinc/pro/selenium/pageObjects/PopUps.java
@@ -278,7 +278,7 @@ public class PopUps {
 					@Override
 					public ClickableObject click(){
 						super.click();
-						assertTrue(selenium.isVisible(myEnum));
+						assertTrue(!selenium.isVisible(myEnum));
 						return this;
 					}
 				};
