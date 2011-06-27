991eaf3954ab1dc7c9e4503b62e99a041f8c0965
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-06-27 23:05:45 +0000

    fixed so the element clicks the dropdown first.

--- automation/src/main/java/com/inthinc/pro/automation/elements/DhxDropDown.java
+++ automation/src/main/java/com/inthinc/pro/automation/elements/DhxDropDown.java
@@ -9,119 +9,132 @@ import com.inthinc.pro.automation.utils.Id;
 import com.inthinc.pro.automation.utils.Xpath;
 
 public class DhxDropDown extends DropDown implements Selectable {
-	private SeleniumEnumWrapper makeDropDown;
-	private String page;
-	private final int pause = 3;
-	
 
-	private SeleniumEnums[] enums;
+    private SeleniumEnumWrapper makeDropDown;
+    private String page;
+    private final int pause = 3;
 
-	public DhxDropDown(SeleniumEnums anEnum) {
-		super(anEnum);
-		enums = new SeleniumEnums[]{myEnum};
-		makeDropDown = new SeleniumEnumWrapper(anEnum);
-	}
-	
-	public DhxDropDown(SeleniumEnums anEnum, Integer replaceNumber) {
-		super(anEnum, replaceNumber);
-		enums = new SeleniumEnums[]{myEnum};
-		makeDropDown = new SeleniumEnumWrapper(anEnum);
-	}
+    private SeleniumEnums[] enums;
 
-	public DhxDropDown(SeleniumEnums anEnum, String replaceWord) {
-		super(anEnum, replaceWord);
-		page = replaceWord;
-		enums = new SeleniumEnums[]{myEnum};
-		makeDropDown = new SeleniumEnumWrapper(anEnum);
-	}
-	
-	public DhxDropDown(SeleniumEnums anEnum, String replaceWord, SeleniumEnums ...enums) {
-		super(anEnum, replaceWord);
-		page = replaceWord;
-		this.enums = enums;
-		makeDropDown = new SeleniumEnumWrapper(anEnum);
-	}
-
-	public DhxDropDown(SeleniumEnums anEnum, String replaceWord,
-			Integer replaceNumber) {
-		super(anEnum, replaceWord, replaceNumber);
-		page = replaceWord;
-		enums = new SeleniumEnums[]{myEnum};
-		makeDropDown = new SeleniumEnumWrapper(anEnum);
-	}
-
-	private DhxDropDown assignIDs() {
-		Integer div = 1;
-		for (SeleniumEnums enume : enums) {
-			if (selenium.isElementPresent(new SeleniumEnumWrapper(enume).replaceWord(page))
-					&& enume != null) {
-				selenium.assignId("//body/div[" + div + "]", enume.toString());
-				div++;
-			}
-		}
-		selenium.click(makeDropDown.setID(makeDropDown.getIDs()[0]+"/../img"));
-		myEnum.setID(myEnum.toString());
-		return this;
-	}
-
-	@Override
-	public SelectableObject select(TextEnum value){
-    	if (value instanceof SeleniumValueEnums){
-    		return select(((SeleniumValueEnums) value).getPosition()+1);
-    	}
-    	return select(value.getText());
+    public DhxDropDown(SeleniumEnums anEnum) {
+	super(anEnum);
+	enums = new SeleniumEnums[] {
+	    myEnum
+	};
+	makeDropDown = new SeleniumEnumWrapper(anEnum);
     }
-	
-	
-	@Override
-	public DhxDropDown select(Integer optionNumber) {
-		assignIDs();
-		selenium.selectDhx(myEnum, optionNumber.toString());
-		return this;
-	}
 
-	public DhxDropDown select(SeleniumValueEnums option) {
-		assignIDs();
-		return select(option.getPosition() + 1);
-	}
+    public DhxDropDown(SeleniumEnums anEnum, Integer replaceNumber) {
+	super(anEnum, replaceNumber);
+	enums = new SeleniumEnums[] {
+	    myEnum
+	};
+	makeDropDown = new SeleniumEnumWrapper(anEnum);
+    }
 
-	@Override
-	public DhxDropDown select(String fullMatch) {
-		select(fullMatch, 1);
-		return this;
-	}
-	
-	
+    public DhxDropDown(SeleniumEnums anEnum, String replaceWord) {
+	super(anEnum, replaceWord);
+	page = replaceWord;
+	enums = new SeleniumEnums[] {
+	    myEnum
+	};
+	makeDropDown = new SeleniumEnumWrapper(anEnum);
+    }
 
-	@Override
-	public DhxDropDown select(String fullMatch, Integer matchNumber) {
-		assignIDs();
-		matchNumber--;
-		String xpath =makeXpath(Id.text(fullMatch));
-		getMatches(xpath, matchNumber).click();
-		selenium.pause(pause, "Wait for elements to refresh");
-		return this;
-	}
+    public DhxDropDown(SeleniumEnums anEnum, String replaceWord,
+	    SeleniumEnums... enums) {
+	super(anEnum, replaceWord);
+	page = replaceWord;
+	this.enums = enums;
+	makeDropDown = new SeleniumEnumWrapper(anEnum);
+    }
 
-	@Override
-	public DhxDropDown selectPartMatch(String partialMatch) {
-		return selectPartMatch(partialMatch, 1);
-	}
+    public DhxDropDown(SeleniumEnums anEnum, String replaceWord,
+			Integer replaceNumber) {
+	super(anEnum, replaceWord, replaceNumber);
+	page = replaceWord;
+	enums = new SeleniumEnums[] {
+	    myEnum
+	};
+	makeDropDown = new SeleniumEnumWrapper(anEnum);
+    }
 
-	@Override
-	public DhxDropDown selectPartMatch(String partialMatch,
+    private DhxDropDown assignIDs() {
+	Integer div = 1;
+	if (page == null) {
+	    page = "";
+	}
+	for (SeleniumEnums enume : enums) {
+	    if (selenium.isElementPresent(new SeleniumEnumWrapper(enume)
+		    .replaceWord(page))
+					&& enume != null) {
+		selenium.assignId("//body/div[" + div + "]", enume.toString());
+		div++;
+	    }
+	}
+	makeDropDown.setID(makeDropDown.getIDs()[0] + "/../img");
+	myEnum.setID(myEnum.toString());
+	return this;
+    }
+
+    @Override
+    public SelectableObject select(TextEnum value) {
+	if (value instanceof SeleniumValueEnums) {
+	    return select(((SeleniumValueEnums) value).getPosition() + 1);
+	}
+	return select(value.getText());
+    }
+
+    @Override
+    public DhxDropDown select(Integer optionNumber) {
+	assignIDs();
+	selenium.click(makeDropDown);
+	selenium.selectDhx(myEnum, optionNumber.toString());
+	return this;
+    }
+
+    public DhxDropDown select(SeleniumValueEnums option) {
+	assignIDs();
+	return select(option.getPosition() + 1);
+    }
+
+    @Override
+    public DhxDropDown select(String fullMatch) {
+	select(fullMatch, 1);
+	return this;
+    }
+
+    @Override
+    public DhxDropDown select(String fullMatch, Integer matchNumber) {
+	assignIDs();
+	matchNumber--;
+	String xpath = makeXpath(Id.text(fullMatch));
+	selenium.click(makeDropDown);
+	getMatches(xpath, matchNumber).click();
+	selenium.pause(pause, "Wait for elements to refresh");
+	return this;
+    }
+
+    @Override
+    public DhxDropDown selectPartMatch(String partialMatch) {
+	return selectPartMatch(partialMatch, 1);
+    }
+
+    @Override
+    public DhxDropDown selectPartMatch(String partialMatch,
 			Integer matchNumber) {
-		assignIDs();
-		matchNumber--;
-		String xpath =makeXpath(Id.contains( Id.text(), partialMatch));
-		getMatches(xpath, matchNumber).click();
-		selenium.pause(pause, "Wait for elements to refresh");
-		return this;
-	}
-	
-	private String makeXpath(String secondDiv){
-		String xpath = Xpath.start().div(Id.id(myEnum.getIDs()[0]))
+	assignIDs();
+	matchNumber--;
+	String xpath = makeXpath(Id.contains(Id.text(), partialMatch));
+	selenium.click(makeDropDown);
+	getMatches(xpath, matchNumber).click();
+	selenium.pause(pause, "Wait for elements to refresh");
+	return this;
+    }
+
+    private String makeXpath(String secondDiv) {
+	String xpath = Xpath.start().div(Id.id(myEnum.getIDs()[0]))
 		.div(secondDiv).toString();
-		return xpath;
-	}
+	return xpath;
+    }
 }
