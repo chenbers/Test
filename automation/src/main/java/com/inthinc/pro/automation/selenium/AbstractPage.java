67b83abe4d852d66d9e4189b3d22950642fb9e25
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-06-27 21:15:25 +0000

    added getLinkByText(String text)

--- automation/src/main/java/com/inthinc/pro/automation/selenium/AbstractPage.java
+++ automation/src/main/java/com/inthinc/pro/automation/selenium/AbstractPage.java
@@ -5,63 +5,76 @@ import java.util.ArrayList;
 import org.openqa.selenium.WebDriver;
 
 import com.inthinc.pro.automation.elements.ElementBase;
+import com.inthinc.pro.automation.elements.TextLink;
+import com.inthinc.pro.automation.enums.SeleniumEnumWrapper;
 import com.inthinc.pro.automation.enums.SeleniumEnums;
 import com.inthinc.pro.automation.utils.MasterTest;
 
 public abstract class AbstractPage extends MasterTest implements Page {
-	protected SeleniumEnums url;
-	protected ArrayList<SeleniumEnums> checkMe;
-	private CoreMethodInterface selenium;
-	protected WebDriver webDriver;
-	protected String currentPage;
-	public static ArrayList<Class<? extends AbstractPage>> instantiatedPages = new ArrayList<Class<? extends AbstractPage>>(); 
-	
-	public AbstractPage() {
-		selenium = super.getSelenium();
-		webDriver = selenium.getWrappedDriver();
-		checkMe = new ArrayList<SeleniumEnums>();
 
-		Class<? extends AbstractPage> derivedClass = this.getClass();
-		if (!instantiatedPages.contains(derivedClass)) {
-			instantiatedPages.add(derivedClass);
-		}
+    protected SeleniumEnums url;
+    protected ArrayList<SeleniumEnums> checkMe;
+    private CoreMethodInterface selenium;
+    protected WebDriver webDriver;
+    protected String currentPage;
+    public static ArrayList<Class<? extends AbstractPage>> instantiatedPages = new ArrayList<Class<? extends AbstractPage>>();
+
+    
+    
+    public TextLink getLinkByText(String text) {
+	return new TextLink(new SeleniumEnumWrapper("GET_LINK_BY_TEXT",
+		"link="+text));
+    }
+
+    public AbstractPage() {
+	selenium = super.getSelenium();
+	webDriver = selenium.getWrappedDriver();
+	checkMe = new ArrayList<SeleniumEnums>();
+
+	Class<? extends AbstractPage> derivedClass = this.getClass();
+	if (!instantiatedPages.contains(derivedClass)) {
+	    instantiatedPages.add(derivedClass);
 	}
+    }
 
-	public Boolean verifyOnPage() { // TODO: dtanner or jwimmer: come up with a better name
-		ElementBase test = new ElementBase() {};
-		return test.validateElementsPresent(checkMe);
-	}
+    public Boolean verifyOnPage() { // TODO: dtanner or jwimmer: come up with a
+				    // better name
+	ElementBase test = new ElementBase() {
+	};
+	return test.validateElementsPresent(checkMe);
+    }
 
-	@Override
-	public String getExpectedPath() {
-		return url.getURL();
-	}
+    @Override
+    public String getExpectedPath() {
+	return url.getURL();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.inthinc.pro.web.selenium.Page#validateURL()
-	 */
-	public AbstractPage validateURL() {
-		boolean results = getCurrentLocation().contains(getExpectedPath());
-		if (!results)
-			addError("validateURL", getCurrentLocation() + " does not contain "
-					+ getExpectedPath() + " ?", ErrorLevel.FAIL);
-		return this;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.inthinc.pro.web.selenium.Page#validateURL()
+     */
+    public AbstractPage validateURL() {
+	boolean results = getCurrentLocation().contains(getExpectedPath());
+	if (!results)
+	    addError("validateURL", getCurrentLocation() + " does not contain "
+					+ getExpectedPath() + " ?",
+		    ErrorLevel.FAIL);
+	return this;
+    }
 
-	@Override
-	public Page load() {
-		open(url);
-		return this;
-	}
+    @Override
+    public Page load() {
+	open(url);
+	return this;
+    }
 
-	@Override
-	public AbstractPage validate() {
-		addError(
+    @Override
+    public AbstractPage validate() {
+	addError(
 				"AbstractPage.validate()",
 				"automation cannot validate AbstractPage OR there is no validate() method for the concrete page being tested.",
 				ErrorLevel.FAIL);
-		return this;
-	}
+	return this;
+    }
 }
