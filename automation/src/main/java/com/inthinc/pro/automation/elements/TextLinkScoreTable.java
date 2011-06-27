bea7d0809cef4f479602c80ea4fd0868b6d96191
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-06-27 22:57:09 +0000

    altered the setMyEnum to give us the ID of the actual value

--- automation/src/main/java/com/inthinc/pro/automation/elements/TextLinkScoreTable.java
+++ automation/src/main/java/com/inthinc/pro/automation/elements/TextLinkScoreTable.java
@@ -2,19 +2,40 @@ package com.inthinc.pro.automation.elements;
 
 import com.inthinc.pro.automation.elements.ElementInterface.Clickable;
 import com.inthinc.pro.automation.elements.ElementInterface.TableBased;
+import com.inthinc.pro.automation.enums.SeleniumEnumWrapper;
 import com.inthinc.pro.automation.enums.SeleniumEnums;
 
 public class TextLinkScoreTable extends TextTableLink implements Clickable,
 		TableBased {
 
-	public TextLinkScoreTable(SeleniumEnums anEnum) {
-		super(anEnum);
+    public TextLinkScoreTable(SeleniumEnums anEnum) {
+	super(anEnum);
+    }
+
+    @Override
+    public TextLinkScoreTable replaceNumber(Integer row) {
+	super.replaceNumber(row--);
+	return this;
+    }
+
+    @Override
+    public void setMyEnum(SeleniumEnums anEnum) {
+	myEnum = new SeleniumEnumWrapper(anEnum);
+	String[] newIds = new String[myEnum.getIDs().length];
+	String scoreBoxLink = "/span/div/div/div/div/div/a";
+	for (int i = 0; i < myEnum.getIDs().length; i++) {
+	    String newId = "";
+	    String id = myEnum.getIDs()[i];
+	    if (id.startsWith("//")) {
+		newId = id + scoreBoxLink;
+	    } else if (!id.contains("=")) {
+		newId = "//td[@id='" + id + "']" + scoreBoxLink;
+	    } else {
+		continue;
+	    }
+	    newIds[i] = newId;
 	}
-	
-	@Override
-	public TextLinkScoreTable replaceNumber(Integer row){
-		super.replaceNumber(row--);
-		return this;
-	}
+	myEnum.setID(newIds);
+    }
 
 }
