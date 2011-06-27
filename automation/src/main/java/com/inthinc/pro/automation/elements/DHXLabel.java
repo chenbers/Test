7b6480f95b33ed0e9c673c98f8b45f60f339d46d
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-06-27 23:10:04 +0000

    Added a label for DHX DropDowns

---
+++ automation/src/main/java/com/inthinc/pro/automation/elements/DHXLabel.java
@@ -0,0 +1,51 @@
+package com.inthinc.pro.automation.elements;
+
+import com.inthinc.pro.automation.enums.SeleniumEnumWrapper;
+import com.inthinc.pro.automation.enums.SeleniumEnums;
+import com.inthinc.pro.automation.enums.TextEnum;
+
+public class DHXLabel extends TextObject {
+
+    public DHXLabel(SeleniumEnums anEnum, TextEnum replaceWord) {
+	super(anEnum, replaceWord);
+    }
+
+    public DHXLabel(SeleniumEnums anEnum, String replaceWord,
+	    Integer replaceNumber) {
+	super(anEnum, replaceWord, replaceNumber);
+    }
+
+    public DHXLabel(SeleniumEnums anEnum, String replaceWord) {
+	super(anEnum, replaceWord);
+    }
+
+    public DHXLabel(SeleniumEnums anEnum, Integer replaceNumber) {
+	super(anEnum, replaceNumber);
+    }
+
+    public DHXLabel(SeleniumEnums anEnum) {
+	super(anEnum);
+    }
+
+    @Override
+    public void setMyEnum(SeleniumEnums anEnum) {
+	myEnum = new SeleniumEnumWrapper(anEnum);
+	String[] newIds = new String[myEnum.getIDs().length];
+	String oneUpFirstTd = parentXpath + parentXpath + parentXpath
+		+ parentXpath + "/td[1]";
+	for (int i = 0; i < myEnum.getIDs().length; i++) {
+	    String newId = "";
+	    String id = myEnum.getIDs()[i];
+	    if (id.startsWith("//")) {
+		newId = id + oneUpFirstTd;
+	    } else if (!id.contains("=")) {
+		newId = "//input[@id='" + id + "']" + oneUpFirstTd;
+	    } else {
+		continue;
+	    }
+	    newIds[i] = newId;
+	}
+	myEnum.setID(newIds);
+    }
+
+}
