e2d5a0ba2cb2b4063e23aa2ed38e693c4a7ab71a
cjennings <cjennings@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-06-30 18:40:16 +0000

    US3254-TA7593 - removed waysmart only flag on options we will support in tiwipro zones

--- dao/src/main/java/com/inthinc/pro/model/zone/option/ZoneAvailableOption.java
+++ dao/src/main/java/com/inthinc/pro/model/zone/option/ZoneAvailableOption.java
@@ -12,15 +12,15 @@ import com.inthinc.pro.model.zone.option.type.OptionValue;
 import com.inthinc.pro.model.zone.option.type.ZoneVehicleType;
 
 public enum ZoneAvailableOption implements BaseEnum {
-    VEHICLE_TYPE(1, "VEHICLE_TYPE", ZoneOptionType.VEHICLE_TYPE, ZoneVehicleType.ALL, true),
-    SPEED_LIMIT(2, "SPEED_LIMIT", ZoneOptionType.SPEED, new SpeedValue(0), true),
-    CAUTION_AREA(3, "CAUTION_AREA", ZoneOptionType.OFF_ON, OffOn.OFF, true),
+    VEHICLE_TYPE(1, "VEHICLE_TYPE", ZoneOptionType.VEHICLE_TYPE, ZoneVehicleType.ALL, false),
+    SPEED_LIMIT(2, "SPEED_LIMIT", ZoneOptionType.SPEED, new SpeedValue(0), false),
+    CAUTION_AREA(3, "CAUTION_AREA", ZoneOptionType.OFF_ON, OffOn.OFF, false),
     REPORT_ON_ARRIVAL_DEPARTURE(4, "REPORT_ON_ARRIVAL_DEPARTURE", ZoneOptionType.OFF_ON, OffOn.ON, false),
-    MONITOR_IDLE(5, "MONITOR_IDLE", ZoneOptionType.OFF_ON, OffOn.ON, true),
-    SEATBELT_VIOLATION(6, "SEATBELT_VIOLATION", ZoneOptionType.OFF_ON_DEVICE, OffOnDevice.DEVICE, true),
-    SPEEDING_VIOLATION(7, "SPEEDING_VIOLATION", ZoneOptionType.OFF_ON_DEVICE, OffOnDevice.DEVICE, true),
-    DRIVER_ID_VIOLATION(8, "DRIVER_ID_VIOLATION", ZoneOptionType.OFF_ON_DEVICE, OffOnDevice.DEVICE, true),
-    MASTER_BUZZER(9, "MASTER_BUZZER", ZoneOptionType.OFF_ON_DEVICE, OffOnDevice.DEVICE, true),
+    MONITOR_IDLE(5, "MONITOR_IDLE", ZoneOptionType.OFF_ON, OffOn.ON, false),
+    SEATBELT_VIOLATION(6, "SEATBELT_VIOLATION", ZoneOptionType.OFF_ON_DEVICE, OffOnDevice.DEVICE, false),
+    SPEEDING_VIOLATION(7, "SPEEDING_VIOLATION", ZoneOptionType.OFF_ON_DEVICE, OffOnDevice.DEVICE, false),
+    DRIVER_ID_VIOLATION(8, "DRIVER_ID_VIOLATION", ZoneOptionType.OFF_ON_DEVICE, OffOnDevice.DEVICE, false),
+    MASTER_BUZZER(9, "MASTER_BUZZER", ZoneOptionType.OFF_ON_DEVICE, OffOnDevice.DEVICE, false),
     HARD_TURN_EVENT(10, "HARD_TURN_EVENT", ZoneOptionType.OFF_ON_DEVICE, OffOnDevice.DEVICE, true),
     HARD_BRAKE_EVENT(11, "HARD_BRAKE_EVENT", ZoneOptionType.OFF_ON_DEVICE, OffOnDevice.DEVICE, true),
     HARD_VERTICAL_EVENT(12, "HARD_VERTICAL_EVENT", ZoneOptionType.OFF_ON_DEVICE, OffOnDevice.DEVICE, true);
