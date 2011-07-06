b8b17c0a9fd8ea3110a574a2045476755b226f32
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-07-06 14:46:27 +0000

    automation: JUST formatting code back into Inthinc Conventions

--- automation/src/main/java/com/inthinc/pro/automation/elements/ClickableTableObject.java
+++ automation/src/main/java/com/inthinc/pro/automation/elements/ClickableTableObject.java
@@ -5,39 +5,33 @@ import com.inthinc.pro.automation.elements.ElementInterface.TableBased;
 import com.inthinc.pro.automation.enums.SeleniumEnums;
 import com.inthinc.pro.automation.enums.TextEnum;
 
-public class ClickableTableObject extends ClickableObject implements
-	ClickableTableBased,
-	TableBased {
+public class ClickableTableObject extends ClickableObject implements ClickableTableBased, TableBased {
 
-    public ClickableTableObject(SeleniumEnums anEnum, String replaceWord,
-	    Integer replaceNumber) {
-	super(anEnum, replaceWord, replaceNumber);
+    public ClickableTableObject(SeleniumEnums anEnum, String replaceWord, Integer replaceNumber) {
+        super(anEnum, replaceWord, replaceNumber);
     }
 
     public ClickableTableObject(SeleniumEnums anEnum) {
-	super(anEnum);
+        super(anEnum);
     }
 
     public ClickableTableObject(SeleniumEnums anEnum, Integer replaceNumber) {
-	super(anEnum, replaceNumber);
+        super(anEnum, replaceNumber);
     }
 
     public ClickableTableObject(SeleniumEnums anEnum, String replaceWord) {
-	super(anEnum, replaceWord);
+        super(anEnum, replaceWord);
     }
 
     public ClickableTableObject(SeleniumEnums anEnum, TextEnum replaceWord) {
-	super(anEnum, replaceWord);
+        super(anEnum, replaceWord);
     }
 
     @Override
     @Deprecated
     public ClickableTableObject click() {
-	addError(
-				".click()",
-				"please supply an Integer number for the row on the table)",
-				ErrorLevel.FAIL);
-	return null;
+        addError(".click()", "please supply an Integer number for the row on the table)", ErrorLevel.FAIL);
+        return null;
     }
 
     /**
@@ -49,139 +43,118 @@ public class ClickableTableObject extends ClickableObject implements
      * @return
      */
     public ClickableTableObject click(Integer row) {
-	replaceNumber(row);
-	super.click();
-	return this;
+        replaceNumber(row);
+        super.click();
+        return this;
     }
 
     @Override
     public Boolean isVisible(Integer row) {
-	replaceNumber(row);
-	return super.isVisible();
+        replaceNumber(row);
+        return super.isVisible();
     }
 
     @Override
     public Boolean isPresent(Integer row) {
-	replaceNumber(row);
-	return super.isPresent();
+        replaceNumber(row);
+        return super.isPresent();
     }
 
     @Override
     public ElementInterface focus(Integer row) {
-	replaceNumber(row);
-	return super.focus();
+        replaceNumber(row);
+        return super.focus();
     }
 
     @Override
     public Boolean assertVisibility(Integer row, Boolean visible) {
-	replaceNumber(row);
-	return super.assertVisibility(visible);
+        replaceNumber(row);
+        return super.assertVisibility(visible);
     }
 
     @Override
     @Deprecated
     public Boolean isVisible() {
-	addError(
-				".isVisible()",
-				"please supply an Integer number for the row on the table)",
-				ErrorLevel.FAIL);
-	return null;
+        addError(".isVisible()", "please supply an Integer number for the row on the table)", ErrorLevel.FAIL);
+        return null;
     }
 
     @Override
     @Deprecated
     public Boolean isPresent() {
-	addError(
-				".isPresent()",
-				"please supply an Integer number for the row on the table)",
-				ErrorLevel.FAIL);
-	return null;
+        addError(".isPresent()", "please supply an Integer number for the row on the table)", ErrorLevel.FAIL);
+        return null;
     }
 
     @Override
     @Deprecated
     public ElementInterface focus() {
-	addError(
-				".focus()",
-				"please supply an Integer number for the row on the table)",
-				ErrorLevel.FAIL);
-	return null;
+        addError(".focus()", "please supply an Integer number for the row on the table)", ErrorLevel.FAIL);
+        return null;
     }
 
     @Override
     @Deprecated
     public Boolean assertVisibility(Boolean visible) {
-	addError(
-			".assertVisibility()",
-			"please supply an Integer number for the row on the table)",
-			ErrorLevel.FAIL);
-	return null;
+        addError(".assertVisibility()", "please supply an Integer number for the row on the table)", ErrorLevel.FAIL);
+        return null;
     }
 
     @Override
     public Boolean validateVisibility(Integer row, Boolean visible) {
-	replaceNumber(row);
-	return super.validateVisibility(visible);
+        replaceNumber(row);
+        return super.validateVisibility(visible);
     }
 
     @Override
     public Boolean validatePresence(Integer row, Boolean present) {
-	replaceNumber(row);
-	return super.validatePresence(present);
+        replaceNumber(row);
+        return super.validatePresence(present);
     }
 
     @Override
     public Boolean assertPresence(Integer row, Boolean present) {
-	replaceNumber(row);
-	return super.assertPresence(present);
+        replaceNumber(row);
+        return super.assertPresence(present);
     }
 
     @Override
     public Boolean isClickable(Integer row) {
-	replaceNumber(row);
-	return super.isClickable();
+        replaceNumber(row);
+        return super.isClickable();
     }
 
     @Override
     public Boolean validateClickable(Integer row, Boolean clickable) {
-	replaceNumber(row);
-	return super.validateClickable(clickable);
+        replaceNumber(row);
+        return super.validateClickable(clickable);
     }
 
     @Override
     public Boolean assertClickable(Integer row, Boolean clickable) {
-	replaceNumber(row);
-	return super.assertClickable(clickable);
+        replaceNumber(row);
+        return super.assertClickable(clickable);
     }
 
     @Override
     @Deprecated
     public Boolean isClickable() {
-	addError(
-		".isClickable()",
-		"please supply an Integer number for the row on the table)",
-		ErrorLevel.FAIL);
-	return null;
+        addError(".isClickable()", "please supply an Integer number for the row on the table)", ErrorLevel.FAIL);
+        return null;
     }
 
     @Override
     @Deprecated
     public Boolean validateClickable(Boolean clickable) {
-	addError(
-		".validateClickable()",
-		"please supply an Integer number for the row on the table)",
-		ErrorLevel.FAIL);
-	return null;
+        addError(".validateClickable()", "please supply an Integer number for the row on the table)", ErrorLevel.FAIL);
+        return null;
     }
 
     @Override
     @Deprecated
     public Boolean assertClickable(Boolean clickable) {
-	addError(
-		".assertClickable()",
-		"please supply an Integer number for the row on the table)",
-		ErrorLevel.FAIL);
-	return null;
+        addError(".assertClickable()", "please supply an Integer number for the row on the table)", ErrorLevel.FAIL);
+        return null;
     }
 
 }
