b711c42b4144088a124fa2a75efe421a89d04db7
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-07-11 21:52:37 +0000

    formatted

--- automation/src/main/java/com/inthinc/pro/automation/elements/TextObject.java
+++ automation/src/main/java/com/inthinc/pro/automation/elements/TextObject.java
@@ -7,84 +7,83 @@ import com.inthinc.pro.automation.enums.TextEnum;
 public class TextObject extends ElementBase implements TextBased {
 
     public TextObject(SeleniumEnums anEnum) {
-	super(anEnum);
+        super(anEnum);
     }
 
     public TextObject(SeleniumEnums anEnum, Integer replaceNumber) {
-	super(anEnum, replaceNumber);
+        super(anEnum, replaceNumber);
     }
 
     public TextObject(SeleniumEnums anEnum, String replaceWord) {
-	super(anEnum, replaceWord);
+        super(anEnum, replaceWord);
     }
 
-    public TextObject(SeleniumEnums anEnum, String replaceWord,
-	    Integer replaceNumber) {
-	super(anEnum, replaceWord, replaceNumber);
+    public TextObject(SeleniumEnums anEnum, String replaceWord, Integer replaceNumber) {
+        super(anEnum, replaceWord, replaceNumber);
     }
 
     public TextObject(SeleniumEnums anEnum, TextEnum replaceWord) {
-	super(anEnum, replaceWord);
+        super(anEnum, replaceWord);
     }
 
     @Override
     public Boolean assertEquals() {
-	return assertEquals(myEnum, myEnum, myEnum);
+        return assertEquals(myEnum, myEnum, myEnum);
     }
 
     @Override
     public Boolean assertEquals(String compareAgainst) {
-	return assertEquals(compareAgainst, myEnum);
+        return assertEquals(compareAgainst, myEnum);
     }
 
     @Override
     public Boolean assertNotEquals(String compareAgainst) {
-	return assertNotEquals(compareAgainst, myEnum);
+        return assertNotEquals(compareAgainst, myEnum);
     }
 
     @Override
     public Boolean compare() {
-	return compare(myEnum.getText());
+        return compare(myEnum.getText());
     }
 
     @Override
     public Boolean compare(String expected) {
-	return compare(expected, getText());
+        return compare(expected, getText());
     }
 
     @Override
     public String getText() {
-	return selenium.getText(myEnum);
+        return selenium.getText(myEnum);
     }
 
     @Override
     public String toString() {
-	return getText();
+        return getText();
     }
 
     @Override
     public Boolean validate() {
-	return validateEquals(myEnum.getText(), getText());
+        return validateEquals(myEnum.getText(), getText());
     }
 
     @Override
     public Boolean validate(String expected) {
-	return compare(expected);
+        return compare(expected);
     }
 
     @Override
     public Boolean validate(TextEnum expected) {
-	return compare(expected.getText());
+        return compare(expected.getText());
     }
 
     @Override
     public Boolean validate(TextEnum expected, String replaceOld, String withNew) {
-	return compare(expected.getText().replace(replaceOld, withNew));
+        return compare(expected.getText().replace(replaceOld, withNew));
     }
 
     @Override
     public Boolean validateContains(String expectedPart) {
-	String actual = getText();
-	return validateStringContains(actual, expectedPart);
+        String actual = getText();
+        return validateStringContains(actual, expectedPart);
     }
 }
