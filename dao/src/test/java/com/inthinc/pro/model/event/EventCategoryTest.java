aceb831a8c92d301b326ff67ec66558df31c6013
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-07-12 17:25:28 +0000

    IN-PROGRESS - task US3514: CAS043883: Ability to see valid and invalid driver id and occupant id logins https://rally1.rallydev.com/slm/detail/ar/3520948997 updated unit test

--- dao/src/test/java/com/inthinc/pro/model/event/EventCategoryTest.java
+++ dao/src/test/java/com/inthinc/pro/model/event/EventCategoryTest.java
@@ -2,16 +2,17 @@ package com.inthinc.pro.model.event;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.TreeMap;
 
 import org.junit.Test;
 
-
 public class EventCategoryTest {
 
-    Integer[] expectedCounts = {
+    Integer[] expectedNoteInCatCounts = {
             10,
-            16,
+            20,
             0,
             11,
             2,
@@ -24,7 +25,37 @@ public class EventCategoryTest {
         int i = 0;
         for (EventCategory cat : EventCategory.values()) {
             List<NoteType> noteTypeList = cat.getNoteTypesInCategory();
-            assertEquals(cat + "", expectedCounts[i++].intValue(), noteTypeList.size());
+            assertEquals(cat + "", expectedNoteInCatCounts[i++].intValue(), noteTypeList.size());
         }
+    }
+    
+    Integer[] expectedTypeInCatCounts = {
+    		4,	//  EMERGENCY
+    		3,	//  HOS
+    		3,	//  NONE
+    		1,	//  TEXT
+    		9,	//  VIOLATION
+    		19,	//  WARNING
+    		2	//  ZONE
+    };
+    
+    @Test
+    public void getCategoryForEventType() {
+    	TreeMap<String, HashMap<Integer,EventType>> map = new TreeMap<String, HashMap<Integer,EventType>>();
+    	Integer count = 0;
+    	for(EventType type :EventType.values()) {
+    		EventCategory cat = EventCategory.getCategoryForEventType(type);
+
+            if (!map.containsKey(cat.toString())) {
+                map.put(cat.toString(), new HashMap<Integer, EventType>());
+            }
+            map.get(cat.toString()).put(count, type);
+            count++;
+    	}
+    	int i = 0;
+    	for(String cat: map.keySet()){
+    		System.out.println(map.get(cat).size()+" "+cat);
+    		assertEquals(cat, expectedTypeInCatCounts[i++].intValue(), map.get(cat).size());
+    	}
     }
 }
