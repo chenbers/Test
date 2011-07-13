3e089b1f5c3211fd48388d66bf18aaee4ce31d71
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-07-13 22:29:32 +0000

    adding Executive Live Fleet expansion enum

---
+++ web/src/test/java/com/inthinc/pro/selenium/pageEnums/LiveFleetExpansionEnum.java
@@ -0,0 +1,43 @@
+package com.inthinc.pro.selenium.pageEnums;
+
+import com.inthinc.pro.automation.enums.SeleniumEnums;
+
+public enum LiveFleetExpansionEnum implements SeleniumEnums {
+
+    TITLE("Live Fleet", "//form[@id='refresh']/../../span[@class='map']"),
+    REFRESH(null, "refresh:details-liveFleetMapRefresh"),
+    RESTORE(null, "refresh:details-liveFleetMapRestore"),
+    
+    LEGEND_TITLE("Fleet Legend", "//form[@id='bubbleForm']/../div/div/div/span[@class='legend']"),
+    LEGENT_ENTRY(null, "icos2:###"),
+    
+    ;
+
+    private String text, url;
+    private String[] IDs;
+
+    private LiveFleetExpansionEnum(String url) {
+    this.url = url;
+    }
+
+    private LiveFleetExpansionEnum(String text, String... IDs) {
+    this.text = text;
+    this.IDs = IDs;
+    }
+
+    @Override
+    public String[] getIDs() {
+    return IDs;
+    }
+
+    @Override
+    public String getText() {
+    return text;
+    }
+
+    @Override
+    public String getURL() {
+    return url;
+    }
+
+}
