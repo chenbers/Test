cb4670038fef4221327e7e7eaca2fd74e1db4395
tnilson <tnilson@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-07-13 22:21:03 +0000

    Update for unique employee id.

--- web/src/test/java/com/inthinc/pro/selenium/testSuites/AddEditUser.java
+++ web/src/test/java/com/inthinc/pro/selenium/testSuites/AddEditUser.java
@@ -7,6 +7,7 @@ import com.inthinc.pro.automation.utils.RandomValues;
 import com.inthinc.pro.selenium.pageEnums.AdminTables.AdminUsersEntries;
 import com.inthinc.pro.selenium.pageEnums.AdminUserDetailsEnum;
 import com.inthinc.pro.selenium.pageEnums.AdminUsersEnum;
+import com.inthinc.pro.selenium.pageEnums.TAE.TimeZones;
 import com.inthinc.pro.selenium.pageObjects.PageAdminUserDetails;
 import com.inthinc.pro.selenium.pageObjects.PageAdminUsers;
 import com.inthinc.pro.selenium.pageObjects.PageMyAccount;
@@ -21,7 +22,7 @@ public class AddEditUser extends WebRallyTest {
 	private PageAdminUserDetails myAdminUserDetails;
 	private PageAdminUsers myAdminUsers;
 	private RandomValues random;
-	private String USERNAME = "prime";
+	private String USERNAME = "TinaAuto";
 	private String PASSWORD = "password";
 	 
 	@Before
@@ -74,6 +75,79 @@ public class AddEditUser extends WebRallyTest {
 	
 	}
 	
-
+	@Test
+    public void DuplicateEmployeeIDError() {
+        set_test_case("TC5704");
+        
+        //.0 Login
+        myAccount.loginProcess(USERNAME, PASSWORD);
+        myAccount._link().admin().click();
 	
+        //.1 Search for Employee ID in column and save the id to be used while creating a new user.
+           
+              
+        myAdminUsers._link().adminUsers().click();
+        //myAdminUsers._link().editColumns().click();
+       // myAdminUsers._popUp().editColumns()._checkBox().check(12);
+       // myAdminUsers._popUp().editColumns()._button().save().click();
+        myAdminUsers._textField().search().type("Tina");
+        myAdminUsers._button().search().click();
+        //myAdminUsers._link().sortByColumn(AdminUsersEntries.EMPLOYEE_ID).click();
+        //myAdminUsers._link().sortByColumn(AdminUsersEntries.EMPLOYEE_ID).click();
+        String myString = myAdminUsers._text().tableEntry(AdminUsersEntries.EMPLOYEE_ID).getText(1);
+        
+        //.2 Create a new user with duplicate employee id.
+        
+       
+        myAdminUsers._link().adminAddUser().click();
+        myAddEditUser._textField().userFields(AdminUsersEntries.FIRST_NAME).type("testing");
+        myAddEditUser._textField().userFields(AdminUsersEntries.LAST_NAME).type("testing");
+        myAddEditUser._checkBox().driverInformation().check();
+        myAddEditUser._dropDown().driverTeam().select("Top - Test Group RW");
+        myAddEditUser._checkBox().userInformation().uncheck();
+        myAddEditUser._dropDown().timeZone().select(TimeZones.US_SAMOA);
+        myAddEditUser._textField().userFields(AdminUsersEntries.EMPLOYEE_ID).type(myString);
+        myAddEditUser._button().saveTop().click();
+        
+        
+        //.3 Verify error messages display 
+        //TODO: fix errors 
+        logger.debug("1: "+myAddEditUser._text().driverError(AdminUsersEntries.EMPLOYEE_ID).getText());
+        logger.debug("2: "+myAddEditUser._text().masterError().getText());
+        logger.debug("3: "+myAddEditUser._text().personError(AdminUsersEntries.EMPLOYEE_ID).getText());
+        //myAddEditUser._text().
+        myAddEditUser._text().
+        myAddEditUser._textField().userFields(AdminUsersEntries.EMPLOYEE_ID).validate ("Employee ID is already in use");
+        
+        //.4 Cancel Changes
+        
+        myAddEditUser._button().cancelTop().click();
+        
+        
+	}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 }
