3ebbc926e895d3569d0ae7cfa739af57648803c0
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-07-15 21:36:41 +0000

    Adding the FuelEfficiencyExpansion page

---
+++ web/src/test/java/com/inthinc/pro/selenium/pageEnums/FuelEfficiencyExpansionEnum.java
@@ -0,0 +1,53 @@
+package com.inthinc.pro.selenium.pageEnums;
+
+import com.inthinc.pro.automation.enums.SeleniumEnums;
+
+public enum FuelEfficiencyExpansionEnum implements SeleniumEnums {
+
+    DURATION(null, "details-mpgChart_form:details-mpgChart-***"),
+    TOOLS(null, "details-mpgChart_toolImage"),
+    EMAIL_REPORT(emailReport, "details-mpgChart_detail_form:details-mpgChart_email:anchor"),
+    EXPORT_PDF(exportPDF, "details-mpgChart_detail_form:details-mpgChart-export:anchor"),
+    
+    RESTORE(null, "details-mpgChart_details"),
+    
+    TITLE("                 Fuel Efficiency                 ", "//span[@class='gas']"),
+    
+    SORT_DIVISION_TEAM("Division/Team", "mpgTableForm:details:nameheader:sortDiv"),
+    SORT_LIGHT("Light", "mpgTableForm:details:lightheader:sortDiv"),
+    SORT_MEDIUM("Medium", "mpgTableForm:details:mediumheader:sortDiv"),
+    SORT_HEAVY("Heavy", "mpgTableForm:details:heavyheader:sortDiv"),
+    
+    ENTRY_DIVISION_TEAM(null, "mpgTableForm:details:###:name"),
+    ENTRY_LIGHT(null, "mpgTableForm:details:###:light"),
+    ENTRY_MEDIUM(null, "mpgTableForm:details:###:medium"),
+    ENTRY_HEAVY(null, "mpgTableForm:details:###:heavy"),
+    
+    ;
+
+    private String text, url;
+    private String[] IDs;
+    
+    private FuelEfficiencyExpansionEnum(String url){
+        this.url = url;
+    }
+    private FuelEfficiencyExpansionEnum(String text, String ...IDs){
+        this.text=text;
+        this.IDs = IDs;
+    }
+
+    @Override
+    public String[] getIDs() {
+        return IDs;
+    }
+
+    @Override
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String getURL() {
+        return url;
+    }
+}
---
+++ web/src/test/java/com/inthinc/pro/selenium/pageObjects/PageExecutiveFuelEfficiencyExpansion.java
@@ -0,0 +1,130 @@
+package com.inthinc.pro.selenium.pageObjects;
+
+import com.inthinc.pro.automation.elements.Button;
+import com.inthinc.pro.automation.elements.Text;
+import com.inthinc.pro.automation.elements.TextLink;
+import com.inthinc.pro.automation.elements.TextTable;
+import com.inthinc.pro.automation.elements.TextTableLink;
+import com.inthinc.pro.selenium.pageEnums.FuelEfficiencyExpansionEnum;
+import com.inthinc.pro.selenium.pageEnums.TAE.DurationEnumeration;
+
+public class PageExecutiveFuelEfficiencyExpansion extends ExecutiveExpansionBar {
+
+    public PageExecutiveFuelEfficiencyExpansion() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    
+    public class FuelEfficiencyExpansionLinks extends ExpansionBarLinks{
+        
+        public TextLink duration(DurationEnumeration duration){
+            return new TextLink(FuelEfficiencyExpansionEnum.DURATION);
+        }
+        
+        public TextTableLink entryGroup(){
+            return new TextTableLink(FuelEfficiencyExpansionEnum.ENTRY_DIVISION_TEAM);
+        }
+        
+        public TextLink sortGroup(){
+            return new TextLink(FuelEfficiencyExpansionEnum.SORT_DIVISION_TEAM);
+        }
+        
+        public TextLink sortLight(){
+            return new TextLink(FuelEfficiencyExpansionEnum.SORT_LIGHT);
+        }
+        
+        public TextLink sortMedium(){
+            return new TextLink(FuelEfficiencyExpansionEnum.SORT_MEDIUM);
+        }
+        
+        public TextLink sortHeavy(){
+            return new TextLink(FuelEfficiencyExpansionEnum.SORT_HEAVY);
+        }
+    }
+    public class FuelEfficiencyExpansionTexts extends ExpansionBarTexts{
+        
+        public Text title(){
+            return new Text(FuelEfficiencyExpansionEnum.TITLE);
+        }
+        
+        public TextTable entryLight(){
+            return new TextTable(FuelEfficiencyExpansionEnum.ENTRY_LIGHT);
+        }
+        
+        public TextTable entryMedium(){
+            return new TextTable(FuelEfficiencyExpansionEnum.ENTRY_MEDIUM);
+        }
+        
+        public TextTable entryHeavy(){
+            return new TextTable(FuelEfficiencyExpansionEnum.ENTRY_HEAVY);
+        }
+    }
+    public class FuelEfficiencyExpansionTextFields extends ExpansionBarTextFields{}
+    public class FuelEfficiencyExpansionButtons extends ExpansionBarButtons{
+        
+        public Button tools(){
+            return new Button(FuelEfficiencyExpansionEnum.TOOLS);
+        }
+        
+        public Button emailReport(){
+            return new Button(FuelEfficiencyExpansionEnum.EMAIL_REPORT);
+        }
+        
+        public Button exportPDF(){
+            return new Button(FuelEfficiencyExpansionEnum.EXPORT_PDF);
+        }
+        
+        public Button restore(){
+            return new Button(FuelEfficiencyExpansionEnum.RESTORE);
+        }
+    }
+    public class FuelEfficiencyExpansionDropDowns extends ExpansionBarDropDowns{}
+    public class FuelEfficiencyExpansionPopUps extends MastheadPopUps{
+        public FuelEfficiencyExpansionPopUps(){
+            super(page, Types.REPORT, 2);
+        }
+        
+        public Email emailReport(){
+            return new Email();
+        }
+    }
+    
+    
+    public class FuelEfficiencyExpansionPager{
+        public Paging pageIndex(){
+            return new Paging();
+        }
+    }
+    
+    
+    public FuelEfficiencyExpansionPager _page(){
+        return new FuelEfficiencyExpansionPager();
+    }
+    
+    public FuelEfficiencyExpansionLinks _link(){
+        return new FuelEfficiencyExpansionLinks();
+    }
+    
+    public FuelEfficiencyExpansionTexts _text(){
+        return new FuelEfficiencyExpansionTexts();
+    }
+        
+    public FuelEfficiencyExpansionButtons _button(){
+        return new FuelEfficiencyExpansionButtons();
+    }
+    
+    public FuelEfficiencyExpansionTextFields _textField(){
+        return new FuelEfficiencyExpansionTextFields();
+    }
+    
+    public FuelEfficiencyExpansionDropDowns _dropDown(){
+        return new FuelEfficiencyExpansionDropDowns();
+    }
+    
+    public FuelEfficiencyExpansionPopUps _popUp(){
+        return new FuelEfficiencyExpansionPopUps();
+    }
+    
+    
+
+}
