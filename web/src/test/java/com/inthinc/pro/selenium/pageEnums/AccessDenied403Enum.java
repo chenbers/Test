937723810dfb9e4747f2026ac5674653e8be4246
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-07-18 19:58:09 +0000

    Added 403 error page

---
+++ web/src/test/java/com/inthinc/pro/selenium/pageEnums/AccessDenied403Enum.java
@@ -0,0 +1,42 @@
+package com.inthinc.pro.selenium.pageEnums;
+
+import com.inthinc.pro.automation.enums.SeleniumEnums;
+
+public enum AccessDenied403Enum implements SeleniumEnums {
+
+    TITLE("                   Access Denied                ", "//div[@class='panel_title']"),
+    
+    P1(null, "//div[@class='panel_content']/p/span"),
+    
+    LINE(null, "//ul/li[###]/span"),
+    
+    RETURN_TO_HOMEPAGE("return to home page", "error403")
+    ;
+
+    private String text, url;
+    private String[] IDs;
+
+    private AccessDenied403Enum(String url) {
+        this.url = url;
+    }
+
+    private AccessDenied403Enum(String text, String... IDs) {
+        this.text = text;
+        this.IDs = IDs;
+    }
+
+    @Override
+    public String[] getIDs() {
+        return IDs;
+    }
+
+    @Override
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String getURL() {
+        return url;
+    }
+}
---
+++ web/src/test/java/com/inthinc/pro/selenium/pageObjects/Page403ErrorAccessDenied.java
@@ -0,0 +1,46 @@
+package com.inthinc.pro.selenium.pageObjects;
+
+import com.inthinc.pro.automation.elements.Text;
+import com.inthinc.pro.automation.elements.TextLink;
+import com.inthinc.pro.automation.elements.TextTable;
+import com.inthinc.pro.selenium.pageEnums.AccessDenied403Enum;
+
+public class Page403ErrorAccessDenied extends Masthead {
+
+    public Page403ErrorAccessDenied() {
+        checkMe.add(AccessDenied403Enum.TITLE);
+        checkMe.add(AccessDenied403Enum.RETURN_TO_HOMEPAGE);
+        checkMe.add(AccessDenied403Enum.P1);
+    }
+    
+    
+    public class Error403Links extends MastheadLinks{
+        
+        public TextLink returnToHomePage(){
+            return new TextLink(AccessDenied403Enum.RETURN_TO_HOMEPAGE);
+        }
+    }
+    public class Error403Texts extends MastheadTexts{
+        
+        public Text title(){
+            return new Text(AccessDenied403Enum.TITLE);
+        }
+        
+        public Text firstParagraph(){
+            return new Text(AccessDenied403Enum.P1);
+        }
+        
+        public TextTable lineItem(){
+            return new TextTable(AccessDenied403Enum.LINE);
+        }
+    }
+    
+    public Error403Links _link(){
+        return new Error403Links();
+    }
+    
+    public Error403Texts _text(){
+        return new Error403Texts();
+    }
+        
+}
