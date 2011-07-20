98e9560812caa50e5aed28956c3ac8710ba50348
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-07-20 20:49:39 +0000

    Adding the DriverLogins page

---
+++ web/src/test/java/com/inthinc/pro/selenium/pageEnums/NotificationsDriverLoginsEnum.java
@@ -0,0 +1,39 @@
+package com.inthinc.pro.selenium.pageEnums;
+
+import com.inthinc.pro.automation.enums.SeleniumEnums;
+
+public enum NotificationsDriverLoginsEnum implements SeleniumEnums {
+
+    DEFAULT_URL("app/notifications/driverLogins"),
+    
+    TITLE("Driver Logins", "//span[@class='event']"),
+    MESSAGE("                       All driver login notifications.             ", "//span[@class='panel_links']"),
+    
+    ;
+
+    private String text, url;
+    private String[] IDs;
+    
+    private NotificationsDriverLoginsEnum(String url){
+        this.url = url;
+    }
+    private NotificationsDriverLoginsEnum(String text, String ...IDs){
+        this.text=text;
+        this.IDs = IDs;
+    }
+    
+    @Override
+    public String[] getIDs() {
+        return IDs;
+    }
+    
+    @Override
+    public String getText() {
+        return text;
+    }
+    
+    @Override
+    public String getURL() {
+        return url;
+    }
+}
---
+++ web/src/test/java/com/inthinc/pro/selenium/pageObjects/PageNotificationsDriverLogins.java
@@ -0,0 +1,82 @@
+package com.inthinc.pro.selenium.pageObjects;
+
+import com.inthinc.pro.automation.elements.Text;
+import com.inthinc.pro.selenium.pageEnums.NotificationsDriverLoginsEnum;
+
+public class PageNotificationsDriverLogins extends NotificationsEventsBar {
+
+    public PageNotificationsDriverLogins() {
+        page = "driverLogins";
+        url = NotificationsDriverLoginsEnum.DEFAULT_URL;
+        checkMe.add(NotificationsDriverLoginsEnum.TITLE);
+        checkMe.add(NotificationsDriverLoginsEnum.MESSAGE);
+    }
+    
+    
+    public class DriverLoginsLinks extends EventsBarLinks{}
+    public class DriverLoginsTexts extends EventsBarTexts{
+        public Text title(){
+            return new Text(NotificationsDriverLoginsEnum.TITLE);
+        }
+        
+        public Text message(){
+            return new Text(NotificationsDriverLoginsEnum.MESSAGE);
+        }
+    }
+    public class DriverLoginsTextFields extends EventsBarTextFields{}
+    public class DriverLoginsButtons extends EventsBarButtons{}
+    public class DriverLoginsDropDowns extends EventsBarDropDowns{}
+    public class DriverLoginsPopUps extends MastheadPopUps {
+        public DriverLoginsPopUps(){
+            super(page, Types.REPORT, 3);
+        }
+        
+        public EditColumns editColumns() {
+            return new EditColumns();
+        }
+        
+        public Email emailReport(){
+            return new Email();
+        }
+        
+        public ExcludeEvent excludeEvent(){
+            return new ExcludeEvent(true);
+        }
+    }
+    
+    
+    public class DriverLoginsPager{
+        public Paging pageIndex(){
+            return new Paging();
+        }
+    }
+    
+    
+    public DriverLoginsPager _page(){
+        return new DriverLoginsPager();
+    }
+    
+    public DriverLoginsLinks _link(){
+        return new DriverLoginsLinks();
+    }
+    
+    public DriverLoginsTexts _text(){
+        return new DriverLoginsTexts();
+    }
+        
+    public DriverLoginsButtons _button(){
+        return new DriverLoginsButtons();
+    }
+    
+    public DriverLoginsTextFields _textField(){
+        return new DriverLoginsTextFields();
+    }
+    
+    public DriverLoginsDropDowns _dropDown(){
+        return new DriverLoginsDropDowns();
+    }
+    
+    public DriverLoginsPopUps _popUp(){
+        return new DriverLoginsPopUps();
+    }
+}
