8d41b6e71dbd5bac29303c72ef29f3769c77454f
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-07-21 17:17:35 +0000

    formatted

--- web/src/test/java/com/inthinc/pro/selenium/pageObjects/PageFuelStops.java
+++ web/src/test/java/com/inthinc/pro/selenium/pageObjects/PageFuelStops.java
@@ -10,172 +10,170 @@ import com.inthinc.pro.automation.elements.TextTableLink;
 import com.inthinc.pro.selenium.pageEnums.FuelStopsEnum;
 
 public class PageFuelStops extends HOSBar {
-    
-    private static final String page = "fuelStopsTable";
+
+    public class FuelStopsButtons extends HOSBarButtons {
+
+        public TextButton add() {
+            return new TextButton(FuelStopsEnum.ADD);
+        }
+
+        public TextButton refresh() {
+            return new TextButton(FuelStopsEnum.REFRESH);
+        }
+    }
+
+    public class FuelStopsDropDowns extends HOSBarDropDowns {}
 
     public class FuelStopsLinks extends HOSBarLinks {
 
-	public TextLink sortDateTime() {
-	    return new TextLink(FuelStopsEnum.SORT_DATE_TIME);
-	}
+        public TextLink sortDateTime() {
+            return new TextLink(FuelStopsEnum.SORT_DATE_TIME);
+        }
 
-	public TextLink sortVehicle() {
-	    return new TextLink(FuelStopsEnum.SORT_VEHICLE);
-	}
+        public TextLink sortEdited() {
+            return new TextLink(FuelStopsEnum.SORT_EDITED);
+        }
 
-	public TextLink sortVehicleFuel() {
-	    return new TextLink(FuelStopsEnum.SORT_VEHICLE_FUEL);
-	}
+        public TextLink sortLocation() {
+            return new TextLink(FuelStopsEnum.SORT_LOCATION);
+        }
 
-	public TextLink sortTrailerFuel() {
-	    return new TextLink(FuelStopsEnum.SORT_TRAILER_FUEL);
-	}
+        public TextLink sortOdometerAfter() {
+            return new TextLink(FuelStopsEnum.SORT_AFTER);
+        }
 
-	public TextLink sortOdometerBefore() {
-	    return new TextLink(FuelStopsEnum.SORT_BEFORE);
-	}
+        public TextLink sortOdometerBefore() {
+            return new TextLink(FuelStopsEnum.SORT_BEFORE);
+        }
 
-	public TextLink sortOdometerAfter() {
-	    return new TextLink(FuelStopsEnum.SORT_AFTER);
-	}
+        public TextLink sortTrailer() {
+            return new TextLink(FuelStopsEnum.SORT_TRAILER);
+        }
 
-	public TextLink sortTrailer() {
-	    return new TextLink(FuelStopsEnum.SORT_TRAILER);
-	}
+        public TextLink sortTrailerFuel() {
+            return new TextLink(FuelStopsEnum.SORT_TRAILER_FUEL);
+        }
 
-	public TextLink sortLocation() {
-	    return new TextLink(FuelStopsEnum.SORT_LOCATION);
-	}
+        public TextLink sortVehicle() {
+            return new TextLink(FuelStopsEnum.SORT_VEHICLE);
+        }
 
-	public TextLink sortEdited() {
-	    return new TextLink(FuelStopsEnum.SORT_EDITED);
-	}
+        public TextLink sortVehicleFuel() {
+            return new TextLink(FuelStopsEnum.SORT_VEHICLE_FUEL);
+        }
 
-	public TextTableLink valueEdit() {
-	    return new TextTableLink(FuelStopsEnum.VALUE_EDIT);
-	}
-    }
-
-    public class FuelStopsTexts extends HOSBarTexts {
-
-	public TextTable sortDateTime() {
-	    return new TextTable(FuelStopsEnum.VALUE_DATE_TIME);
-	}
-
-	public TextTable valueVehicle() {
-	    return new TextTable(FuelStopsEnum.VALUE_VEHICLE);
-	}
-
-	public TextTable valueVehicleFuel() {
-	    return new TextTable(FuelStopsEnum.VALUE_VEHICLE_FUEL);
-	}
-
-	public TextTable valueTrailerFuel() {
-	    return new TextTable(FuelStopsEnum.VALUE_TRAILER_FUEL);
-	}
-
-	public TextTable valueOdometerBefore() {
-	    return new TextTable(FuelStopsEnum.VALUE_BEFORE);
-	}
-
-	public TextTable valueOdometerAfter() {
-	    return new TextTable(FuelStopsEnum.VALUE_AFTER);
-	}
-
-	public TextTable valueTrailer() {
-	    return new TextTable(FuelStopsEnum.VALUE_TRAILER);
-	}
-
-	public TextTable valueLocation() {
-	    return new TextTable(FuelStopsEnum.VALUE_LOCATION);
-	}
-
-	public TextTable valueEdited() {
-	    return new TextTable(FuelStopsEnum.VALUE_EDITED);
-	}
-
-	public Text counter() {
-	    return new Text(FuelStopsEnum.COUNTER);
-	}
-
-	public Text title() {
-	    return new Text(FuelStopsEnum.TITLE);
-	}
-
-	public TextFieldLabel labelVehicleField() {
-	    return new TextFieldLabel(FuelStopsEnum.VEHICLE_TEXT_FIELD);
-	}
-
-	public Text labelDateRange() {
-	    return new Text(FuelStopsEnum.DATE_RANGE_LABEL);
-	}
-    }
-
-    public class FuelStopsTextFields extends HOSBarTextFields {
-
-	public TextFieldSuggestions vehicle() {
-	    return new TextFieldSuggestions(FuelStopsEnum.VEHICLE_TEXT_FIELD,
-		    FuelStopsEnum.VEHICLE_SUGGESTION_BOX);
-	}
-    }
-
-    public class FuelStopsButtons extends HOSBarButtons {
-	
-	public TextButton refresh(){
-	    return new TextButton(FuelStopsEnum.REFRESH);
-	}
-	
-	public TextButton add(){
-	    return new TextButton(FuelStopsEnum.ADD);
-	}
-    }
-
-    public class FuelStopsDropDowns extends HOSBarDropDowns {
-    }
-
-    public class FuelStopsPopUps extends MastheadPopUps {
-	public FuelStopsPopUps(){
-	    super(page);
-	}
-	
-	public EditColumns editColumns(){
-	    return new EditColumns();
-	}
+        public TextTableLink valueEdit() {
+            return new TextTableLink(FuelStopsEnum.VALUE_EDIT);
+        }
     }
 
     public class FuelStopsPager {
 
-	public Paging pageIndex() {
-	    return new Paging();
-	}
+        public Paging pageIndex() {
+            return new Paging();
+        }
     }
 
-    public FuelStopsLinks _link() {
-	return new FuelStopsLinks();
+    public class FuelStopsPopUps extends MastheadPopUps {
+        public FuelStopsPopUps() {
+            super(page);
+        }
+
+        public EditColumns editColumns() {
+            return new EditColumns();
+        }
     }
 
-    public FuelStopsTexts _text() {
-	return new FuelStopsTexts();
+    public class FuelStopsTextFields extends HOSBarTextFields {
+
+        public TextFieldSuggestions vehicle() {
+            return new TextFieldSuggestions(FuelStopsEnum.VEHICLE_TEXT_FIELD, FuelStopsEnum.VEHICLE_SUGGESTION_BOX);
+        }
     }
+
+    public class FuelStopsTexts extends HOSBarTexts {
+
+        public Text counter() {
+            return new Text(FuelStopsEnum.COUNTER);
+        }
+
+        public Text labelDateRange() {
+            return new Text(FuelStopsEnum.DATE_RANGE_LABEL);
+        }
+
+        public TextFieldLabel labelVehicleField() {
+            return new TextFieldLabel(FuelStopsEnum.VEHICLE_TEXT_FIELD);
+        }
+
+        public TextTable sortDateTime() {
+            return new TextTable(FuelStopsEnum.VALUE_DATE_TIME);
+        }
+
+        public Text title() {
+            return new Text(FuelStopsEnum.TITLE);
+        }
+
+        public TextTable valueEdited() {
+            return new TextTable(FuelStopsEnum.VALUE_EDITED);
+        }
+
+        public TextTable valueLocation() {
+            return new TextTable(FuelStopsEnum.VALUE_LOCATION);
+        }
+
+        public TextTable valueOdometerAfter() {
+            return new TextTable(FuelStopsEnum.VALUE_AFTER);
+        }
+
+        public TextTable valueOdometerBefore() {
+            return new TextTable(FuelStopsEnum.VALUE_BEFORE);
+        }
+
+        public TextTable valueTrailer() {
+            return new TextTable(FuelStopsEnum.VALUE_TRAILER);
+        }
+
+        public TextTable valueTrailerFuel() {
+            return new TextTable(FuelStopsEnum.VALUE_TRAILER_FUEL);
+        }
+
+        public TextTable valueVehicle() {
+            return new TextTable(FuelStopsEnum.VALUE_VEHICLE);
+        }
+
+        public TextTable valueVehicleFuel() {
+            return new TextTable(FuelStopsEnum.VALUE_VEHICLE_FUEL);
+        }
+    }
+
+    private static final String page = "fuelStopsTable";
 
     public FuelStopsButtons _button() {
-	return new FuelStopsButtons();
-    }
-
-    public FuelStopsTextFields _textField() {
-	return new FuelStopsTextFields();
+        return new FuelStopsButtons();
     }
 
     public FuelStopsDropDowns _dropDown() {
-	return new FuelStopsDropDowns();
+        return new FuelStopsDropDowns();
     }
 
-    public FuelStopsPopUps _popUp() {
-	return new FuelStopsPopUps();
+    public FuelStopsLinks _link() {
+        return new FuelStopsLinks();
     }
 
     public FuelStopsPager _page() {
-	return new FuelStopsPager();
+        return new FuelStopsPager();
+    }
+
+    public FuelStopsPopUps _popUp() {
+        return new FuelStopsPopUps();
+    }
+
+    public FuelStopsTexts _text() {
+        return new FuelStopsTexts();
+    }
+
+    public FuelStopsTextFields _textField() {
+        return new FuelStopsTextFields();
     }
 
 }
