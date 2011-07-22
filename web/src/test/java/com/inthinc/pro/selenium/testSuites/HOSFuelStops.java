51f335a650467a0d9d80b8cfc03b681efe781147
tnilson <tnilson@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-07-22 22:57:33 +0000

    Added Changes to the HOS Fuel Stops along with TODO notes. Tina.

--- web/src/test/java/com/inthinc/pro/selenium/testSuites/HOSFuelStops.java
+++ web/src/test/java/com/inthinc/pro/selenium/testSuites/HOSFuelStops.java
@@ -6,11 +6,10 @@ import org.junit.Test;
 import com.inthinc.pro.automation.utils.RandomValues;
 import com.inthinc.pro.selenium.pageObjects.PageFuelStops;
 import com.inthinc.pro.selenium.pageObjects.PageFuelStopsAddEdit;
-import com.inthinc.pro.selenium.pageObjects.PageMyAccount;
+
 
 public class HOSFuelStops extends WebRallyTest {
 
-    private PageMyAccount myAccount;
     private RandomValues random;
     private PageFuelStops myFuelStops;
     private PageFuelStopsAddEdit myFuelStopsAddEdit;
@@ -20,7 +19,6 @@ public class HOSFuelStops extends WebRallyTest {
     @Before
     public void setupPage() {
         random = new RandomValues();
-        myAccount = new PageMyAccount();
         myFuelStops = new PageFuelStops();
         myFuelStopsAddEdit = new PageFuelStopsAddEdit();
     }
@@ -30,15 +28,15 @@ public class HOSFuelStops extends WebRallyTest {
         set_test_case("TC5627");
 
         // .0 Login
-        myAccount.loginProcess(USERNAME, PASSWORD);
-        myAccount._link().hos().click();
+        myFuelStops.loginProcess(USERNAME, PASSWORD);
+        myFuelStops._link().hos().click();
         myFuelStops._link().hosFuelStops().click();
         myFuelStops._textField().vehicle().type("108406");
         myFuelStops._textField().vehicle().getSuggestion("108406").click();
         myFuelStops._button().add().click();
         myFuelStopsAddEdit._button().bottomSave().click();
         myFuelStopsAddEdit._text().errorMaster().validate("1 error(s) occurred. Please verify all the data entered is correct.");
-        myFuelStopsAddEdit._text().errorVehicleFuel().validate("Vehicle or Trailer fuel required.");
+        myFuelStopsAddEdit._text().errorVehicleFuel().validate("Vehicle fuel is required.");
         myFuelStopsAddEdit._button().bottomCancel().click();
 
     }
@@ -46,8 +44,8 @@ public class HOSFuelStops extends WebRallyTest {
     @Test
     public void CancelAddFuelStop() {
         set_test_case("TC5628");
-        myAccount.loginProcess(USERNAME, PASSWORD);
-        myAccount._link().hos().click();
+        myFuelStops.loginProcess(USERNAME, PASSWORD);
+        myFuelStops._link().hos().click();
         myFuelStops._link().hosFuelStops().click();
         myFuelStops._textField().vehicle().type("108406");
         myFuelStops._textField().vehicle().getSuggestion("108406").click();
@@ -59,15 +57,15 @@ public class HOSFuelStops extends WebRallyTest {
         myFuelStopsAddEdit._textField().vehicleFuel().type("123");
         myFuelStopsAddEdit._dropDown().driver().select(2);
         // myFuelStopsAddEdit._text().labelLocation().validate("");
-        myFuelStopsAddEdit._button().bottomCancel().click();
-
+        myFuelStopsAddEdit._button().topCancel().click();
+        //TODO: verify that a new fuel entry was not added.
     }
 
     @Test
     public void AddFuelStop() {
         set_test_case("TC5631");
-        myAccount.loginProcess(USERNAME, PASSWORD);
-        myAccount._link().hos().click();
+        myFuelStops.loginProcess(USERNAME, PASSWORD);
+        myFuelStops._link().hos().click();
         myFuelStops._link().hosFuelStops().click();
         myFuelStops._textField().vehicle().type("108406");
         myFuelStops._textField().vehicle().getSuggestion("108406").click();
@@ -77,14 +75,14 @@ public class HOSFuelStops extends WebRallyTest {
         myFuelStopsAddEdit._textField().vehicleFuel().type("123");
         myFuelStopsAddEdit._dropDown().driver().select(2);
         myFuelStopsAddEdit._button().bottomSave().click();
-
+        //TODO: verify the new fuel entry displays
     }
 
     @Test
     public void CancelEditFuelStop() {
         set_test_case("TC5632");
-        myAccount.loginProcess(USERNAME, PASSWORD);
-        myAccount._link().hos().click();
+        myFuelStops.loginProcess(USERNAME, PASSWORD);
+        myFuelStops._link().hos().click();
         myFuelStops._link().hosFuelStops().click();
         myFuelStops._textField().vehicle().type("108406");
         myFuelStops._textField().vehicle().getSuggestion("108406").click();
@@ -96,14 +94,14 @@ public class HOSFuelStops extends WebRallyTest {
         myFuelStopsAddEdit._textField().vehicleFuel().type("456");
         myFuelStopsAddEdit._textField().trailerFuel().type("456");
         myFuelStopsAddEdit._button().bottomCancel().click();
-
+        //TODO: verify the edits were not saved.
     }
 
     @Test
     public void EditFuelStop() {
         set_test_case("TC5630");
-        myAccount.loginProcess(USERNAME, PASSWORD);
-        myAccount._link().hos().click();
+        myFuelStops.loginProcess(USERNAME, PASSWORD);
+        myFuelStops._link().hos().click();
         myFuelStops._link().hosFuelStops().click();
         myFuelStops._textField().vehicle().type("108406");
         myFuelStops._textField().vehicle().getSuggestion("108406").click();
@@ -113,44 +111,47 @@ public class HOSFuelStops extends WebRallyTest {
         myFuelStopsAddEdit._textField().vehicleFuel().type("456");
         myFuelStopsAddEdit._textField().trailerFuel().type("456");
         myFuelStopsAddEdit._button().bottomSave().click();
-
+        //TODO: verify edits to fuel stop entry
     }
 
     @Test
     public void CancelDeleteFuelStop() {
         set_test_case("TC5633");
-        myAccount.loginProcess(USERNAME, PASSWORD);
-        myAccount._link().hos().click();
+        myFuelStops.loginProcess(USERNAME, PASSWORD);
+        myFuelStops._link().hos().click();
         myFuelStops._link().hosFuelStops().click();
         myFuelStops._textField().vehicle().type("108406");
         myFuelStops._textField().vehicle().getSuggestion("108406").click();
         myFuelStops._checkBox().entryCheckBox().row(1).check();
         myFuelStops._button().delete().click();
         myFuelStops._popUp().delete()._button().cancel().click();
-
+        //TODO: verify fuel stop entry was not deleted.
     }
 
     @Test
     public void DeleteFuelStop() {
         set_test_case("TC5629");
-        myAccount.loginProcess(USERNAME, PASSWORD);
-        myAccount._link().hos().click();
+        myFuelStops.loginProcess(USERNAME, PASSWORD);
+        myFuelStops._link().hos().click();
         myFuelStops._link().hosFuelStops().click();
         myFuelStops._textField().vehicle().type("108406");
         myFuelStops._textField().vehicle().getSuggestion("108406").click();
         myFuelStops._checkBox().entryCheckBox().row(1).check();
         myFuelStops._button().delete().click();
         myFuelStops._popUp().delete()._button().delete().click();
-
+        //TODO: verify deletion of fuel stop entry.
     }
 
     @Test
     public void EditColumns() {
         set_test_case("TC5702");
-        myAccount.loginProcess(USERNAME, PASSWORD);
-        myAccount._link().hos().click();
+        myFuelStops.loginProcess(USERNAME, PASSWORD);
+        myFuelStops._link().hos().click();
         myFuelStops._link().hosFuelStops().click();
-        // myFuelStops._button().().click();
+        myFuelStops._popUp().editColumns()._checkBox().row(3);
+        myFuelStops._popUp().editColumns()._button().save().click();
+       //TODO: finish this test case //myFuelStops.
+      
 
     }
 }
