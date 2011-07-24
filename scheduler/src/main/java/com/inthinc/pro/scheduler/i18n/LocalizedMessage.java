2e12cecee5c058cef835506622aee2ef2e3115be
cjennings <cjennings@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-07-24 22:55:41 +0000

    messed up the check in again (another try)

--- scheduler/src/main/java/com/inthinc/pro/scheduler/i18n/LocalizedMessage.java
+++ scheduler/src/main/java/com/inthinc/pro/scheduler/i18n/LocalizedMessage.java
@@ -1,5 +1,6 @@
 package com.inthinc.pro.scheduler.i18n;
 
+import java.text.MessageFormat;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -19,11 +20,21 @@ public class LocalizedMessage
             currentLocale = locale;
         else
             currentLocale = Locale.getDefault();
-            
+        
         ResourceBundle myResources = ResourceBundle.getBundle(BUNDLE_NAME, currentLocale);
+        
+        String message = null;
         if (myResources.containsKey(key))
-            return myResources.getString(key);
-        return key;
+            message =  myResources.getString(key);
+        else 
+            message = key;
+        
+        if(parameters != null)
+            message = MessageFormat.format(message, parameters);
+
+            
+        return message;
+            
     }
     
     
