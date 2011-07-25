b8df8c90c3271390b343b8ffff194e23c671133b
tnilson <tnilson@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-07-25 22:43:36 +0000

    Added Test Cases and updated others for Fuel Stop page. Tina

--- web/src/test/java/com/inthinc/pro/selenium/testSuites/HOSFuelStops.java
+++ web/src/test/java/com/inthinc/pro/selenium/testSuites/HOSFuelStops.java
@@ -50,13 +50,13 @@ public class HOSFuelStops extends WebRallyTest {
         myFuelStops._textField().vehicle().type("108406");
         myFuelStops._textField().vehicle().getSuggestion("108406").click();
         myFuelStops._button().add().click();
-        myFuelStopsAddEdit._text().labelVehicle().validate("108406");
+        myFuelStopsAddEdit._text().valueVehicle().validatePresence(true);
         // myFuelStopsAddEdit._textField().date();
-        myFuelStopsAddEdit._text().timeMessage().validate("Fuel stops more than 25 days old will not be included in the IFTA aggregation.");
+        myFuelStopsAddEdit._text().timeMessage().validatePresence(true);
         myFuelStopsAddEdit._textField().trailer().type("123");
         myFuelStopsAddEdit._textField().vehicleFuel().type("123");
         myFuelStopsAddEdit._dropDown().driver().select(2);
-        // myFuelStopsAddEdit._text().labelLocation().validate("");
+        myFuelStopsAddEdit._text().valueLocation().validatePresence(true);
         myFuelStopsAddEdit._button().topCancel().click();
         //TODO: verify that a new fuel entry was not added.
     }
@@ -145,13 +145,115 @@ public class HOSFuelStops extends WebRallyTest {
     @Test
     public void EditColumns() {
         set_test_case("TC5702");
+        //0. Login
         myFuelStops.loginProcess(USERNAME, PASSWORD);
         myFuelStops._link().hos().click();
         myFuelStops._link().hosFuelStops().click();
-        myFuelStops._popUp().editColumns()._checkBox().row(3);
+        
+        //1. Edit - uncheck columns and save
+        myFuelStops._button().editColumns().click();
+        myFuelStops._popUp().editColumns()._checkBox().row(3).uncheck();
+        myFuelStops._popUp().editColumns()._checkBox().row(7).uncheck();
         myFuelStops._popUp().editColumns()._button().save().click();
-       //TODO: finish this test case //myFuelStops.
-      
+        
+        //2. Verify columns do not display after saving.
+        myFuelStops._link().sortVehicle().validatePresence(false);
+        myFuelStops._link().sortVehicleFuel().validatePresence(false);
+        
+        //3. Edit columns and select the previously unchecked columns.
+        myFuelStops._button().editColumns().click();
+        myFuelStops._popUp().editColumns()._checkBox().row(3).check();
+        myFuelStops._popUp().editColumns()._checkBox().row(7).check();
+        myFuelStops._popUp().editColumns()._button().save().click();
+   
+        //4. Verify columns display after saving.
+        myFuelStops._link().sortVehicle().validatePresence(true);
+        myFuelStops._link().sortVehicleFuel().validatePresence(true);
 
     }
+    @Test
+    public void VehicleField() {
+        set_test_case("TC5700");
+        //0. Login
+        myFuelStops.loginProcess(USERNAME, PASSWORD);
+        myFuelStops._link().hos().click();
+        myFuelStops._link().hosFuelStops().click();
+        
+        //1. Vehicle field remains blank when only clicking on refresh
+        myFuelStops._textField().vehicle().clear();
+        myFuelStops._button().refresh().click();
+        myFuelStops._textField().vehicle().validate("");
+       
+        //2. Enter a valid TIWI ID - should not offer suggestions
+        myFuelStops._textField().vehicle().type("Tina");
+        myFuelStops._textField().vehicle().getSuggestion(1).validate("No vehicles found");
+        
+        //3. Enter a valid MCM ID - select from suggestions
+        myFuelStops._textField().vehicle().clear();
+        myFuelStops._textField().vehicle().type("108406");
+        myFuelStops._textField().vehicle().getSuggestion("108406").click();
+        myFuelStops._textField().vehicle().validatePresence(true);
+                
+    }
+    
+    @Test
+    public void FuelFields() {
+        set_test_case("TC5703");
+        //0. Login
+        myFuelStops.loginProcess(USERNAME, PASSWORD);
+        myFuelStops._link().hos().click();
+        myFuelStops._link().hosFuelStops().click();
+    
+        //1. Get vehicle and click on Add
+        myFuelStops._textField().vehicle().type("108406");
+        myFuelStops._textField().vehicle().getSuggestion("108406").click();
+        myFuelStops._button().add().click();
+        
+        //2. Enter Alpha char and save.
+        myFuelStopsAddEdit._textField().vehicleFuel().type("abcdefg");
+        myFuelStopsAddEdit._dropDown().driver().select(2);
+        myFuelStopsAddEdit._button().topSave().click();
+        
+        //3. validate error message.
+        myFuelStopsAddEdit._text().errorVehicleFuel().validate("Must be a number greater than zero");
+        myFuelStopsAddEdit._button().bottomCancel().click();
+        
+        //4. Enter special char and save
+        myFuelStops._button().add().click();
+        myFuelStopsAddEdit._textField().vehicleFuel().type("&$#!");
+        myFuelStopsAddEdit._dropDown().driver().select(2);
+        myFuelStopsAddEdit._button().topSave().click();
+        
+        //5. validate error message.
+        myFuelStopsAddEdit._text().errorVehicleFuel().validate("Must be a number greater than zero");
+        myFuelStopsAddEdit._button().bottomCancel().click();
+        
+        //6. Enter zero and save
+        myFuelStops._button().add().click();
+        myFuelStopsAddEdit._textField().vehicleFuel().type("0");
+        myFuelStopsAddEdit._dropDown().driver().select(2);
+        myFuelStopsAddEdit._button().topSave().click();
+        
+        //7. validate error message.
+        myFuelStopsAddEdit._text().errorVehicleFuel().validate("Vehicle or Trailer fuel required.");
+        myFuelStopsAddEdit._button().bottomCancel().click();
+        
+        //8. Enter fractions and save
+        myFuelStops._button().add().click();
+        
+        myFuelStopsAddEdit._textField().vehicleFuel().type("1.5");
+        myFuelStopsAddEdit._dropDown().driver().select(2);
+        myFuelStopsAddEdit._button().topSave().click();
+        
+        //9. Validate fraction fuel entry was saved
+        myFuelStops._text().valueVehicleFuel().row(1).validate("1.5Gallons");
+        
+        //10. Delete fraction fuel entry. 
+        myFuelStops._checkBox().entryCheckBox().row(1).check();
+        myFuelStops._button().delete().click();
+        myFuelStops._popUp().delete()._button().delete().click();
+               
+    
+    } 
+    
 }
