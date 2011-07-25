4c83f8629f517a3fd68a96e7bb56ff192a221202
cjennings <cjennings@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-07-25 18:06:22 +0000

    us3139 - localizing hos violations

--- reports/src/main/java/com/inthinc/pro/reports/hos/model/Violation.java
+++ reports/src/main/java/com/inthinc/pro/reports/hos/model/Violation.java
@@ -14,14 +14,24 @@ public class Violation {
     RuleViolationTypes type;
     Long minutes;
     RuleSetType ruleSetType;
+    Locale locale;
     
-    ResourceBundle resourceBundle = ReportType.HOS_VIOLATIONS_DETAIL_REPORT.getResourceBundle(Locale.getDefault());
+    ResourceBundle resourceBundle;
 
-    
     public Violation(RuleSetType ruleSetType, RuleViolationTypes type, Long minutes) {
         this.type = type;
         this.minutes = minutes;
         this.ruleSetType = ruleSetType;
+        this.locale = Locale.getDefault();
+        this.resourceBundle = ReportType.HOS_VIOLATIONS_DETAIL_REPORT.getResourceBundle(locale);
+    }
+    
+    public Violation(RuleSetType ruleSetType, RuleViolationTypes type, Long minutes, Locale locale) {
+        this.type = type;
+        this.minutes = minutes;
+        this.ruleSetType = ruleSetType;
+        this.locale = locale;
+        this.resourceBundle = ReportType.HOS_VIOLATIONS_DETAIL_REPORT.getResourceBundle(locale);
     }
     public RuleViolationTypes getType() {
         return type;
@@ -44,14 +54,19 @@ public class Violation {
     }
     
     public String getDetails() {
-        String details = ViolationsDetails.getDetails(ruleSetType, type);
+        String details = ViolationsDetails.getDetails(ruleSetType, type, getLocale());
         if (details == null || details.isEmpty()) {
             details = MessageUtil.getBundleString(resourceBundle, "violation." + getType().getName()); 
         }
         return details;
     }
-
     public String getCfr() {
         return ViolationsDetails.getCFR(ruleSetType, type);
+    }
+    public Locale getLocale() {
+        return locale;
+    }
+    public void setLocale(Locale locale) {
+        this.locale = locale;
     }
 }
