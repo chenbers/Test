8041975e16fedc3ca1d40e686397f29112f287c3
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-09-02 18:32:11 +0000

    Adding an element for spinners, and updating the TimeOfDay element appropriately

---
+++ automation/src/main/java/com/inthinc/pro/automation/elements/TextFieldWithSpinner.java
@@ -0,0 +1,54 @@
+package com.inthinc.pro.automation.elements;
+
+import com.inthinc.pro.automation.enums.SeleniumEnumWrapper;
+import com.inthinc.pro.automation.enums.SeleniumEnums;
+import com.inthinc.pro.automation.utils.Id;
+import com.inthinc.pro.automation.utils.Xpath;
+
+public class TextFieldWithSpinner extends TextField {
+    
+    private SeleniumEnumWrapper spinnerEnum;
+    
+    public TextFieldWithSpinner(SeleniumEnums anEnum){
+        super(anEnum, ("Edit"));
+        spinnerEnum = new SeleniumEnumWrapper(anEnum);
+        spinnerEnum.replaceWord("Buttons");
+    }
+    
+    public TextFieldWithSpinner(SeleniumEnums anEnum, String type){
+        super(anEnum, (type+"Edit"));
+        spinnerEnum = new SeleniumEnumWrapper(anEnum);
+        spinnerEnum.replaceWord(type + "Buttons");
+    }
+    
+    private void setIds(Integer upOrDown){
+        String[] newIDs = spinnerEnum.getIDs();
+        int last = newIDs.length - 1;
+        newIDs[last] = Xpath.start().table(Id.id(newIDs[last])).tbody().tr(upOrDown.toString()).td().input().toString();
+    }
+    
+    public Button up(){
+        setIds(1);
+        return new Button(spinnerEnum){
+            @Override
+            public Button click(){
+                selenium.mouseDown(spinnerEnum);
+                selenium.mouseUp(spinnerEnum);
+                return this;
+            }
+        };
+    }
+    
+    public Button down(){
+        setIds(2);
+        return new Button(spinnerEnum){
+            @Override
+            public Button click(){
+                selenium.mouseDown(spinnerEnum);
+                selenium.mouseUp(spinnerEnum);
+                return this;
+            }
+        };
+    }
+
+}
--- automation/src/main/java/com/inthinc/pro/automation/elements/TimeOfDay.java
+++ automation/src/main/java/com/inthinc/pro/automation/elements/TimeOfDay.java
@@ -22,28 +22,16 @@ public class TimeOfDay extends MasterTest{
     }
 
     
-    public TextField hours(){
-        return new TextField(setID(), "hh");
+    public TextFieldWithSpinner hours(){
+        return new TextFieldWithSpinner(setID(), "hh");
     }
     
-    public Spinner hourSpinner(){
-        return new Spinner("hh");
+    public TextFieldWithSpinner minutes(){
+        return new TextFieldWithSpinner(setID(), "mm");
     }
     
-    public TextField minutes(){
-        return new TextField(setID(), "mm");
-    }
-    
-    public Spinner minutesSpinner(){
-        return new Spinner("mm");
-    }
-    
-    public TextField seconds(){
-        return new TextField(setID(), "ss");
-    }
-    
-    public Spinner secondsSpinner(){
-        return new Spinner("ss");
+    public TextFieldWithSpinner seconds(){
+        return new TextFieldWithSpinner(setID(), "ss");
     }
     
     public DropDown amPm(){
@@ -75,45 +63,5 @@ public class TimeOfDay extends MasterTest{
         }
         return temp;
     }
-    
-    public class Spinner {
-        private SeleniumEnumWrapper spinnerEnum;
-        
-        public Spinner(String type){
-            spinnerEnum = new SeleniumEnumWrapper(myEnum);
-            spinnerEnum.replaceWord(type+"Buttons");
-        }
-        
-        private void setIds(Integer upOrDown){
-            String[] newIDs = spinnerEnum.getIDs();
-            int last = newIDs.length - 1;
-            newIDs[last] = Xpath.start().table(Id.id(newIDs[last])).tbody().tr(upOrDown.toString()).td().input().toString();
-        }
-        
-        public Button up(){
-            setIds(1);
-            return new Button(spinnerEnum){
-                @Override
-                public Button click(){
-                    selenium.mouseDown(spinnerEnum);
-                    selenium.mouseUp(spinnerEnum);
-                    return this;
-                }
-            };
-        }
-        
-        public Button down(){
-            setIds(2);
-            return new Button(spinnerEnum){
-                @Override
-                public Button click(){
-                    selenium.mouseDown(spinnerEnum);
-                    selenium.mouseUp(spinnerEnum);
-                    return this;
-                }
-            };
-        }
-    }
-    
     
 }
