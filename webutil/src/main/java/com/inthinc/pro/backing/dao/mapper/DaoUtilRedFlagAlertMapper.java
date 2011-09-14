f5701785320ac08eb2daf761cf0f175b4e6691aa
jhoward <jhoward@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-09-14 21:09:37 +0000

    fix to get getAlert methods to work in the util

--- webutil/src/main/java/com/inthinc/pro/backing/dao/mapper/DaoUtilRedFlagAlertMapper.java
+++ webutil/src/main/java/com/inthinc/pro/backing/dao/mapper/DaoUtilRedFlagAlertMapper.java
@@ -1,9 +1,11 @@
 package com.inthinc.pro.backing.dao.mapper;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 import com.inthinc.pro.dao.annotations.ConvertColumnToField;
 import com.inthinc.pro.dao.annotations.ConvertFieldToColumn;
+import com.inthinc.pro.model.AlertMessageType;
 import com.inthinc.pro.model.RedFlagAlert;
 import com.inthinc.pro.model.configurator.TiwiproSpeedingConstants;
 
@@ -57,6 +59,27 @@ public class DaoUtilRedFlagAlertMapper extends DaoUtilMapper {
                 buf.append(" ");
             }
             ((Map<String, Object>)value).put("speedSettings", buf.toString());
+        }
+    }
+    @SuppressWarnings("unchecked")
+    @ConvertFieldToColumn(fieldName = "types")
+    public void alertTypeMaskToColumn(RedFlagAlert redFlagAlert, Object value)
+    {
+        if (Map.class.isInstance(value) && (redFlagAlert.getTypes() != null))
+        {
+            Long alertTypeMask = AlertMessageType.convertTypes(redFlagAlert.getTypes());
+            ((Map<String, Object>)value).put("alertTypeMask", alertTypeMask);
+        }
+    }
+    @ConvertColumnToField(columnName = "alertTypeMask")
+    public void alertTypeMaskToModel(RedFlagAlert redFlagAlert, Object value)
+    {
+        if (redFlagAlert == null || value == null)
+            return;
+
+        if (value instanceof Long)
+        {
+            redFlagAlert.setTypes(new ArrayList<AlertMessageType>(AlertMessageType.getAlertMessageTypes((Long)value)));
         }
     }
 }
