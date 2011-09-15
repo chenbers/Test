9f94de091f61d83e9298d158514bb83e916f97bc
tnilson <tnilson@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-09-15 00:05:38 +0000

    Submitting new account login with changes.

--- web/src/test/java/com/inthinc/pro/selenium/testSuites/MyAccountTest.java
+++ web/src/test/java/com/inthinc/pro/selenium/testSuites/MyAccountTest.java
@@ -14,8 +14,8 @@ public class MyAccountTest extends WebRallyTest {
 	
 	private PageMyAccount myAccountPage;
 	private RandomValues random;
-	private String USERNAME = "tnilson";
-	private String PASSWORD = "password123";
+	private String USERNAME = "tinaauto";
+	private String PASSWORD = "password";
 	
 	@Before
 	public void setupPage() {
@@ -35,28 +35,28 @@ public class MyAccountTest extends WebRallyTest {
 		my._link().myAccount().click();
 		
 	/*Login Info*/
-		my._text().userName().validate("tnilson");
+		my._text().userName().validate("tinaauto");
 		my._text().locale().validate(Locale.ENGLISH.getText());
 		my._text().measurement().validate(Measurement.ENGLISH.getText());
-		my._text().fuelEfficiency().validate(Fuel_Ratio.ENGLISH_MILES_UK.getText());
+		my._text().fuelEfficiency().validate(Fuel_Ratio.ENGLISH_MILES_US.getText());
 		
 	/*Account Info*/
-		my._text().name().validate("Tina L Nilson");
-		my._text().group().validate("Top");
-		my._text().team().validate("Skip's Team");
+		my._text().name().validate("Tina Automation");
+		my._text().group().validate("Tina's Auto Team");
+		my._text().team().validate("Tina's Auto Team");
 	
 	/*Red Flags*/
-		my._text().redFlagInfo().validate("Text Message 1");
-		my._text().redFlagWarn().validate("E-mail 1");
-		my._text().redFlagCritical().validate("Phone 1");
+		my._text().redFlagInfo().validate("E-mail 1");
+		my._text().redFlagWarn().validate("Phone 2");
+		my._text().redFlagCritical().validate("Text Message 1");
 		
 	/*Contact Info*/
-		my._text().email1().validate("tina1965@test.com");
-		my._text().email2().validate("tlc1965@test.com");
-		my._text().phone1().validate("801-777-7777");
-		my._text().phone2().validate("801-999-9999");
-		my._text().textMessage1().validate("8017779999@tmomail.net");
-		my._text().textMessage2().validate("8019997777@tmomail.net");
+		my._text().email1().validate("tinaauto@test.com");
+		my._text().email2().validate("712@test.com");
+		my._text().phone1().validate("111-222-3333");
+		my._text().phone2().validate("444-555-6666");
+		my._text().textMessage1().validate("2223334444@tmomail.com");
+		my._text().textMessage2().validate("5556667777@tmomail.com");
 	}
 
 }
