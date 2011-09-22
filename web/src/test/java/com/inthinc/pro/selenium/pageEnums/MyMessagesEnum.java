fc6b053292543ea1cb71966cdb97fadcae904852
tfollmerjr <tfollmerjr@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-09-22 18:29:17 +0000

    PageMyMessages and MyMessagesEnum

---
+++ web/src/test/java/com/inthinc/pro/selenium/pageEnums/MyMessagesEnum.java
@@ -0,0 +1,93 @@
+package com.inthinc.pro.selenium.pageEnums;
+
+import com.inthinc.pro.automation.interfaces.SeleniumEnums;
+
+public enum MyMessagesEnum implements SeleniumEnums {
+    
+    TITLE(null, "//span[@class='textmsg']"),
+    DEFAULT_URL("/app/messages"),
+    
+    TEAM_DROP_DOWN("Team:", "//div[@class='dhx_combo_input']"),
+    
+    START_DATE("Start:", "display-form:calendarStart"),
+    STOP_DATE("End:", "display-form:calendarEnd"),
+    
+    REFRESH_BUTTON("Refresh", "display-form:refresh"),
+    
+    REPLY_TO_SELECTED(null, "display-form:reply"),
+    
+    SELECT_ALL_CHECK_BOX(null, "display-form:messages:selectAll"),
+    
+    SENT_SORT("Sent", "display-form:messages:dateheader:sortDiv"),
+    
+    TO_HEADER("To", "display-form:messages:toheader:sortDiv"),
+    FROM_HEADER("From", "display-form:messages:fromheader:sortDiv"),
+    MESSAGE_HEADER("Messages", "display-form:messages:msgheader:sortDiv"),
+    
+    TABLE_CHECK_BOX(null, "display-form:messages:###:select"),
+    SENT_DATE_TABLE(null, "display-form:messages:###:date"),
+    TO_NAME_TABLE(null, "display-form:messages:###:to"),
+    FROM_NAME_TABLE(null, "display-form:messages:###:from"),
+    MESSAGE_TABLE(null, "display-form:messages:###:msg"),
+    
+    INBOX_BUTTON("Inbox","side-nav-form:inbox-vlt-inbox"),
+    COMPOSE_BUTTON("Compose", "side-nav-form:inbox-vlt-sendMail"),
+    SENT_BUTTON("Sent", "side-nav-form:inbox-vlt-sentMail"),
+    
+    SENT_ITEMS("Sent Items", "//tr[1]/td[@class='inbox'][1]"),
+    NUMBER_MESSAGES("# Messages", "//tr[1]/td[@class='inbox'][2]"),
+    
+    DRIVER_REMOVE_ALL(null, "display-form:driverListremoveAlllink"),
+    DRIVER_REMOVE(null, "display-form:driverListremovelink"),
+    DRIVER_COPY(null, "display-form:driverListcopylink"),
+    DRIVER_COPY_ALL(null, "display-form:driverListcopyAlllink"),
+    VEHICLE_REMOVE_ALL(null, "display-form:vehicleListremoveAlllink"),
+    VEHICLE_REMOVE(null, "display-form:vehicleListremovelink"),
+    VEHICLE_COPY(null, "display-form:vehicleListcopylink"),
+    VEHICLE_COPY_ALL(null, "display-form:vehicleListcopyAlllink"),
+    GROUP_REMOVE_ALL(null, "display-form:groupListremoveAlllink"),
+    GROUP_REMOVE(null, "display-form:groupListremovelink"),
+    GROUP_COPY(null, "display-form:groupListcopylink"),
+    GROUP_COPY_ALL(null, "display-form:groupListcopyAlllink"),
+    
+    //TODO DTANNER TO FILL IN
+    DRIVER_HEADER("Drivers", ""),
+    DRIVER_LEFT(null, ""),
+    DRIVER_RIGHT(null, ""),
+    VEHICLE_HEADER("Vehicles", ""),
+    VEHICLE_LEFT(null, ""),
+    VEHICLE_RIGHT(null, ""),
+    GROUP_HEADER("Groups", ""),
+    GROUP_LEFT(null, ""),
+    GROUP_RIGHT(null, ""),
+    COMPOSE_MSG_HEADER("Compose Message", ""),
+    COMPOSE_MSG_TEXT_FIELD(null, "display-form:msgText")
+    ;
+    
+    private String text, url;
+    private String[] IDs;
+    
+    private MyMessagesEnum(String url){
+        this.url = url;
+    }
+    private MyMessagesEnum(String text, String ...IDs){
+        this.text=text;
+        this.IDs = IDs;
+    }
+
+    @Override
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String[] getIDs() {
+        return IDs;
+    }
+
+    @Override
+    public String getURL() {
+        return url;
+    }
+    
+}
---
+++ web/src/test/java/com/inthinc/pro/selenium/pageObjects/PageMyMessages.java
@@ -0,0 +1,172 @@
+package com.inthinc.pro.selenium.pageObjects;
+
+import com.inthinc.pro.automation.elements.Button;
+import com.inthinc.pro.automation.elements.CheckBox;
+import com.inthinc.pro.automation.elements.DropDown;
+import com.inthinc.pro.automation.elements.Text;
+import com.inthinc.pro.automation.elements.TextField;
+import com.inthinc.pro.automation.elements.TextLink;
+import com.inthinc.pro.selenium.pageEnums.MyMessagesEnum;
+
+public class PageMyMessages extends NavigationBar {
+    
+    public PageMyMessages() {
+        url = MyMessagesEnum.DEFAULT_URL;
+        checkMe.add(MyMessagesEnum.TEAM_DROP_DOWN);
+        checkMe.add(MyMessagesEnum.TITLE);
+    }
+    
+    public class MyMessagesLinks extends NavigationBarLinks {
+        public TextLink replyToSelected() {
+            return new TextLink(MyMessagesEnum.REPLY_TO_SELECTED);
+        }   
+    }
+    
+    public class MyMessagesTextFields extends NavigationBarTextFields{
+        public TextField startDate(){
+            return new TextField(MyMessagesEnum.START_DATE);
+        }
+    
+        public TextField stopDate(){
+            return new TextField(MyMessagesEnum.STOP_DATE);
+        }
+        
+        public TextField composeMsgTextField(){
+            return new TextField(MyMessagesEnum.COMPOSE_MSG_TEXT_FIELD);
+        }
+    }
+    
+    public class MyMessagesDropDowns extends NavigationBarDropDowns{
+        public DropDown teamDropDown(){
+            return new DropDown(MyMessagesEnum.TEAM_DROP_DOWN);
+        }
+    }
+    
+    public class MyMessagesInboxButtons extends NavigationBarButtons {
+        public Button inboxButton(){
+            return new Button(MyMessagesEnum.INBOX_BUTTON);
+        }
+        
+        public Button composeButton(){
+            return new Button(MyMessagesEnum.COMPOSE_BUTTON);
+        }
+        
+        public Button sentButton(){
+            return new Button(MyMessagesEnum.SENT_BUTTON);
+        }
+        
+        public Button refreshButton(){
+            return new Button(MyMessagesEnum.REFRESH_BUTTON);
+        } 
+        
+        public Button driverRemoveAll(){
+            return new Button(MyMessagesEnum.DRIVER_REMOVE_ALL);
+        }
+        
+        public Button driverRemove(){
+            return new Button(MyMessagesEnum.DRIVER_REMOVE);
+        }
+        
+        public Button driverCopy(){
+            return new Button(MyMessagesEnum.DRIVER_COPY);
+        }
+        
+        public Button driverCopyAll(){
+            return new Button(MyMessagesEnum.DRIVER_COPY_ALL);
+        }
+        
+        public Button vehicleRemoveAll(){
+            return new Button(MyMessagesEnum.VEHICLE_REMOVE_ALL);
+        }
+        
+        public Button vehicleRemove(){
+            return new Button(MyMessagesEnum.VEHICLE_REMOVE);
+        }
+        
+        public Button vehicleCopy(){
+            return new Button(MyMessagesEnum.VEHICLE_COPY);
+        }
+        
+        public Button vehicleCopyAll(){
+            return new Button(MyMessagesEnum.VEHICLE_COPY_ALL);
+        }
+        
+        public Button groupRemoveAll(){
+            return new Button(MyMessagesEnum.GROUP_REMOVE_ALL);
+        }
+        
+        public Button groupRemove(){
+            return new Button(MyMessagesEnum.GROUP_REMOVE);
+        }
+        
+        public Button groupCopy(){
+            return new Button(MyMessagesEnum.GROUP_COPY);
+        }
+        
+        public Button groupCopyAll(){
+            return new Button(MyMessagesEnum.GROUP_COPY_ALL);
+        }
+    }
+    
+    public class MyMessagesText {
+        
+        public Text driverHeader() {
+            return new Text(MyMessagesEnum.DRIVER_HEADER);
+        }
+        
+        public Text vehicleHeader() {
+            return new Text(MyMessagesEnum.VEHICLE_HEADER);
+        }
+        
+        public Text groupHeader() {
+            return new Text(MyMessagesEnum.GROUP_HEADER);
+        }
+        
+        public Text composeMsgHeader() {
+            return new Text(MyMessagesEnum.COMPOSE_MSG_HEADER);
+        }
+        
+        public Text sentItems() {
+            return new Text(MyMessagesEnum.SENT_ITEMS);
+        }
+        
+        public Text numberOfMessages() {
+            return new Text(MyMessagesEnum.NUMBER_MESSAGES);
+        }
+    }
+    
+    public class MyMessagesCheckBox {
+        
+        public CheckBox selectAllCheckBox(){
+            return new CheckBox(MyMessagesEnum.SELECT_ALL_CHECK_BOX);
+        }
+        
+        public CheckBox tableCheckBox(){
+            return new CheckBox(MyMessagesEnum.TABLE_CHECK_BOX);
+        }
+    }
+    
+    public MyMessagesLinks _link() {
+        return new MyMessagesLinks();
+    }
+
+    public MyMessagesTextFields _textField() {
+        return new MyMessagesTextFields();
+    }
+    
+    public MyMessagesDropDowns _dropDown() {
+        return new MyMessagesDropDowns();
+    }
+    
+    public MyMessagesInboxButtons _button() {
+        return new MyMessagesInboxButtons();
+    }
+    
+    public MyMessagesText _text() {
+        return new MyMessagesText();
+    }
+    
+    public MyMessagesCheckBox _checkBox() {
+        return new MyMessagesCheckBox();
+    }
+}
