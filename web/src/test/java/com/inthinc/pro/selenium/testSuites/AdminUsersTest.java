d2190e574cba61397ce3e5ace69660a8184acf40
tnilson <tnilson@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-09-22 21:17:43 +0000

    Added fixes for HOS Fuel Stop and updated new script for AdminUsersTest.

--- web/src/test/java/com/inthinc/pro/selenium/testSuites/AdminUsersTest.java
+++ web/src/test/java/com/inthinc/pro/selenium/testSuites/AdminUsersTest.java
@@ -13,12 +13,14 @@ import com.inthinc.pro.selenium.pageEnums.TAE.Locale;
 import com.inthinc.pro.selenium.pageEnums.TAE.Measurement;
 import com.inthinc.pro.selenium.pageObjects.PageAdminUsers;
 import com.inthinc.pro.selenium.pageObjects.PageAdminUserDetails;
+import com.inthinc.pro.selenium.pageObjects.PageMyAccount;
 
-/*My Account test*/
+/*My Account test, compares the user account to the my account for accuracy*/
 public class AdminUsersTest extends WebRallyTest {
 	
 	
 	private PageAdminUsers my;
+	private PageMyAccount myAccount;
 	private PageAdminUserDetails myAdminUserDetails;
 	private RandomValues random;
 	private String USERNAME = "tinaauto";
@@ -28,8 +30,16 @@ public class AdminUsersTest extends WebRallyTest {
 	public void setupPage() {
 		random = new RandomValues();
 		my = new PageAdminUsers();
+		myAccount = new PageMyAccount();
 		myAdminUserDetails = new PageAdminUserDetails();
 	}
+	
+	/**
+	 * AccountInformation test verifies MyAccount data with Admin User data.
+	 * testDependencies:
+	 * must have admin access
+	 * 
+	 */
 	
 	@Test
 	public void AccountInformation(){
@@ -54,108 +64,73 @@ public class AdminUsersTest extends WebRallyTest {
 	
 		
 	/*User Info*/
-		myAdminUserDetails._text().labels(AdminUserDetailsEnum.FIRST_NAME).validate("First Name:");
-		myAdminUserDetails._text().values(AdminUserDetailsEnum.FIRST_NAME).validate("Tina");
-		myAdminUserDetails._text().labels(AdminUserDetailsEnum.MIDDLE_NAME).validate("Middle Name:");
-		myAdminUserDetails._text().values(AdminUserDetailsEnum.MIDDLE_NAME).validate("Test");
-		myAdminUserDetails._text().labels(AdminUserDetailsEnum.LAST_NAME).validate("Last Name:");
-		myAdminUserDetails._text().values(AdminUserDetailsEnum.LAST_NAME).validate("Automation");
-		myAdminUserDetails._text().labels(AdminUserDetailsEnum.SUFFIX).validate("Suffix:");
-		myAdminUserDetails._text().values(AdminUserDetailsEnum.SUFFIX).validate("Jr.");
-		myAdminUserDetails._text().labels(AdminUserDetailsEnum.DOB).validate("DOB:");
-		myAdminUserDetails._text().values(AdminUserDetailsEnum.DOB).validate("Sep 1, 1980");
-		myAdminUserDetails._text().labels(AdminUserDetailsEnum.GENDER).validate("Gender:");
-		myAdminUserDetails._text().values(AdminUserDetailsEnum.GENDER).validate("Female");
+		
+		String firstname =myAdminUserDetails._text().values(AdminUserDetailsEnum.FIRST_NAME).getText();
+		String middlename =myAdminUserDetails._text().values(AdminUserDetailsEnum.MIDDLE_NAME).getText();
+		String lastname =myAdminUserDetails._text().values(AdminUserDetailsEnum.LAST_NAME).getText();
+		String suffix =myAdminUserDetails._text().values(AdminUserDetailsEnum.SUFFIX).getText();
+		
 		
 	/*Driver Info*/
-		myAdminUserDetails._text().labels(AdminUserDetailsEnum.LICENSE_NUMBER).validate("Driver License #:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.LICENSE_NUMBER).validate("123456789");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.CLASS).validate("Class:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.CLASS).validate("C");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.STATE).validate("State:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.STATE).validate("Utah");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.EXPIRATION).validate("Expiration:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.EXPIRATION).validate("Sep 1, 2020");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.CERTIFICATIONS).validate("Certifications:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.CERTIFICATIONS).validate("HAZMAT");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.DOT).validate("DOT:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.DOT).validate("US Oil 8 Day");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.TEAM).validate("Team:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.TEAM).validate("Tina's Auto Team");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.DRIVER_STATUS).validate("Status:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.DRIVER_STATUS).validate("Active");
+		
+        String team =myAdminUserDetails._text().values(AdminUserDetailsEnum.TEAM).getText();
         
      /*Employee Info*/
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.EMP_ID).validate("Employee ID:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.EMP_ID).validate("TINAAUTO");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.REPORTS_TO).validate("Reports To:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.REPORTS_TO).validate("David");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.TITLE).validate("Title:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.TITLE).validate("Driver Manager");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.LOCALE).validate("Locale:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.LOCALE).validate("English (United States)");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.TIME_ZONE).validate("Time Zone:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.TIME_ZONE).validate("US/Mountain");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.MEASUREMENT).validate("Measurement:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.MEASUREMENT).validate("English");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.FUEL_EFFICIENCY_RATIO).validate("Fuel Efficiency Ratio:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.FUEL_EFFICIENCY_RATIO).validate("Miles Per Gallon (US)");
+      
+        String locale = myAdminUserDetails._text().values(AdminUserDetailsEnum.LOCALE).getText();
+        String measurement =myAdminUserDetails._text().values(AdminUserDetailsEnum.MEASUREMENT).getText();
+        String fuelratio =myAdminUserDetails._text().values(AdminUserDetailsEnum.FUEL_EFFICIENCY_RATIO).getText();
 
       
      /* Login Info*/
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.USER_NAME).validate("User Name:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.USER_NAME).validate("tinaauto");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.GROUP).validate("Group:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.GROUP).validate("Tina's Auto Team");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.ROLES).validate("Roles:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.ROLES).validate("Normal\nAdmin");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.USER_STATUS).validate("Status:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.USER_STATUS).validate("Active");  
-        
+       
+        String loginname =myAdminUserDetails._text().values(AdminUserDetailsEnum.USER_NAME).getText();
+        String group =myAdminUserDetails._text().values(AdminUserDetailsEnum.GROUP).getText();
+               
       /*Notifications*/
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.EMAIL_1).validate("E-mail 1:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.EMAIL_1).validate("tinaauto@test.com");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.EMAIL_2).validate("E-mail 2:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.EMAIL_2).validate("712@test.com");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.TEXT_1).validate("Text Message 1:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.TEXT_1).validate("2223334444@tmomail.com");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.TEXT_2).validate("Text Message 2:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.TEXT_2).validate("5556667777@tmomail.com");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.PHONE_1).validate("Phone 1:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.PHONE_1).validate("111-222-3333");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.PHONE_2).validate("Phone 2:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.PHONE_2).validate("444-555-6666");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.INFORMATION).validate("Information:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.INFORMATION).validate("E-mail 1");
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.WARNING).validate("Warning:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.WARNING).validate("Phone 2"); 
-        myAdminUserDetails._text().labels(AdminUserDetailsEnum.CRITICAL).validate("Critical:");
-        myAdminUserDetails._text().values(AdminUserDetailsEnum.CRITICAL).validate("Text Message 1"); 
         
+        String mailone =myAdminUserDetails._text().values(AdminUserDetailsEnum.EMAIL_1).getText();
+        String mailtwo =myAdminUserDetails._text().values(AdminUserDetailsEnum.EMAIL_2).getText();
+        String textone =myAdminUserDetails._text().values(AdminUserDetailsEnum.TEXT_1).getText();
+        String texttwo =myAdminUserDetails._text().values(AdminUserDetailsEnum.TEXT_2).getText();
+        String phoneone =myAdminUserDetails._text().values(AdminUserDetailsEnum.PHONE_1).getText();
+       
+        String phonetwo =myAdminUserDetails._text().values(AdminUserDetailsEnum.PHONE_2).getText();
+        String information =myAdminUserDetails._text().values(AdminUserDetailsEnum.INFORMATION).getText();
+        String warning =myAdminUserDetails._text().values(AdminUserDetailsEnum.WARNING).getText();
+        String critical =myAdminUserDetails._text().values(AdminUserDetailsEnum.CRITICAL).getText();
         
+                
+        /*click into My Account to compare*/
         
+        myAccount._link().myAccount().click();
+               
+        /*compare Account info */
+        myAccount._text().name().validate(firstname +" "+ middlename+" "+lastname+" "+suffix);
+        myAccount._text().group().validate(group);
+        myAccount._text().team().validate(team);
+               
+        /*compare Login info */
+        myAccount._text().userName().validate(loginname);
+        myAccount._text().locale().validate(locale);
+        myAccount._text().measurement().validate(measurement);
+        myAccount._text().fuelEfficiency().validate(fuelratio);
+                     
+        /*compare Red flag preferences */
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-	}   
+        myAccount._text().redFlagInfo().validate(information);
+        myAccount._text().redFlagWarn().validate(warning);
+        myAccount._text().redFlagCritical().validate(critical);
+         
+        /* compare contact info*/
+        myAccount._text().email1().validate(mailone);
+        myAccount._text().email2().validate(mailtwo);
+        myAccount._text().phone1().validate(phoneone);
+        myAccount._text().phone2().validate(phonetwo);
+        myAccount._text().textMessage1().validate(textone);
+        myAccount._text().textMessage2().validate(texttwo);
+                
+       	}   
         
 }     
         
