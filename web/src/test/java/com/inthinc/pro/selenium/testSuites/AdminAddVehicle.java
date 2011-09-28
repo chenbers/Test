98ce486fba067d7c3ace86c5415083eceecdc570
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-09-28 20:05:57 +0000

    IN-PROGRESS - task US4129: Ensure validity of Automation Test Results https://rally1.rallydev.com/slm/detail/ar/4120131996 updated test to use AutomationLogins

--- web/src/test/java/com/inthinc/pro/selenium/testSuites/AdminAddVehicle.java
+++ web/src/test/java/com/inthinc/pro/selenium/testSuites/AdminAddVehicle.java
@@ -1,21 +1,32 @@
 package com.inthinc.pro.selenium.testSuites;
 
+import java.util.EnumSet;
+
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.inthinc.pro.automation.enums.Addresses;
+import com.inthinc.pro.automation.enums.AutomationLogins;
+import com.inthinc.pro.automation.enums.LoginCapabilities;
 import com.inthinc.pro.automation.enums.UniqueValues;
 import com.inthinc.pro.automation.utils.Unique;
 import com.inthinc.pro.selenium.pageObjects.PageAdminVehicleEdit;
 
 public class AdminAddVehicle extends WebRallyTest {
     
-    private final String username = "autovehicle";
-    private final String password = "passw0rd";
+    private static String username;
+    private static String password;
     
     private PageAdminVehicleEdit editVehicle;
     private Unique values;
     
+    @BeforeClass
+    public static void beforeClass(){
+        AutomationLogins login = AutomationLogins.getOneBy(EnumSet.of(LoginCapabilities.RoleAdmin));
+        username = login.getUserName();
+        password = login.getPassword();
+    }
     @Before
     public void before(){
         editVehicle = new PageAdminVehicleEdit();
@@ -26,7 +37,7 @@ public class AdminAddVehicle extends WebRallyTest {
     @Test
     public void defaultCommandButton(){
         set_test_case("TC166");
-        
+        //TODO: dtanner: Implement this test
     }
     
     
@@ -40,11 +51,13 @@ public class AdminAddVehicle extends WebRallyTest {
         print(editVehicle._text().errorVIN().getText());
         editVehicle._textField().VIN().type(values.getUniqueValue(17, UniqueValues.VEHICLE_VIN));
         print(editVehicle._text().errorVIN().getText());
+        //TODO: dtanner: finish this test???
     }
     
     @Test
     public void secondaryTestForWebDriver(){
         editVehicle.loginProcess(username, password);
+        //TODO: jwimmer: remove this test
     }
 
 }
