069ff0ba58644adcbc287b39b271f12eafc11ea8
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-10-03 14:59:21 +0000

    updating

--- automation/src/main/java/com/inthinc/pro/automation/elements/TDSelector.java
+++ automation/src/main/java/com/inthinc/pro/automation/elements/TDSelector.java
@@ -10,9 +10,7 @@ import com.inthinc.pro.automation.enums.SeleniumEnumWrapper;
 import com.inthinc.pro.automation.interfaces.SeleniumEnums;
 import com.inthinc.pro.automation.interfaces.SeleniumValueEnums;
 import com.inthinc.pro.automation.interfaces.TextEnum;
-import com.inthinc.pro.automation.utils.Id;
 import com.inthinc.pro.automation.utils.RandomValues;
-import com.inthinc.pro.automation.utils.Xpath;
 
 public class TDSelector extends SelectableObject implements Selectable {
     
@@ -33,17 +31,23 @@ public class TDSelector extends SelectableObject implements Selectable {
         super(anEnum);
     }
     
+    private void setMyEnum(){
+        String[] newIds = new String[myEnum.getIDs().length];
+        for (int i=0;i<myEnum.getIDs().length;i++){
+            String newId = "";
+            String id = myEnum.getIDs()[i];
+            if(!id.startsWith("//") || !id.contains("=")){
+                newId = "//tbody[@id='"+id+"']";
+            }
+            newIds[i]=newId;
+        }
+        myEnum.setID(newIds);
+    }
+    
 
     @Override
     public SelectableObject select(String desiredOption) {
-        String id = myEnum.getLocators().get(0);
-        SeleniumEnumWrapper temp = new SeleniumEnumWrapper(myEnum);
-        temp.replaceOldWithNew("###", "");
-        id = "//tr[contains(@id,'" + id + "')]/td[text()='" + desiredOption+"']";
-        temp.setID(id);
-        List<WebElement> elements = getSelenium().findElements(temp);
-        elements.get(0).click();
-        return this;
+        return select(desiredOption, 1);
     }
     
     public SelectableObject select(TextEnum value){
@@ -55,71 +59,75 @@ public class TDSelector extends SelectableObject implements Selectable {
     
     @Override
     public String getText(Integer optionNumber){
+        SeleniumEnumWrapper temp = new SeleniumEnumWrapper(myEnum);
+        
         return selenium.getSelectOptions(myEnum)[--optionNumber];
     }
     
-    public SelectableObject selectRandom() {
-        String[] allOptions =selenium.getSelectOptions(myEnum);
-        int randomIndex = RandomValues.newOne().getInt(allOptions.length);
-        return select(randomIndex);
+    private SeleniumEnumWrapper addQualifier(String qualifier, String td){
+        SeleniumEnumWrapper temp = new SeleniumEnumWrapper(myEnum);
+        String[] ids = temp.getIDs();
+        for (int i=0;i<ids.length;i++){
+            ids[i] += "/tr[" + qualifier + "]" + td;
+        }
+        return temp;
     }
-
+    
+    private SeleniumEnumWrapper addQualifier(String qualifier){
+        return addQualifier(qualifier, "");
+    }
+    
+    public SelectableObject selectRandom() {
+        String id = myEnum.getLocators().get(0);
+        SeleniumEnumWrapper temp = new SeleniumEnumWrapper(myEnum);
+        temp.replaceOldWithNew("###", "");
+        id = "//tr[contains(@id,'" + id + "')]";
+        temp.setID(id);
+        List<WebElement> elements = getSelenium().findElements(temp);
+        int randomIndex = RandomValues.newOne().getInt(elements.size());
+        elements.get(randomIndex).click();
+        return this;
+    }
+    
     @Override
     public SelectableObject select(Integer optionNumber) {
-        optionNumber--;
-        selenium.select(myEnum, "index="+optionNumber);
-        String selected = selenium.getSelectedIndex(myEnum);
-        assertEquals(optionNumber.toString(), selected);
+        String id = myEnum.getLocators().get(0);
+        SeleniumEnumWrapper temp = new SeleniumEnumWrapper(myEnum);
+        temp.replaceOldWithNew("###", "");
+        id = "//tr[contains(@id,'" + id + "')]";
+        temp.setID(id);
+        List<WebElement> elements = getSelenium().findElements(temp);
+        elements.get(optionNumber).click();
         return this;
     }
 
     @Override
     public SelectableObject select(String desiredOption, Integer matchNumber) {
-        matchNumber--;
-        String xpath = getSelectIDAsXpath();
-        if (xpath==null){
-            xpath = getSelectXpath();
-        }
-        selenium.click(xpath, Id.text(desiredOption), matchNumber);
+        String id = myEnum.getLocators().get(0);
+        SeleniumEnumWrapper temp = new SeleniumEnumWrapper(myEnum);
+        temp.replaceOldWithNew("###", "");
+        id = "//tr[contains(@id,'" + id + "')]/td[text()='" + desiredOption+"']";
+        temp.setID(id);
+        List<WebElement> elements = getSelenium().findElements(temp);
+        elements.get(matchNumber).click();
         return this;
     }
-
+    
     @Override
     public SelectableObject selectPartMatch(String partialMatch, Integer matchNumber) {
-        matchNumber--;
-        String xpath = getSelectIDAsXpath();
-        if (xpath==null){
-            xpath = getSelectXpath();
-        }
-        selenium.click(xpath, Id.contains(Id.text(), partialMatch), matchNumber);
+        String id = myEnum.getLocators().get(0);
+        SeleniumEnumWrapper temp = new SeleniumEnumWrapper(myEnum);
+        temp.replaceOldWithNew("###", "");
+        id = "//tr[contains(@id,'" + id + "')]/td[contains(text(),'" + partialMatch+"')]";
+        temp.setID(id);
+        List<WebElement> elements = getSelenium().findElements(temp);
+        elements.get(matchNumber).click();
         return this;
     }
     
-    private String getSelectIDAsXpath(){
-        List<String> ids = myEnum.getLocators();
-        for (String locator: ids){
-            if (!locator.contains("=") || !locator.startsWith("//")){
-                return Xpath.start().select(Id.id(locator)).toString();
-            }
-        }
-        return null;
-    }
-    
-    private String getSelectXpath(){
-        List<String> ids = myEnum.getLocators();
-        for (String locator: ids){
-            if (!locator.contains("=") && locator.startsWith("//")){
-                return locator;
-            }
-        }
-        return null;
-    }
 
     @Override
     public SelectableObject selectPartMatch(String partialMatch) {
         return selectPartMatch(partialMatch, 1);
     }
-    
-
-
 }
