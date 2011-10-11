d46eb752bd8fcdf2abfacb6c859a9cc326a4ad9f
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-10-11 22:32:16 +0000

    automation: centralizing automation logins

--- automation/src/main/java/com/inthinc/pro/automation/enums/AutomationLogins.java
+++ automation/src/main/java/com/inthinc/pro/automation/enums/AutomationLogins.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Set;
 
 import org.apache.commons.lang.math.RandomUtils;
+import org.apache.log4j.Logger;
 
 import com.inthinc.pro.automation.utils.RandomValues;
 import com.mysql.jdbc.log.Log;
@@ -25,18 +26,20 @@ public enum AutomationLogins {
     Daisy1("Daisy1", "password","Tina's Auto Team", AutoAccounts.prime, null),
     danniauto("danniauto", "password", "Top",  AccountCapabilities.LoginExpire90, AccountCapabilities.PasswordExpire90, AccountCapabilities.PasswordRequireInitalChangeRequire, AccountCapabilities.HOSEnabled),
     dastardly("dastardly", "Muttley","Test Group WR", AutoAccounts.prime, LoginCapabilities.NoteTesterData), 
-    hosDriver00("hosDriver00","password", "Top", AutoAccounts.prime, AccountCapabilities.LoginExpire90, AccountCapabilities.PasswordExpire90, AccountCapabilities.PasswordRequireInitalChangeRequire, AccountCapabilities.HOSEnabled, LoginCapabilities.IsDriver),
+    hosDriver00("hosDriver00","password", "Top", AutoAccounts.prime, LoginCapabilities.IsDriver, LoginCapabilities.HasVehicle, LoginCapabilities.HasWaySmart, LoginCapabilities.RoleAdmin), //TODO: vehicle should be named the SAME as the username?
     hosDriver01("hosDriver01","password", "Top", AccountCapabilities.LoginExpire90, AccountCapabilities.PasswordExpire90, AccountCapabilities.PasswordRequireInitalChangeRequire, AccountCapabilities.HOSEnabled, LoginCapabilities.IsDriver),
     jesse1("jesse1", "password","Tina's Auto Team", AutoAccounts.prime, null),
     pitstop("pitstop", "Muttley","Top", AutoAccounts.prime, LoginCapabilities.NoteTesterData), 
-    tinaauto("tinaauto", "password","Tina's Auto Team", AutoAccounts.prime, LoginCapabilities.RoleAdmin ,LoginCapabilities.HasDevice, LoginCapabilities.HasVehicle, LoginCapabilities.IsDriver),    
+    tinaauto("tinaauto", "password","Tina's Auto Team", AutoAccounts.prime, LoginCapabilities.RoleAdmin, LoginCapabilities.IsDriver),    
     whiplash("whiplash", "Muttley","Test Group WR", AutoAccounts.prime, LoginCapabilities.StatusInactive),
     
     passwordchanger00("passwordchanger00", "password", "Top", AutoAccounts.prime, LoginCapabilities.PasswordChanging),
     passwordchanger01("passwordchanger01", "password", "Top", AutoAccounts.prime, LoginCapabilities.PasswordChanging),
     passwordchanger02("passwordchanger02", "password", "Top", AutoAccounts.prime, LoginCapabilities.PasswordChanging),
-    
-    
+    tiwi00("tiwi00", "password", "Tina's Auto Team", AutoAccounts.prime, LoginCapabilities.HasTiwiPro, LoginCapabilities.IsDriver, LoginCapabilities.HasVehicle, LoginCapabilities.RoleAdmin),
+    tiwi01("tiwi01", "password", "Tina's Auto Team", AutoAccounts.prime, LoginCapabilities.HasTiwiPro, LoginCapabilities.IsDriver, LoginCapabilities.HasVehicle, LoginCapabilities.RoleAdmin),
+    tiwi02("tiwi02", "password", "Tina's Auto Team", AutoAccounts.prime, LoginCapabilities.HasTiwiPro, LoginCapabilities.IsDriver, LoginCapabilities.HasVehicle, LoginCapabilities.RoleAdmin),
+    tiwi03("tiwi03", "password", "Tina's Auto Team", AutoAccounts.prime, LoginCapabilities.HasTiwiPro, LoginCapabilities.IsDriver, LoginCapabilities.HasVehicle, LoginCapabilities.RoleAdmin),
     
     
     
@@ -54,6 +57,7 @@ public enum AutomationLogins {
 	private String group;
 	private Set<Capabilities> capabilities;
 	private AutoAccounts account;
+	private final static Logger logger = Logger.getLogger(AutomationLogins.class);
 	
 	private AutomationLogins(String username, String password, String group, AutoAccounts account, Capabilities... capabilities) {
 	    this.username = username;
@@ -103,27 +107,49 @@ public enum AutomationLogins {
         }
         return results;
     }
-
-    public static AutomationLogins getOneBy(String groupName) {
-        List<AutomationLogins> allMatching = getAllBy(groupName);
-        if (!allMatching.isEmpty()) {
-            int loginIndex = RandomUtils.nextInt(allMatching.size());
-            return allMatching.get(loginIndex);
-        } else {
-            System.out.println("AutomationLogins.getOneBy(" + groupName + ") returning null! ");
-            return null;// TODO: jwimmer: what is the best course of action if no matching accounts are found?
+    public static List<AutomationLogins> getAllBy(AutoAccounts account) {
+        ArrayList<AutomationLogins> results = new ArrayList<AutomationLogins>();
+        for (AutomationLogins item : EnumSet.allOf(AutomationLogins.class)) {
+            if(item.getAccount().equals(account)) {
+                results.add(item);
+            }
         }
+        return results;
+    }
+    public static List<AutomationLogins> getAllBy(AutoAccounts account, Capabilities... capabilities){
+        List<AutomationLogins> byAccount = getAllBy(account);
+        List<AutomationLogins> results = new ArrayList<AutomationLogins>();
+        Set<Capabilities> setCapabilities = new HashSet<Capabilities>();
+        for(Capabilities c: capabilities)
+            setCapabilities.add(c);
+        
+        for(AutomationLogins login: byAccount){
+            if(login.capabilities.containsAll(setCapabilities))
+                results.add(login);
+        }
+        return results;  
+    }
+    public static AutomationLogins getOneBy(AutoAccounts account, Capabilities... capabilities){
+        return getOneBy(getAllBy(account, capabilities));
+    }
+    private static AutomationLogins getOneBy(List<AutomationLogins> list){
+        if (!list.isEmpty()) {
+            int loginIndex = RandomUtils.nextInt(list.size());
+            return list.get(loginIndex);
+        } else {
+            logger.warn("AutomationLogins.getOneBy(" + list + ") returning null! ");
+            return null;
+        } 
+    }
+    public static AutomationLogins getOneBy(AutoAccounts account){
+        return getOneBy(getAllBy(account));    
+    }
+    public static AutomationLogins getOneBy(String groupName) {
+        return getOneBy(getAllBy(groupName));
     }
 
 	public static AutomationLogins getOneBy(Capabilities... capabilities) {
-	    List<AutomationLogins> allMatching = getAllBy(capabilities);
-	    if(!allMatching.isEmpty()){
-	        int loginIndex = RandomUtils.nextInt(allMatching.size());
-	        return allMatching.get(loginIndex);
-	    } else {
-	        System.out.println("AutomationLogins.getOneBy("+capabilities+") returning null! ");
-	        return null;//TODO: jwimmer: what is the best course of action if no matching accounts are found?
-	    }  
+	    return getOneBy(getAllBy(capabilities));
 	}
 	public static AutomationLogins getOne(){
 	    return getOneBy(LoginCapabilities.StatusActive);
@@ -132,7 +158,9 @@ public enum AutomationLogins {
 	public String getGroup() {
 	    return group;
 	}
-	
+	public AutoAccounts getAccount() {
+	    return account;
+	}
 	private enum AutoAccounts {
 	    prime(AccountCapabilities.HOSEnabled, AccountCapabilities.WaySmartEnabled, AccountCapabilities.LoginExpireNever, AccountCapabilities.PasswordExpireNever, AccountCapabilities.PasswordMinStrengthNone, AccountCapabilities.PasswordRequireInitialChangeNone),
 	    ;
