a315fc9c0b19db29476aa8c49424ecb1d9415dca
tnilson <tnilson@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-10-21 21:33:38 +0000

    Committing my updates to HOS logs. Still have more work to do.

--- web/src/test/java/com/inthinc/pro/selenium/testSuites/HOSReportsTest.java
+++ web/src/test/java/com/inthinc/pro/selenium/testSuites/HOSReportsTest.java
@@ -6,16 +6,19 @@ import org.junit.Test;
 
 import com.inthinc.pro.automation.enums.LoginCapabilities;
 import com.inthinc.pro.automation.models.AutomationUser;
+import com.inthinc.pro.selenium.pageEnums.AdminTables.AdminUsersEntries;
 import com.inthinc.pro.selenium.pageObjects.HOSRecordOfDutyStatus;
 import com.inthinc.pro.selenium.pageObjects.PageHOSReports;
 import com.inthinc.pro.selenium.pageObjects.PageAdminUsers;
 import com.inthinc.pro.selenium.pageObjects.PageAdminUserDetails;
+import com.inthinc.pro.selenium.pageObjects.PageAdminOrganization;
 
 
 public class HOSReportsTest extends WebRallyTest {
     
     private PageHOSReports myHOSReports;
     private PageAdminUsers myAdminUsers;
+    private PageAdminOrganization myAdminOrg;
     private PageAdminUserDetails myAdminUserDetails;
     private AutomationUser user;
     private AutomationUser driver;
@@ -35,19 +38,45 @@ public class HOSReportsTest extends WebRallyTest {
         set_test_case("TC5644");
         
     
-        // 0.Login
+      // 0.Login
     
         myHOSReports.loginProcess(user);
                     
-      //1. Go to user and grab team name; then go to organization page, find team address and top level address. Save for later comparison.
+      //1. Go to user and grab org division address;  Save for later comparison.
+        
         myAdminUsers._link().admin().click();
+        myAdminUsers._link().adminOrganization().click();
+        myAdminOrg.getFleet().getDivision(1).text().click();
+        myAdminOrg._text().labelAddressOne().getText();
+        String valueAddressOne = myAdminOrg._text().valueAddressOne().getText();
+        myAdminOrg._text().labelCity().getText();
+        String valueCity = myAdminOrg._text().valueCity().getText();
+        myAdminOrg._text().labelState().getText();
+        String valueState = myAdminOrg._text().valueState().getText();
+        myAdminOrg._text().labelZipCode().getText();
+        String valueZipCode = myAdminOrg._text().valueZipCode().getText();
+        
+        
+      //2. Search for driver user name.
+        
         myAdminUsers._textField().search().type(driver.getUsername());
         myAdminUsers._button().search().click();
-       // myAdminUsers._link().
+        myAdminUsers._link().tableEntryUserFullName().row(1).click();
         
+        
+      // 3. grab team name and address; Save for later comparison. 
+        
+        String valueTeam = myAdminUserDetails._text().values(AdminUsersEntries.TEAM).getText();
+       
+        myAdminUsers._link().adminOrganization().click();
+        myAdminOrg.getFleet().getDivision(1).arrow().click();
+        myAdminOrg.getFleet().getTeam(valueTeam);
+        //TODO: Waiting on new page object method so I can get the team from the organization page.
+        
+            
               
         
-        //2. Click on HOS Reports  
+        //4. Click on HOS Reports  
         
         myHOSReports._link().hos().click();
         myHOSReports._link().hosReports().click();
@@ -60,7 +89,7 @@ public class HOSReportsTest extends WebRallyTest {
         HOSRecordOfDutyStatus report = myHOSReports._reports()._recordOfDutyStatusReport();
               
                
-        //3. Open Report in HTML
+        //5. Open Report in HTML
         myHOSReports._link().html().click();
         //report._text().valueDate(1).validate("");
         report._text().valueStartOdometer(1).validate();//TODO: need to validate something here.
