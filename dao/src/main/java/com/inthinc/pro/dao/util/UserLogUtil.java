3728c0b24c5b1d7ae9c190a154739bff0054cdfb
cjennings <cjennings@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2011-10-31 19:52:21 +0000

    added a check for loggers info enabled to temporarily get around the problem of using a newer spring security with dao jar

--- dao/src/main/java/com/inthinc/pro/dao/util/UserLogUtil.java
+++ dao/src/main/java/com/inthinc/pro/dao/util/UserLogUtil.java
@@ -23,11 +23,15 @@ public class UserLogUtil {
     public static void logBeforeMethodWithUser(Method method, Object[] args, Object target) {
         log = LogFactory.getLog(target.getClass());
 
-        // log to file
-        String message = method.getName()+"("+argsToString(args)+") [" + findUserName()+"] ; ";
-        log.info(message);// log using the class to which the method belongs
-        logger.info(message);// specific to user.log
-
+        // cj - added this check because of the coupling of the spring security with user lookup wasn't
+        // working when dao jar is used with a new spring security version (e.g. 3.1) -- need to figure out
+        // a way to decouple this
+        if (log.isInfoEnabled() || logger.isInfoEnabled()) {
+            // log to file
+            String message = method.getName()+"("+argsToString(args)+") [" + findUserName()+"] ; ";
+            log.info(message);// log using the class to which the method belongs
+            logger.info(message);// specific to user.log
+        }
         // log to DB
         // TODO: jwimmer: follow up with DaveHarry and MattRaby about correct structure and procedure to get this into DB
         // UserLogItem userLogItem = new UserLogItem(method.toGenericString(), findUserName(), args, new Date());
