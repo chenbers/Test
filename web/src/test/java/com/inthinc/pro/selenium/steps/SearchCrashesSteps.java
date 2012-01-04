501b651cd8c758945c338472c7666a74ce02cae7
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2012-01-04 17:12:05 +0000

    initial jbehave steps class for SearchCrashes

---
+++ web/src/test/java/com/inthinc/pro/selenium/steps/SearchCrashesSteps.java
@@ -0,0 +1,18 @@
+package com.inthinc.pro.selenium.steps;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Pending;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+
+public class SearchCrashesSteps extends AdminSteps { 
+    
+    //TODO: tina: replace these with the pageObjects that you will need...
+//    private PageAdminVehicles vehicles = new PageAdminVehicles();
+//    private PageAdminVehicleView vehicleDetails = new PageAdminVehicleView();
+//    private PageAdminVehicleEdit vehicleAddEdit = new PageAdminVehicleEdit();
+    
+    
+
+}
