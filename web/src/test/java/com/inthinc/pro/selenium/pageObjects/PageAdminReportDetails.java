328a885e3f06620dd64b3c6752b29b7043dc4b3c
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2012-01-04 20:51:10 +0000

    automation: missing page object

---
+++ web/src/test/java/com/inthinc/pro/selenium/pageObjects/PageAdminReportDetails.java
@@ -0,0 +1,96 @@
+package com.inthinc.pro.selenium.pageObjects;
+
+import com.inthinc.pro.automation.elements.Text;
+import com.inthinc.pro.automation.elements.TextButton;
+import com.inthinc.pro.automation.elements.TextLabel;
+import com.inthinc.pro.automation.elements.TextLink;
+import com.inthinc.pro.automation.enums.SeleniumEnumWrapper;
+import com.inthinc.pro.selenium.pageEnums.AdminBarEnum;
+import com.inthinc.pro.selenium.pageEnums.AdminReportDetailsEnum;
+import com.inthinc.pro.selenium.pageEnums.AdminTables.ReportsColumns;
+
+public class PageAdminReportDetails extends AdminBar {
+
+    public PageAdminReportDetails() {
+        url = null;
+        checkMe.add(AdminReportDetailsEnum.NAME_STATUS_TIME);
+        checkMe.add(AdminReportDetailsEnum.REPORT_OCCURANCE);
+        checkMe.add(AdminReportDetailsEnum.REPORT_SETTINGS);
+        checkMe.add(AdminReportDetailsEnum.EMAIL_SECTION);
+    }
+
+    public ReportDetailsPopUps _popUp() {
+        return new ReportDetailsPopUps();
+    }
+
+    public class ReportDetailsPopUps extends AdminBarPopUps {
+
+        public ReportDetailsPopUps() {
+            super(page);
+        }
+
+        public AdminDelete deleteReport() {
+            return new AdminDelete(false);
+        }
+    }
+
+    public class ReportDetailsButtons extends AdminBarButtons {
+
+        public TextButton delete() {
+            return new TextButton(AdminBarEnum.DETAILS_DELETE, page);
+        }
+
+        public TextButton edit() {
+            return new TextButton(AdminBarEnum.EDIT, page);
+        }
+    }
+
+    public class ReportDetailsDropDowns extends AdminBarDropDowns {}
+
+    public class ReportDetailsLinks extends AdminBarLinks {
+
+        public TextLink backToReports() {
+            return new TextLink(AdminBarEnum.GO_BACK, page);
+        }
+    }
+
+    public class ReportDetailsTextFields extends AdminBarTextFields {}
+
+    public class ReportDetailsTexts extends AdminBarTexts {
+
+        public TextLabel labels(ReportsColumns value) {
+            SeleniumEnumWrapper temp = new SeleniumEnumWrapper(value);
+            temp.setID("display-form:"+temp.getIDs()[0]);
+            return new TextLabel(temp);
+        }
+
+        public Text values(ReportsColumns value) {
+            SeleniumEnumWrapper temp = new SeleniumEnumWrapper(value);
+            temp.setID("display-form:"+temp.getIDs()[0]);
+            return new Text(temp);
+        }
+
+    }
+
+    private static String page = "person";
+
+    public ReportDetailsButtons _button() {
+        return new ReportDetailsButtons();
+    }
+
+    public ReportDetailsDropDowns _dropDown() {
+        return new ReportDetailsDropDowns();
+    }
+
+    public ReportDetailsLinks _link() {
+        return new ReportDetailsLinks();
+    }
+
+    public ReportDetailsTexts _text() {
+        return new ReportDetailsTexts();
+    }
+
+    public ReportDetailsTextFields _textField() {
+        return new ReportDetailsTextFields();
+    }
+}
---
+++ web/src/test/java/com/inthinc/pro/selenium/pageObjects/PageAdminReports.java
@@ -0,0 +1,126 @@
+package com.inthinc.pro.selenium.pageObjects;
+
+import java.util.Iterator;
+
+import com.inthinc.pro.automation.elements.TextButton;
+import com.inthinc.pro.automation.elements.TextField;
+import com.inthinc.pro.automation.elements.TextLink;
+import com.inthinc.pro.automation.elements.TextTable;
+import com.inthinc.pro.automation.elements.TextTableLink;
+import com.inthinc.pro.automation.elements.ElementInterface.Checkable;
+import com.inthinc.pro.automation.elements.ElementInterface.TextBased;
+import com.inthinc.pro.selenium.pageEnums.AdminBarEnum;
+import com.inthinc.pro.selenium.pageEnums.AdminReportsEnum;
+import com.inthinc.pro.selenium.pageEnums.AdminTables.ReportsColumns;
+
+public class PageAdminReports extends AdminTables {
+
+    public PageAdminReports() {
+        page = "person";
+        url = AdminReportsEnum.DEFAULT_URL;
+        checkMe.add(AdminReportsEnum.BATCH_EDIT);
+        checkMe.add(AdminReportsEnum.EDIT_COLUMNS_LINK);
+        checkMe.add(AdminReportsEnum.SEARCH_BUTTON);
+    }
+
+    public class AdminReportsButtons extends AdminTablesButtons {
+
+        public TextButton delete() {
+            return new TextButton(AdminBarEnum.DELETE, page);
+        }
+
+    }
+
+    public class AdminReportsDropDowns extends AdminTablesDropDowns {}
+
+    public class AdminReportsLinks extends AdminTablesLinks {
+
+        public TextTableLink tableEntryReportName() {
+            return new TextTableLink(AdminBarEnum.TABLE_ENTRIES, page, ReportsColumns.NAME);
+        }
+        public TextLink sortByColumn(ReportsColumns column) {
+            return new TextLink(AdminBarEnum.TABLE_HEADERS, page, column);
+        }
+
+    }
+
+    public class AdminReportsTextFields extends AdminTablesTextFields {
+
+        public TextField search() {
+            return new TextField(AdminBarEnum.SEARCH_TEXTFIELD, page);
+        }
+
+    }
+    
+    public class AdminReportsTexts extends AdminTablesTexts {
+
+        public TextTable tableEntry(ReportsColumns column) {
+            return new TextTable(AdminBarEnum.TABLE_ENTRIES, page, column);
+        }
+        
+    }
+
+    public AdminReportsButtons _button() {
+        return new AdminReportsButtons();
+    }
+
+    public AdminReportsDropDowns _dropDown() {
+        return new AdminReportsDropDowns();
+    }
+
+    public AdminReportsLinks _link() {
+        return new AdminReportsLinks();
+    }
+
+    public AdminReportsTexts _text() {
+        return new AdminReportsTexts();
+    }
+
+    public AdminReportsTextFields _textField() {
+        return new AdminReportsTextFields();
+    }
+    
+    public AdminReportsPopUps _popUp(){
+        return new AdminReportsPopUps();
+    }
+
+    public class AdminReportsPopUps extends AdminTablesPopUps {
+        public AdminDelete delete(){
+            return new AdminDelete(true);
+        }
+    }
+    
+    //Helper methods
+    public PageAdminReports showAllColumns() {
+        this._link().editColumns().click();
+        Iterator<Checkable> colCheckBoxes = this._popUp().editColumns()._checkBox().iterator();
+        while(colCheckBoxes.hasNext()){
+            colCheckBoxes.next().check();
+        }
+        this._popUp().editColumns()._button().save().click();
+        return this;
+    }
+    public PageAdminReports search(String searchString) {
+        this._textField().search().type(searchString);
+        this._button().search().click();
+        return this;
+    }
+    public PageAdminReports clickNameMatching(ReportsColumns column, String value){
+        this.showAllColumns();
+        this.search(value);
+        Iterator<TextBased> rowIterator = this._text().tableEntry(column).iterator();
+        boolean clicked = false;
+        int rowNumber = 0;
+        while(rowIterator.hasNext()&&!clicked){
+            TextBased rowCol = rowIterator.next();
+            rowNumber++;
+            if(rowCol.getText().equals(value)){
+                this._link().tableEntryReportName().row(rowNumber).click();
+                return this;
+            }
+        }
+        addError("clickNameMatching("+column+", "+value+")", ErrorLevel.FATAL);
+        return this;
+    }
+    
+}
