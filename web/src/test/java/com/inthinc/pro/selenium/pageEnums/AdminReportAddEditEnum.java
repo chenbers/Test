0a46ca5c7239c05f092f0fde26fc61627e8cb27a
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2012-01-04 20:50:49 +0000

    automation: missing page object

---
+++ web/src/test/java/com/inthinc/pro/selenium/pageEnums/AdminReportAddEditEnum.java
@@ -0,0 +1,61 @@
+package com.inthinc.pro.selenium.pageEnums;
+
+import com.inthinc.pro.automation.interfaces.SeleniumEnums;
+
+public enum AdminReportAddEditEnum implements SeleniumEnums {
+    DEFAULT_URL("/app/admin/editReport"),
+
+    DROP_DOWNS(null, "edit-form:editReportSchedule-***"),
+
+    GROUP_DHX(null, "edit_form:editReportSchedule-group"),
+    TEXT_FIELDS(null, "edit-form:editReportSchedule-***"),
+
+    NAME(null,          "edit_form:scheduleName"),
+    STATUS(null,        "edit_form:editReportScheduleStatus"),
+    TIME_OF_DAY(null,   "editReportScheduletimeOfDay-timeOfDayTime"),
+    TIME_AM_PM(null,    "editReportScheduletimeOfDay-timeOfDayAfternoon"),
+    
+    OCCURANCE(null,    "id('reportScheduleForm')/x:div/x:div[2]/x:div/x:table/x:tbody/x:tr/x:td[1]/x:table[2]/x:tbody/x:tr[1]/x:td[2]"),
+    DAY_OF_WEEK(null,  "id('reportScheduleForm')/x:div/x:div[2]/x:div/x:table/x:tbody/x:tr/x:td[1]/x:table[2]/x:tbody/x:tr[2]/x:td[2]"),
+    DAY_OF_MONTH(null, "id('reportScheduleForm')/x:div/x:div[2]/x:div/x:table/x:tbody/x:tr/x:td[1]/x:table[2]/x:tbody/x:tr[2]/x:td[2]"),
+    
+    REPORT(null,   "id('reportScheduleForm:reportConfigTable')/x:table/x:tbody/x:tr[1]/x:td[2]"),
+    DAYS_REPORTED(null, "reportSchedule_daysDuration", "reportSchedule_daysTimeFrame"),
+    GROUP(null,    "reportSchedule_group"),
+    LIST(null, "reportSchedule_list"),
+    DRIVER(null,    "reportSchedule_driverName", "reportSchedule_vehicleName"),
+    IFTA(null, "reportSchedule_iftaOnly"),
+    OWNER(null, "reportSchedule_ownerName"),   
+    EMAIL(null, "id('reportScheduleForm')/x:div/x:div[2]/x:div/x:table/x:tbody/x:tr/x:td[3]/x:table/x:tbody/x:tr/x:td"),
+    
+    CANCEL("Cancel", "edit-form:editReportScheduleCancel***"),
+    SAVE(save, "edit-form:editReportScheduleSave***"),    
+    ;
+
+    private String text, url;
+    private String[] IDs;
+
+    private AdminReportAddEditEnum(String url) {
+	this.url = url;
+    }
+
+    private AdminReportAddEditEnum(String text, String... IDs) {
+	this.text = text;
+	this.IDs = IDs;
+    }
+
+    @Override
+    public String[] getIDs() {
+	return IDs;
+    }
+
+    @Override
+    public String getText() {
+	return text;
+    }
+
+    @Override
+    public String getURL() {
+	return url;
+    }
+}
---
+++ web/src/test/java/com/inthinc/pro/selenium/pageObjects/PageAdminReportAddEdit.java
@@ -0,0 +1,181 @@
+package com.inthinc.pro.selenium.pageObjects;
+
+import com.inthinc.pro.automation.elements.Button;
+import com.inthinc.pro.automation.elements.CheckBox;
+import com.inthinc.pro.automation.elements.DHXDropDown;
+import com.inthinc.pro.automation.elements.DropDown;
+import com.inthinc.pro.automation.elements.Selector;
+import com.inthinc.pro.automation.elements.Text;
+import com.inthinc.pro.automation.elements.TextButton;
+import com.inthinc.pro.automation.elements.TextField;
+import com.inthinc.pro.automation.elements.TextFieldError;
+import com.inthinc.pro.automation.elements.TextFieldLabel;
+import com.inthinc.pro.automation.interfaces.SeleniumEnums;
+import com.inthinc.pro.selenium.pageEnums.AdminBarEnum;
+import com.inthinc.pro.selenium.pageEnums.AdminReportAddEditEnum;
+import com.inthinc.pro.selenium.pageEnums.AdminTables.ReportsColumns;
+
+public class PageAdminReportAddEdit extends AdminBar {
+
+    private static String page = "Report";
+
+    public PageAdminReportAddEdit() {
+        url = AdminReportAddEditEnum.DEFAULT_URL;
+        checkMe.add(AdminReportAddEditEnum.NAME);
+        checkMe.add(AdminReportAddEditEnum.STATUS);
+        checkMe.add(AdminReportAddEditEnum.TIME_OF_DAY);
+        checkMe.add(AdminReportAddEditEnum.TIME_AM_PM);
+        checkMe.add(AdminReportAddEditEnum.OCCURANCE);
+        checkMe.add(AdminReportAddEditEnum.REPORT);
+        checkMe.add(AdminReportAddEditEnum.EMAIL);
+    }
+
+    public AddEditReportPopUps _popUp() {
+        return new AddEditReportPopUps();
+    }
+
+    public class AddEditReportPopUps extends MastheadPopUps {}
+
+    public class AddEditReportCheckBoxs {
+
+        public CheckBox driverInformation() {
+            return new CheckBox(AdminReportAddEditEnum.TEXT_FIELDS, "isDriver");
+        }
+
+        public CheckBox ReportInformation() {
+            return new CheckBox(AdminReportAddEditEnum.TEXT_FIELDS, "isReport");
+        }
+
+    }
+
+    public AddEditReportCheckBoxs _checkBox() {
+        return new AddEditReportCheckBoxs();
+    }
+
+    public class AddEditReportSelects {
+
+        public Selector rolesLeft() {
+            return new Selector(AdminBarEnum.SELECTOR, page, "from");
+        }
+
+        public Selector rolesRight() {
+            return new Selector(AdminBarEnum.SELECTOR, page, "picked");
+        }
+    }
+
+    public AddEditReportSelects _select() {
+        return new AddEditReportSelects();
+    }
+
+    public AddEditReportLinks _link() {
+        return new AddEditReportLinks();
+    }
+
+    public class AddEditReportLinks extends AdminBarLinks {}
+
+    public AddEditReportTexts _text() {
+        return new AddEditReportTexts();
+    }
+
+    public class AddEditReportTexts extends AdminBarTexts {
+
+        public Text masterError() {
+            return new Text(AdminBarEnum.MASTER_ERROR);
+        }
+
+        public TextFieldLabel personLabel(ReportsColumns label) {
+            return new TextFieldLabel(AdminReportAddEditEnum.TEXT_FIELDS, label);
+        }
+
+        public TextFieldLabel driverLabel(ReportsColumns label) {
+            return new TextFieldLabel(AdminReportAddEditEnum.TEXT_FIELDS, "driver_", label);
+        }
+
+        public TextFieldLabel ReportLabel(ReportsColumns label) {
+            return new TextFieldLabel(AdminReportAddEditEnum.TEXT_FIELDS, "Report_", label);
+        }
+
+        public TextFieldError personError(ReportsColumns label) {
+            return new TextFieldError(AdminReportAddEditEnum.TEXT_FIELDS, label);
+        }
+
+        public TextFieldError driverError(ReportsColumns label) {
+            return new TextFieldError(AdminReportAddEditEnum.TEXT_FIELDS, "driver_", label);
+        }
+
+        public TextFieldError ReportError(ReportsColumns label) {
+            return new TextFieldError(AdminReportAddEditEnum.TEXT_FIELDS, "Report_", label);
+        }
+
+    }
+
+    public AddEditReportTextFields _textField() {
+        return new AddEditReportTextFields();
+    }
+
+    public class AddEditReportTextFields extends AdminBarTextFields {
+
+        public TextField personFields(ReportsColumns textField) {
+            return new TextField(AdminReportAddEditEnum.TEXT_FIELDS, textField);
+        }
+
+        public TextField driverFields(ReportsColumns textField) {
+            return new TextField(AdminReportAddEditEnum.TEXT_FIELDS, "driver_", textField);
+        }
+
+        public TextField ReportFields(ReportsColumns textField) { 
+            return new TextField(AdminReportAddEditEnum.TEXT_FIELDS, "Report_", textField);
+        }
+    }
+
+    public AddEditReportButtons _button() {
+        return new AddEditReportButtons();
+    }
+
+    public class AddEditReportButtons extends AdminBarButtons {
+
+        public TextButton saveTop() {
+            return new TextButton(AdminReportAddEditEnum.SAVE, "1");
+        }
+
+        public TextButton saveBottom() {
+            return new TextButton(AdminReportAddEditEnum.SAVE, "2");
+        }
+
+        public TextButton cancelTop() {
+            return new TextButton(AdminReportAddEditEnum.CANCEL, "1");
+        }
+
+        public TextButton cancelBottom() {
+            return new TextButton(AdminReportAddEditEnum.CANCEL, "2");
+        }
+    }
+
+    public AddEditReportDropDowns _dropDown() {
+        return new AddEditReportDropDowns();
+    }
+
+    public class AddEditReportDropDowns extends AdminBarDropDowns {
+
+        private SeleniumEnums[] enums = { AdminReportAddEditEnum.GROUP_DHX};
+
+        public DropDown regularDropDowns(ReportsColumns dropDown) {
+            return new DropDown(AdminReportAddEditEnum.DROP_DOWNS, dropDown);
+        }
+
+        public DHXDropDown ReportGroup() {
+            return new DHXDropDown(AdminReportAddEditEnum.GROUP_DHX, enums);
+        }
+        
+        public DropDown ReportStatus(){
+            return new DropDown(AdminReportAddEditEnum.STATUS);
+        }
+        
+        
+    }
+
+    @Override
+    public String getExpectedPath() {
+        return AdminReportAddEditEnum.DEFAULT_URL.getURL();
+    }
+}
