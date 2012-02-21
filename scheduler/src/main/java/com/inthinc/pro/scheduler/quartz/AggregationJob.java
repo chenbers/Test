d08699b280838173865c5a6a30adc69ebebb515c
bmiller <bmiller@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2012-02-21 18:03:46 +0000

    Added aggregation job to scheduler.

---
+++ scheduler/src/main/java/com/inthinc/pro/scheduler/quartz/AggregationJob.java
@@ -0,0 +1,37 @@
+package com.inthinc.pro.scheduler.quartz;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import com.inthinc.pro.aggregation.TripWS;
+
+public class AggregationJob extends QuartzJobBean {
+    private static final Logger logger = Logger.getLogger(AggregationJob.class);
+
+
+    @Override
+    protected void executeInternal(JobExecutionContext arg0) throws JobExecutionException {
+//        logger.setLevel(Level.DEBUG);
+        logger.debug("START PROCESSING WAYSMART TRIP AGGREGATION");
+        processWSTripAggegation();
+        logger.debug("END PROCESSING WAYSMART TRIP AGGREGATION");
+
+    }
+    
+
+    private void processWSTripAggegation() {
+		TripWS tripWS = new TripWS();
+		try {
+			tripWS.updateTripsWS();
+		} catch (Exception e)
+		{
+			logger.error("Exception: " + e);
+			e.printStackTrace();
+		}
+    }
+}
