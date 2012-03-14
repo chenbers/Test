a5c5c0089f403ed66086a6457ef9d1dfebda92fe
mweiss <mweiss@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2012-03-14 20:01:54 +0000

    Added a few methods

--- web/src/test/java/com/inthinc/pro/selenium/steps/StepsAdmin.java
+++ web/src/test/java/com/inthinc/pro/selenium/steps/StepsAdmin.java
@@ -1,14 +1,43 @@
 package com.inthinc.pro.selenium.steps;
 
+import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Pending;
 import org.jbehave.core.annotations.When;
 
 import com.inthinc.pro.automation.enums.ErrorLevel;
 import com.inthinc.pro.selenium.pageObjects.PageAdminCustomRoleAddEdit;
 import com.inthinc.pro.selenium.pageObjects.PageAdminCustomRoles;
+import com.inthinc.pro.selenium.pageObjects.PageAdminUserDetails;
+import com.inthinc.pro.selenium.pageObjects.PageAdminUsers;
 
 
 public class StepsAdmin extends LoginSteps { 
+    
+    private static final PageAdminUsers adminUsers = new PageAdminUsers();
+    private static final PageAdminUserDetails adminDetails = new PageAdminUserDetails();
+    
+    @When("i select admin")
+    public void whenISelectAdmin() {
+        adminUsers._link().admin();
+    }
+    
+    @When("i select users")
+    public void whenISelectUsers() {
+        adminUsers._link().adminUsers();
+    }
+    
+    @When("i select a valid user")
+    public void whenISelectAValidUser() {
+        adminUsers._link().tableEntryUserFullName().row(1).click();
+    }
+    
+    @When("i click the edit link")
+    public void whenIClickTheEditLink() {
+      adminUsers._link().edit().row(1).click();
+    }
+    
+    
+    
     
     //TODO: steps that apply to any ADMIN tab tests ???
     
