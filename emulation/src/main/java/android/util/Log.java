9bca55e98e627b0aef79f1e7643390cf326a710d
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2012-03-28 18:28:53 +0000

    added Trace level method t

--- emulation/src/main/java/android/util/Log.java
+++ emulation/src/main/java/android/util/Log.java
@@ -5,6 +5,23 @@ import org.apache.log4j.Level;
 import com.inthinc.pro.automation.utils.MasterTest;
 
 public class Log {
+
+	/**
+	 * t : Trace level logger
+	 * @param print message to log
+	 */
+	public static void t(String print){
+		MasterTest.print(print, Level.TRACE, 3);
+	}
+
+	/**
+	 * t: Trace level logger
+	 * @param TAG
+	 * @param string
+	 */
+	public static void t(String TAG, Object ...string) {
+		MasterTest.print(TAG, Level.TRACE, string);
+	}
 	
 	public static void d(String print){
 		MasterTest.print(print, Level.DEBUG, 3);
