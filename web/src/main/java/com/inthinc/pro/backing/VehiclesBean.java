c6efb2479f395f6ed38b64489fab6f59b7f7beb9
jwimmer <jwimmer@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2012-04-05 18:27:44 +0000

    release bugFix: stacktrace on add vehicle page

--- web/src/main/java/com/inthinc/pro/backing/VehiclesBean.java
+++ web/src/main/java/com/inthinc/pro/backing/VehiclesBean.java
@@ -772,7 +772,7 @@ public class VehiclesBean extends BaseAdminBean<VehiclesBean.VehicleView> implem
         }
 
         public String getProductTypeName() {
-            if(editableVehicleSettings == null) return ProductType.UNKNOWN.name();
+            if(editableVehicleSettings == null) return ProductType.UNKNOWN.getDescription();
 //            return editableVehicleSettings.getProductType().getDescription().getProductName();
             return editableVehicleSettings.getProductDisplayName();
         }
