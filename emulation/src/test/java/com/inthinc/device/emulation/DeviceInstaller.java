d6447dcb79722161063f8a2a2cd29ad478f9526e
tfollmerjr <tfollmerjr@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2012-04-17 15:23:09 +0000

    Adding a single trip faker for tiwipro/waysmart and a device installer for waysmarts

---
+++ emulation/src/test/java/com/inthinc/device/emulation/DeviceInstaller.java
@@ -0,0 +1,23 @@
+package com.inthinc.device.emulation;
+
+import com.inthinc.device.devices.WaysmartDevice;
+import com.inthinc.device.devices.WaysmartDevice.Direction;
+import com.inthinc.device.objects.AutomationDeviceEvents;
+import com.inthinc.pro.automation.enums.Addresses;
+
+public class DeviceInstaller {
+
+	public static void main(String[] args) {
+		
+		WaysmartDevice device = new WaysmartDevice("300235555777777", "MCM013795", Addresses.PROD, Direction.sat);
+		
+		device.getTripTracker().getTrip("898 Canyon Ridge Way, Midvale, UT 84047", "4225 Lake Park Blvd, West Valley City, UT 84120");
+		
+		device.getState().setVehicleID("RELEASETEST").setAccountID(1);
+		
+		AutomationDeviceEvents.install(device);
+		device.flushNotes();
+
+	}
+
+}
