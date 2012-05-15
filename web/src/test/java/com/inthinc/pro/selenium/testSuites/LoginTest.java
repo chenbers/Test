f4427ca2c02cdb6120d209741ea49a68f0de5dc1
mweiss <mweiss@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2012-05-15 16:15:18 +0000

    Deleted LoginStoryTest.java and moved code into LoginTest.java

--- web/src/test/java/com/inthinc/pro/selenium/testSuites/LoginTest.java
+++ web/src/test/java/com/inthinc/pro/selenium/testSuites/LoginTest.java
@@ -1,3 +1,27 @@
+package com.inthinc.pro.selenium.testSuites;
+
+import org.jbehave.core.annotations.UsingSteps;
+import org.junit.Test;
+
+import com.inthinc.pro.automation.annotations.AutomationAnnotations.PageObjects;
+import com.inthinc.pro.automation.annotations.AutomationAnnotations.StoryPath;
+import com.inthinc.pro.selenium.pageObjects.PageAdminUsers;
+import com.inthinc.pro.selenium.pageObjects.PageExecutiveDashboard;
+import com.inthinc.pro.selenium.pageObjects.PageLogin;
+import com.inthinc.pro.selenium.steps.LoginSteps;
+
+@UsingSteps(instances={LoginSteps.class})
+@PageObjects(list={PageLogin.class, PageExecutiveDashboard.class, PageAdminUsers.class})
+@StoryPath(path="login.story")
+public class LoginTest extends WebStories {
+    
+    @Test
+    public void test(){}
+
+}
+
+
+
 //NO LONGER NEEDED, STORY FILE WRITTEN
 
 //package com.inthinc.pro.selenium.testSuites;
