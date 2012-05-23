6994d8ee6a2694a861b93ee419231a384c682f99
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2012-05-23 23:26:29 +0000

    Fixing the names to be JBehave AutoPageRunner friendly

--- web/src/test/java/com/inthinc/pro/selenium/pageEnums/TeamTimeFrames.java
+++ web/src/test/java/com/inthinc/pro/selenium/pageEnums/TeamTimeFrames.java
@@ -7,17 +7,17 @@ import com.inthinc.pro.automation.interfaces.SeleniumEnums;
 import com.inthinc.pro.automation.objects.AutomationCalendar;
 
 public enum TeamTimeFrames implements SeleniumEnums {
-    DAYS_AGO_TODAY(AutomationCalendar.getTimeFrameOptions()[0], "timeFrameForm:timeFrameToday"),
-    DAYS_AGO_YESTERDAY(AutomationCalendar.getTimeFrameOptions()[1], "timeFrameForm:timeFrameOneDay"),
-    DAYS_AGO_2(AutomationCalendar.getFiveDayPeriodShort()[0], "timeFrameForm:timeFrameTwoDay"),
-    DAYS_AGO_3(AutomationCalendar.getFiveDayPeriodShort()[1], "timeFrameForm:timeFrameThreeDay"),
-    DAYS_AGO_4(AutomationCalendar.getFiveDayPeriodShort()[2], "timeFrameForm:timeFrameFourDay"),
-    DAYS_AGO_5(AutomationCalendar.getFiveDayPeriodShort()[3], "timeFrameForm:timeFrameFiveDay"),
-    DAYS_AGO_6(AutomationCalendar.getFiveDayPeriodShort()[4], "timeFrameForm:timeFrameSixDay"),
+    TODAY(AutomationCalendar.getTimeFrameOptions()[0], "timeFrameForm:timeFrameToday"),
+    YESTERDAY(AutomationCalendar.getTimeFrameOptions()[1], "timeFrameForm:timeFrameOneDay"),
+    TWO_DAYS_AGO(AutomationCalendar.getFiveDayPeriodShort()[0], "timeFrameForm:timeFrameTwoDay"),
+    THREE_DAYS_AGO(AutomationCalendar.getFiveDayPeriodShort()[1], "timeFrameForm:timeFrameThreeDay"),
+    FOUR_DAYS_AGO(AutomationCalendar.getFiveDayPeriodShort()[2], "timeFrameForm:timeFrameFourDay"),
+    FIVE_DAYS_AGO(AutomationCalendar.getFiveDayPeriodShort()[3], "timeFrameForm:timeFrameFiveDay"),
+    SIX_DAYS_AGO(AutomationCalendar.getFiveDayPeriodShort()[4], "timeFrameForm:timeFrameSixDay"),
 
-    THIS_WEEK("7 Days", "timeFrameForm:timeFrameWeek"),
-    THIS_MONTH(AutomationCalendar.getCurrentMonth(), "timeFrameForm:timeFrameMonth"),
-    THIS_YEAR("365 Days", "timeFrameForm:timeFrameYear"), ;
+    WEEK("7 Days", "timeFrameForm:timeFrameWeek"),
+    MONTH(AutomationCalendar.getCurrentMonth(), "timeFrameForm:timeFrameMonth"),
+    YEAR("365 Days", "timeFrameForm:timeFrameYear"), ;
 
     private String text, url;
     private String[] IDs;
