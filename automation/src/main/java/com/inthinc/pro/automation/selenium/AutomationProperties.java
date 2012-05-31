ae5952a06a6a18f39c3e5bd53d04b2c46b0c61dc
dtanner <dtanner@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2012-05-31 20:16:53 +0000

    Playing with how this file is used to see if it will fix the Hudson build

--- automation/src/main/java/com/inthinc/pro/automation/selenium/AutomationProperties.java
+++ automation/src/main/java/com/inthinc/pro/automation/selenium/AutomationProperties.java
@@ -6,13 +6,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.inthinc.pro.automation.AutomationPropertiesBean;
 
 public class AutomationProperties {
+
+    private static final String[] configFiles = { "classpath:spring/applicationContext-automation.xml" };
+    private final BeanFactory factory;
+    private final AutomationPropertiesBean apb;
     
-    private static final String[] configFiles = {"classpath:spring/applicationContext-automation.xml"};
-    private static final BeanFactory factory = new ClassPathXmlApplicationContext(configFiles);
-    private static final AutomationPropertiesBean apb = (AutomationPropertiesBean) factory.getBean("automationPropertiesBean");
+    private static AutomationProperties local;
     
-        public static AutomationPropertiesBean getPropertyBean(){
-            return apb; 
+    private AutomationProperties(){
+        factory = new ClassPathXmlApplicationContext(configFiles);
+        apb = (AutomationPropertiesBean) factory.getBean("automationPropertiesBean");
+    }
+
+    public static AutomationPropertiesBean getPropertyBean() {
+        if (local==null){
+            local = new AutomationProperties();
         }
+        return local.apb;
+    }
 
 }
