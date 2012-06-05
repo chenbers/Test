513ec2a880bf9107af33556650ebc3b517925bd7
bmiller <bmiller@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2012-06-05 19:55:16 +0000

    Refactoring Location/Trip info from DriverDAO/VehicleDAO into LocationDAO to facilitate Cassandra implementation

---
+++ dao/src/main/java/com/inthinc/pro/dao/LocationDAO.java
@@ -0,0 +1,24 @@
+package com.inthinc.pro.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import com.inthinc.pro.model.DriverLocation;
+import com.inthinc.pro.model.LastLocation;
+import com.inthinc.pro.model.Trip;
+
+public interface LocationDAO 
+{
+    public LastLocation getLastLocationForDriver(Integer driverID);
+	public List<DriverLocation> getDriverLocations(Integer groupID);
+    public LastLocation getLastLocationForVehicle(Integer vehicleID);
+    public Trip getLastTripForVehicle(Integer vehicleID);
+    public List<Trip> getTripsForVehicle(Integer vehicleID, Date startDate, Date endDate);
+    public Trip getLastTripForDriver(Integer driverID);
+    public List<Trip> getTripsForDriver(Integer driverID, Date startDate, Date endDate);
+    
+    public VehicleDAO getVehicleDAO();
+	public void setVehicleDAO(VehicleDAO vehicleDAO);
+	public DriverDAO getDriverDAO();
+	public void setDriverDAO(DriverDAO driverDAO);
+}
