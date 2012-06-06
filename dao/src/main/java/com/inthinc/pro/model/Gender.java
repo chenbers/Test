d67cead0078ad5143217037ed3dfc182fa7dd8ab
cjennings <cjennings@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2012-06-06 22:14:26 +0000

    de7920 - fix for clearing the gender when batch editing people

--- dao/src/main/java/com/inthinc/pro/model/Gender.java
+++ dao/src/main/java/com/inthinc/pro/model/Gender.java
@@ -9,7 +9,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public enum Gender implements BaseEnum
 {
-    MALE(1, "MALE"), FEMALE(2, "FEMALE");
+    NOT_SPECIFIED(0, ""), MALE(1, "MALE"), FEMALE(2, "FEMALE");
 
     private int    code;
     private String description;
