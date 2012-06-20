eed294c04a8ed2bb55aa6e6325df4c7878b58a91
mweiss <mweiss@b60b91c4-0efc-49c2-9fcd-097b01f658ff>
2012-06-20 20:33:44 +0000

    Test will be updated later once Admin page has been reworked

--- web/src/test/java/com/inthinc/pro/selenium/testSuites/AdminTest.java
+++ web/src/test/java/com/inthinc/pro/selenium/testSuites/AdminTest.java
@@ -1,31 +1,3 @@
 package com.inthinc.pro.selenium.testSuites;
 
-import java.util.List;
-
-import org.jbehave.core.steps.CandidateSteps;
-
-import com.inthinc.pro.automation.selenium.AbstractPage;
-import com.inthinc.pro.selenium.steps.AdminSteps;
-//TEST BEING TAKEN OFFLINE FOR NOW
-//
-//public class AdminTest extends WebStories {
-//
-//
-//    @Override
-//    public List<CandidateSteps> candidateSteps() {
-//        return candidateSteps(new AdminSteps());
-//    }
-//    
-//    @Override
-//    protected List<String> storyPaths() {
-//        return storyPaths("Admin.story");
-//    }
-//
-//    @Override
-//    public List<AbstractPage> requiredPageObjectsList() {
-//        return pageList();
-//    }
-//    
-//    
-//    
-//}
+public class AdminTest extends WebStories {}
