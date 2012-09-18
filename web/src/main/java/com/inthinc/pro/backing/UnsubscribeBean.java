12ae61201bd13bcbfa02c437406f76e99696cfc7
Jacquie Howard <jhoward@inthinc.com>
2012-09-17 23:05:59 -0600

    Added confirm step to unsubscribing from reports.

--- web/src/main/java/com/inthinc/pro/backing/UnsubscribeBean.java
+++ web/src/main/java/com/inthinc/pro/backing/UnsubscribeBean.java
@@ -5,8 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
+import org.ajax4jsf.model.KeepAlive;
 import org.apache.log4j.Logger;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 
@@ -17,77 +21,124 @@ import com.inthinc.pro.model.Person;
 import com.inthinc.pro.model.ReportSchedule;
 import com.inthinc.pro.reports.ReportGroup;
 import com.inthinc.pro.util.MessageUtil;
+import com.inthinc.pro.validators.EmailValidator;
 
+@KeepAlive
 public class UnsubscribeBean {
-    
+
     private static final Logger logger = Logger.getLogger(UnsubscribeBean.class);
+
+    private static final String WRONG_EMAIL_ADDRESS_KEY = "unsubscribe_wrongEmailErrorMessage";
 
     private String emailAddress;
     private Integer reportScheduleID;
-    
+
     private ReportScheduleDAO reportScheduleDAO;
     private PersonDAO personDAO;
     private String encryptPassword;
-    
-    private String messageType;
-    private String messageName;
-    
-    private Boolean showLoginLink = Boolean.TRUE;
-    
 
-    public void unsubscribeToReportSchedule(){
-        if(emailAddress == null || reportScheduleID == null){
+    private String messageType;
+//    private String messageName;
+
+    private Boolean showLoginLink = Boolean.TRUE;
+
+    private Person person;
+
+    private String enteredEmailAddress;
+    private String decryptedEmailAddress;
+    private ReportSchedule reportSchedule;
+
+    public void prepareToUnsubscribeToReportSchedule() {
+        if (emailAddress == null || reportScheduleID == null) {
             throw new InvalidParameterException("Email Address or reportScheduleID are null.");
         }
-        
-        Locale locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
-        if (!LocaleBean.supportedLocale(locale))
-            locale = Locale.ENGLISH;
-        
-        StandardPBEStringEncryptor textEncryptor = new StandardPBEStringEncryptor();
-        textEncryptor.setPassword(encryptPassword);
-        textEncryptor.setStringOutputType("hexadecimal");
-        
-        String decryptedEmailAddress = textEncryptor.decrypt(emailAddress);
-        
-        ReportSchedule reportSchedule = reportScheduleDAO.findByID(reportScheduleID);
-        if(reportSchedule != null){
+        setupMessageType();
+        setupReportSchedule();
+        setupDecryptedEmailAddress();
+    }
+
+    public String unsubscribeAction() {
+
+        if (reportSchedule != null) {
             ReportGroup reportGroup = ReportGroup.valueOf(reportSchedule.getReportID());
             if (reportGroup.getEntityType() == EntityType.ENTITY_INDIVIDUAL_DRIVER) {
-                
+
                 Person person = personDAO.findByEmail(decryptedEmailAddress);
                 if (person == null) {
                     logger.debug("Cannot find person record with emailAddress = " + decryptedEmailAddress);
-                    return;
+                    return "";
                 }
                 if (person.getDriverID() == null) {
                     logger.debug("Cannot find driverID for person with emailAddress = " + decryptedEmailAddress);
-                    return;
+                    return "";
                 }
                 List<Integer> idList = reportSchedule.getDriverIDList();
                 if (idList == null) {
                     logger.debug("Cannot find driverIDList for reportID = " + reportScheduleID);
-                    return;
+                    return "";
                 }
                 List<Integer> modIdList = new ArrayList<Integer>();
                 for (Integer id : idList) {
                     if (id.equals(person.getDriverID()) || id.equals(ReportScheduleBean.ALL_DRIVERS_ID))
                         continue;
-                    
+
                     modIdList.add(id);
                 }
                 reportSchedule.setDriverIDList(modIdList);
                 setShowLoginLink(Boolean.FALSE);
-            }
-            else {
+            } else {
                 reportSchedule.getEmailTo().remove(decryptedEmailAddress);
             }
             Integer result = reportScheduleDAO.update(reportSchedule);
             logger.debug("ReportSchedule Update Called, Result: " + result);
-            messageType = MessageUtil.getMessageString("unsubscribe_report", locale);
-            messageName = reportSchedule.getName();
-        }else{
+        } else {
             logger.debug("ReportScheduler does not exist for ID = " + reportScheduleID);
+            return "";
+        }
+        return "pretty:unsubscribeSuccessful";
+    }
+
+    public void allDone() {
+
+        if (reportScheduleID == null) {
+            throw new InvalidParameterException("ReportScheduleID is null.");
+        }
+        setupMessageType();
+        setupReportSchedule();
+        logger.debug("Unsubscribe successful.");
+    }
+
+    private void setupMessageType() {
+        Locale locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
+        if (!LocaleBean.supportedLocale(locale))
+            locale = Locale.ENGLISH;
+        messageType = MessageUtil.getMessageString("unsubscribe_report", locale);
+    }
+
+    private void setupReportSchedule() {
+        reportSchedule = reportScheduleDAO.findByID(reportScheduleID);
+        if (reportSchedule == null) {
+            logger.debug("ReportScheduler does not exist for ID = " + reportScheduleID);
+        }
+    }
+
+    private void setupDecryptedEmailAddress() {
+        StandardPBEStringEncryptor textEncryptor = new StandardPBEStringEncryptor();
+        textEncryptor.setPassword(encryptPassword);
+        textEncryptor.setStringOutputType("hexadecimal");
+        decryptedEmailAddress = textEncryptor.decrypt(emailAddress);
+    }
+
+    public void validateEmail(FacesContext context, UIComponent component, Object value) {
+
+        String valueStr = (String) value;
+        if (valueStr != null && valueStr.length() > 0) {
+            new EmailValidator().validate(context, component, value);
+            
+            if (!valueStr.equals(decryptedEmailAddress)) {
+                String errorMessage = MessageUtil.getMessageString(WRONG_EMAIL_ADDRESS_KEY);
+                throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, errorMessage, null));
+            }
         }
     }
 
@@ -115,33 +166,33 @@ public class UnsubscribeBean {
         return reportScheduleDAO;
     }
 
-    
     public String getEncryptPassword() {
         return encryptPassword;
     }
 
-    
     public void setEncryptPassword(String encryptPassword) {
         this.encryptPassword = encryptPassword;
     }
 
-
-    public void setMessageName(String messageName) {
-        this.messageName = messageName;
-    }
+//    public void setMessageName(String messageName) {
+//        this.messageName = messageName;
+//    }
 
     public String getMessageName() {
-        return messageName;
+        if (reportSchedule != null)
+            return reportSchedule.getName();
+        else
+            return null;
     }
 
-    public void setMessageType(String messageType) {
-        this.messageType = messageType;
-    }
+//    public void setMessageType(String messageType) {
+//        this.messageType = messageType;
+//    }
 
     public String getMessageType() {
         return messageType;
     }
-    
+
     public PersonDAO getPersonDAO() {
         return personDAO;
     }
@@ -158,5 +209,27 @@ public class UnsubscribeBean {
         this.showLoginLink = showLoginLink;
     }
 
-    
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public String getDecryptedEmailAddress() {
+        return decryptedEmailAddress;
+    }
+
+    public void setDecryptedEmailAddress(String decryptedEmailAddress) {
+        this.decryptedEmailAddress = decryptedEmailAddress;
+    }
+
+    public String getEnteredEmailAddress() {
+        return enteredEmailAddress;
+    }
+
+    public void setEnteredEmailAddress(String enteredEmailAddress) {
+        this.enteredEmailAddress = enteredEmailAddress;
+    }
 }
