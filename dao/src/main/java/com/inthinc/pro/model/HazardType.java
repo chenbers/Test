8a1ce2885a1c86ed5921a2ea14149aa3848d5a30
Jason Wimmer <jwimmer@inthinc.com>
2012-11-15 17:21:54 -0700

    US6113: updated to match what firmware is expecting - ids match - micro hazard is its own group - punctuation is significant in translations

--- dao/src/main/java/com/inthinc/pro/model/HazardType.java
+++ dao/src/main/java/com/inthinc/pro/model/HazardType.java
@@ -21,32 +21,30 @@ import com.inthinc.pro.model.zone.option.type.OptionValue;
 @XmlEnum
 @JsonSerialize(using = HazardTypeSerializer.class)
 public enum HazardType implements OptionValue {
-    OTHER(0,                        5*MeasurementConversionUtil.METERS_IN_MILE , Severity.URGENT, Period.hours(72), "", "Other"),
-    WEATHER_SLIPPERY(1,             10*MeasurementConversionUtil.METERS_IN_MILE, Severity.URGENT, Period.hours(72), "Weather", "Slippery (Wet/Snow/Ice)"),
-    WEATHER_FLOODING(2,             10*MeasurementConversionUtil.METERS_IN_MILE, Severity.URGENT, Period.hours(72), "Weather", "Flooding"), 
-    WEATHER_VISIBILITY(3,           10*MeasurementConversionUtil.METERS_IN_MILE, Severity.URGENT, Period.hours(72), "Weather", "Low Visibility (Fog/Smoke)"),
+    WEATHER_SLIPPERY(1,             10*MeasurementConversionUtil.METERS_IN_MILE, Severity.URGENT, Period.hours(72), "Weather", "Wet/Slippery"),
+    WEATHER_FLOODING(2,             10*MeasurementConversionUtil.METERS_IN_MILE, Severity.URGENT, Period.hours(72), "Weather", "Flooding"),
+    WEATHER_SNOWICE(3,              10*MeasurementConversionUtil.METERS_IN_MILE, Severity.URGENT, Period.hours(72), "Weather", "Show/Ice") ,
+    WEATHER_VISIBILITY(5,           10*MeasurementConversionUtil.METERS_IN_MILE, Severity.URGENT, Period.hours(72), "Weather", "Low Visibility (Fog/Smoke)"),
     WEATHER_WINDGUSTS(4,            10*MeasurementConversionUtil.METERS_IN_MILE, Severity.URGENT, Period.hours(72), "Weather", "Wind Gusts"),
-    ROADACTIVITY_TRAFFIC(5,         5*MeasurementConversionUtil.METERS_IN_MILE , Severity.NORMAL, Period.hours(4), "Road Activity", "Traffic"),
-    ROADACTIVITY_DEBRIS(6,          5*MeasurementConversionUtil.METERS_IN_MILE , Severity.NORMAL, Period.hours(72), "Road Activity", "Debris"),
-    ROADRESTRICTIONS_WEIGHT(7,      10*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.years(1), "Road Restrictions", "Weight Restrictions (Bridge)"),
-    ROADRESTRICTIONS_HEIGHT(8,      10*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.years(1), "Road Restrictions", "Height Restrictions (Overpass Clearance)"),
-    ROADRESTRICTIONS_WIDTH(9,       10*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.years(1), "Road Restrictions", "Width Restrictions (Narrow Lane)"),
-    ROADRESTRICTIONS_BAN_CLOSURE(10,10*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.years(1), "Road Restrictions", "Road Ban/Closure"),
-    ROADRESTRICTIONS_SHARPTURN(11,  10*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.years(1), "Road Restrictions", "Sharp Turn"),
-    ROADRESTRICTIONS_STEEPGRADE(12, 10*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.years(1), "Road Restrictions", "Steep Grade"),
-    ROADRESTRICTIONS_MICRO(13,      15*MeasurementConversionUtil.METERS_IN_FOOT, Severity.NORMAL, Period.years(1), "Micro Hazards", "Oil Well Head"),
-  
-
-/*  //This section more closely matches PRD 1.4, but does NOT coincide with firmware's doc???
-    WEATHER_OTHER(14, 10*MeasurementConversionUtil.METERS_IN_MILE, Severity.URGENT, Period.hours(72), "Weather - Other"),
-    WEATHER_SNOWICE(15, 10*MeasurementConversionUtil.METERS_IN_MILE, Severity.URGENT, Period.hours(72), "Weather - Snow/Ice"),
-    ROADACTIVITY_CONSTRUCTION(16 , 5*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.months(3), "Road Activity - Construction"),
-    ROADACTIVITY_ACCIDENT(17, 5*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.hours(4), "Road Activity - Accident"),
-    ROADACTIVITY_DISABLEDVEHICLE(18, 5*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.hours(4), "Road Activity - Disabled Vehicle"),
-    ROADACTIVITY_OTHER(19, 5*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.hours(72), "Road Activity - Other"),
-    ROADRESTRICTIONS_CLOSURE(20, 5*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.years(1), "Road Restrictions - Closure"), //JUST closure above combined with road ban
-    ROADRESTRICTIONS_OTHER(21, 5*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.years(1), "Road Restrictions - Other"),
-  */
+    WEATHER_OTHER(6,                10*MeasurementConversionUtil.METERS_IN_MILE, Severity.URGENT, Period.hours(72), "Weather", "Other"),
+    
+    ROADACTIVITY_TRAFFIC(21,        5*MeasurementConversionUtil.METERS_IN_MILE , Severity.NORMAL, Period.hours(4), "Road Activity", "Traffic"),
+    ROADACTIVITY_DEBRIS(9,          5*MeasurementConversionUtil.METERS_IN_MILE , Severity.NORMAL, Period.hours(72), "Road Activity", "Debris"),
+    ROADACTIVITY_CONSTRUCTION(7,    5*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.months(3), "Road Activity", "Construction"),
+    ROADACTIVITY_ACCIDENT(8,        5*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.hours(4), "Road Activity", "Accident"),
+    ROADACTIVITY_DISABLEDVEHICLE(10,5*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.hours(4), "Road Activity", "Disabled Vehicle"),
+    ROADACTIVITY_OTHER(11,          5*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.hours(72), "Road Activity", "Other"),
+    
+    ROADRESTRICTIONS_WEIGHT(12,     10*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.years(1), "Road Restrictions", "Weight Restrictions (Bridge)"),
+    ROADRESTRICTIONS_HEIGHT(13,     10*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.years(1), "Road Restrictions", "Height Restrictions (Overpass Clearance)"),
+    ROADRESTRICTIONS_WIDTH(20,      10*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.years(1), "Road Restrictions", "Width Restrictions (Narrow Lane)"),
+    ROADRESTRICTIONS_BAN(14,        10*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.years(1), "Road Restrictions", "Road Ban"),
+    ROADRESTRICTIONS_CLOSURE(15,    5*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.years(1), "Road Restrictions", "Closure"), //JUST closure above combined with road ban
+    ROADRESTRICTIONS_SHARPTURN(16,  10*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.years(1), "Road Restrictions", "Sharp Turn"),
+    ROADRESTRICTIONS_STEEPGRADE(17, 10*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.years(1), "Road Restrictions", "Steep Grade"),
+    ROADRESTRICTIONS_OTHER(18,      5*MeasurementConversionUtil.METERS_IN_MILE, Severity.NORMAL, Period.years(1), "Road Restrictions", "Other"),
+    
+    MICRO_WELLHEAD(19,              15*MeasurementConversionUtil.METERS_IN_FOOT, Severity.NORMAL, Period.years(1), "Micro Hazards", "Oil Well Head"),
     ;
 
     private int code;
@@ -86,7 +84,7 @@ public enum HazardType implements OptionValue {
     }
     @XmlElement(name = "details")
     public String getDetails() {
-        return group +" - "+ name;
+        return group +" "+ name;
     }
     @XmlElement(name = "urgent")
     public boolean isUrgent() {
--- dao/src/test/java/com/inthinc/pro/model/HazardTest.java
+++ dao/src/test/java/com/inthinc/pro/model/HazardTest.java
@@ -1,10 +1,12 @@
 package com.inthinc.pro.model;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.nio.ByteBuffer;
 import java.util.Date;
 
+import org.joda.time.DateTime;
+import org.joda.time.Period;
 import org.junit.Test;
 
 
@@ -38,6 +40,38 @@ public class HazardTest {
         System.out.println("m_timeStart: "+new Date(m_timeStart));
         System.out.println("m_timeEnd: "+new Date(m_timeEnd));
     }
+    private int daysInAYear = 365;
+    private int hoursInADay = 24;
+    private int minutesInAnHour = 60;
+    private int secondsInAMinute = 60;
+    private int secondsInOneYear = 1  * daysInAYear * hoursInADay * minutesInAnHour * secondsInAMinute;
+    private int secondsInTwoYears = 2 * daysInAYear * hoursInADay * minutesInAnHour * secondsInAMinute;
+    @Test
+    public void periodTest_howManySecondsInAYear(){
+        DateTime startTime = new DateTime();
+        Date newEndTime = startTime.plus(Period.years(1)).toDate();
+        System.out.println("startTime: "+startTime);
+        System.out.println("endTime: "+newEndTime);
+        System.out.println(newEndTime.getTime() - startTime.getMillis());
+
+        assertEquals((newEndTime.getTime() - startTime.getMillis())/1000 , secondsInOneYear);
+        newEndTime = startTime.plus(Period.years(2)).toDate();
+        assertEquals((newEndTime.getTime() - startTime.getMillis())/1000, secondsInTwoYears);
+    }
+    
+    @Test
+    public void periodTest_howManySecondsInALeapYear(){
+        DateTime firstDayOfLeapYear = new DateTime(2012, 1, 1, 0, 0, 0, 0);
+        Date newEndTime = firstDayOfLeapYear.plus(Period.years(1)).toDate();
+        System.out.println("startTime: "+firstDayOfLeapYear);
+        System.out.println("endTime: "+newEndTime);
+        System.out.println(newEndTime.getTime() - firstDayOfLeapYear.getMillis());
+
+        assertEquals((newEndTime.getTime() - firstDayOfLeapYear.getMillis())/1000 , secondsInOneYear);
+        newEndTime = firstDayOfLeapYear.plus(Period.years(2)).toDate();
+        assertEquals((newEndTime.getTime() - firstDayOfLeapYear.getMillis()) , secondsInTwoYears);
+    }
+    
     public static byte[] hexStringToByteArray(String s) {
         int len = s.length();
         byte[] data = new byte[len / 2];
@@ -47,4 +81,6 @@ public class HazardTest {
         }
         return data;
     }
+    
+  
 }
