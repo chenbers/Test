96ffbe4e4f4b10d25be3af9466c9fe37fc1231af
SpyderWeiss <mweiss@inthinc.com>
2012-11-28 10:10:24 -0700

    I split the former FormsAdmin page into FormsManage and FormsPublished to match the new format on the site.  I still need to move some universal items that are on the forms bar further up to a higher level, but this split is mostly finished now.

---
+++ web/src/test/java/com/inthinc/pro/selenium/pageEnums/FormsManageEnum.java
@@ -0,0 +1,81 @@
+package com.inthinc.pro.selenium.pageEnums;
+
+import com.inthinc.pro.automation.interfaces.SeleniumEnums;
+import com.inthinc.pro.automation.utils.Id;
+import com.inthinc.pro.automation.utils.Xpath;
+
+public enum FormsManageEnum implements SeleniumEnums {
+    DEFAULT_URL(appUrl + "/forms"),
+    TITLE("Forms", "//span[@class='admin']"),
+
+    //No longer on page SEARCH_HEADER("Search:", "//div[@id='staging-forms-table_filter']"),
+    //No longer on page FORMS_HEADER("Forms", "//label[@for='formSelection']"),
+    
+    NEW_FORM_BUTTON(null, "//i[@class='icon-plus icon-large']"),
+    
+    MANAGE_TAB("Working", "//div[2]/div[1]/div/ul/li[1]/a"),
+    PUBLISHED_TAB("Published", "//div[2]/div[1]/div/ul/li[2]/a"),
+    SUBMISSIONS_TAB("Published", "//div[2]/div[1]/div/ul/li[3]/a"),
+    CUSTOMERS_TAB("Published", "//div[2]/div[1]/div/ul/li[4]/a"),
+    
+    RECORDS_DROPDOWN("records per page", "//select[@name='staging-forms-table_length']"),
+    SEARCH_TEXTFIELD("Search:", "//div[@id='staging-forms-table_filter']/label/input"),
+    
+    //No longer on page SELECT_MANAGE_LINK("Select", "//th[@id='column-working-select']"),
+    NAME_LINK("Name", "//th[@id='column-staging-name']"),  
+    BASE_FORM_ID_LINK("Base Form ID", "//th[@id='column-staging-baseFormID']"),
+    VERSION_LINK("Version", "//th[@id='column-staging-version']"),
+    DESCRIPTION_LINK("Description", "//th[@id='column-staging-description']"),
+    STATUS_LINK("Status", "//th[@id='column-status']"),
+    TRIGGER_LINK("Trigger", "//th[@id='column-staging-trigger']"),
+    PUBLISH_LINK("Publish", "//th[@id='column-staging-publish']"),
+    EDIT_LINK("Edit", "//th[@id='column-staging-edit']"),
+
+    SELECT_ALL_CHECKBOX(null, "//th[@id='column-staging-select']/input[@id='select-all']"),
+    CHECKBOX_ENTRY(null, "//table[@id='staging-forms-table']/tbody/tr[###]/td[1]"),
+    
+    NAME_MANAGE_ENTRY("Name", "//table[@id='staging-forms-table']/tbody/tr[###]/td[1]"),
+    BASE_FORM_ID_MANAGE_ENTRY("Base Form ID", "//table[@id='staging-forms-table']/tbody/tr[###]/td[2]"),
+    VERSION_MANAGE_ENTRY("Version", "//table[@id='staging-forms-table']/tbody/tr[###]/td[3]"),
+    DESCRIPTION_MANAGE_ENTRY("Description", "//table[@id='staging-forms-table']/tbody/tr[###]/td[4]"),
+    STATUS_MANAGE_ENTRY("Status", "//table[@id='staging-forms-table']/tbody/tr[###]/td[5]"),
+    TRIGGER_MANAGE_ENTRY("Trigger", "//table[@id='staging-forms-table']/tbody/tr[###]/td[6]"),
+    PUBLISH_ENTRY_LINK("Publish", "//table[@id='staging-forms-table']/tbody/tr[###]/td[7]"),
+    EDIT_ENTRY_LINK("Edit", "//table[@id='staging-forms-table']/tbody/tr[###]/td[8]"),
+    
+    NO_RECORDS_FOUND_MANAGE_ERROR("No matching records found", "//table[@id='staging-forms-table']/tbody/tr/td[@class='dataTables_empty']"),
+    
+    ENTRIES_MANAGE_TEXT("Showing ### to ### of ### entries", "//div[@id='staging-forms-table_info']"),
+    
+    PREVIOUS_MANAGE("Previous", "//li[@class='prev']"), 
+    PAGE_NUMBER_MANAGE(null, "//li[###]/a[@href='#']"),
+    NEXT_MANAGE("Next", "//li[@class='next']"),
+    
+    ;
+
+    private String text, url;
+    private String[] IDs;
+    
+    private FormsManageEnum(String url){
+    	this.url = url;
+    }
+    private FormsManageEnum(String text, String ...IDs){
+        this.text=text;
+    	this.IDs = IDs;
+    }
+
+    @Override
+    public String[] getIDs() {
+        return IDs;
+    }
+
+    @Override
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String getURL() {
+        return url;
+    }
+}
--- web/src/test/java/com/inthinc/pro/selenium/testSuites/FormsOptionsEditorTest.java
+++ web/src/test/java/com/inthinc/pro/selenium/testSuites/FormsOptionsEditorTest.java
@@ -8,7 +8,7 @@ import com.inthinc.pro.automation.annotations.AutomationAnnotations.StoryPath;
 import com.inthinc.pro.selenium.pageObjects.PageExecutiveDashboard;
 import com.inthinc.pro.selenium.pageObjects.PageExecutiveOverallExpansion;
 import com.inthinc.pro.selenium.pageObjects.PageFormsAdd;
-import com.inthinc.pro.selenium.pageObjects.PageFormsAdmin;
+import com.inthinc.pro.selenium.pageObjects.PageFormsManage;
 import com.inthinc.pro.selenium.pageObjects.PageFormsSubmissions;
 import com.inthinc.pro.selenium.pageObjects.PageLogin;
 import com.inthinc.pro.selenium.steps.LoginSteps;
@@ -16,7 +16,7 @@ import com.inthinc.pro.selenium.steps.LoginSteps;
 @UsingSteps(instances={LoginSteps.class})
 @PageObjects(list={PageLogin.class, PageExecutiveDashboard.class, 
 		PageExecutiveOverallExpansion.class, PageFormsSubmissions.class,
-        PageFormsAdd.class, PageFormsAdmin.class })
+        PageFormsAdd.class, PageFormsManage.class })
 @StoryPath(path="FormsOptionsEditor.story")
 public class FormsOptionsEditorTest extends WebStories  {
 
