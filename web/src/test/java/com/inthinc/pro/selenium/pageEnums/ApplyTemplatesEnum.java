e9dc52d70581963da70dd4495bb2e347f0ad0f49
tfollmerjr <tfollmerjr@inthinc.com>
2012-12-06 16:40:48 -0700

    configurator page object and enum changes

--- web/src/test/java/com/inthinc/pro/selenium/pageEnums/ApplyTemplatesEnum.java
+++ web/src/test/java/com/inthinc/pro/selenium/pageEnums/ApplyTemplatesEnum.java
@@ -5,13 +5,16 @@ import com.inthinc.pro.automation.interfaces.SeleniumEnums;
 public enum ApplyTemplatesEnum implements SeleniumEnums {
 	
 	ACCOUNT_DROPDOWN("Account:","select-account"),
+	
 	GROUP_NAV_TREE("Group","group-dropdown"),
+	
 	TEMPLATE_BUTTON("Template",""),
 	
 	AUDITION_LINK("Audition ...","audition-template"),
 	APPLY_LINK("Apply ...","apply-template"),
 	
 	ACCEPT_TEMPLATE_VALUE_CHECKBOX("Accept Template Value","select-all"),
+	
 	VEHICLE_TEXT("Vehicle","column-setting-vehicleID"),
 	SETTING_ID_TEXT("SetingID","column-setting-settingID"),
 	SETTING_TEXT("Setting","column-setting-setting"),
