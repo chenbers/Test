3bb3c486e0bbd0e449bf5549f038ce5281147875
Jacquie Howard <jhoward@inthinc.com>
2012-12-19 11:49:54 -0700

    tidying up commented out code and simplifying a bit.

--- web/src/main/java/com/inthinc/pro/backing/WS850SettingManager.java
+++ web/src/main/java/com/inthinc/pro/backing/WS850SettingManager.java
@@ -14,14 +14,6 @@ import com.inthinc.pro.model.configurator.SpeedingConstants;
 import com.inthinc.pro.model.configurator.VehicleSetting;
 
 public class WS850SettingManager extends VehicleSettingManager {
-//    public final int NUM_SPEEDS = 15;
-//    public final Integer[] DEFAULT_SPEED_SETTING = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};    
-////    public final Integer[] DEFAULT_SPEED_SETTING = {5, 10, 15, 20, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25};    
-//    public final String DEFAULT_SPEED_SET = "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0";
-//    public final Integer[] SPEED_LIMITS = {5,10,15,20,25,30,35,40,45,50,55,60,65,70,75};
-//    public final String[] SPEED_FIELDS = {"speed0","speed1","speed2","speed3","speed4","speed5","speed6","speed7","speed8","speed9","speed10","speed11","speed12","speed13","speed14"};
-//
-//    private static final Double DEFAULT_MAX_SPEED_LIMIT = 78.0;
 
     public WS850SettingManager(ConfiguratorDAO configuratorDAO, SensitivitySliders sensitivitySliders, VehicleSetting vehicleSetting) {
         
@@ -41,7 +33,6 @@ public class WS850SettingManager extends VehicleSettingManager {
         Double maxSpeed = SpeedingConstants.INSTANCE.DEFAULT_MAX_SPEED_LIMIT;
         Integer idlingThresholdSeconds = IdlingSetting.DEFAULT.getSeconds();
         
-//        public WS850EditableVehicleSettings(Integer[] speedSettings, Integer hardAcceleration, Integer hardBrake, Integer hardTurn, Integer hardVertical, Integer dotVehicleType) {
         return new WS850EditableVehicleSettings(vehicleID==null?-1:vehicleID, speedSettings, hardAcceleration, hardBrake, hardTurn,hardVertical,maxSpeed,VehicleDOTType.NON_DOT.getConfiguratorSetting(), idlingThresholdSeconds);
     }
     protected EditableVehicleSettings createFromExistingValues(Integer vehicleID, VehicleSetting vs){
