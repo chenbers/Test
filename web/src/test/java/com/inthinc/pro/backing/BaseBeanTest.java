5a3c291dc3c96fbde1dbe67c17ba4741261d6e89
Matt Raby <mraby@inthinc.com>
2013-05-09 16:27:15 -0600

    updated basebeantest to reference just the applicationcontext-mockdao.xml for it's dataaccess wiring. added the bean definitions to test/resources/applicationcontext-mockdao.xml. removed the essentially blank mockdao.xml in the main/resources dir.

--- web/src/test/java/com/inthinc/pro/backing/BaseBeanTest.java
+++ web/src/test/java/com/inthinc/pro/backing/BaseBeanTest.java
@@ -49,7 +49,6 @@ import com.inthinc.pro.security.userdetails.ProUser;
 
 // any classes that extend this one will also have access to these configurations
 @ContextConfiguration(locations={"classpath:spring/applicationContext-mockdao.xml",
-                                 "classpath:spring/applicationContext-dao.xml",
                                  "classpath:spring/applicationContext-beans.xml",
                                  "classpath:spring/applicationContext-security.xml",
                                  "classpath:spring/applicationContext-reports.xml"},
