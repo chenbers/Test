627e293aecdda5ae643f2f0f0431699573d6c3a2
Jason Wimmer <jwimmer@inthinc.com>
2013-10-08 16:09:48 -0600

    adding test for DE9012 scenario (no drivers in parent group)

--- web/src/test/java/com/inthinc/pro/backing/TeamCommonBeanTest.java
+++ web/src/test/java/com/inthinc/pro/backing/TeamCommonBeanTest.java
@@ -2,19 +2,71 @@ package com.inthinc.pro.backing;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import junit.framework.Assert;
 
+import static org.easymock.EasyMock.expect;
+import static org.easymock.classextension.EasyMock.createMock;
+import static org.easymock.classextension.EasyMock.replay;
+
+import org.easymock.classextension.EasyMock;
+import org.eclipse.jdt.internal.core.CreateMethodOperation;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.junit.Test;
 
+import com.inthinc.pro.dao.report.GroupReportDAO;
+import com.inthinc.pro.model.AggregationDuration;
+import com.inthinc.pro.model.Duration;
+import com.inthinc.pro.model.Group;
+import com.inthinc.pro.model.GroupHierarchy;
 import com.inthinc.pro.model.TimeFrame;
+import com.inthinc.pro.model.aggregation.GroupTrendWrapper;
 import com.inthinc.pro.model.aggregation.Trend;
 
 public class TeamCommonBeanTest {
+    @Test
+    public void getOverallScoreUsingTrendScore_nullSubGroupsAggregateDriverTrends_noNPE() throws SecurityException, NoSuchMethodException {
+    	Group mockGroup = EasyMock.createNiceMock(Group.class);
+    	Integer fakeGroupID = 2;
+    	Integer fakeParentGroupID = 1;
+    	
+    	expect(mockGroup.getParentID()).andReturn(fakeParentGroupID);
+    	replay(mockGroup);
+    	
+    	GroupHierarchy mockGroupHierarchy = createMock(GroupHierarchy.class);
+    	Method mockGetGroupHierarchy = TeamCommonBean.class.getMethod("getGroupHierarchy", null);
+    	Method mockGetTimeFrame = TeamCommonBean.class.getMethod("getTimeFrame", null);
+
+    	TeamCommonBean partialMockTeamCommonBean = EasyMock.createNiceMock(TeamCommonBean.class, mockGetGroupHierarchy, mockGetTimeFrame);
+    	EasyMock.expect(partialMockTeamCommonBean.getGroupHierarchy()).andReturn(mockGroupHierarchy);
+    	TimeFrame mockTimeFrame = TimeFrame.MONTH; 
+    	
+    	EasyMock.expect(partialMockTeamCommonBean.getTimeFrame()).andReturn(mockTimeFrame);
+    	EasyMock.expectLastCall().anyTimes();
+    	
+    	Map<String, GroupTrendWrapper> fakeCachedTrendResults = new HashMap<String,GroupTrendWrapper>();
+    	partialMockTeamCommonBean.setCachedTrendResults(fakeCachedTrendResults);
+    	replay(partialMockTeamCommonBean);
+    	
+    	GroupReportDAO mockGroupReportDAO = createMock(GroupReportDAO.class);
+    	partialMockTeamCommonBean.setGroup(mockGroup);
+    	expect(mockGroupReportDAO.getSubGroupsAggregateDriverTrends(fakeParentGroupID, Duration.THREE, mockGroupHierarchy)).andReturn(null);
+    	replay(mockGroupReportDAO);
+    	try {
+    		partialMockTeamCommonBean.getOverallScoreUsingTrendScore(mockGroupReportDAO);
+    	} catch( NullPointerException npe) {
+    		npe.printStackTrace();
+    		fail();
+    	}
+    }
+	
     @Test
     public void testPopulateDateGaps() {
         TeamCommonBean teamCommonBean = new TeamCommonBean();
@@ -60,4 +112,5 @@ public class TeamCommonBeanTest {
         
         assertEquals(4, trendList.size());
     }
+
 }
