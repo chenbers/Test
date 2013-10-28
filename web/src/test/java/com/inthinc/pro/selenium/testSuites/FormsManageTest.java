34a234c2794d81e2aa2b35b60071b4ecf2e08258
SpyderWeiss <mweiss@inthinc.com>
2013-10-28 14:10:36 -0600

    Added missing pageobject.

--- web/src/test/java/com/inthinc/pro/selenium/testSuites/FormsManageTest.java
+++ web/src/test/java/com/inthinc/pro/selenium/testSuites/FormsManageTest.java
@@ -8,15 +8,18 @@ import com.inthinc.pro.automation.annotations.AutomationAnnotations.StoryPath;
 import com.inthinc.pro.selenium.pageObjects.PageExecutiveDashboard;
 import com.inthinc.pro.selenium.pageObjects.PageExecutiveOverallExpansion;
 import com.inthinc.pro.selenium.pageObjects.PageFormsAdd;
+import com.inthinc.pro.selenium.pageObjects.PageFormsEdit;
 import com.inthinc.pro.selenium.pageObjects.PageFormsManage;
 import com.inthinc.pro.selenium.pageObjects.PageFormsPublished;
 import com.inthinc.pro.selenium.pageObjects.PageLogin;
+import com.inthinc.pro.selenium.steps.FormsGenerationSteps;
 import com.inthinc.pro.selenium.steps.LoginSteps;
 
-@UsingSteps(instances={LoginSteps.class})
+@UsingSteps(instances={LoginSteps.class, FormsGenerationSteps.class})
 @PageObjects(list={PageLogin.class, 
 		PageExecutiveDashboard.class, PageExecutiveOverallExpansion.class, 
-        PageFormsManage.class, PageFormsAdd.class, PageFormsPublished.class })
+        PageFormsManage.class, PageFormsAdd.class, PageFormsPublished.class,
+        PageFormsEdit.class})
 @StoryPath(path="FormsManage.story")
 public class FormsManageTest extends WebStories  {
 
