eae642df6c9fb02eaa416f03848573ceae65102a
SpyderWeiss <mweiss@inthinc.com>
2014-03-03 16:23:36 -0700

    Added a missing page object.

--- web/src/test/java/com/inthinc/pro/selenium/testSuites/NotificationsCrashHistoryTest.java
+++ web/src/test/java/com/inthinc/pro/selenium/testSuites/NotificationsCrashHistoryTest.java
@@ -10,6 +10,7 @@ import com.inthinc.pro.selenium.pageObjects.PageExecutiveDashboard;
 import com.inthinc.pro.selenium.pageObjects.PageLogin;
 import com.inthinc.pro.selenium.pageObjects.PageNotificationsCrashHistory;
 import com.inthinc.pro.selenium.pageObjects.PageNotificationsCrashHistoryAddEdit;
+import com.inthinc.pro.selenium.pageObjects.PageNotificationsCrashReport;
 import com.inthinc.pro.selenium.pageObjects.PageNotificationsRedFlags;
 import com.inthinc.pro.selenium.pageObjects.PageReportsDrivers;
 import com.inthinc.pro.selenium.pageObjects.PageTeamDriverStatistics;
@@ -17,7 +18,7 @@ import com.inthinc.pro.selenium.pageObjects.PageVehiclePerformance;
 import com.inthinc.pro.selenium.steps.LoginSteps;
 
 @UsingSteps(instances={LoginSteps.class})
-@PageObjects(list={PageLogin.class, PageExecutiveDashboard.class, PageNotificationsCrashHistory.class,
+@PageObjects(list={PageLogin.class, PageExecutiveDashboard.class, PageNotificationsCrashHistory.class, PageNotificationsCrashReport.class,
 		PageNotificationsCrashHistoryAddEdit.class, PageNotificationsRedFlags.class, PageDriverPerformance.class, 
 		PageVehiclePerformance.class, PageReportsDrivers.class, PageTeamDriverStatistics.class})
 @StoryPath(path="NotificationsCrashHistory.story")
