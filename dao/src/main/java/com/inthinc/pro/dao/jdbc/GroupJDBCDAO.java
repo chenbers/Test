d53ff4762e497e92a48769f797ca733088f05956
alexandru-matei <alexandru.matei@infrasoft.ro>
2014-03-05 13:25:33 +0200

    US7844 - Add update method, debug/cleanup other methods, improve tests a bit, add update test. Must do something about path (probably next commit).

--- dao/src/main/java/com/inthinc/pro/dao/jdbc/GroupJDBCDAO.java
+++ dao/src/main/java/com/inthinc/pro/dao/jdbc/GroupJDBCDAO.java
@@ -1,13 +1,15 @@
 package com.inthinc.pro.dao.jdbc;
 
-import com.inthinc.pro.aggregation.util.DateUtil;
 import com.inthinc.pro.dao.GroupDAO;
-import com.inthinc.pro.model.*;
+import com.inthinc.pro.model.Address;
+import com.inthinc.pro.model.DOTOfficeType;
+import com.inthinc.pro.model.Group;
+import com.inthinc.pro.model.GroupStatus;
+import com.inthinc.pro.model.GroupType;
 import com.mysql.jdbc.Statement;
 import org.apache.commons.lang.NotImplementedException;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
-import org.opengis.filter.expression.Add;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
@@ -15,9 +17,16 @@ import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
-import java.sql.*;
+import java.sql.Connection;
 import java.sql.Date;
-import java.util.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class GroupJDBCDAO extends SimpleJdbcDaoSupport implements GroupDAO {
@@ -25,14 +34,31 @@ public class GroupJDBCDAO extends SimpleJdbcDaoSupport implements GroupDAO {
     private static final long serialVersionUID = 2855781183498326572L;
 
     private static final String GET_GROUP = "select g.groupID, g.acctID, g.parentID, g.name, g.desc, g.status, g.groupPath, g.addrID, g.addrID2, g.level, g.managerID, g.mapZoom, g.mapLat, g.mapLng, g.zoneRev, g.aggDate, g.newAggDate, g.dotOfficeType  from groups g ";
-    private static final String GET_GROUP_ACCT = "select g.groupID, g.acctID, g.parentID, g.name, g.desc, g.status, g.groupPath, g.addrID, g.addrID2, g.level, g.managerID, g.mapZoom, g.mapLat, g.mapLng, g.zoneRev, g.aggDate, g.newAggDate, g.dotOfficeType  from groups g where g.status <> 3 ";
+    private static final String GET_GROUP_ACCT = "select g.groupID, g.acctID, g.parentID, g.name, g.desc, g.status, g.groupPath, g.addrID, g.addrID2, g.level, g.managerID, g.mapZoom, g.mapLat, g.mapLng, g.zoneRev, g.aggDate, g.newAggDate, g.dotOfficeType  from groups g where g.status <> 3 and g.acctID = :acctID";
     private static final String FIND_GROUP_BY_ID = "select g.groupID, g.acctID, g.parentID, g.name, g.desc, g.status, g.groupPath, g.addrID, g.addrID2, g.level, g.managerID, g.mapZoom, g.mapLat, g.mapLng, g.zoneRev, g.aggDate, g.newAggDate, g.dotOfficeType  from groups g where g.groupID =:groupID";
     private static final String DEL_GROUP_BY_ID = "DELETE FROM groups WHERE groupID = ?";
     private static final String ADD_NEW_ADDRESS = "INSERT INTO address (acctID, addr1, addr2, city, stateID, zip) VALUES (?, ?, ?, ?, ?, ?);";
 
-    private static final String INSERT_GROUP_ACCOUNT = "insert into groups (groupID, acctID, parentID, name, `desc`, status, groupPath, dotOfficeType , level, managerID, mapZoom, mapLat, mapLng, zoneRev, aggDate, addrID)  values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? );";
+    private static final String INSERT_GROUP_ACCOUNT = "insert into groups (acctID, parentID, name, `desc`, status, groupPath, dotOfficeType , level, managerID, mapZoom, mapLat, mapLng, zoneRev, aggDate, addrID)  values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+    private static final String UPDATE_GROUP_ACCOUNT = "UPDATE groups " +
+            " set " +
+            " acctID = ?," +
+            " parentID = ?," +
+            " name = ?," +
+            " `desc` = ?," +
+            " status = ?," +
+            " groupPath = ?," +
+            " dotOfficeType = ?," +
+            " level = ?," +
+            " managerID = ?," +
+            " mapZoom = ?," +
+            " mapLat = ?," +
+            " mapLng = ?," +
+            " zoneRev = ?," +
+            " aggDate = ?," +
+            " addrID = ?" +
+            " where groupID = ?";
 
-    private static final String ADD_ADDRID = "SELECT LAST_INSERT_ID();";
 
     private static final DateTimeFormatter dateFormatter = DateTimeFormat.forPattern("yyyy-MM-dd");
 
@@ -54,15 +80,16 @@ public class GroupJDBCDAO extends SimpleJdbcDaoSupport implements GroupDAO {
             groupItem.setMapLat(groupItem.getMapLat());
             groupItem.setMapLng(groupItem.getMapLng());
             groupItem.setDotOfficeType(rs.getObject("dotOfficeType") == null ? null : DOTOfficeType.valueOf(rs.getInt("dotOfficeType")));
-            groupItem.setAddress(new Address());
-            groupItem.setAddressID(getIntOrNullFromRs(rs, "addrID"));
-            groupItem.setType(rs.getObject("level") == null? null : GroupType.valueOf(rs.getInt("level")));
+            Address address = new Address();
+            address.setAddrID(getIntOrNullFromRs(rs, "addrID"));
+            groupItem.setAddress(address);
+            groupItem.setAddressID(address.getAddrID());
+            groupItem.setType(rs.getObject("level") == null ? null : GroupType.valueOf(rs.getInt("level")));
 
             return groupItem;
         }
     };
 
-    
 
     public Integer getIntOrNullFromRs(ResultSet rs, String key) throws SQLException {
         Object obj = rs.getObject(key);
@@ -77,14 +104,11 @@ public class GroupJDBCDAO extends SimpleJdbcDaoSupport implements GroupDAO {
         params.put("acctID", acctID);
         params.put("groupID", groupID);
 
-        StringBuilder groupSelect = new StringBuilder(GET_GROUP);
-        List <Group> allGroups = getGroupsByAcctID(acctID);
-        List <Group> groupHierarchy = new ArrayList<Group>();
+        List<Group> allGroups = getGroupsByAcctID(acctID);
+        List<Group> groupHierarchy = new ArrayList<Group>();
 
-        for (Group group : allGroups)
-        {
-            if (group.getGroupID().equals(groupID))
-            {
+        for (Group group : allGroups) {
+            if (group.getGroupID().equals(groupID)) {
                 groupHierarchy.add(group);
                 addChildren(allGroups, groupHierarchy, group.getGroupID());
                 break;
@@ -117,60 +141,70 @@ public class GroupJDBCDAO extends SimpleJdbcDaoSupport implements GroupDAO {
         Map<String, Object> args = new HashMap<String, Object>();
         args.put("groupID", groupID);
         StringBuilder groupSelectAcct = new StringBuilder(FIND_GROUP_BY_ID);
-        return getSimpleJdbcTemplate().queryForObject(groupSelectAcct.toString(), groupParameterizedRow, args );
+        return getSimpleJdbcTemplate().queryForObject(groupSelectAcct.toString(), groupParameterizedRow, args);
     }
 
     @Override
-    public Integer create(Integer integer,final Group entity) {
+    public Integer create(Integer id, final Group entity) {
         JdbcTemplate jdbcTemplate = getJdbcTemplate();
         KeyHolder keyHolder = new GeneratedKeyHolder();
-        createAddress(entity.getAddress());
+        entity.setAddress(createAddressIfNeeded(entity.getAccountID(), entity.getAddress()));
         PreparedStatementCreator psc = new PreparedStatementCreator() {
-
             @Override
             public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
                 PreparedStatement ps = con.prepareStatement(INSERT_GROUP_ACCOUNT, Statement.RETURN_GENERATED_KEYS);
-//                groupID, acctID, parentID, name, desc, status, groupPath, addrID, addrID2, level, managerID, mapZoom, mapLat, mapLng, zoneRev, aggDate, dotOfficeType
-                ps.setInt(1, entity.getGroupID());
-                ps.setInt(2, entity.getAccountID());
-                ps.setInt(3, entity.getParentID());
-                ps.setString(4, entity.getName());
-                ps.setString(5, entity.getDescription());
-                ps.setInt(6, entity.getStatus().getCode());
-
-                //TODO see how i can make getpath
-                ps.setString(7, entity.getPath());
-
-                //TODO study and modify address & insert in table address with new id
-                ps.setInt(8, entity.getDotOfficeType().getCode());
-
-                ps.setInt(9, entity.getType().getCode());
-                if(entity.getManagerID()==null){
-                    ps.setNull(10, Types.NULL);
-                }else{
-                    ps.setInt(10, entity.getManagerID());
+                ps.setInt(1, entity.getAccountID());
+                ps.setInt(2, entity.getParentID());
+                ps.setString(3, entity.getName() == null ? "" : entity.getName().trim());
+                ps.setString(4, entity.getDescription());
+                if (entity.getStatus() == null || entity.getStatus().getCode()==null) {
+                    ps.setNull(5, Types.NULL);
+                } else {
+                    ps.setInt(5, entity.getStatus().getCode());
                 }
-                ps.setInt(11, entity.getMapZoom());
-                ps.setDouble(12, entity.getMapLat());
-                ps.setDouble(13, entity.getMapLng());
 
-                //TODO study and modify zoneRev
-                if(entity.getZoneRev()==null){
-                    ps.setInt(14, 1);
-                }else{
-                    ps.setInt(14, entity.getZoneRev());
+                ps.setString(6, entity.getPath());
+
+                if (entity.getDotOfficeType() == null || entity.getDotOfficeType().getCode()==null) {
+                    ps.setNull(7, Types.NULL);
+                } else {
+                    ps.setInt(7, entity.getDotOfficeType().getCode());
                 }
-                java.util.Date rightNow = new java.util.Date();
-                ps.setDate(15, new Date(rightNow.getTime()));
 
-                if(entity.getAddress()==null){
-                    ps.setNull(16, Types.NULL);
-                }else{
-                    ps.setInt(16, entity.getAddressID());
+                if (entity.getType() == null || entity.getType().getCode()==null) {
+                    ps.setNull(8, Types.NULL);
+                } else {
+                    ps.setInt(8, entity.getType().getCode());
+                }
+
+                if (entity.getManagerID() == null) {
+                    ps.setNull(9, Types.NULL);
+                } else {
+                    ps.setInt(9, entity.getManagerID());
+                }
+                ps.setInt(10, entity.getMapZoom());
+                ps.setDouble(11, entity.getMapLat());
+                ps.setDouble(12, entity.getMapLng());
+
+                if (entity.getZoneRev() == null) {
+                    ps.setInt(13, Types.NULL);
+                } else {
+                    ps.setInt(13, entity.getZoneRev());
+                }
+
+                if (entity.getAggDate() == null) {
+                    ps.setNull(14, Types.NULL);
+                } else {
+                    ps.setDate(14, new java.sql.Date(dateFormatter.parseDateTime(entity.getAggDate()).getMillis()));
+                }
+
+                if (entity.getAddress() == null) {
+                    ps.setNull(15, Types.NULL);
+                } else {
+                    ps.setInt(15, entity.getAddress().getAddrID());
                 }
                 logger.debug(ps.toString());
                 return ps;
-
             }
         };
 
@@ -179,41 +213,111 @@ public class GroupJDBCDAO extends SimpleJdbcDaoSupport implements GroupDAO {
     }
 
     @Override
-    public Integer update(Group entity) {
-        throw new NotImplementedException();
+    public Integer update(final Group entity) {
+        JdbcTemplate jdbcTemplate = getJdbcTemplate();
+        PreparedStatementCreator psc = new PreparedStatementCreator() {
+
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                if (entity.getGroupID() == null)
+                    throw new SQLException("Cannot update group with null id.");
+
+                PreparedStatement ps = con.prepareStatement(UPDATE_GROUP_ACCOUNT);
+                ps.setInt(1, entity.getAccountID());
+                ps.setInt(2, entity.getParentID());
+                ps.setString(3, entity.getName() == null ? "" : entity.getName().trim());
+                ps.setString(4, entity.getDescription());
+                if (entity.getStatus() == null || entity.getStatus().getCode()==null) {
+                    ps.setNull(5, Types.NULL);
+                } else {
+                    ps.setInt(5, entity.getStatus().getCode());
+                }
+
+                ps.setString(6, entity.getPath());
+
+                if (entity.getDotOfficeType() == null || entity.getDotOfficeType().getCode()==null) {
+                    ps.setNull(7, Types.NULL);
+                } else {
+                    ps.setInt(7, entity.getDotOfficeType().getCode());
+                }
+
+                if (entity.getType() == null || entity.getType().getCode()==null) {
+                    ps.setNull(8, Types.NULL);
+                } else {
+                    ps.setInt(8, entity.getType().getCode());
+                }
+
+                if (entity.getManagerID() == null) {
+                    ps.setNull(9, Types.NULL);
+                } else {
+                    ps.setInt(9, entity.getManagerID());
+                }
+                ps.setInt(10, entity.getMapZoom());
+                ps.setDouble(11, entity.getMapLat());
+                ps.setDouble(12, entity.getMapLng());
+
+                if (entity.getZoneRev() == null) {
+                    ps.setInt(13, Types.NULL);
+                } else {
+                    ps.setInt(13, entity.getZoneRev());
+                }
+
+                if (entity.getAggDate() == null) {
+                    ps.setNull(14, Types.NULL);
+                } else {
+                    ps.setDate(14, new java.sql.Date(dateFormatter.parseDateTime(entity.getAggDate()).getMillis()));
+                }
+
+                if (entity.getAddress() == null) {
+                    ps.setNull(15, Types.NULL);
+                } else {
+                    ps.setInt(15, entity.getAddress().getAddrID());
+                }
+
+                ps.setInt(16, entity.getGroupID());
+
+                logger.debug(ps.toString());
+                return ps;
+            }
+        };
+
+        jdbcTemplate.update(psc);
+        return entity.getGroupID();
     }
 
     @Override
     public Integer deleteByID(Integer groupID) {
-        return getJdbcTemplate().update(DEL_GROUP_BY_ID, new Object[] { groupID });
+        return getJdbcTemplate().update(DEL_GROUP_BY_ID, new Object[]{groupID});
     }
 
-    public void createTestDevice(int testAccountId, int testGroupId) {
+    public void createTestGroup(int testAccountId, int testGroupId) {
+        deleteByID(testGroupId);
         Map<String, String> params = new HashMap<String, String>();
         params.put("groupID", String.valueOf(testGroupId));
         params.put("acctID", String.valueOf(testAccountId));
-        getSimpleJdbcTemplate().update("insert into groups (groupID, acctID, name, `desc`, parentID, status, groupPath, addrID, mapZoom, zoneRev, mapLat, mapLng) values (:groupID, :acctID, 'test-name', 'test-desc', 4, 1, '/0/4/5/', 972, 17, 2, '28.065', '-82.3664')", params);
+        getSimpleJdbcTemplate().update("insert into groups (groupID, acctID, name, `desc`, parentID, status, groupPath, addrID, mapZoom, zoneRev, mapLat, mapLng) values (:groupID, :acctID, 'test-group-name', 'test-group-desc', 4, 1, '/0/4/5/', 972, 17, 2, '28.065', '-82.3664')", params);
     }
 
-    public void deleteTestDevice(int testGroupId) {
-        Map<String, String> params = new HashMap<String, String>();
-        params.put("groupID", String.valueOf(testGroupId));
-        getSimpleJdbcTemplate().update("delete from groups where groupID = :groupID", params);
+    public void deleteTestGroup(int testGroupId) {
+        deleteByID(testGroupId);
     }
 
-    private void addChildren(List<Group> allGroups , List<Group> groupHierarchy, Integer parentID)
-    {
-        for (Group group : allGroups)
-        {
-            if (group.getParentID().equals(parentID))
-            {
+    private void addChildren(List<Group> allGroups, List<Group> groupHierarchy, Integer parentID) {
+        for (Group group : allGroups) {
+            if (group.getParentID().equals(parentID)) {
                 groupHierarchy.add(group);
                 addChildren(allGroups, groupHierarchy, group.getGroupID());
             }
         }
     }
 
-    public Integer createAddress(final Address address) {
+    public Address createAddressIfNeeded(Integer accountID, Address address) {
+        if (address != null && address.getAddrID() != null)
+            return address;
+
+        final Address newAddress = new Address();
+        newAddress.setAccountID(accountID);
+
         JdbcTemplate jdbcTemplate = getJdbcTemplate();
         KeyHolder keyHolder = new GeneratedKeyHolder();
         PreparedStatementCreator psc = new PreparedStatementCreator() {
@@ -223,41 +327,41 @@ public class GroupJDBCDAO extends SimpleJdbcDaoSupport implements GroupDAO {
                 PreparedStatement ps = con.prepareStatement(ADD_NEW_ADDRESS, Statement.RETURN_GENERATED_KEYS);
 
 //                acctID, addr1, addr2, city, stateID, zip
-                ps.setInt(1, address.getAccountID());
-                if(address.getAddr1()==null){
+                ps.setInt(1, newAddress.getAccountID());
+                if (newAddress.getAddr1() == null) {
                     ps.setNull(2, Types.NULL);
-                }else{
-                    ps.setString(2, address.getAddr1());
+                } else {
+                    ps.setString(2, newAddress.getAddr1());
                 }
-                if(address.getAddr2()==null){
+                if (newAddress.getAddr2() == null) {
                     ps.setNull(3, Types.NULL);
-                }else{
-                    ps.setString(3, address.getAddr2());
+                } else {
+                    ps.setString(3, newAddress.getAddr2());
                 }
-                if(address.getCity()==null){
+                if (newAddress.getCity() == null) {
                     ps.setNull(4, Types.NULL);
-                }else{
-                    ps.setString(4, address.getCity());
+                } else {
+                    ps.setString(4, newAddress.getCity());
                 }
 
-                if(address.getState()==null){
+                if (newAddress.getState() == null) {
                     ps.setNull(5, Types.NULL);
-                }else{
-                    ps.setInt(5, address.getState().getStateID());
+                } else {
+                    ps.setInt(5, newAddress.getState().getStateID());
                 }
 
-                if(address.getZip()==null){
+                if (newAddress.getZip() == null) {
                     ps.setNull(6, Types.NULL);
-                }else{
-                    ps.setString(6, address.getZip());
+                } else {
+                    ps.setString(6, newAddress.getZip());
                 }
 
                 return ps;
             }
         };
         jdbcTemplate.update(psc, keyHolder);
-        return keyHolder.getKey().intValue();
+        newAddress.setAddrID(keyHolder.getKey().intValue());
+        return newAddress;
     }
-
 }
 
--- dao/src/test/java/com/inthinc/pro/dao/jdbc/GroupJDBCDAOTest.java
+++ dao/src/test/java/com/inthinc/pro/dao/jdbc/GroupJDBCDAOTest.java
@@ -24,38 +24,71 @@ public class GroupJDBCDAOTest extends SimpleJdbcDaoSupport {
     public static void setupOnce() {
         groupJDBCDAO = new GroupJDBCDAO();
         groupJDBCDAO.setDataSource(new ITDataSource().getRealDataSource());
-        groupJDBCDAO.createTestDevice(ACCOUNT_ID, GROUP_ID);
+        groupJDBCDAO.createTestGroup(ACCOUNT_ID, GROUP_ID);
     }
 
     @AfterClass
     public static void tearDownOnce() {
         try {
-            groupJDBCDAO.deleteTestDevice(GROUP_ID);
+            groupJDBCDAO.deleteTestGroup(GROUP_ID);
         } catch (Throwable t) {/*ignore*/}
     }
 
     @Test
-         public void getGroupHierarchyTest() throws Exception
-    {
+    public void getGroupHierarchyTest() throws Exception {
         List<Group> groupList = groupJDBCDAO.getGroupHierarchy(ACCOUNT_ID, GROUP_ID);
         assertTrue(groupList.size() > 0);
+        boolean found = false;
+        for (Group gr: groupList){
+            if (gr.getGroupID().equals(GROUP_ID)){
+                found = true;
+                break;
+            }
+        }
+        assertTrue(found);
     }
 
     @Test
-    public void getGroupsByAcctIDTest() throws Exception
-    {
+    public void getGroupsByAcctIDTest() throws Exception {
         List<Group> groupListByAccount = groupJDBCDAO.getGroupsByAcctID(ACCOUNT_ID);
         assertTrue(groupListByAccount.size() > 0);
+        boolean found = false;
+        for (Group gr: groupListByAccount){
+            if (gr.getGroupID().equals(GROUP_ID)){
+                found = true;
+                break;
+            }
+        }
+        assertTrue(found);
+
+        boolean onlyMyAccountId = true;
+        for (Group gr: groupListByAccount){
+            if (!gr.getAccountID().equals(ACCOUNT_ID)){
+                onlyMyAccountId = false;
+                break;
+            }
+        }
+        assertTrue(onlyMyAccountId);
+
     }
 
     @Test
-    public void findByIDTest() throws Exception
-    {
+    public void findByIDTest() throws Exception {
         Group groupFindByID = groupJDBCDAO.findByID(GROUP_ID);
         assertNotNull(groupFindByID);
+        assertTrue(groupFindByID.getGroupID().equals(GROUP_ID));
     }
 
-
+    @Test
+    public void updateTest() throws Exception {
+        Group groupFindByID = groupJDBCDAO.findByID(GROUP_ID);
+        assertNotNull(groupFindByID);
+        groupFindByID.setName("abc123");
+        groupJDBCDAO.update(groupFindByID);
+        Group groupFindByID2 = groupJDBCDAO.findByID(GROUP_ID);
+        assertNotNull(groupFindByID2);
+        assertEquals(groupFindByID2.getName(), "abc123");
+    }
 }
 
 
