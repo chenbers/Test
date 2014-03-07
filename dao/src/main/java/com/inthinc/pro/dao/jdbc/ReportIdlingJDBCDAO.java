7dd008bf84ebef548dce1e51d0a959c271a06634
vali-enache <vali.enache@infrasoft.ro>
2014-03-07 11:43:18 +0200

    Add on ReportIdlingJDBCDAOTest 2 methods(insert,delete )

--- dao/src/main/java/com/inthinc/pro/dao/jdbc/ReportIdlingJDBCDAO.java
+++ dao/src/main/java/com/inthinc/pro/dao/jdbc/ReportIdlingJDBCDAO.java
@@ -3,7 +3,6 @@ package com.inthinc.pro.dao.jdbc;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,9 +20,8 @@ public class ReportIdlingJDBCDAO extends SimpleJdbcDaoSupport implements ReportI
     private static final String IDLING_REPORT_DATA = "SELECT di.driverID as driverID, di.driverName as driverName, di.groupID as groupID, di.groupName as groupName, sum(agg.driveTime) as driveTime,"
             + "sum(agg.idleLo) as lowIdleTime, sum(agg.idleHi) as highIdleTime, (BIT_OR(agg.emuFeatureMask) & 4 != 0) as hasRPM, driver.status " + "FROM driverInfo di "
             + "LEFT JOIN driver ON (driver.driverID = di.driverID) "
-            + "LEFT JOIN agg on agg.driverID=di.driverID WHERE di.groupID in (select groupID from groups where groupPath like :groupID)" +
-            " AND agg.aggDate between :intervalStart AND :intervalEnd " +
-            " GROUP BY di.driverID;";
+            + "LEFT JOIN agg on agg.driverID=di.driverID WHERE di.groupID in (select groupID from groups where groupPath like :groupID) "
+            + "AND agg.aggDate between :intervalStart AND :intervalEnd GROUP BY di.driverID;";
 
     @Override
     public List<IdlingReportItem> getIdlingReportData(Integer groupID, Interval interval) {
@@ -57,31 +55,5 @@ public class ReportIdlingJDBCDAO extends SimpleJdbcDaoSupport implements ReportI
             return idlingReportItem;
         }
     };
-
-    public void createTest(int groupId, Date date) {
-        Map<String, String> params = new HashMap<String, String>();
-        params.put("groupId", String.valueOf(groupId));
-        params.put("groupPath", "/" + String.valueOf(groupId) + "/");
-        getSimpleJdbcTemplate().update("insert into agg (aggID, driverID, vehicleID,deviceID,odometer1,odometer2,odometer3,odometer4,odometer5,odometer6,coachingEvents,  " +
-                "mpgOdometer1,mpgOdometer2,mpgOdometer3,idleHi,idleLo,driveTime,trips,seatbeltCoaching,speedCoaching1,speedCoaching2,speedCoaching3,speedCoaching4,speedCoaching5, " +
-                "seatbeltEvents,seatbeltClicks,speedEvents1,speedEvents2,speedEvents3,speedEvents4,speedEvents5,speedEvents1To7MphOver, speedEvents8To14MphOver, speedEvents15PlusMphOver, speedEventsOver80Mph," +
-                "aggressiveBrakeEvents,aggressiveAccelEvents,aggressiveLeftEvents,aggressiveRightEvents, aggressiveBumpEvents,speedOdometer1, speedOdometer2, speedOdometer3,speedOdometer4, speedOdometer5," +
-                "idleLoEvents, idleHiEvents,rpmEvents,crashEvents, emuFeatureMask, emuRpmDriveTime, driverStartingOdometer, driverEndingOdometer, driverCrashOdometer, driverCrashDays, driverCrashTotal," +
-                " vehicleStartingOdometer, vehicleEndingOdometer, vehicleCrashOdometer,vehicleCrashTotal, backingEvents,backingTime,aggDate,modified )" +
-                " values ( 8888888, 1111111 ,4069,0,0,0,0,0,1,1,0, 3,0,0,0,0,4,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,800,1,2,3,0,0,0,0,0,0,'2013-09-13','2014-09-13');", params);
-
-
-        getSimpleJdbcTemplate().update("insert into driverInfo (groupId, driverID, driverName) values (:groupId, 1111111, 'test-name');", params);
-        getSimpleJdbcTemplate().update("insert into groups (`desc`,groupId, acctID, name, parentID,status,groupPath) values ( 'test-desc',:groupId, '1', 'test-name', 4, 4,:groupPath);", params);
-    }
-
-    public void deleteTest(int groupId) {
-        Map<String, String> params = new HashMap<String, String>();
-        params.put("groupId", String.valueOf(groupId));
-        getSimpleJdbcTemplate().update("delete from driverInfo where groupId = :groupId;", params);
-        getSimpleJdbcTemplate().update("delete from groups where groupId = :groupId;", params);
-        getSimpleJdbcTemplate().update("delete from agg where aggID = 8888888", params);
-
-    }
 
 }
--- dao/src/test/java/com/inthinc/pro/dao/jdbc/ReportIdlingJDBCDAOTest.java
+++ dao/src/test/java/com/inthinc/pro/dao/jdbc/ReportIdlingJDBCDAOTest.java
@@ -3,16 +3,17 @@ package com.inthinc.pro.dao.jdbc;
 import com.inthinc.pro.model.IdlingReportItem;
 import it.config.ITDataSource;
 import it.config.IntegrationConfig;
-import it.config.ReportTestConst;
-import org.joda.time.DateMidnight;
+import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 import org.joda.time.Interval;
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
 
-import java.util.Date;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
@@ -20,11 +21,17 @@ import static junit.framework.Assert.assertEquals;
 /**
  * Created by vali_enache on 2/21/14.
  */
-public class ReportIdlingJDBCDAOTest {
+public class ReportIdlingJDBCDAOTest extends SimpleJdbcDaoSupport {
+    private static final Logger logger = Logger.getLogger(ReportIdlingJDBCDAOTest.class);
     static ReportIdlingJDBCDAO reportIdlingJDBCDAO;
     static int TEST_GROUP_ID = 999999999;
     static DateTime SEP_12_2013 = new DateTime(1378944000000l);// 1375401600000l);
     static Interval interval = new Interval(SEP_12_2013, SEP_12_2013.plusDays(2));
+    static int TEST_DRIVER_ID = 88888888;
+    static String TEST_DRIVER_NAME = "7777777";
+    static int TEST_AGG_ID = 666666666;
+    static int TEST_VEHICLE_ID = 555555;
+    static int TEST_DEVICE_ID = 444444;
 
 
     @BeforeClass
@@ -32,16 +39,6 @@ public class ReportIdlingJDBCDAOTest {
         IntegrationConfig config = new IntegrationConfig();
         reportIdlingJDBCDAO = new ReportIdlingJDBCDAO();
         reportIdlingJDBCDAO.setDataSource(new ITDataSource().getRealDataSource());
-        //create
-        reportIdlingJDBCDAO.createTest(TEST_GROUP_ID, new org.joda.time.DateTime().minusHours(1).toDate());
-    }
-
-    @AfterClass
-    public static void tearDownOnce() {
-        try {
-            //delete
-            reportIdlingJDBCDAO.deleteTest(TEST_GROUP_ID);
-        } catch (Throwable t) {/*ignore*/}
     }
 
     /*
@@ -49,8 +46,143 @@ public class ReportIdlingJDBCDAOTest {
     * */
     @Test
     public void testGetIdlingReportData() {
+        // createTest
+        insert(TEST_GROUP_ID, TEST_DRIVER_ID, TEST_DRIVER_NAME, TEST_AGG_ID, TEST_VEHICLE_ID, TEST_DEVICE_ID);
+
         List<IdlingReportItem> reportList = reportIdlingJDBCDAO.getIdlingReportData(TEST_GROUP_ID, interval);
         assertEquals(reportList.size(), 1);
+
+        // deleteTest
+        try {
+            deleteTest(TEST_GROUP_ID, TEST_AGG_ID);
+        } catch (Exception e) {
+        }
+
     }
 
+    private static final String INSERT_GROUP = "INSERT IGNORE INTO groups (`desc`,groupId, acctID, name, parentID,status,groupPath) VALUES('test-desc',?,1,'test-name',4,4,'/999999999/')";
+    private static final String INSERT_DRIVERINFO = "INSERT IGNORE INTO driverInfo (groupId, driverID, driverName) VALUES(?,?,?)";
+    private static final String INSERT_AGG = "INSERT IGNORE INTO agg (aggID, driverID, vehicleID,deviceID,odometer1,odometer2,odometer3,odometer4,odometer5,odometer6,coachingEvents,  " +
+            "mpgOdometer1,mpgOdometer2,mpgOdometer3,idleHi,idleLo,driveTime,trips,seatbeltCoaching,speedCoaching1,speedCoaching2,speedCoaching3,speedCoaching4,speedCoaching5, " +
+            "seatbeltEvents,seatbeltClicks,speedEvents1,speedEvents2,speedEvents3,speedEvents4,speedEvents5,speedEvents1To7MphOver, speedEvents8To14MphOver, speedEvents15PlusMphOver, speedEventsOver80Mph," +
+            "aggressiveBrakeEvents,aggressiveAccelEvents,aggressiveLeftEvents,aggressiveRightEvents, aggressiveBumpEvents,speedOdometer1, speedOdometer2, speedOdometer3,speedOdometer4, speedOdometer5," +
+            "idleLoEvents, idleHiEvents,rpmEvents,crashEvents, emuFeatureMask, emuRpmDriveTime, driverStartingOdometer, driverEndingOdometer, driverCrashOdometer, driverCrashDays, driverCrashTotal," +
+            " vehicleStartingOdometer, vehicleEndingOdometer, vehicleCrashOdometer,vehicleCrashTotal, backingEvents,backingTime,aggDate,modified)" +
+            " VALUES( ?, ? ,?,?,0,0,0,0,1,1,0, 3,0,0,0,0,4,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,800,1,2,3,0,0,0,0,0,0,'2013-09-13','2014-09-13')";
+
+    public static void insert(Integer groupId, Integer driverId, String driverName, Integer aggID, Integer vehicleID, Integer deviceID) {
+
+        Connection conn = null;
+        PreparedStatement statement_group = null;
+        PreparedStatement statement_driverInfo = null;
+        PreparedStatement statement_agg = null;
+
+        ResultSet resultSet = null;
+
+        if (groupId != 0) {
+            try {
+                conn = new ITDataSource().getRealDataSource().getConnection();
+                ;
+
+                //insert into groups
+                statement_group = (PreparedStatement) conn.prepareStatement(INSERT_GROUP);
+                statement_group.setLong(1, groupId);
+                statement_group.executeUpdate();
+
+                //insert into driverInfo
+                statement_driverInfo = (PreparedStatement) conn.prepareStatement(INSERT_DRIVERINFO);
+                statement_driverInfo.setLong(1, groupId);
+                statement_driverInfo.setLong(2, driverId);
+                statement_driverInfo.setString(3, driverName);
+                statement_driverInfo.executeUpdate();
+
+                //insert into agg aggID, driverID, vehicleID,deviceID,
+                statement_agg = (PreparedStatement) conn.prepareStatement(INSERT_AGG);
+                statement_agg.setLong(1, aggID);
+                statement_agg.setLong(2, driverId);
+                statement_agg.setLong(3, vehicleID);
+                statement_agg.setLong(4, deviceID);
+                statement_agg.executeUpdate();
+
+            } catch (Throwable e) { // handle database alerts in the usual manner
+                logger.error("Exception: " + e);
+                e.printStackTrace();
+
+            }   // end catch
+            finally { // clean up and release the connection
+                //              socket.close();
+                try {
+                    if (conn != null)
+                        conn.close();
+                    if (statement_group != null)
+                        statement_group.close();
+                    if (statement_driverInfo != null)
+                        statement_driverInfo.close();
+                    if (statement_driverInfo != null)
+                        statement_driverInfo.close();
+                    if (resultSet != null)
+                        resultSet.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+    }
+
+    private static final String CLEAR_GROUP = "delete from groups where groupId =  ?";
+    private static final String CLEAR_DRIVERINFO = "delete from driverInfo where groupId =  ?";
+    private static final String CLEAR_AGG = "delete from agg where aggId =  ?";
+
+    private static void deleteTest(Integer groupId, Integer aggId) throws SQLException {
+        Connection conn = null;
+        PreparedStatement statement_group = null;
+        PreparedStatement statement_driver_info = null;
+        PreparedStatement statement_agg = null;
+
+        ResultSet resultSet = null;
+
+        int delGroupId = (int) (groupId % 32);
+        String groupSqlStatement = String.format(CLEAR_GROUP, delGroupId);
+        int delAggId = (int) (aggId % 32);
+        String aggSqlStatement = String.format(CLEAR_AGG, delAggId);
+        String driverInfoSqlStatement = String.format(CLEAR_DRIVERINFO, delGroupId);
+
+        try {
+            conn = new ITDataSource().getRealDataSource().getConnection();
+
+            // delete from groups
+            statement_group = conn.prepareStatement(groupSqlStatement);
+            statement_group.setInt(1, groupId);
+            logger.debug(statement_group.toString());
+            statement_group.executeUpdate();
+
+            // delete from driverInfo
+            statement_driver_info = conn.prepareStatement(driverInfoSqlStatement);
+            statement_driver_info.setInt(1, groupId);
+            logger.debug(statement_driver_info.toString());
+            statement_driver_info.executeUpdate();
+
+            // delete from agg
+            statement_agg = conn.prepareStatement(aggSqlStatement);
+            statement_agg.setInt(1, aggId);
+            logger.debug(statement_agg.toString());
+            statement_agg.executeUpdate();
+
+        }   // end try
+        catch (SQLException e) { // handle database hosLogs in the usual manner
+
+            throw e;
+        }   // end catch
+        finally { // clean up and release the connection
+            if (conn != null)
+                conn.close();
+            if (statement_group != null)
+                statement_group.close();
+            if (statement_driver_info != null)
+                statement_driver_info.close();
+            if (statement_agg != null)
+                statement_agg.close();
+            if (resultSet != null)
+                resultSet.close();
+        }
+    }
 }
