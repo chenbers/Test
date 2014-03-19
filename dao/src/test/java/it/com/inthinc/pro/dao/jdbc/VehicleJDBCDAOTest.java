7847700875aa4d1bed004c82f3f3923cd53b9972
Statia 1 <statia1@infrasoft.ro>
2014-03-19 15:41:49 +0200

    work in progress VehicleJDBCDAO

--- dao/src/test/java/it/com/inthinc/pro/dao/jdbc/VehicleJDBCDAOTest.java
+++ dao/src/test/java/it/com/inthinc/pro/dao/jdbc/VehicleJDBCDAOTest.java
@@ -1,6 +1,7 @@
 package it.com.inthinc.pro.dao.jdbc;
 
 import com.inthinc.pro.dao.LocationDAO;
+import com.inthinc.pro.dao.cassandra.LocationCassandraDAO;
 import com.inthinc.pro.dao.hessian.proserver.SiloService;
 import com.inthinc.pro.dao.hessian.proserver.SiloServiceCreator;
 import com.inthinc.pro.dao.jdbc.VehicleJDBCDAO;
@@ -9,7 +10,9 @@ import it.com.inthinc.pro.dao.model.GroupData;
 import it.com.inthinc.pro.dao.model.ITData;
 import it.config.ITDataSource;
 import it.config.IntegrationConfig;
+import org.apache.commons.lang.math.RandomUtils;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.joda.time.Interval;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
@@ -22,15 +25,17 @@ import javax.sql.DataSource;
 import javax.swing.text.DateFormatter;
 import java.io.InputStream;
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class VehicleJDBCDAOTest extends SimpleJdbcDaoSupport {
 
-    private LocationDAO locationDAO;
+    private LocationCassandraDAO locationDAO;
     private Interval interval;
 
     public Interval getInterval() {
@@ -41,49 +46,38 @@ public class VehicleJDBCDAOTest extends SimpleJdbcDaoSupport {
         this.interval = interval;
     }
 
-    public LocationDAO getLocationDAO() {
-        return locationDAO;
-    }
-
-    public void setLocationDAO(LocationDAO locationDAO) {
-        this.locationDAO = locationDAO;
-    }
-
     private static SiloService siloService;
-    private static final String ALERT_TEST_XML = "AlertTest.xml";
+    private static final String XML_DATA_FILE = "ReportTest.xml";
     private static ITData itData = new ITData();
+    private static int randomInt = RandomUtils.nextInt(99999);
 
-//    static int VEHICLE_ID = 483;
-//    static int DRIVER_ID = 492;
-//    static int GROUP_ID = 101;
-//    static String VIN = "JTDJT923785171463";
-//
-//    String str = "2014-01-22";
-//
-//    Date startDate = new Date(2014-01-22);
-//    Date endDate = new Date (2014-01-22);
+    private Date toUTC(Date date){
+        DateTime dt = new DateTime(date.getTime()).toDateTime(DateTimeZone.UTC);
+        return dt.toDate();
+    }
 
-//    @BeforeClass
-//    public static void setupOnce() {
-//        vehicleJDBCDAO = new VehicleJDBCDAO();
-//        vehicleJDBCDAO.setDataSource(new ITDataSource().getRealDataSource());
-//    }
 @Before
 public void setUpBeforeTest() throws Exception {
     IntegrationConfig config = new IntegrationConfig();
     String host = config.get(IntegrationConfig.SILO_HOST).toString();
     Integer port = Integer.valueOf(config.get(IntegrationConfig.SILO_PORT).toString());
+
     siloService = new SiloServiceCreator(host, port).getService();
 
+    initApp();
     itData = new ITData();
 
-    InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(ALERT_TEST_XML);
 
-    if (itData.parseTestData(stream, siloService, false, false)) {
+    InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(XML_DATA_FILE);
+
+    if (!itData.parseTestData(stream, siloService, false, false)) {
         throw new Exception("Error parsing Test data xml file");
     }
 
 }
+
+    private static void initApp() throws Exception {
+    }
 
 
     @Test
@@ -118,24 +112,20 @@ public void setUpBeforeTest() throws Exception {
         }
     }
 
-//    @Test
-//    public void getMilesDrivenTest() throws Exception {
-//        VehicleJDBCDAO vehicleDAO = new VehicleJDBCDAO();
-//        DataSource dataSource = new ITDataSource().getRealDataSource();
-//        vehicleDAO.setDataSource(dataSource);
-//
-//        for (int teamIdx = ITData.GOOD; teamIdx <= ITData.BAD; teamIdx++) {
-//            GroupData team = itData.teamGroupData.get(teamIdx);
-//            team.vehicle.getVehicleID();
-//            Integer vehicle = vehicleDAO.getMilesDriven(team.vehicle.getVehicleID());
-//
-//        }
-//
-////              assertTrue(!vehicle.equals("0"));
-//
-//
-//    }
+    @Test
+    public void getMilesDrivenTest() throws Exception {
+        VehicleJDBCDAO vehicleDAO = new VehicleJDBCDAO();
+        DataSource dataSource = new ITDataSource().getRealDataSource();
+        vehicleDAO.setDataSource(dataSource);
 
+        for (int teamIdx = ITData.GOOD; teamIdx <= ITData.BAD; teamIdx++) {
+            GroupData team = itData.teamGroupData.get(teamIdx);
+            team.vehicle.getVehicleID();
+            Integer vehicle = vehicleDAO.getMilesDriven(team.vehicle.getVehicleID());
+
+            assertTrue(!vehicle.equals("0"));
+        }
+    }
 
 
     @Test
@@ -154,32 +144,58 @@ public void setUpBeforeTest() throws Exception {
         }
 
     }
-//
-//    @Test
-//    public void findByIDTest() throws Exception {
-//        Vehicle findByID = vehicleJDBCDAO.findByID(VEHICLE_ID);
-//
-//        assertNotNull(findByID);
-//
-//    }
-//
-//    @Test
-//    public void findByDriverInGroupTest() throws Exception {
-//        Vehicle findByDriverInGroup = vehicleJDBCDAO.findByDriverInGroup(DRIVER_ID, GROUP_ID);
-//
-//        assertNotNull(findByDriverInGroup);
-//    }
-//
-//    @Test
-//    public void findByVINTest() throws Exception {
-//        Vehicle findByVIN = vehicleJDBCDAO.findByVIN(VIN);
-//        assertNotNull(findByVIN);
-//    }
-//        @Test
-//    public void findByDriverID() throws Exception {
-//        Vehicle findByDriverID = vehicleJDBCDAO.findByDriverID(DRIVER_ID);
-//        assertNotNull(findByDriverID);
-//    }
+
+
+    @Test
+    public void findByDriverInGroupTest() throws Exception {
+        VehicleJDBCDAO vehicleDAO = new VehicleJDBCDAO();
+        DataSource dataSource = new ITDataSource().getRealDataSource();
+        vehicleDAO.setDataSource(dataSource);
+        for (GroupData testGroupData : itData.teamGroupData) {
+            Driver driverID = testGroupData.driver;
+            Group groupID = testGroupData.group;
+
+            Vehicle findByDriverInGroup = vehicleDAO.findByDriverInGroup(driverID.getDriverID() ,groupID.getGroupID());
+
+            assertTrue(!findByDriverInGroup.equals("0"));
+
+        }
+    }
+
+    @Test
+    public void findByVINTest() throws Exception {
+        VehicleJDBCDAO vehicleDAO = new VehicleJDBCDAO();
+        DataSource dataSource = new ITDataSource().getRealDataSource();
+        vehicleDAO.setDataSource(dataSource);
+        for (GroupData testGroupData : itData.teamGroupData) {
+            Vehicle vehicle = testGroupData.vehicle;
+
+            Vehicle findByVIN = vehicleDAO.findByVIN(vehicle.getVIN());
+
+            assertNotNull(findByVIN);
+
+            assertTrue(!findByVIN.equals("0"));
+        }
+
+
+    }
+
+    @Test
+    public void findByDriverID() throws Exception {
+        VehicleJDBCDAO vehicleDAO = new VehicleJDBCDAO();
+        DataSource dataSource = new ITDataSource().getRealDataSource();
+        vehicleDAO.setDataSource(dataSource);
+        for (GroupData testGroupData : itData.teamGroupData) {
+            Driver driver = testGroupData.driver;
+
+            Vehicle findByDriverID = vehicleDAO.findByDriverID(driver.getDriverID());
+
+            assertNotNull(findByDriverID);
+
+            assertTrue(!findByDriverID.equals("0"));
+        }
+
+    }
 
 //    @Test
 //    public void getVehiclesNearLocTest() throws Exception {
@@ -188,5 +204,70 @@ public void setUpBeforeTest() throws Exception {
 //        assertNotNull(getTripsList);
 //
 //    }
+
+//    @Test
+//    public void createTest_updateTest() {
+//
+//        //create  method  test
+//        boolean returnsVehicleID = false;
+//        VehicleJDBCDAO vehicleDAO = new VehicleJDBCDAO();
+//        DataSource dataSource = new ITDataSource().getRealDataSource();
+//        vehicleDAO.setDataSource(dataSource);
+//
+//        Vehicle vehicle = new Vehicle();
+//        vehicle.setGroupID(2);
+//        vehicle.setColor("Black");
+//        vehicle.setMake("Toyota");
+//        vehicle.setModel("Tacoma");
+//        vehicle.setName("IT" + randomInt);
+//
+//        State state = new State();
+//        state.setAbbrev("UT");
+//        state.setName("Utah");
+//        state.setStateID(45);
+//
+//        vehicle.setState(state);
+//        vehicle.setStatus(Status.INACTIVE);
+//        vehicle.setVIN("VIN" + randomInt);
+//        vehicle.setVtype(VehicleType.LIGHT);
+//        vehicle.setYear(2007);
+//
+//
+//        Integer vehicleID = vehicleDAO.create(vehicle.getVehicleID(), vehicle);
+//        returnsVehicleID = (vehicleID != null);
+//        assertTrue(returnsVehicleID);
+//
+//        //update  method   test
+//        Vehicle vehicleUpdate = new Vehicle();
+//        vehicleUpdate.setGroupID(2);
+//        vehicleUpdate.setColor("Red");
+//        vehicleUpdate.setMake("Toyota");
+//        vehicleUpdate.setModel("Celica");
+//        vehicleUpdate.setName("NAME" + randomInt);
+//
+//        State stateUpdate = new State();
+//        stateUpdate.setAbbrev("GA");
+//        stateUpdate.setName("Georgia");
+//        stateUpdate.setStateID(11);
+//
+//        vehicleUpdate.setState(stateUpdate);
+//        vehicleUpdate.setStatus(Status.ACTIVE);
+//        vehicleUpdate.setVIN("VIN" + randomInt);
+//        vehicleUpdate.setVtype(VehicleType.MEDIUM);
+//        vehicleUpdate.setYear(2008);
+//        vehicleUpdate.setVehicleID(vehicleID);
+//
+//        vehicleDAO.update(vehicleUpdate);
+//
+//         //find vehicle by ID  test
+//        vehicleDAO.findByID(vehicleID);
+//
+//        //delete vehicle  when finish
+//        vehicleDAO.deleteByID(vehicleID);
+//
+//    }
+
+
+
 
 }
