ef21338d7b2f1fba5746b8daca8d758e8158f649
SpyderWeiss <mweiss@inthinc.com>
2014-03-19 14:15:15 -0600

    Needed to increase the pause time after clicking the try again button, sometimes tests are failing because they can't find the try again button soon enough after clicking it and waiting for the page to reload.

--- automation/src/main/java/com/inthinc/pro/automation/selenium/CoreMethodLib.java
+++ automation/src/main/java/com/inthinc/pro/automation/selenium/CoreMethodLib.java
@@ -814,7 +814,7 @@ public class CoreMethodLib extends WebDriverBackedSelenium implements CoreMethod
             while(verifyIsTextOnPage("Unable to connect")) {
                     Log.warning("There may have been a page timeout during this test.");
                         click(tryAgainButton);
-                        AutomationThread.pause(10);
+                        AutomationThread.pause(30);
                         z++;
                         if (z == 15) {
                             Log.error("Unable to re-establish a connection, ending test.");
