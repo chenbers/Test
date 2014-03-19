21feef48a4e7dbeb77fe8a8e6997f10e6b16e913
vali-enache <vali.enache@infrasoft.ro>
2014-03-19 15:09:14 +0200

    Add DriverPerformanceBeanTestP1

--- web/src/test/java/com/inthinc/pro/backing/DriverPerformanceBeanTestP1.java
+++ web/src/test/java/com/inthinc/pro/backing/DriverPerformanceBeanTestP1.java
@@ -1,35 +1,40 @@
 package com.inthinc.pro.backing;
 
 
-import com.inthinc.pro.backing.ui.EventReportItem;
 import com.inthinc.pro.backing.ui.ScoreBox;
 import com.inthinc.pro.backing.ui.ScoreBoxSizes;
-import com.inthinc.pro.model.CrashSummary;
+import com.inthinc.pro.backing.ui.TripDisplay;
+import com.inthinc.pro.dao.hessian.ZoneHessianDAO;
+import com.inthinc.pro.dao.mock.proserver.SiloServiceCreator;
+import com.inthinc.pro.map.AddressLookup;
+import com.inthinc.pro.map.GoogleAddressLookup;
 import com.inthinc.pro.model.Driver;
-import com.inthinc.pro.model.MeasurementType;
+import com.inthinc.pro.model.LatLng;
 import com.inthinc.pro.model.Person;
 import com.inthinc.pro.model.ScoreType;
-import com.inthinc.pro.model.event.SpeedingEvent;
-import com.inthinc.pro.util.MessageUtil;
+import com.inthinc.pro.model.Trip;
+import com.inthinc.pro.model.Zone;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.TimeZone;
 
-public class DriverPerformanceBeanTest extends BaseBeanTest
+public class DriverPerformanceBeanTestP1 extends BaseBeanTest
 {
     private Map<String, Integer> scoreMap;
     private Map<String, String>  styleMap;
-    private CrashSummary crashSummary;
-    
+    final Date startDate = new org.joda.time.DateTime(2012, 11, 25, 13, 45, 27, 1).toDate();
+    final Date endDate = new org.joda.time.DateTime(2012, 11, 27, 13, 45, 27, 1).toDate();
+    private  List<LatLng> route ;
+    static TimeZone timeZone = TimeZone.getTimeZone("US/Mountain");
+
     @BeforeClass
     public static void setUpBeforeClass() throws Exception
     {
@@ -67,6 +72,30 @@ public class DriverPerformanceBeanTest extends BaseBeanTest
 
         d.setPerson(p);
         driverPerformanceBean.setDriver(d);
+        Locale locale = new Locale("en","US");
+
+        AddressLookup addressLookup  = new GoogleAddressLookup();
+        addressLookup.setLocale(locale) ;
+
+        ZoneHessianDAO zoneHessianDAO = new ZoneHessianDAO();
+        zoneHessianDAO.setSiloService(new SiloServiceCreator().getService());
+        List<Zone> zones = zoneHessianDAO.getZones(1);
+
+        route = new ArrayList<LatLng>();
+        route.add(new LatLng(32.96453094482422f, -117.12944793701172f ));
+
+        Trip trip = new Trip();
+        trip.setStartTime(startDate);
+        trip.setEndTime(endDate);
+        trip.setMileage(1);
+        trip.setRoute(route);
+
+        TripDisplay tripDisplay = new TripDisplay( trip,  timeZone,  addressLookup , zones,  locale);
+        driverPerformanceBean.setLastTrip(tripDisplay);
+        driverPerformanceBean.getProUser().setZones(zones);
+        driverPerformanceBean.setReportAddressLookupBean(addressLookup);
+        driverPerformanceBean.setDisabledGoogleMapsInReportsAddressLookupBean(addressLookup);
+        driverPerformanceBean.setLastTrip(tripDisplay);
         
         // Test Scores and Styles
         scoreMap = new HashMap<String, Integer>();
@@ -92,17 +121,17 @@ public class DriverPerformanceBeanTest extends BaseBeanTest
 
         driverPerformanceBean.setScoreMap(scoreMap);
         driverPerformanceBean.setStyleMap(styleMap);
-        Double crashesPerMillion ;
-        crashesPerMillion=123.3;
-        crashSummary.setCrashesPerMillionMiles(crashesPerMillion);
+
         Integer score = driverPerformanceBean.getScoreMap().get( ScoreType.SCORE_SPEEDING_21_30.toString());
-//        assertEquals( score.toString(), "8");
-//        assertEquals( driverPerformanceBean.getStyleMap().get( ScoreType.SCORE_SPEEDING_21_30.toString() ) , "score_med_1" );
+        assertEquals( score.toString(), "8");
+        assertEquals( driverPerformanceBean.getStyleMap().get( ScoreType.SCORE_SPEEDING_21_30.toString() ) , "score_med_1" );
         
         score = driverPerformanceBean.getScoreMap().get( ScoreType.SCORE_SPEEDING_65_80.toString());
-//        assertEquals( score.toString(), "48");
-//        assertEquals( driverPerformanceBean.getStyleMap().get( ScoreType.SCORE_SPEEDING_65_80.toString() ) , "score_med_5" );
+        assertEquals( score.toString(), "48");
+        assertEquals( driverPerformanceBean.getStyleMap().get( ScoreType.SCORE_SPEEDING_65_80.toString() ) , "score_med_5" );
 
+        //test buildReportCriteria
+        driverPerformanceBean.getCrashSummary();
         assertNotNull(driverPerformanceBean.buildReportCriteria());
     }
 }
