0fa02a4afecf4acd55bc734e210725d6dc1cf184
vali-enache <vali.enache@infrasoft.ro>
2014-03-24 15:53:40 +0200

    implement findById

--- dao/src/main/java/com/inthinc/pro/dao/jdbc/AccountJDBCDAO.java
+++ dao/src/main/java/com/inthinc/pro/dao/jdbc/AccountJDBCDAO.java
@@ -1,18 +1,25 @@
 package com.inthinc.pro.dao.jdbc;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.lang.NotImplementedException;
-import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
-
 import com.inthinc.pro.dao.AccountDAO;
 import com.inthinc.pro.model.Account;
+import com.inthinc.pro.model.Status;
+import org.apache.commons.lang.NotImplementedException;
+import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AccountJDBCDAO extends SimpleJdbcDaoSupport implements AccountDAO {
 
     private static final String ALL_VALID_ACCOUNTIDS = "select acctID from account where status=1";
+    private static final String FIND_ALL_ACCOUNT_IDS = "SELECT acctID FROM  account ";
+
+    private static final String FIND_BY_ID = "select * from account ac where ac.acctID= :acctID" ;
 
     @Override
     public List<Long> getAllValidAcctIDs() {
@@ -52,12 +59,19 @@ public class AccountJDBCDAO extends SimpleJdbcDaoSupport implements AccountDAO {
 
     @Override
     public List<Account> getAllAcctIDs() {
-        throw new NotImplementedException();
+        Map<String, Object> args = new HashMap<String, Object>();
+        List<Account> accountList = (List<Account>) getSimpleJdbcTemplate().queryForObject(FIND_ALL_ACCOUNT_IDS, accountListParameterizedRowMapper,args );
+         return accountList;
     }
 
     @Override
-    public Account findByID(Integer id) {
-        throw new NotImplementedException();
+    public Account findByID(Integer acctId) {
+        Map<String, Object> args = new HashMap<String, Object>();
+        args.put("acctID", acctId);
+        StringBuilder findAccount = new StringBuilder(FIND_BY_ID);
+        Account account = getSimpleJdbcTemplate().queryForObject(findAccount.toString(), accountParameterizedRowMapper, args);
+        return account;
+//        throw new NotImplementedException();
     }
 
     @Override
@@ -74,4 +88,28 @@ public class AccountJDBCDAO extends SimpleJdbcDaoSupport implements AccountDAO {
     public Integer deleteByID(Integer id) {
         throw new NotImplementedException();
     }
+    private ParameterizedRowMapper<Account> accountParameterizedRowMapper = new ParameterizedRowMapper<Account>() {
+        @Override
+        public Account mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Account account = new Account();
+            account.setAccountID(rs.getInt("ac.acctID"));
+            account.setStatus(Status.valueOf(rs.getInt("ac.status")));
+            account.setAcctName(rs.getString("ac.name"));
+            account.setUnkDriverID(rs.getInt("ac.unkDriverID"));
+            account.setBillID(rs.getInt("ac.billID"));
+            return account;
+        }
+    };
+    private ParameterizedRowMapper<Account> accountListParameterizedRowMapper = new ParameterizedRowMapper<Account>() {
+        @Override
+        public Account mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Account accountList =new Account();
+            accountList.setAccountID(rs.getInt("ac.acctID"));
+            accountList.setStatus(Status.valueOf(rs.getInt("ac.status")));
+            accountList.setAcctName(rs.getString("ac.name"));
+            accountList.setUnkDriverID(rs.getInt("ac.unkDriverID"));
+            accountList.setBillID(rs.getInt("ac.billID"));
+            return accountList;
+        }
+    };
 }
