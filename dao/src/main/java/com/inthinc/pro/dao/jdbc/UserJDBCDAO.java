8ca77c07ea031353371a6bbdf84f8ea6bcb1419e
zimbran.radu <radu.zimbran@infrasoft.ro>
2014-03-26 14:28:08 +0200

    US7907 working

--- dao/src/main/java/com/inthinc/pro/dao/jdbc/UserJDBCDAO.java
+++ dao/src/main/java/com/inthinc/pro/dao/jdbc/UserJDBCDAO.java
@@ -1,18 +1,34 @@
 package com.inthinc.pro.dao.jdbc;
 
 import com.inthinc.pro.dao.UserDAO;
+import com.inthinc.pro.dao.hessian.exceptions.EmptyResultSetException;
 import com.inthinc.pro.model.FuelEfficiencyType;
 import com.inthinc.pro.model.Gender;
+import com.inthinc.pro.model.GoogleMapType;
 import com.inthinc.pro.model.Group;
 import com.inthinc.pro.model.MeasurementType;
 import com.inthinc.pro.model.Person;
 import com.inthinc.pro.model.Status;
 import com.inthinc.pro.model.User;
+import com.inthinc.pro.model.security.AccessPoint;
+import com.mysql.jdbc.Statement;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -21,17 +37,26 @@ import java.util.TimeZone;
 
 public class UserJDBCDAO extends SimpleJdbcDaoSupport implements UserDAO {
 
-    //    private static final String SELECT_USER_STRING = "select u.userID, u.groupID, u.personID, u.status, u.modified, u.lastLogin, u.passwordDT, u.password, u.username, u.mapType from user u where 1=1 ";
-    private static final String ROLE_SELECT = "SELECT u.userID, u.roleID FROM role as r JOIN userRole as u USING (roleID) WHERE u.userID IN (:userID)";
-//    private static final String SELECT_USER_STRING ="Select t.tzName,p.personID, p.acctID, p.tzID, p.modified, p.status, p.measureType, p.fuelEffType, p.addrID, p.locale, p.reportsTo," +
+    //    private static final String SELECT_USER_STRING ="Select t.tzName,p.personID, p.acctID, p.tzID, p.modified, p.status, p.measureType, p.fuelEffType, p.addrID, p.locale, p.reportsTo," +
 //            "    p.title,p.dept,p.empid, p.first, p.middle, p.last,p.suffix, p.gender, p.height, p.weight, p.dob, p.info, p.warn, p.crit, p.priEmail, p.secEmail, p.priPhone," +
 //            "    p.secPhone, p.priText, p.secText, u.userID, u.groupID, u.personID, u.status, u.modified, u.lastLogin, u.passwordDT, u.password, u.username, u.mapType " +
 //            "    from user u, person p,timezone t  where d.driverID = :driverID and p.personID = d.personID and p.tzID=t.tzID; ";
+    //    private static final String SELECT_USER_STRING = "select u.userID, u.groupID, u.personID, u.status, u.modified, u.lastLogin, u.passwordDT, u.password, u.username, u.mapType from user u where 1=1 ";
+
+    private static final String ROLE_ACCESS_SELECT = "SELECT DISTINCT ra.mode, ra.accessPtID, mode FROM roleAccess ra WHERE roleID IN (:roleID)";
+    private static final String ROLE_SELECT = "SELECT u.userID, u.roleID FROM role as r JOIN userRole as u USING (roleID) WHERE u.userID IN (:userID)";
+    private static final String MAP_LAYERS_SELECT = "SELECT uml.customMapID FROM userMapLayer uml where uml.userID = :userID";
     private static final String SELECT_USER_STRING = "Select t.tzName,p.personID, p.acctID, p.tzID, p.modified, p.status, p.measureType, p.fuelEffType, p.addrID, p.locale," +
             " p.reportsTo, p.title,p.dept,p.empid, p.first, p.middle, p.last,p.suffix, p.gender, p.height, p.weight, p.dob, p.info, p.warn, p.crit, p.priEmail, p.secEmail, p.priPhone," +
             " p.secPhone, p.priText, p.secText, u.userID, u.groupID, u.personID, u.status, u.modified, u.lastLogin, u.passwordDT, u.password, u.username, u.mapType from user u, person p," +
-            " timezone t where p.personID = u.personID and p.tzID=t.tzID and u.userID=ur.userID ";
-    private static final String SELECT_USER_STRING_BY_NAME = " and u.username like :username";
+            " timezone t where p.personID = u.personID and p.tzID=t.tzID ";
+    private static final String SELECT_USER_STRING_BY_NAME = " and u.username like :username and u.status <> 3";
+    private static final String SELECT_USER_STRING_BY_PERSONID = " and u.personID like :personID and u.status <> 3";
+    private static final String SELECT_USER_STRING_BY_ID = " and u.userID like :userID and u.status <> 3";
+    private static final String GROUP_ID_DEEP = SELECT_USER_STRING + " and u.groupID in (select groupID from groups where groupPath like :groupID) and u.status <> 3";
+    private static final String INSERT_USER = "insert into user (groupID, personID, status, passwordDT, username, password, mapType)" +
+                                              "VALUES (?, ?, ?, ?, ?, ?, ?)";
+
 
     private ParameterizedRowMapper<User> userParameterizedRow = new ParameterizedRowMapper<User>() {
         @Override
@@ -68,19 +93,21 @@ public class UserJDBCDAO extends SimpleJdbcDaoSupport implements UserDAO {
 
             User userItem = new User();
             userItem.setGroupID(getIntOrNullFromRS(rs, "u.groupID"));
-            userItem.setLastLogin(getDateOrNullFromRS(rs, "u.activated"));
+            userItem.setLastLogin(getDateOrNullFromRS(rs, "u.lastLogin"));
             userItem.setPassword(getStringOrNullFromRS(rs, "u.password"));
             userItem.setPasswordDT(getDateOrNullFromRS(rs, "u.passwordDT"));
-            userItem.setPersonID(getIntOrNullFromRS(rs, "u.presonID"));
+            userItem.setPersonID(getIntOrNullFromRS(rs, "u.personID"));
             userItem.setUsername(getStringOrNullFromRS(rs, "u.username"));
             userItem.setUserID(getIntOrNullFromRS(rs, "u.userID"));
             userItem.setStatus(rs.getObject("u.status") == null ? null : Status.valueOf(rs.getInt("u.status")));
+
+            userItem.setMapType(rs.getObject("u.status") == null ? null : GoogleMapType.valueOf(rs.getInt("u.mapType")));
             userItem.setPerson(person);
 
             userItem.setRoles(getUserRoles(rs.getInt("u.userID")));
-//            userItem.setSelectedMapLayerIDs();
-//            userItem.setAccessPoints();
-//            userItem.setMapType();
+            userItem.setSelectedMapLayerIDs(getUserMapLayers(rs.getInt("u.userID")));
+
+            userItem.setAccessPoints(getUserAccessPoint(getUserRoles(rs.getInt("u.userID"))));
 
             return userItem;
         }
@@ -89,16 +116,38 @@ public class UserJDBCDAO extends SimpleJdbcDaoSupport implements UserDAO {
     private ParameterizedRowMapper<Integer> roleParameterizedRow = new ParameterizedRowMapper<Integer>() {
         @Override
         public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
-            Integer roleId=getIntOrNullFromRS(rs, "u.roleID");
+            Integer roleId = getIntOrNullFromRS(rs, "u.roleID");
             return roleId;
         }
+    };
+
+    private ParameterizedRowMapper<Integer> mapLayersParameterizedRow = new ParameterizedRowMapper<Integer>() {
+        @Override
+        public Integer mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Integer mapLayers = getIntOrNullFromRS(rs, "uml.customMapID");
+            return mapLayers;
+        }
+    };
+
+    private ParameterizedRowMapper<AccessPoint> accessPointParameterizedRow = new ParameterizedRowMapper<AccessPoint>() {
+        @Override
+        public AccessPoint mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+            AccessPoint accessPoint = new AccessPoint();
+            accessPoint.setAccessPtID(getIntOrNullFromRS(rs, "ra.accessPtID"));
+            accessPoint.setMode(getIntOrNullFromRS(rs, "ra.mode"));
+
+
+            return accessPoint;
+        }
+
     };
 
 
     @Override
     public User findByUserName(String username) {
         Map<String, Object> params = new HashMap<String, Object>();
-        params.put("u.username", username);
+        params.put("username", username);
 
         StringBuilder userSelect = new StringBuilder(SELECT_USER_STRING);
         userSelect.append(SELECT_USER_STRING_BY_NAME);
@@ -110,22 +159,68 @@ public class UserJDBCDAO extends SimpleJdbcDaoSupport implements UserDAO {
 
     @Override
     public List<User> getUsersInGroupHierarchy(Integer groupID) {
-        return null;
+        try {
+            Map<String, Object> params = new HashMap<String, Object>();
+            params.put("groupID", groupID);
+
+            List<User> userList = getUsersByGroupIDDeep(groupID);
+
+            return userList;
+
+        } catch (EmptyResultSetException e) {
+            return Collections.emptyList();
+        }
     }
 
     @Override
     public User getUserByPersonID(Integer personID) {
-        return null;
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("personID", personID);
+
+        StringBuilder userSelect = new StringBuilder(SELECT_USER_STRING);
+        userSelect.append(SELECT_USER_STRING_BY_PERSONID);
+
+        User userRet = getSimpleJdbcTemplate().queryForObject(userSelect.toString(), userParameterizedRow, params);
+        return userRet;
     }
 
     @Override
     public User findByID(Integer integer) {
-        return null;
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("userID", integer);
+
+        StringBuilder userSelect = new StringBuilder(SELECT_USER_STRING);
+        userSelect.append(SELECT_USER_STRING_BY_ID);
+
+        User userRet = getSimpleJdbcTemplate().queryForObject(userSelect.toString(), userParameterizedRow, params);
+        return userRet;
     }
 
     @Override
-    public Integer create(Integer integer, User entity) {
-        return null;
+    public Integer create(Integer integer, final User entity) {
+        JdbcTemplate jdbcTemplate = getJdbcTemplate();
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        PreparedStatementCreator psc = new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement ps = con.prepareStatement(INSERT_USER, Statement.RETURN_GENERATED_KEYS);
+//                groupID, personID, status, passwordDT, username, password, mapType
+                ps.setInt(1, entity.getGroupID());
+                ps.setInt(2, entity.getPersonID());
+                ps.setInt(3, entity.getStatus().getCode());
+                DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                df.setTimeZone(TimeZone.getTimeZone("UTC"));
+                String passwordDT = df.format(toUTC(new Date()));
+                ps.setString(4, passwordDT);
+                ps.setString(5, entity.getUsername());
+                ps.setString(6, entity.getPassword());
+                ps.setInt(7, entity.getMapType().getCode());
+
+                return ps;
+            }
+            };
+        jdbcTemplate.update(psc, keyHolder);
+        return keyHolder.getKey().intValue();
     }
 
     @Override
@@ -150,14 +245,49 @@ public class UserJDBCDAO extends SimpleJdbcDaoSupport implements UserDAO {
         return rs.getObject(columnName) == null ? null : rs.getDate(columnName);
     }
 
-    public List<Integer> getUserRoles(Integer userID){
+    private Date toUTC(Date date){
+        DateTime dt = new DateTime(date.getTime()).toDateTime(DateTimeZone.UTC);
+        return dt.toDate();
+    }
+
+    public List<Integer> getUserRoles(Integer userID) {
         Map<String, Object> params = new HashMap<String, Object>();
-        params.put("u.userID", userID);
+        params.put("userID", userID);
 
         StringBuilder roleSelect = new StringBuilder(ROLE_SELECT);
 
         List<Integer> roleRet = getSimpleJdbcTemplate().query(roleSelect.toString(), roleParameterizedRow, params);
         return roleRet;
+    }
+
+    public List<Integer> getUserMapLayers(Integer userID) {
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("userID", userID);
+
+        StringBuilder mapLayersSelect = new StringBuilder(MAP_LAYERS_SELECT);
+
+        List<Integer> mapLayersRet = getSimpleJdbcTemplate().query(mapLayersSelect.toString(), mapLayersParameterizedRow, params);
+        return mapLayersRet;
+    }
+
+    public List<AccessPoint> getUserAccessPoint(List<Integer> roleID) {
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("roleID", roleID);
+
+        StringBuilder userAccessPointSelect = new StringBuilder(ROLE_ACCESS_SELECT);
+
+        List<AccessPoint> mapLayersRet = getSimpleJdbcTemplate().query(userAccessPointSelect.toString(), accessPointParameterizedRow, params);
+        return mapLayersRet;
+    }
+
+    private List<User> getUsersByGroupIDDeep(Integer groupID) {
+
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("groupID", "%/" + groupID + "/%");
+        StringBuilder groupIdDeep = new StringBuilder(GROUP_ID_DEEP);
+        List<User> vehiclesIn = getSimpleJdbcTemplate().query(groupIdDeep.toString(), userParameterizedRow, params);
+
+        return vehiclesIn;
     }
 
 }
