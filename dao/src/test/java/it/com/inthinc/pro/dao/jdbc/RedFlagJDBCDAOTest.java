b64fa494c09cccfb4803de46ccd50c4bf59e8eda
zimbran.radu <radu.zimbran@infrasoft.ro>
2014-04-01 17:37:01 +0300

    US7895 finished

--- dao/src/test/java/it/com/inthinc/pro/dao/jdbc/RedFlagJDBCDAOTest.java
+++ dao/src/test/java/it/com/inthinc/pro/dao/jdbc/RedFlagJDBCDAOTest.java
@@ -9,13 +9,20 @@ import com.inthinc.pro.dao.hessian.proserver.SiloService;
 import com.inthinc.pro.dao.hessian.proserver.SiloServiceCreator;
 import com.inthinc.pro.dao.jdbc.RedFlagJDBCDAO;
 import com.inthinc.pro.model.Account;
+import com.inthinc.pro.model.RedFlag;
 import com.inthinc.pro.model.app.States;
+import com.inthinc.pro.model.pagination.PageParams;
 import com.inthinc.pro.model.pagination.Range;
+import com.inthinc.pro.model.pagination.SortOrder;
+import com.inthinc.pro.model.pagination.TableFilterField;
+import com.inthinc.pro.model.pagination.TableSortField;
+import it.com.inthinc.pro.dao.model.GroupData;
 import it.com.inthinc.pro.dao.model.GroupListData;
 import it.com.inthinc.pro.dao.model.ITData;
 import it.com.inthinc.pro.dao.model.ITDataExt;
 import it.config.ITDataSource;
 import it.config.IntegrationConfig;
+import it.util.DataGenForIntegrationTesting;
 import it.util.DataGenForReportPaginationTesting;
 import jxl.write.DateTime;
 import org.apache.log4j.Logger;
@@ -26,27 +33,27 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class RedFlagJDBCDAOTest extends BaseJDBCTest{
 
     private static final Logger logger = Logger.getLogger(ReportPaginationJDBCTest.class);
     private static SiloService siloService;
-    private static ITDataExt itData;
+    private static ITData itData;
 
-    private static final String PAGINATION_BASE_DATA_XML = "ReportPageTest.xml";
-    private static Integer countPerGroup;
+    private static final String INTEGRATION_TEST_XML = "IntegrationTest.xml";
+    private static Integer levelCount = 11;
     private static Integer goodGroupID;
+    private static Integer goodVehicleID;
     private static String goodGroupName;
     private static String badGroupName;
 
-    TestFilterParams[] redFlagFilterTestList = {
-            new TestFilterParams("groupID", "XXX", 0),
-            new TestFilterParams("groupID", goodGroupID.toString(), countPerGroup),
-            new TestFilterParams("vehicleID", "XXX", 0),
-            new TestFilterParams("vehicleID", "e", countPerGroup),
-            new TestFilterParams("driverID", "XXX", 0),
-            new TestFilterParams("driverID", "e", countPerGroup),
+    TestFilterParams[] redFlagFilterTestListFlag = {
+            new TestFilterParams("level", 6 , 0),
+            new TestFilterParams("driverName", "xxx", 0),
+            new TestFilterParams("vehicleName", "xxx", 0),
     };
 
     @BeforeClass
@@ -60,11 +67,11 @@ public class RedFlagJDBCDAOTest extends BaseJDBCTest{
         siloService = new SiloServiceCreator(host, port).getService();
 
         initApp();
-        itData = new ITDataExt();
+        itData = new ITData();
 
-        InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(PAGINATION_BASE_DATA_XML);
-        countPerGroup = DataGenForReportPaginationTesting.NUM_DRIVERS_VEHICLES_DEVICES;
-        if (!itData.parseTestDataExt(stream, siloService, countPerGroup)) {
+        InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream(INTEGRATION_TEST_XML);
+//        if (!itData.parseTestData(stream, siloService, false, false, true, true)) {
+        if (!itData.parseTestData(stream, siloService, true, true, true)) {
             throw new Exception("Error parsing Test data xml file");
         }
 
@@ -73,9 +80,7 @@ public class RedFlagJDBCDAOTest extends BaseJDBCTest{
         Account account = accountDAO.findByID(itData.account.getAccountID());
         itData.account.setUnkDriverID(account.getUnkDriverID());
 
-        goodGroupID = itData.teamGroupListData.get(ITData.GOOD).group.getGroupID();
-        goodGroupName = itData.teamGroupListData.get(ITData.GOOD).group.getName();
-        badGroupName = itData.teamGroupListData.get(ITData.BAD).group.getName();
+        goodGroupID=itData.fleetGroup.getGroupID();
 
     }
 
@@ -110,17 +115,42 @@ public class RedFlagJDBCDAOTest extends BaseJDBCTest{
 
          // no filters
          for (int teamIdx = ITData.GOOD; teamIdx <= ITData.BAD; teamIdx++) {
-
-        GroupListData team = itData.teamGroupListData.get(teamIdx);
-        Integer redFlagCount = redFlagJDBCDAO.getRedFlagCount(team.group.getGroupID(), today.minusYears(2).toDate() , today.toDate(), 0, null);
+             GroupData team = itData.teamGroupData.get(teamIdx);
+             Integer redFlagCount = redFlagJDBCDAO.getRedFlagCount(goodGroupID, today.minusDays(1).toDate() , today.toDate(), 0, null);
              assertTrue(redFlagCount>0);
+             PageParams pageParams = new PageParams();
+             pageParams.setStartRow(0);
+             pageParams.setEndRow(redFlagCount-1);
+
+         List<RedFlag> redFlagList = redFlagJDBCDAO.getRedFlagPage(goodGroupID, today.minusDays(1).toDate() , today.toDate(), 0, pageParams);
+             assertTrue(!redFlagList.isEmpty());
 
 
          }
+         //filters
+         //critical events
+             GroupData team = itData.teamGroupData.get(ITData.GOOD);
+             Integer allDriverCount = redFlagJDBCDAO.getRedFlagCount(goodGroupID, today.minusDays(1).toDate() , today.toDate(), 0, null);
+             for (TestFilterParams testFilterParams : redFlagFilterTestListFlag) {
+                 PageParams pageParams = new PageParams();
+                 pageParams.setStartRow(0);
+                 pageParams.setEndRow(allDriverCount-1);
+                 List<TableFilterField> filterList = new ArrayList<TableFilterField>();
+                 filterList.add(new TableFilterField(testFilterParams.field, testFilterParams.value));
+                 Integer redFlagCount = redFlagJDBCDAO.getRedFlagCount(goodGroupID, today.minusDays(1).toDate() , today.toDate(), 0, filterList);
+
+                 assertTrue(redFlagCount==0);
+                 pageParams.setFilterList(filterList);
+                 List<RedFlag> list = redFlagJDBCDAO.getRedFlagPage(goodGroupID, today.minusDays(1).toDate() , today.toDate(), 0, pageParams);
+                 assertTrue(list.isEmpty());
+
+             }
 
      }
 
+
 }
+
 class TestFilterParams {
     String field;
     Object value;
@@ -133,3 +163,6 @@ class TestFilterParams {
         this.expectedCount = expectedCount;
     }
 }
+
+
+
