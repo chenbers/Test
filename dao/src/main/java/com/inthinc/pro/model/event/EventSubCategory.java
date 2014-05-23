2d93240c3698b801f2a8a0f79d8827cca784dcfb
Ionut M <ionut.murariu@infrasoft.ro>
2014-05-23 13:40:31 +0300

    work in progress

--- dao/src/main/java/com/inthinc/pro/model/event/EventSubCategory.java
+++ dao/src/main/java/com/inthinc/pro/model/event/EventSubCategory.java
@@ -70,8 +70,11 @@ public enum EventSubCategory implements BaseEnum
                     AlertMessageType.ALERT_TYPE_DVIR_NO_POST_TRIP_INSPECTION, AlertMessageType.ALERT_TYPE_DVIR_REPAIR)),
     REVERSE(15,EnumSet.of(EventType.BACKING), null),
     TRAILER(16, EnumSet.of(EventType.TRAILER_DATA, EventType.TRAILER_PROGRAMMED), null),
-    CONDITIONAL(17, EnumSet.of(EventType.ZONES_ARRIVAL, EventType.ZONES_DEPARTURE),
-            EnumSet.of(AlertMessageType.ATTR_BATTERY_VOLTAGE));
+    CONDITIONAL(17,
+            EnumSet.of(EventType.BATTERY_VOLTAGE, EventType.ENGINE_TEMP, EventType.TRANSMISSION_TEMP, EventType.DPF_FLOW_RATE, EventType.OIL_PRESSURE,
+                       EventType.ENGINE_HOURS, EventType.MALFUNCTION_INDICATOR_LAMP, EventType.CHECK_ENGINE, EventType.MAINTENANCE_CAPABILITIES),
+            EnumSet.of(AlertMessageType.ATTR_BATTERY_VOLTAGE,AlertMessageType.ATTR_ENGINE_TEMP,AlertMessageType.ATTR_TRANSMISSION_TEMP,AlertMessageType.ATTR_DPF_FLOW_RATE,AlertMessageType.ATTR_OIL_PRESSURE,
+                       AlertMessageType.ATTR_ENGINE_HOURS,AlertMessageType.ATTR_MALFUNCTION_INDICATOR_LAMP,AlertMessageType.ATTR_CHECK_ENGINE,AlertMessageType.ATTR_MAINTENANCE_CAPABILITIES));
     
     private int code;
     private Set<EventType> eventTypeSet;
