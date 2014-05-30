e2e721b2ce2754910dd6cd7513710736623677ca
alexandru-matei <alexandru.matei@infrasoft.ro>
2014-05-30 12:38:12 +0300

    US8390 - Fix duration so it works on trend too.

--- dao/src/main/java/com/inthinc/pro/model/Duration.java
+++ dao/src/main/java/com/inthinc/pro/model/Duration.java
@@ -25,7 +25,7 @@ public enum Duration implements BaseEnum
     THREE(/*3*/ 2, "3 months", 90, "MMM", 2, 3, 3), 
     SIX(/*4*/ 3, "6 months", 180, "MMM", 2, 6, 4), 
     TWELVE(/*5*/ 4, "12 months", 360, "MMM", 2, 12, 5),
-    SEVEN_DAYS(7, "7 days", 7, "dd", 1, 1, 1);
+    SEVEN_DAYS(5, "7 days", 7, "dd", 0, 7, 6);
 
     /*
      * dvqCode
