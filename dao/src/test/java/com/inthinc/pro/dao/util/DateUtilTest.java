bc14e6e6c8309f9e50d7ae47c88a9c9c97ee3955
Jason Wimmer <jwimmer@inthinc.com>
2014-06-13 15:04:19 -0600

    DE9632: forgot to import Ignore

--- dao/src/test/java/com/inthinc/pro/dao/util/DateUtilTest.java
+++ dao/src/test/java/com/inthinc/pro/dao/util/DateUtilTest.java
@@ -6,6 +6,7 @@ import org.joda.time.DateMidnight;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.Interval;
+import org.junit.Ignore;
 import org.junit.Test;
 
 
