a9012fbfcbe921b4fd6044ac85e36e0d38df6cf7
Aaron Nelson <anelson@inthinc.com>
2014-06-23 14:16:28 -0600

    US8482: Modify link to Assets
    Remove direct link to trailers, and replace with a link to vehicles titled "Assets"

--- web/src/main/java/com/inthinc/pro/backing/NavigationBarBean.java
+++ web/src/main/java/com/inthinc/pro/backing/NavigationBarBean.java
@@ -1,8 +1,17 @@
 package com.inthinc.pro.backing;
 
 public class NavigationBarBean extends BaseBean {
+    private String assetsUrl;
     private String formUrl;
     private String trailerUrl;
+
+    public String getAssetsUrl() {
+        return assetsUrl;
+    }
+
+    public void setAssetsUrl(String assetsUrl) {
+        this.assetsUrl = assetsUrl;
+    }
 
     public String getFormUrl() {
         return formUrl;
