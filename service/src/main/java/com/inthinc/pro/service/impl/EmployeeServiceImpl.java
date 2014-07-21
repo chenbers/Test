20aa731fbd251503e3443a1c66b134410079ca91
alexandru-matei <alexandru.matei@infrasoft.ro>
2014-07-21 16:13:24 +0300

    US8394 - Uncommented missing method.

--- service/src/main/java/com/inthinc/pro/service/impl/EmployeeServiceImpl.java
+++ service/src/main/java/com/inthinc/pro/service/impl/EmployeeServiceImpl.java
@@ -265,7 +265,7 @@ public class EmployeeServiceImpl extends AbstractService<Driver, DriverDAOAdapte
         return Response.status(Status.NOT_FOUND).build();
     }
 
-/*
+
     @Override
     public Response getGroupDriverLocations(Integer groupID) {
         List<DriverLocation> list = null;
@@ -280,7 +280,6 @@ public class EmployeeServiceImpl extends AbstractService<Driver, DriverDAOAdapte
         return Response.ok(new GenericEntity<List<DriverLocation>>(list) {
         }).build();
     }
-*/
 
     public static String getSimpleDateFormat() {
         return SIMPLE_DATE_FORMAT;
