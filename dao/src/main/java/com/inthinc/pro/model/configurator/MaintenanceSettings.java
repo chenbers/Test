dc34d71cb91d5eab8d55b68e22c73dff8279bf48
Jason Wimmer <jwimmer@inthinc.com>
2014-08-14 16:42:42 -0600

    US8723: reordering by id for clarity

--- dao/src/main/java/com/inthinc/pro/model/configurator/MaintenanceSettings.java
+++ dao/src/main/java/com/inthinc/pro/model/configurator/MaintenanceSettings.java
@@ -9,20 +9,19 @@ import java.util.Map;
 @XmlRootElement
 public enum MaintenanceSettings implements Serializable {
     ENABLE_MAINTENANCE(189, "enable_maintenance"),
+    SET_ENGINE_TEMP(190, "set_engine_temp"),
+    SET_TRANS_TEMP(191, "set_trans_temp"),
+    DPF_FLOW_RATE(192, "dpf_flow_rate"),
+    SET_OIL_PRESSURE(193, "set_oil_pressure"),
+    SET_BATT_VOLTAGE(194, "set_batt_voltage"),
+    MAINT_BY_ENGINE_HOURS_INTERVAL(195, "maint_by_engine_hours_threshold"),
+    MAINT_BY_DIST_START(196, "maint_by_dist_start"),
+    MAINT_BY_DIST_INTERVAL(197, "maint_by_dist_threshold"),
+    SETTINGS_MASK(198,"maintenance_bitmask"),
     MALFUNCTION_INDICATORS(199, "malfunction_indicators"),
     RED_STOP(200, "red_stop"),
     AMBER_WARNING(201, "amber_warning"),
     PROTECT(202, "protect"),
-    SET_BATT_VOLTAGE(194, "set_batt_voltage"),
-    SET_ENGINE_TEMP(190, "set_engine_temp"),
-    SET_TRANS_TEMP(191, "set_trans_temp"),
-    SET_OIL_PRESSURE(193, "set_oil_pressure"),
-    DPF_FLOW_RATE(192, "dpf_flow_rate"),
-    MAINT_BY_DIST_INTERVAL(197, "maint_by_dist_threshold"),
-    MAINT_BY_DIST_START(196, "maint_by_dist_start"),
-    MAINT_BY_ENGINE_HOURS_INTERVAL(195, "maint_by_engine_hours_threshold"),
-    //MAINT_BY_ENGINE_HOURS_START(203, "maint_by_engine_hours_start"),
-    SETTINGS_MASK(198,"maintenance_bitmask")
     ;
 
     private String description;
