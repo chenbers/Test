d42bb0c0d04ddbb31e7ad82a3191441910543d46
Jason Wimmer <jwimmer@inthinc.com>
2014-08-22 14:40:01 -0600

    US8363: rebase cleanup

--- dao/src/main/java/com/inthinc/pro/model/event/Event.java
+++ dao/src/main/java/com/inthinc/pro/model/event/Event.java
@@ -466,11 +466,6 @@ public class Event extends BaseEntity implements Comparable<Event>, Serializable
         this.deviceName = deviceName;
     }
 
-    //@XmlElement  // unblock this to get duration also
-    public Integer getDuration() {
-        return Math.abs(getStopTime() - getStartTime());
-    }
-
     @XmlElement
     public String getStartTime() {
         String startTime="";
