9ea599a93f5a03bcc4c9fca408d9d372fa23dc70
billmiller1001 <bmiller@inthinc.com>
2014-10-21 10:08:57 -0600

    Moved noteparser to its own project so deleting from here and updating poms.

--- noteparser/src/main/java/com/inthinc/pro/comm/parser/attrib/AttribParser.java
+++
@@ -1,9 +0,0 @@
-package com.inthinc.pro.comm.parser.attrib;
-
-import java.util.Map;
-
-
-public interface AttribParser {
-	int parseAttrib(byte[] data, int offset, String code, Map<String, Object> attribMap);
-	Object parseString(String val);
-}
--- noteparser/src/main/java/com/inthinc/pro/comm/parser/note/NoteParser.java
+++
@@ -1,8 +0,0 @@
-package com.inthinc.pro.comm.parser.note;
-
-import java.util.Map;
-
-
-public interface NoteParser {
-	public Map<String, Object> parseNote(byte[] data);
-}
