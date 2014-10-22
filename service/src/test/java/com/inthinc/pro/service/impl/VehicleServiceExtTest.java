95d4422f2c8ea45f484e29fa81e294a83dc11fcf
alexandru-matei <alexandru.matei@infrasoft.ro>
2014-10-22 16:10:58 +0300

    US8749 - Test last location.

--- service/src/test/java/com/inthinc/pro/service/impl/VehicleServiceExtTest.java
+++ service/src/test/java/com/inthinc/pro/service/impl/VehicleServiceExtTest.java
@@ -1,13 +1,20 @@
 package com.inthinc.pro.service.impl;
 
+import com.inthinc.pro.dao.EventDAO;
 import com.inthinc.pro.dao.GroupDAO;
 import com.inthinc.pro.dao.VehicleDAO;
+import com.inthinc.pro.dao.report.VehicleReportDAO;
 import com.inthinc.pro.model.Group;
+import com.inthinc.pro.model.GroupHierarchy;
+import com.inthinc.pro.model.LastLocation;
 import com.inthinc.pro.model.Status;
 import com.inthinc.pro.model.Vehicle;
 import com.inthinc.pro.model.VehicleType;
 import com.inthinc.pro.service.VehicleServiceExt;
+import com.inthinc.pro.service.adapters.VehicleDAOAdapter;
 import com.inthinc.pro.service.it.BaseTest;
+import mockit.Mocked;
+import mockit.NonStrictExpectations;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -36,6 +43,19 @@ public class VehicleServiceExtTest extends BaseTest {
     public VehicleDAO vehicleDAO;
 
     @Autowired
+    private EventDAO eventDAO;
+
+    @Autowired
+    private VehicleReportDAO vehicleReportDAO;
+
+    @Mocked
+    public VehicleDAO mockVehicleDAO;
+
+    private VehicleDAOAdapter vehicleDAOAdapter;
+
+    private VehicleDAOAdapter mockVehicleDAOAdapter;
+
+    @Autowired
     public GroupDAO groupDAO;
 
     @Autowired
@@ -47,9 +67,21 @@ public class VehicleServiceExtTest extends BaseTest {
     private Vehicle vehicle2;
     private Vehicle vehicle3;
     private Group group;
+    private LastLocation mockLastLocation;
 
     @Before
     public void createTestData() {
+        mockLastLocation = new LastLocation();
+        mockLastLocation.setVehicleStatus("aok");
+
+        vehicleDAOAdapter.setVehicleDAO(vehicleDAO);
+        vehicleDAOAdapter.setEventDAO(eventDAO);
+        vehicleDAOAdapter.setVehicleReportDAO(vehicleReportDAO);
+
+        mockVehicleDAOAdapter.setVehicleDAO(mockVehicleDAO);
+        mockVehicleDAOAdapter.setEventDAO(eventDAO);
+        mockVehicleDAOAdapter.setVehicleReportDAO(vehicleReportDAO);
+
         testVehicles = new HashMap<Integer, Vehicle>();
         vehicle1 = new Vehicle();
         vehicle2 = new Vehicle();
@@ -160,6 +192,35 @@ public class VehicleServiceExtTest extends BaseTest {
         assertEquals(3, found);
     }
 
+
+    @Test
+    public void getLocationTest() {
+        VehicleServiceExtImpl vehicleServiceExtImpl = (VehicleServiceExtImpl) vehicleServiceExt;
+        try {
+            vehicleServiceExtImpl.setDao(mockVehicleDAOAdapter);
+
+            new NonStrictExpectations() {{
+
+                mockVehicleDAO.findByName(vehicle1.getName()); result = vehicle1;
+                mockVehicleDAO.getLastLocation(vehicle1.getVehicleID()); result = mockLastLocation;
+                mockVehicleDAO.findByName(vehicle2.getName()); result = vehicle1;
+                mockVehicleDAO.getLastLocation(vehicle2.getVehicleID()); result = mockLastLocation;
+                mockVehicleDAO.findByName(vehicle3.getName()); result = vehicle1;
+                mockVehicleDAO.getLastLocation(vehicle3.getVehicleID()); result = mockLastLocation;
+            }};
+
+            for (int i = 1; i <= 3; i++) {
+                Response response = vehicleServiceExt.getLastLocation("dname_" + i);
+                assertNotNull(response.getEntity());
+                LastLocation lastLocation = (LastLocation) response.getEntity();
+                assertNotNull(lastLocation);
+                assertEquals(lastLocation.getVehicleStatus(), "aok");
+            }
+        }finally {
+            vehicleServiceExtImpl.setDao(vehicleDAOAdapter);
+        }
+    }
+
     public VehicleDAO getVehicleDAO() {
         return vehicleDAO;
     }
@@ -182,5 +243,93 @@ public class VehicleServiceExtTest extends BaseTest {
 
     public void setVehicleServiceExt(VehicleServiceExt vehicleServiceExt) {
         this.vehicleServiceExt = vehicleServiceExt;
+    }
+
+    public EventDAO getEventDAO() {
+        return eventDAO;
+    }
+
+    public void setEventDAO(EventDAO eventDAO) {
+        this.eventDAO = eventDAO;
+    }
+
+    public VehicleReportDAO getVehicleReportDAO() {
+        return vehicleReportDAO;
+    }
+
+    public void setVehicleReportDAO(VehicleReportDAO vehicleReportDAO) {
+        this.vehicleReportDAO = vehicleReportDAO;
+    }
+
+    public VehicleDAO getMockVehicleDAO() {
+        return mockVehicleDAO;
+    }
+
+    public void setMockVehicleDAO(VehicleDAO mockVehicleDAO) {
+        this.mockVehicleDAO = mockVehicleDAO;
+    }
+
+    public VehicleDAOAdapter getVehicleDAOAdapter() {
+        return vehicleDAOAdapter;
+    }
+
+    public void setVehicleDAOAdapter(VehicleDAOAdapter vehicleDAOAdapter) {
+        this.vehicleDAOAdapter = vehicleDAOAdapter;
+    }
+
+    public VehicleDAOAdapter getMockVehicleDAOAdapter() {
+        return mockVehicleDAOAdapter;
+    }
+
+    public void setMockVehicleDAOAdapter(VehicleDAOAdapter mockVehicleDAOAdapter) {
+        this.mockVehicleDAOAdapter = mockVehicleDAOAdapter;
+    }
+
+    public Map<Integer, Vehicle> getTestVehicles() {
+        return testVehicles;
+    }
+
+    public void setTestVehicles(Map<Integer, Vehicle> testVehicles) {
+        this.testVehicles = testVehicles;
+    }
+
+    public Vehicle getVehicle1() {
+        return vehicle1;
+    }
+
+    public void setVehicle1(Vehicle vehicle1) {
+        this.vehicle1 = vehicle1;
+    }
+
+    public Vehicle getVehicle2() {
+        return vehicle2;
+    }
+
+    public void setVehicle2(Vehicle vehicle2) {
+        this.vehicle2 = vehicle2;
+    }
+
+    public Vehicle getVehicle3() {
+        return vehicle3;
+    }
+
+    public void setVehicle3(Vehicle vehicle3) {
+        this.vehicle3 = vehicle3;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public LastLocation getMockLastLocation() {
+        return mockLastLocation;
+    }
+
+    public void setMockLastLocation(LastLocation mockLastLocation) {
+        this.mockLastLocation = mockLastLocation;
     }
 }
