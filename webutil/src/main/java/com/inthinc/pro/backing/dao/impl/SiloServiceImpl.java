0a4d42ff1105b94f020a22db4e0e3b0f1b6abc71
alexandru-matei <alexandru.matei@infrasoft.ro>
2014-10-28 10:20:26 +0200

    US8750 - Add missing annotation for use in tiwiproutil.

--- webutil/src/main/java/com/inthinc/pro/backing/dao/impl/SiloServiceImpl.java
+++ webutil/src/main/java/com/inthinc/pro/backing/dao/impl/SiloServiceImpl.java
@@ -422,6 +422,7 @@ public class SiloServiceImpl implements SiloService {
 
     @Override
     @MethodDescription(description = "Fetches a vehicle by its current name.", crudType=CrudType.READ, modelClass=com.inthinc.pro.model.Vehicle.class)
+    @DAODescription(daoID="vehicleDAO", daoMethod="findByName")
     public Map<String, Object> getVehicleByName( @DaoParam(name = "name") String name) throws ProDAOException {
         // TODO Auto-generated method stub
         return null;
