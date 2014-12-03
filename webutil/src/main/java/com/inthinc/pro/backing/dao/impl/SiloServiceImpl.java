3a8b27e80f6d1f5c86d24a39b4a03dc66050edaf
Jason Wimmer <jwimmer@inthinc.com>
2014-12-03 10:05:37 -0700

    Revert "US8750 - Add missing annotation for use in tiwiproutil."
    This reverts commit 0a4d42ff1105b94f020a22db4e0e3b0f1b6abc71.

--- webutil/src/main/java/com/inthinc/pro/backing/dao/impl/SiloServiceImpl.java
+++ webutil/src/main/java/com/inthinc/pro/backing/dao/impl/SiloServiceImpl.java
@@ -422,7 +422,6 @@ public class SiloServiceImpl implements SiloService {
 
     @Override
     @MethodDescription(description = "Fetches a vehicle by its current name.", crudType=CrudType.READ, modelClass=com.inthinc.pro.model.Vehicle.class)
-    @DAODescription(daoID="vehicleDAO", daoMethod="findByName")
     public Map<String, Object> getVehicleByName( @DaoParam(name = "name") String name) throws ProDAOException {
         // TODO Auto-generated method stub
         return null;
