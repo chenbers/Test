5e9c29a4edd94da6f36a0ed6d612ee6dc019cf97
jonw66 <jwood@inthinc.com>
2015-01-08 18:47:57 -0700

    US8877 - MeasurementType fix

--- dao/src/main/java/com/inthinc/pro/dao/hessian/AlertMessageHessianDAO.java
+++ dao/src/main/java/com/inthinc/pro/dao/hessian/AlertMessageHessianDAO.java
@@ -390,7 +390,7 @@ public class AlertMessageHessianDAO extends GenericHessianDAO<AlertMessage, Inte
             addLocationInfo(event);                     //#12 - #14
             addOdometer(event);                         //#15
             addSpeed(event);                            //#16
-            int mType = personMeasurementType.ordinal();
+            int mType = personMeasurementType.ordinal() + 1;
             parameterList.add(String.valueOf(mType));    //#17 - needs to be 0|1 -- 0-english or 1-metric
             addEventParams(event, alertMessage);        //#18+  if any
 
