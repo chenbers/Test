f2c37a1a9e82fc25b8d8cd3ccb6e7169d8f8788c
alexandru-matei <alexandru.matei@infrasoft.ro>
2015-01-14 10:37:00 +0200

    US8968 -  filter other rulesets except US 8-day, US oil 8-day, US oil no wait 8-day, US waterwell 8-day

--- reports/src/main/java/com/inthinc/pro/reports/hos/WthHosViolationsSummaryReportCriteria.java
+++ reports/src/main/java/com/inthinc/pro/reports/hos/WthHosViolationsSummaryReportCriteria.java
@@ -44,21 +44,22 @@ public class WthHosViolationsSummaryReportCriteria extends ViolationsSummaryRepo
         this.setIncludeInactiveDrivers(ReportCriteria.DEFAULT_INCLUDE_ZERO_MILES_DRIVERS);
         this.setIncludeZeroMilesDrivers(ReportCriteria.DEFAULT_INCLUDE_ZERO_MILES_DRIVERS);
     }
+
     public void init(GroupHierarchy accountGroupHierarchy, List<Integer> groupIDList, Interval interval) {
         List<Group> reportGroupList = getReportGroupList(groupIDList, accountGroupHierarchy);
         List<Driver> driverList = getReportDriverList(reportGroupList);
-        Map<Driver, List<HOSRecord>> driverHOSRecordMap = new HashMap<Driver, List<HOSRecord>> ();
+        Map<Driver, List<HOSRecord>> driverHOSRecordMap = new HashMap<Driver, List<HOSRecord>>();
         for (Driver driver : driverList) {
-            if(includeDriver(getDriverDAO(), driver.getDriverID(), interval)){
+            if (includeDriver(getDriverDAO(), driver.getDriverID(), interval)) {
                 if (driver.getDot() == null)
                     continue;
-                
+
                 DateTimeZone dateTimeZone = DateTimeZone.forTimeZone(driver.getPerson().getTimeZone());
                 Interval queryInterval = DateTimeUtil.getExpandedInterval(interval, dateTimeZone, RuleSetFactory.getDaysBackForRuleSetType(driver.getDot()), RuleSetFactory.getDaysForwardForRuleSetType(driver.getDot()));
                 driverHOSRecordMap.put(driver, hosDAO.getHOSRecords(driver.getDriverID(), queryInterval, false));
             }
         }
-        
+
         List<HOSGroupMileage> groupMileageList = new ArrayList<HOSGroupMileage>();
         List<HOSGroupMileage> groupNoDriverMileageList = new ArrayList<HOSGroupMileage>();
 
@@ -67,14 +68,14 @@ public class WthHosViolationsSummaryReportCriteria extends ViolationsSummaryRepo
             for (HOSGroupMileage mileage : mileageList)
                 if (!groupExists(groupMileageList, mileage.getGroupID()))
                     groupMileageList.add(mileage);
-            List<HOSGroupMileage> zeroMileageList = hosDAO.getHOSMileage(reportGroup.getGroupID(), interval, true); 
+            List<HOSGroupMileage> zeroMileageList = hosDAO.getHOSMileage(reportGroup.getGroupID(), interval, true);
             for (HOSGroupMileage mileage : zeroMileageList)
                 if (!groupExists(groupNoDriverMileageList, mileage.getGroupID()))
                     groupNoDriverMileageList.add(mileage);
         }
 
         initDataSet(interval, accountGroupHierarchy, reportGroupList, driverHOSRecordMap, groupMileageList, groupNoDriverMileageList);
-        
+
     }
 
     private boolean groupExists(List<HOSGroupMileage> groupMileageList, Integer groupID) {
@@ -83,10 +84,10 @@ public class WthHosViolationsSummaryReportCriteria extends ViolationsSummaryRepo
                 return true;
         return false;
     }
-    void initDataSet(Interval interval, GroupHierarchy groupHierarchy,  List<Group> reportGroupList, Map<Driver, List<HOSRecord>> driverHOSRecordMap,
-            List<HOSGroupMileage> groupMileageList, List<HOSGroupMileage> groupNoDriverMileageList)
-    {
-        
+
+    void initDataSet(Interval interval, GroupHierarchy groupHierarchy, List<Group> reportGroupList, Map<Driver, List<HOSRecord>> driverHOSRecordMap,
+                     List<HOSGroupMileage> groupMileageList, List<HOSGroupMileage> groupNoDriverMileageList) {
+
         Map<Integer, WthHosViolationsSummary> dataMap = new TreeMap<Integer, WthHosViolationsSummary>();
         for (Group group : reportGroupList) {
             dataMap.put(group.getGroupID(), new WthHosViolationsSummary(getFullGroupName(groupHierarchy, group.getGroupID())));
@@ -96,9 +97,17 @@ public class WthHosViolationsSummaryReportCriteria extends ViolationsSummaryRepo
         for (Entry<Driver, List<HOSRecord>> entry : driverHOSRecordMap.entrySet()) {
             Driver driver = entry.getKey();
             RuleSetType driverDOTType = driver.getDot();
+
             if (driverDOTType == null)
                 driverDOTType = RuleSetType.NON_DOT;
-            
+
+            // filter other rulesets except US 8-day, US oil 8-day, US oil no wait 8-day, US waterwell 8-day
+            if (!(driverDOTType.equals(RuleSetType.US_OIL_NO_WAIT_8DAY)
+                    || driverDOTType.equals(RuleSetType.US_WATERWELL_8DAY)
+                    || driverDOTType.equals(RuleSetType.US)
+                    || driverDOTType.equals(RuleSetType.US_OIL)))
+                continue;
+
             DateTimeZone driverTimeZone = DateTimeZone.forTimeZone(driver.getPerson().getTimeZone());
             DateTime reportEndDate = new LocalDate(interval.getEnd()).toDateTimeAtStartOfDay(driverTimeZone).plusDays(1).minusSeconds(1);
             if (reportEndDate.isAfterNow())
@@ -106,55 +115,53 @@ public class WthHosViolationsSummaryReportCriteria extends ViolationsSummaryRepo
 
             List<HOSRecord> hosRecordList = entry.getValue();
             Collections.sort(hosRecordList);
-            
-            
+
+
             List<HOSRec> recListForViolationsCalc = HOSUtil.getRecListFromLogList(hosRecordList, reportEndDate.toDate(), !(driverDOTType.equals(RuleSetType.NON_DOT)));
 
             ViolationsSummary summary = findSummary(groupHierarchy, dataMap, driver.getGroupID());
             if (summary == null) {
                 continue;
             }
-            
+
             // violations
             List<ViolationsData> dailyViolations = new DailyViolations().getDailyHosViolationsForReport(interval,
                     driverTimeZone.toTimeZone(),
-                    driverDOTType, 
+                    driverDOTType,
                     recListForViolationsCalc);
-//            dump(driver, dailyViolations);
             updateSummary(summary, dailyViolations);
 
             List<ViolationsData> shiftViolations = new ShiftViolations().getHosViolationsInTimeFrame(
                     interval, driverTimeZone.toTimeZone(),
-                    driverDOTType, 
-                    null, 
+                    driverDOTType,
+                    null,
                     recListForViolationsCalc);
-//            dump(driver, shiftViolations);
             updateSummary(summary, shiftViolations);
 
             updateSummaryDriverCount(summary, driver);
         }
-        
+
         if (groupMileageList != null) {
             for (HOSGroupMileage groupMileage : groupMileageList) {
-                WthHosViolationsSummary summary = (WthHosViolationsSummary)findSummary(groupHierarchy, dataMap, groupMileage.getGroupID());
+                WthHosViolationsSummary summary = (WthHosViolationsSummary) findSummary(groupHierarchy, dataMap, groupMileage.getGroupID());
                 if (summary == null) {
                     continue;
                 }
-                summary.setTotalMiles(summary.getTotalMiles()+groupMileage.getDistance());
+                summary.setTotalMiles(summary.getTotalMiles() + groupMileage.getDistance());
             }
         }
-        
+
         if (groupNoDriverMileageList != null) {
             for (HOSGroupMileage groupMileage : groupNoDriverMileageList) {
-                WthHosViolationsSummary summary = (WthHosViolationsSummary)findSummary(groupHierarchy, dataMap, groupMileage.getGroupID());
+                WthHosViolationsSummary summary = (WthHosViolationsSummary) findSummary(groupHierarchy, dataMap, groupMileage.getGroupID());
                 if (summary == null) {
                     continue;
                 }
-                summary.setTotalMiles(summary.getTotalMiles()+groupMileage.getDistance());
-                summary.setTotalMilesNoDriver(summary.getTotalMilesNoDriver()+groupMileage.getDistance());
+                summary.setTotalMiles(summary.getTotalMiles() + groupMileage.getDistance());
+                summary.setTotalMilesNoDriver(summary.getTotalMilesNoDriver() + groupMileage.getDistance());
             }
         }
-         
+
         List<WthHosViolationsSummary> dataList = new ArrayList<WthHosViolationsSummary>();
         for (WthHosViolationsSummary summary : dataMap.values()) {
             if (summary.getDriverCnt().intValue() != 0 || summary.getTotalMiles().intValue() != 0 || summary.getTotalMilesNoDriver().intValue() != 0)
@@ -162,57 +169,41 @@ public class WthHosViolationsSummaryReportCriteria extends ViolationsSummaryRepo
         }
 
         setMainDataset(dataList);
-        
+
         addParameter("REPORT_START_DATE", dateTimeFormatter.print(interval.getStart()));
         addParameter("REPORT_END_DATE", dateTimeFormatter.print(interval.getEnd()));
-        
-        
     }
 
-//    private void dump(Driver driver, List<ViolationsData> violations) {
-//        System.out.println("Driver: " + driver.getDriverID() + " " + ((violations == null || violations.size() == 0) ? 0 : violations.size()) + " violations");
-//        if (violations == null)
-//            return;
-//        for (ViolationsData data : violations) {
-//            System.out.println(data.getHosViolationRec().getStartOfDay() + " " + data.getViolationMap().size());
-//            for (Entry<RuleViolationTypes, Long> entry : data.getViolationMap().entrySet()) {
-//                if (entry.getKey().isReportable()) {
-//                    System.out.println(entry.getKey() + " " + entry.getValue());
-//                }
-//            }
-//        }
-//        
-//    }
     @Override
     protected void updateSummaryDriverCount(ViolationsSummary summary, Driver driver) {
         if (driver.getDot() != null && driver.getDot() != RuleSetType.NON_DOT)
             summary.setDriverCnt(summary.getDriverCnt() + 1);
-        
+
     }
 
     @Override
     public List<String> getColumnHeaders() {
         ResourceBundle resourceBundle = ReportType.WEATHERFORD_HOS_VIOLATIONS_SUMMARY_REPORT.getResourceBundle(getLocale());
-        
+
         List<String> columnHeaders = new ArrayList<String>();
-        for (int i = 1; i <=16; i++) {
+        for (int i = 1; i <= 16; i++) {
             if (i > 14 && getUseMetric())
-                columnHeaders.add(MessageUtil.getBundleString(resourceBundle, "column."+i+".tabular.METRIC"));
-            else columnHeaders.add(MessageUtil.getBundleString(resourceBundle, "column."+i+".tabular"));
+                columnHeaders.add(MessageUtil.getBundleString(resourceBundle, "column." + i + ".tabular.METRIC"));
+            else columnHeaders.add(MessageUtil.getBundleString(resourceBundle, "column." + i + ".tabular"));
         }
-        
+
         return columnHeaders;
     }
 
     @Override
     public List<List<Result>> getTableRows() {
-        
-        List<WthHosViolationsSummary> dataList = (List<WthHosViolationsSummary>)getMainDataset();
+
+        List<WthHosViolationsSummary> dataList = (List<WthHosViolationsSummary>) getMainDataset();
         if (dataList == null)
             return null;
-        
-        List<List<Result>>records = new ArrayList<List<Result>>();
-        
+
+        List<List<Result>> records = new ArrayList<List<Result>>();
+
         for (WthHosViolationsSummary summary : dataList) {
             List<Result> row = new ArrayList<Result>();
             row.add(new Result(summary.getGroupName(), summary.getGroupName()));
@@ -231,18 +222,19 @@ public class WthHosViolationsSummaryReportCriteria extends ViolationsSummaryRepo
             row.add(new Result(summary.getDriverCnt().toString(), summary.getDriverCnt()));
             row.add(new Result(Converter.convertRemarkDistance(summary.getTotalMiles(), getUseMetric(), getLocale()), summary.getTotalMiles()));
             row.add(new Result(Converter.convertRemarkDistance(summary.getTotalMilesNoDriver(), getUseMetric(), getLocale()), summary.getTotalMilesNoDriver()));
-            
+
             records.add(row);
         }
 
         return records;
-        
-        
+
+
     }
+
     @Override
     public List<ColumnHeader> getColumnSummaryHeaders() {
         ResourceBundle resourceBundle = ReportType.WEATHERFORD_HOS_VIOLATIONS_SUMMARY_REPORT.getResourceBundle(getLocale());
-        
+
         List<ColumnHeader> columnHeaders = new ArrayList<ColumnHeader>();
         columnHeaders.add(new ColumnHeader(MessageUtil.getBundleString(resourceBundle, "column.1.tabularHeader"), 1));
         columnHeaders.add(new ColumnHeader(MessageUtil.getBundleString(resourceBundle, "column.2to4.tabularHeader"), 3));
