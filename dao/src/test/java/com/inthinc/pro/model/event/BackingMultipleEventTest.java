fae1f5d8975553735d7dc717c9f128c2390da624
Jason Wimmer <jwimmer@inthinc.com>
2015-01-27 11:30:29 -0700

    DE10119: adding test cases splitting the test from one test covering many things to many tests covering one thing each

--- dao/src/test/java/com/inthinc/pro/model/event/BackingMultipleEventTest.java
+++ dao/src/test/java/com/inthinc/pro/model/event/BackingMultipleEventTest.java
@@ -5,34 +5,76 @@ import java.util.Map;
 
 import org.junit.Test;
 
+import com.inthinc.pro.model.BackingEventType;
+
 import static junit.framework.Assert.assertEquals;
 
 public class BackingMultipleEventTest {
-
-    @Test
-    public void testGetEventType(){
-        BackingMultipleEvent backingMultipleEventBacking = new BackingMultipleEvent();
-        backingMultipleEventBacking.setAttribs("80=1");
-        assertEquals(EventType.BACKING,backingMultipleEventBacking.getEventType());
-
-        BackingMultipleEvent backingMultipleEventFirstMoveForward = new BackingMultipleEvent();
-        backingMultipleEventFirstMoveForward.setAttribs("80=2");
-        assertEquals(EventType.FIRST_MOVE_FORWARD,backingMultipleEventFirstMoveForward.getEventType());
-
-        BackingMultipleEvent backingMultipleEventUnknown = new BackingMultipleEvent();
-        backingMultipleEventUnknown.setAttribs("80=3");
-        assertEquals(EventType.BACKING,backingMultipleEventUnknown.getEventType());
-        
-        BackingMultipleEvent backingMultipleEventBackingWithAttrMap = new BackingMultipleEvent();
+    private Map<Object, Object> attrMapFromList(String attribs) {
         Map<Object, Object> attrMap = new HashMap<Object, Object>();
         
-        String[] attribsList = "80=1".split(";");
+        String[] attribsList = attribs.split(";");
         for (String s : attribsList) {
             if (!s.trim().equals("")) {
                 attrMap.put(s.split("=")[0], s.split("=")[1]);
             }
         }
-        backingMultipleEventBackingWithAttrMap.setAttrMap(attrMap);
+        return attrMap;
+    }
+   
+    @Test
+    public void getEventType_backingAttrib_returnBacking() {
+        BackingMultipleEvent backingMultipleEventBacking = new BackingMultipleEvent();
+        backingMultipleEventBacking.setAttribs("80=1");
+        assertEquals(EventType.BACKING,backingMultipleEventBacking.getEventType());
+    }
+    
+    @Test
+    public void getEventType_fmfAttrib_returnFMF(){
+        BackingMultipleEvent backingMultipleEventFirstMoveForward = new BackingMultipleEvent();
+        backingMultipleEventFirstMoveForward.setAttribs("80=2");
+        assertEquals(EventType.FIRST_MOVE_FORWARD,backingMultipleEventFirstMoveForward.getEventType());
+    }
+    
+    @Test
+    public void getEventType_unknownAttrib_returnBacking(){
+        BackingMultipleEvent backingMultipleEventUnknown = new BackingMultipleEvent();
+        backingMultipleEventUnknown.setAttribs("80=3");
+        assertEquals(EventType.BACKING,backingMultipleEventUnknown.getEventType());
+    }
+    
+    @Test
+    public void getEventType_noAttrib_returnBacking(){
+        BackingMultipleEvent backingMultipleEventUnknown = new BackingMultipleEvent();
+        assertEquals(EventType.BACKING,backingMultipleEventUnknown.getEventType());
+    }
+    
+    @Test
+    public void getEventType_backingAttrMap_returnBacking(){
+        BackingMultipleEvent backingMultipleEventBackingWithAttrMap = new BackingMultipleEvent();
+
+        backingMultipleEventBackingWithAttrMap.setAttrMap(attrMapFromList("80=1"));
+        assertEquals(EventType.BACKING,backingMultipleEventBackingWithAttrMap.getEventType());
+    }
+    
+    @Test
+    public void getEventType_fmfAttrMap_returnFMF(){
+        BackingMultipleEvent backingMultipleEventBackingWithAttrMap = new BackingMultipleEvent();
+        backingMultipleEventBackingWithAttrMap.setAttrMap(attrMapFromList("80=2"));
+        assertEquals(EventType.FIRST_MOVE_FORWARD,backingMultipleEventBackingWithAttrMap.getEventType());
+    }
+    
+    @Test
+    public void getEventType_unknownAttrMap_returnBacking(){
+        BackingMultipleEvent backingMultipleEventBackingWithAttrMap = new BackingMultipleEvent();
+        backingMultipleEventBackingWithAttrMap.setAttrMap(attrMapFromList("80=3"));
+        assertEquals(EventType.BACKING,backingMultipleEventBackingWithAttrMap.getEventType());
+    }
+    
+    @Test
+    public void getEventType_noAttrMap_returnBacking(){
+        BackingMultipleEvent backingMultipleEventBackingWithAttrMap = new BackingMultipleEvent();
+        backingMultipleEventBackingWithAttrMap.setAttrMap(attrMapFromList(""));
         assertEquals(EventType.BACKING,backingMultipleEventBackingWithAttrMap.getEventType());
     }
 }
