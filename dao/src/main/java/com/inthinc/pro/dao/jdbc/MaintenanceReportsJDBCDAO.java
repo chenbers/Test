5160c009c98c6433f0b07cbcbefd8d03367865e4
Jason Wimmer <jwimmer@inthinc.com>
2015-02-17 15:37:17 -0700

    US9045: types reference by settingID not CODE

--- dao/src/main/java/com/inthinc/pro/dao/jdbc/MaintenanceReportsJDBCDAO.java
+++ dao/src/main/java/com/inthinc/pro/dao/jdbc/MaintenanceReportsJDBCDAO.java
@@ -33,7 +33,7 @@ public class MaintenanceReportsJDBCDAO extends SimpleJdbcDaoSupport implements M
                     "join vehicle v on (avs.vehicleID = v.vehicleID) " +
                     "join groups g on (v.groupID = g.groupID) " +
                     "where v.groupID in (:groupID_list) " +
-                    "  and settingID in ("+SettingType.MAINT_THRESHOLD_ENGINE_HOURS.getCode()+" , "+SettingType.MAINT_THRESHOLD_ODOMETER.getCode()+", "+SettingType.MAINT_THRESHOLD_ODOMETER_START.getCode()+") " +
+                    "  and settingID in ("+SettingType.MAINT_THRESHOLD_ENGINE_HOURS.getSettingID()+" , "+SettingType.MAINT_THRESHOLD_ODOMETER.getSettingID()+", "+SettingType.MAINT_THRESHOLD_ODOMETER_START.getSettingID()+") " +
                     "order by avs.vehicleID, settingID asc ";
     
 
