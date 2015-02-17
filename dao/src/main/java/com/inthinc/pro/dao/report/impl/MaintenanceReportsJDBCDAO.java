76cf4de4095a011535256a02c0e9dcf8b2d07e85
Jason Wimmer <jwimmer@inthinc.com>
2015-02-17 09:00:04 -0700

    US9045: initial work on maintenanceReportsDAO

--- dao/src/main/java/com/inthinc/pro/dao/report/impl/MaintenanceReportsJDBCDAO.java
+++ dao/src/main/java/com/inthinc/pro/dao/report/impl/MaintenanceReportsJDBCDAO.java
@@ -18,15 +18,23 @@ import org.joda.time.DateTimeZone;
 import org.joda.time.Interval;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.inthinc.pro.ProDAOException;
-import com.inthinc.pro.dao.DriveTimeDAO;
+import com.inthinc.pro.dao.jdbc.AdminVehicleJDBCDAO;
+import com.inthinc.pro.dao.jdbc.GenericJDBCDAO;
+import com.inthinc.pro.dao.report.MaintenanceReportsDAO;
 import com.inthinc.pro.model.Driver;
+import com.inthinc.pro.model.MaintenanceReportItem;
 import com.inthinc.pro.model.Vehicle;
 import com.inthinc.pro.model.aggregation.DriveTimeRecord;
+import com.inthinc.pro.model.configurator.SettingType;
 import com.inthinc.pro.model.event.VehicleEventData;
 
-public class DriveTimeJDBCDAO extends GenericJDBCDAO implements DriveTimeDAO {
+public class MaintenanceReportsJDBCDAO extends GenericJDBCDAO implements MaintenanceReportsDAO {
+    
+    @Autowired
+    AdminVehicleJDBCDAO adminVehicleJDBCDAO;
 
     private static final long serialVersionUID = 1L;
     
@@ -52,6 +60,90 @@ public class DriveTimeJDBCDAO extends GenericJDBCDAO implements DriveTimeDAO {
     private static final String REPORT_ODOMETER_AT_DATE = "select sum(a.odometer6) odiff from agg a where a.vehicleID = ? and a.aggDate <= ?";
 
     private static final DateTimeFormatter dateFormatter = DateTimeFormat.forPattern("yyyy-MM-dd");
+    
+    private static final String VEHICLES_WITH_THRESHOLDS = "SELECT avs.vehicleID vehicleID, v.name vehicleName, v.odometer odometer, v.absOdometer absOdometer, v.groupID vehicleGroupID, settingID, value, g.name vehicleGroupName, acctID, v.year year, v.make make, v.model model " +
+                    "from actualVSet avs " +
+                    "join vehicle v on (avs.vehicleID = v.vehicleID) " +
+                    "join groups g on (v.groupID = g.groupID) " +
+                    "where groupID in (_GROUP_ID_LIST_) " +
+                    "  and settingID in ("+SettingType.MAINT_THRESHOLD_ENGINE_HOURS.getCode()+" , "+SettingType.MAINT_THRESHOLD_ODOMETER+") " +
+                    "order by avs.vehicleID ";
+    
+    private static final String MILES_DRIVEN =
+                    "SELECT MAX(vs.endingOdometer) milesDriven FROM vehicleScoreByDay vs where vs.vehicleID in (_VEHICLE_ID_LIST_)";
+    @Override
+    public List<MaintenanceReportItem> getVehiclesWithThreshold(List<Integer> groupIDList) {
+        List<MaintenanceReportItem> results = new ArrayList<MaintenanceReportItem>();
+        // TODO Auto-generated method stub
+        Connection conn = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        StringBuilder group_id_list = new StringBuilder();
+        String prefix = "";
+        for(Integer groupID : groupIDList) {
+            group_id_list.append(prefix);
+            if(prefix.equals("")) {
+                prefix = " , ";
+            }
+            group_id_list.append(groupID);
+        }
+
+        try
+        {
+            conn = getConnection();
+            statement = (PreparedStatement) conn.prepareStatement(VEHICLES_WITH_THRESHOLDS.replace("_GROUP_ID_LIST_", group_id_list.toString()));
+
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                MaintenanceReportItem item = new MaintenanceReportItem();
+                item.setVehicleName(resultSet.getString("vehicleName"));
+                //item.setCreated(null);//base entity
+                //item.setEventOdometer(eventOdometer);
+                //item.setEventTime(null)
+                //item.setEventType(null);
+                //item.setEventValue(null);
+                item.setGroupID(resultSet.getInt("vehicleGroupID"));
+                item.setGroupName(resultSet.getString("vehicleGroupName"));
+                //item.setModified(modified); //base entity
+                item.setSettingType(SettingType.valueOf(resultSet.getInt("settingID")));
+                item.setThreshold(resultSet.getDouble("value"));
+                item.setVehicleName(resultSet.getString("value"));
+                item.setVehicleID(resultSet.getInt("vehicleID"));
+                StringBuffer ymmString = new StringBuffer(); 
+                ymmString.append(resultSet.getString("year")+" ");
+                ymmString.append(resultSet.getString("make")+" ");
+                ymmString.append(resultSet.getString("model")+" ");
+                item.setYmmString(ymmString.toString());
+                
+                
+                Long absOdometer = resultSet.getObject("absOdometer") == null ? null : (resultSet.getLong("absOdometer"));
+                Long odometer = resultSet.getObject("odometer") == null ? null : resultSet.getLong("odometer");
+                if (absOdometer != null) {
+                    item.setVehicleOdometer(Long.valueOf(absOdometer/100l).intValue()); 
+                }
+                else if (odometer != null) {
+                    Integer milesDriven = getMilesDriven(item.getVehicleID()); 
+                    item.setVehicleOdometer(Long.valueOf((odometer + milesDriven)/100).intValue());
+                }
+                
+               
+                
+                
+                
+            }
+        }   // end try
+        catch (SQLException e)
+        { // handle database hosLogs in the usual manner
+            throw new ProDAOException(statement.toString(), e);
+        }   // end catch
+        finally
+        { // clean up and release the connection
+            close(resultSet);
+            close(statement);
+            close(conn);
+        } // end finally
+        return results;
+    }
 
     public Long getEngineHoursAtDate(Vehicle vehicle, Date evDate) {
         java.sql.Date sqlEvDate = new java.sql.Date(evDate.getTime());
@@ -119,7 +211,7 @@ public class DriveTimeJDBCDAO extends GenericJDBCDAO implements DriveTimeDAO {
         return driveSince;
     }
 
-    @Override
+    
     public  Map<Integer, String> getDriveOdometersAtDates(VehicleEventData vehicleEventData) {
         Map<Integer, String> retMap = new HashMap<Integer, String>();
         List<Object> params = new ArrayList<Object>();
@@ -176,7 +268,7 @@ public class DriveTimeJDBCDAO extends GenericJDBCDAO implements DriveTimeDAO {
         return retMap;
     }
 
-    @Override
+
     public  Map<Integer, String> getDriveOdometersAtLastDates(VehicleEventData vehicleEventData) {
         Map<Integer, String> retMap = new HashMap<Integer, String>();
         List<Object> params = new ArrayList<Object>();
@@ -233,7 +325,7 @@ public class DriveTimeJDBCDAO extends GenericJDBCDAO implements DriveTimeDAO {
     }
 
 
-    @Override
+
     public  Map<Integer, String> getEngineHoursAtDates(VehicleEventData vehicleEventData) {
         Map<Integer, String> retMap = new HashMap<Integer, String>();
         List<Object> params = new ArrayList<Object>();
@@ -289,7 +381,6 @@ public class DriveTimeJDBCDAO extends GenericJDBCDAO implements DriveTimeDAO {
         return retMap;
     }
 
-    @Override
     public  Map<Integer, String> getEngineHoursAtLastDates(VehicleEventData vehicleEventData) {
         Map<Integer, String> retMap = new HashMap<Integer, String>();
         List<Object> params = new ArrayList<Object>();
@@ -387,7 +478,6 @@ public class DriveTimeJDBCDAO extends GenericJDBCDAO implements DriveTimeDAO {
     /* (non-Javadoc)
      * @see com.inthinc.pro.dao.DriveTimeDAO#getPrevEventDates(com.inthinc.pro.model.event.VehicleEventData)
      */
-    @Override
     public Map<Integer, Date> getPrevEventDates(VehicleEventData vehicleEventData) {
         Map<Integer, Date> retMap = new HashMap<Integer, Date>();
         List<Object> params = new ArrayList<Object>();
@@ -574,7 +664,6 @@ public class DriveTimeJDBCDAO extends GenericJDBCDAO implements DriveTimeDAO {
         return driveOdometer;
     }
 
-    @Override
     public List<DriveTimeRecord> getDriveTimeRecordList(Driver driver, Interval queryInterval) {
         
         DateTimeZone driverTimeZone = DateTimeZone.forTimeZone(driver.getPerson().getTimeZone());
@@ -619,8 +708,6 @@ System.out.println("statement:" + statement.toString());
         return driverTimeRecordList;
     }
 
-    
-    @Override
     public List<DriveTimeRecord> getDriveTimeRecordListForGroup(Integer groupID, Interval queryInterval) {
         
 
@@ -708,4 +795,21 @@ System.out.println("statement:" + statement.toString());
         return driverIDs.toString();
     }
 
+    @Override
+    public Integer getMilesDriven(Integer vehicleID) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Map<Integer, Integer> getMilesDriven(Set<Integer> vehicleIDs) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Map<Integer, Integer> getEngineHours(List<Integer> vehicleIDs) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
