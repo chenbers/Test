c37cbf923f064bd4dacb551be18109722b5b3cdf
alexandru-matei <alexandru.matei@infrasoft.ro>
2015-02-25 15:41:50 +0200

    US9005 - Added reason column.

--- dao/src/main/java/com/inthinc/pro/dao/jdbc/EventAggregationJDBCDAO.java
+++ dao/src/main/java/com/inthinc/pro/dao/jdbc/EventAggregationJDBCDAO.java
@@ -29,15 +29,17 @@ public class EventAggregationJDBCDAO extends SimpleJdbcDaoSupport implements Eve
     protected static final boolean INACTIVE_DRIVERS_DEFAULT = false;
     protected static final boolean ZERO_MILES_DRIVERS_DEFAULT = false;
     private DriverDAO driverDAO;
+
+    private static final String CACHED_NOTE_AND_FORGIVEN_SUBSELECT = "(select c.*, f.reason from cachedNoteView c left outer join forgiven f on c.noteID = f.noteID) cnv";
     
     /* Query to return the total number of forgiven events for a single driver by event type */
-    private static final String SELECT_FORGIVEN_EVENT_TOTALS = "SELECT cnv.driverID AS 'driverId', cnv.driverName AS 'driverName', cnv.type AS 'type',cnv.aggType as 'aggType',g.groupID as 'groupID', g.name AS 'groupName', count(noteID) AS 'eventCount', "
+    private static final String SELECT_FORGIVEN_EVENT_TOTALS = "SELECT cnv.driverID AS 'driverId', cnv.driverName AS 'driverName', cnv.type AS 'type',cnv.aggType as 'aggType',g.groupID as 'groupID', g.name AS 'groupName', count(noteID) AS 'eventCount', trim(GROUP_CONCAT(cnv.reason SEPARATOR '; ')) AS reason, "
                     +
                     // "(SELECT count(*) FROM cachedNoteView cnv1 WHERE cnv1.driverID = cnv.driverID AND cnv1.type = cnv.type AND (cnv1.aggType = cnv.aggType OR cnv1.aggType is null) AND forgiven = 1 AND cnv1.time BETWEEN :startDate AND :endDate) AS 'eventCountForgiven' "
                     // +
                     "SUM(cnv.forgiven=1)  AS 'eventCountForgiven' "
                     + // Another way of getting a filtered count cvn.forgiven=1 returns 1 which means true and we can count that.
-                    "FROM cachedNoteView cnv  INNER JOIN groups g ON g.groupID = cnv.driverGroupID "
+                    "FROM "+CACHED_NOTE_AND_FORGIVEN_SUBSELECT+"  INNER JOIN groups g ON g.groupID = cnv.driverGroupID "
                     + "WHERE cnv.driverGroupID IN (:groupList) AND cnv.time BETWEEN :startDate AND :endDate GROUP BY cnv.driverID,cnv.type,cnv.aggType";
     
     public List<DriverForgivenEventTotal> findDriverForgivenEventTotalsByGroups(List<Integer> groupIDs, Interval interval) {
@@ -79,12 +81,16 @@ public class EventAggregationJDBCDAO extends SimpleJdbcDaoSupport implements Eve
                     Object[] mapId = new Object[3];
                     mapId[0] = rs.getInt("driverID");
                     mapId[1] = eventType;
-                    
+
+                    String newReason = rs.getString("reason");
+
                     DriverForgivenEventTotal driverForgivenEventTotal = null;
                     if (driverForgivenEventTotalMap.get(mapId) != null) {
                         driverForgivenEventTotal = driverForgivenEventTotalMap.get(mapId);
                         driverForgivenEventTotal.setEventCount(driverForgivenEventTotal.getEventCount() + rs.getInt("eventCount"));
                         driverForgivenEventTotal.setEventCountForgiven(driverForgivenEventTotal.getEventCountForgiven() + rs.getInt("eventCountForgiven"));
+                        if (newReason != null && !newReason.trim().isEmpty())
+                            driverForgivenEventTotal.setReasons(driverForgivenEventTotal.getReasons() + "; " + newReason);
                     } else {
                         Driver driver = driverDAO.findByID(rs.getInt("driverID"));
                         
@@ -110,6 +116,9 @@ public class EventAggregationJDBCDAO extends SimpleJdbcDaoSupport implements Eve
                             driverForgivenEventTotal.setEventCountForgiven(rs.getInt("eventCountForgiven"));
                             driverForgivenEventTotal.setEventType(eventType);
                             driverForgivenEventTotalMap.put(mapId, driverForgivenEventTotal);
+
+                            if (newReason != null && !newReason.trim().isEmpty())
+                                driverForgivenEventTotal.setReasons(newReason);
                         } else {
                             System.out.println(rs.getString("driverName") + " this record was returned via SQL, but filtered out in java");
                             return null;
