4313c41e978f252d1617824f2c33836615a9fbd9
Jason Wimmer <jwimmer@inthinc.com>
2015-02-26 14:29:14 -0700

    US9045: reactivating 250 miles/hours filter on interval report

--- reports/src/main/java/com/inthinc/pro/reports/performance/MaintenanceIntervalReportCriteria.java
+++ reports/src/main/java/com/inthinc/pro/reports/performance/MaintenanceIntervalReportCriteria.java
@@ -44,7 +44,7 @@ public class MaintenanceIntervalReportCriteria extends ReportCriteria {
     public static class Builder {
         
         private final int DISTANCE_MARGIN = 250;
-        private final int HOUR_MARGIN = 200;
+        private final int HOUR_MARGIN = 250;
         
         private Locale locale;
         
@@ -219,9 +219,10 @@ public class MaintenanceIntervalReportCriteria extends ReportCriteria {
                 Integer distanceOver = calcOverage(baseOdometer, item.getThresholdOdo(), item.getVehicleOdometer());
                 Integer baseHours = 0;
                 Integer hoursOver = calcOverage(baseHours, item.getThresholdHours(), item.getVehicleEngineHours());
-                // if(((hoursOver < 0 && hoursOver > - 250) || (distanceOver < 0 && distanceOver > - 250)) && (item.getThresholdHours() != null || item.getThresholdOdo() != null))
-                backingWrappers.add(new BackingWrapper(item.getVehicleName(), item.getYmmString(), baseOdometer, item.getThresholdOdo(), item.getVehicleOdometer(), distanceOver, baseHours, item.getThresholdHours(),
+                if(((hoursOver < 0 && hoursOver > - HOUR_MARGIN) || (distanceOver < 0 && distanceOver > - DISTANCE_MARGIN)) && (item.getThresholdHours() != null || item.getThresholdOdo() != null)) {
+                    backingWrappers.add(new BackingWrapper(item.getVehicleName(), item.getYmmString(), baseOdometer, item.getThresholdOdo(), item.getVehicleOdometer(), distanceOver, baseHours, item.getThresholdHours(),
                                 item.getVehicleEngineHours(), hoursOver, item.getGroupName()));
+                }
             }
             for(BackingWrapper backingWrapper: backingWrappers) {
                 System.out.println(backingWrapper.toString());
