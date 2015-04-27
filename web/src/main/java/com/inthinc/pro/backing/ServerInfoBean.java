c55ee01d690bad11a3c65d93c5efd5039c8e38e3
Jason Wimmer <jwimmer@inthinc.com>
2015-04-27 11:15:55 -0600

    US9108: simple devops variable display this solution leaves it up to DevOps to populate the variable correctly

--- web/src/main/java/com/inthinc/pro/backing/ServerInfoBean.java
+++ web/src/main/java/com/inthinc/pro/backing/ServerInfoBean.java
@@ -8,22 +8,18 @@ import java.io.Serializable;
 public class ServerInfoBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    private String hostname;
 
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+    
     /**
      * Gets hostname.
      *
      * @return hostname
      */
     public String getHostname() {
-
-        String hostname = System.getenv("HOSTNAME");
-
-        if (hostname == null)
-            hostname = System.getenv("COMPUTERNAME");
-
-        if (hostname == null)
-            hostname = "";
-
         return hostname;
     }
 }
